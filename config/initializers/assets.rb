69e7ca85eb6d63aa4b1882b0f604486ccf20a165
Paul Battley <pbattley@gmail.com>
2018-09-18 17:00:02 +0100

    Add GOV.UK JS to asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w(
+  govuk-frontend/all.js
   govuk-frontend/all.css
   govuk-frontend/all-ie8.css
   html5shiv/dist/html5shiv.js
