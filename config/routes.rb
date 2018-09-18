04b86912819566e6c30d74f4531202631f746415
Paul Battley <pbattley@gmail.com>
2018-09-18 16:20:27 +0100

    Route home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get '/', to: 'home#index', as: :homepage
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
