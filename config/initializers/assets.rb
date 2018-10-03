17245c4e4b7893d260ff86f5e3dea3a7e07b9065
Ben Griffiths <bengriffiths@gmail.com>
2018-10-03 16:22:36 +0100

    Autocorrected rubocop linting errors

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,15 +8,15 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths += [
   Rails.root.join('node_modules'),
-  Rails.root.join('node_modules/govuk-frontend/assets')
+  Rails.root.join('node_modules', 'govuk-frontend', 'assets')
 ]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(
+Rails.application.config.assets.precompile += %w[
   govuk-frontend/all.js
   govuk-frontend/all.css
   govuk-frontend/all-ie8.css
   html5shiv/dist/html5shiv.js
-)
+]
