5a7056327377cf8c5abab4d02a81879a53921759
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-04 11:32:40 +0100

    Add basic validation to supplier and branch

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,5 @@
 class Supplier < ApplicationRecord
   has_many :branches, dependent: :destroy
+
+  validates :name, presence: true
 end
--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Branch, type: :model do
+  subject(:branch) { supplier.branches.build(postcode: 'SW1A 1AA') }
+
+  let(:supplier) { Supplier.new }
+
+  it { is_expected.to be_valid }
+
+  it 'is not valid if postcode is blank' do
+    branch.postcode = ''
+    expect(branch).not_to be_valid
+  end
 end
