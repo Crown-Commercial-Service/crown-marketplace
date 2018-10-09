8d63605c11cf61a89338b4b564aa67e8fe484b2b
Paul Battley <pbattley@gmail.com>
2018-10-09 12:34:04 +0100

    Fix reversed lat/long in point factory use
    This corrects the seeds and includes a migration to swap the lat/long of
    any seeds already created the wrong way round.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_104806) do
+ActiveRecord::Schema.define(version: 2018_10_09_113439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
