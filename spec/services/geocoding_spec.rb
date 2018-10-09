67de757bc799b8235231dec8a19707ccd8750006
Paul Battley <pbattley@gmail.com>
2018-10-09 12:19:31 +0100

    Geocode directly to a point to minimise confusion

--- spec/services/geocoding_spec.rb
+++ spec/services/geocoding_spec.rb
@@ -3,11 +3,33 @@ require 'rails_helper'
 RSpec.describe Geocoding do
   subject(:geocoding) { described_class.new }
 
-  it 'returns longitude and latitude for postcode' do
-    Geocoder::Lookup::Test.add_stub(
-      'TS14 6RD', [{ 'coordinates' => [54.541098, -1.04577] }]
-    )
-    coordinates = geocoding.coordinates(postcode: 'TS14 6RD')
-    expect(coordinates).to eq([54.541098, -1.04577])
+  describe 'point' do
+    subject(:point) { geocoding.point(postcode: postcode) }
+
+    context 'when the postcode is found' do
+      let(:postcode) { 'TS14 6RD' }
+      let(:longitude) { -1.04577 }
+      let(:latitude) { 54.541098 }
+
+      before do
+        Geocoder::Lookup::Test.add_stub(
+          postcode, [{ 'coordinates' => [latitude, longitude] }]
+        )
+      end
+
+      it { is_expected.to have_attributes(longitude: longitude, latitude: latitude) }
+    end
+
+    context 'when the postcode is not found' do
+      let(:postcode) { 'SE99 1AA' }
+
+      before do
+        Geocoder::Lookup::Test.add_stub(
+          postcode, [{ 'coordinates' => nil }]
+        )
+      end
+
+      it { is_expected.to be_nil }
+    end
   end
 end
