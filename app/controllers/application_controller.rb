3db6bcf275b4e863e25ee04390f990efcda96ac4
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-09 11:39:30 +0100

    Require basic HTTP authentication to access app in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.production?
+    http_basic_authenticate_with(
+      name: ENV.fetch('HTTP_BASIC_AUTH_NAME'),
+      password: ENV.fetch('HTTP_BASIC_AUTH_PASSWORD')
+    )
+  end
 end
