49707069cb9e97f37f16c11ccf1069f3d53fe4dc
Paul Battley <pbattley@gmail.com>
2018-10-09 12:19:31 +0100

    Geocode directly to a point to minimise confusion

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -13,16 +13,14 @@ class BranchesController < ApplicationController
       return
     end
 
-    lat_lng = Geocoding.new.coordinates(postcode: @postcode.to_s)
+    point = Geocoding.new.point(postcode: @postcode.to_s)
 
-    unless lat_lng
+    unless point
       flash[:error] = "Couldn't find that postcode"
       redirect_to search_path
       return
     end
 
-    point_factory = RGeo::Geographic.spherical_factory(srid: 4326)
-    point = point_factory.point(lat_lng[1], lat_lng[0])
     @branches = Branch.near(point, within_metres: 1609.34 * Branch::DEFAULT_SEARCH_RANGE_IN_MILES)
   end
 end
--- app/services/geocoding.rb
+++ app/services/geocoding.rb
@@ -1,7 +1,16 @@
 class Geocoding
   DEFAULT_PARAMS = { params: { region: 'uk' } }.freeze
 
-  def coordinates(postcode:)
-    Geocoder.coordinates(postcode, DEFAULT_PARAMS)
+  attr_reader :point_factory
+
+  def initialize
+    @point_factory = RGeo::Geographic.spherical_factory(srid: 4326)
+  end
+
+  def point(postcode:)
+    lat, lng = Geocoder.coordinates(postcode, DEFAULT_PARAMS)
+    return nil unless lat
+
+    point_factory.point(lng, lat)
   end
 end
