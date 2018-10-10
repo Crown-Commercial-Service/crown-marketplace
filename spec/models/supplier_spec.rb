dd50fad0c393e8b4d3c17ddde09a89c8f41a02e9
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-10 15:55:58 +0100

    Add example to ensure branches destroyed when supplier is destroyed
    We're relying on this behaviour in UploadsController#create, so it seems
    like a good idea to test it.

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -9,4 +9,33 @@ RSpec.describe Supplier, type: :model do
     supplier.name = ''
     expect(supplier).not_to be_valid
   end
+
+  context 'when supplier with branches is destroyed' do
+    before do
+      supplier.save!
+    end
+
+    let!(:first_branch) do
+      supplier.branches.create!(
+        postcode: 'SW1A 1AA',
+        contact_name: 'George Henry',
+        contact_email: 'george.henry@example.com'
+      )
+    end
+
+    let!(:second_branch) do
+      supplier.branches.create!(
+        postcode: 'E1 6EA',
+        contact_name: 'Fred Rogers',
+        contact_email: 'fred.rogers@example.com'
+      )
+    end
+
+    it 'destroys all its branches when it is destroyed' do
+      supplier.destroy!
+
+      expect(Branch.find_by(id: first_branch.id)).to be_nil
+      expect(Branch.find_by(id: second_branch.id)).to be_nil
+    end
+  end
 end
