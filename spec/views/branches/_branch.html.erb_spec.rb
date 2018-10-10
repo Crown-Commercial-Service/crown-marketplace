3e438dc9bff47f4848ea293fdc0dd0e3252cb897
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-10 19:50:23 +0100

    Display branch telephone number on branch index page

--- spec/views/branches/_branch.html.erb_spec.rb
+++ spec/views/branches/_branch.html.erb_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'branches/_branch.html.erb' do
   let(:supplier) { Supplier.new(name: 'Supplier') }
+  let(:telephone_number) { '020 7946 0001' }
   let(:contact_name) { 'Henrietta Crouch' }
   let(:contact_email) { 'henrietta.crouch@example.com' }
   let(:branch) do
     supplier.branches.build(
+      telephone_number: telephone_number,
       contact_name: contact_name,
       contact_email: contact_email
     )
@@ -17,6 +19,10 @@ RSpec.describe 'branches/_branch.html.erb' do
 
   it 'displays contact name' do
     expect(rendered).to have_content(contact_name)
+  end
+
+  it 'displays telephone number' do
+    expect(rendered).to have_content(telephone_number)
   end
 
   it 'displays contact email' do
