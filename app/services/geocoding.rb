1a1747193377e33f8bbd413a198c680c1f33cddb
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-10 20:15:20 +0100

    DRY up instantiation of point factory

--- app/services/geocoding.rb
+++ app/services/geocoding.rb
@@ -1,16 +1,14 @@
 class Geocoding
   DEFAULT_PARAMS = { params: { region: 'uk' } }.freeze
 
-  attr_reader :point_factory
-
-  def initialize
-    @point_factory = RGeo::Geographic.spherical_factory(srid: 4326)
+  def self.point_factory
+    @point_factory ||= RGeo::Geographic.spherical_factory(srid: 4326)
   end
 
   def point(postcode:)
     lat, lng = Geocoder.coordinates(postcode, DEFAULT_PARAMS)
     return nil unless lat
 
-    point_factory.point(lng, lat)
+    self.class.point_factory.point(lng, lat)
   end
 end
