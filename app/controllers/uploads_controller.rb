10db1bc31a8f4481c9e28509312eeb42f0bee008
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-10 16:55:47 +0100

    Extract code from UploadsController into new Upload model
    My motivation is to make the logic easier to test, especially as I'm
    about to make it more complicated.

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -4,43 +4,10 @@ class UploadsController < ApplicationController
   def create
     suppliers = JSON.parse(request.body.read)
 
-    error = all_or_none(Supplier) do
-      Supplier.destroy_all
-
-      suppliers.map do |supplier_data|
-        create_supplier!(supplier_data)
-      end
-    end
+    error = Upload.create(suppliers)
 
     raise error if error
 
     render json: {}, status: :created
-  end
-
-  private
-
-  def all_or_none(transaction_class)
-    error = nil
-    transaction_class.transaction do
-      yield
-    rescue ActiveRecord::RecordInvalid => e
-      error = e
-      raise ActiveRecord::Rollback
-    end
-    error
-  end
-
-  def create_supplier!(data)
-    s = Supplier.create!(id: data['supplier_id'], name: data['supplier_name'])
-    branches = data.fetch('branches', [])
-    branches.each do |branch|
-      contact_name = branch.dig('contacts', 0, 'name')
-      contact_email = branch.dig('contacts', 0, 'email')
-      s.branches.create!(
-        postcode: branch['postcode'],
-        contact_name: contact_name,
-        contact_email: contact_email
-      )
-    end
   end
 end
