e8e414ded2816628faa9acd81af334e8615f6602
Chris Roos <chris@seagul.co.uk>
2018-10-15 15:30:02 +0100

    Prevent suppliers having multiple rates for same job type
    We're expecting Suppliers to have a single rate for each job type (at
    least until we introduce the contract length). Adding this validation
    will ensure we fail fast if we accidentally try to import bad data.

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,7 +1,8 @@
 class Rate < ApplicationRecord
   belongs_to :supplier
 
-  validates :job_type, presence: true
+  validates :job_type, presence: true,
+                       uniqueness: { scope: :supplier }
   validates :mark_up, presence: true
 
   def self.nominated_worker
