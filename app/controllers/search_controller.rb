904adbed3e57da456716ff51e5de3271f722ac11
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-16 00:20:43 +0100

    Extract paths for back link and form action into controllers
    I think this makes it easier to see what's going on with the various
    user journeys.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
-  def nominated_worker_question; end
+  def nominated_worker_question
+    @back_path = homepage_path
+    @form_path = nominated_worker_answer_path
+  end
 
   def nominated_worker_answer
     if params[:nominated_worker].present?
@@ -13,11 +16,16 @@ class SearchController < ApplicationController
     end
   end
 
-  def school_postcode_question; end
+  def school_postcode_question
+    @back_path = nominated_worker_question_path(params.permit(:nominated_worker))
+    @form_path = school_postcode_answer_path
+  end
 
   def school_postcode_answer
     redirect_to branches_path(params.permit(:postcode, :nominated_worker))
   end
 
-  def non_nominated_worker_outcome; end
+  def non_nominated_worker_outcome
+    @back_path = nominated_worker_question_path(params.permit(:nominated_worker))
+  end
 end
