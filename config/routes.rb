2942481f7a036186dc43455db913e19aeb14ef4f
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-15 17:15:30 +0100

    Use shorter paths for URLs visible to user

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,17 @@ Rails.application.routes.draw do
   resources :branches, only: :index
   resources :uploads, only: :create
 
-  get '/nominated_worker_question',
-      to: 'search#nominated_worker_question'
+  get '/nominated_worker',
+      to: 'search#nominated_worker_question',
+      as: 'nominated_worker_question'
   get '/nominated_worker_answer',
       to: 'search#nominated_worker_answer'
-  get '/school_postcode_question',
-      to: 'search#school_postcode_question'
+  get '/school_postcode',
+      to: 'search#school_postcode_question',
+      as: 'school_postcode_question'
   get '/school_postcode_answer',
       to: 'search#school_postcode_answer'
-  get '/non_nominated_worker_outcome',
-      to: 'search#non_nominated_worker_outcome'
+  get '/non_nominated_worker',
+      to: 'search#non_nominated_worker_outcome',
+      as: 'non_nominated_worker_outcome'
 end
