0a2adcec5329d91091967a99cfee1ba8337735e3
Chris Roos <chris@seagul.co.uk>
2018-10-16 15:20:58 +0100

    Extract DistanceConvertor
    I want to be able to use this functionality in the `Branch` model (I'm
    working on extracting a `Branch.search` method) and I couldn't easily
    use the methods as they were in `ApplicationHelper`.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
-  ONE_MILE_IN_METRES = 1609.34
-
   def miles_to_metres(miles)
-    ONE_MILE_IN_METRES * miles
+    DistanceConvertor.miles_to_metres(miles)
   end
 
   def metres_to_miles(metres)
-    metres / ONE_MILE_IN_METRES
+    DistanceConvertor.metres_to_miles(metres)
   end
 end
