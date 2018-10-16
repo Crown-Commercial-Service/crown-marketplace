213ee4b0dc5828a9d20f65ca9d8599604260d5fc
Chris Lowis <chris.lowis@gofreerange.com>
2018-10-16 12:35:25 +0100

    Add a Location class
    `point` is derived from `postcode` so it feels like they could be
    encapsulated into a class to allow us to hide away some of the
    validation logic and reduce the number of things assigned to the view
    from this controller. This commit extracts postcode and point into a
    Location class.

---
+++ app/models/location.rb
@@ -0,0 +1,8 @@
+class Location
+  attr_reader :postcode, :point
+
+  def initialize(postcode)
+    @postcode = UKPostcode.parse(postcode)
+    @point = Geocoding.new.point(postcode: @postcode.to_s) if @postcode.valid?
+  end
+end
