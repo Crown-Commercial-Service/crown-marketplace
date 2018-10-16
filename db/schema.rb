5ef9ab0dc197ab9e1e6bbd65eb9b847e5f14c4bc
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-16 17:13:37 +0100

    Remove accreditation_body from suppliers
    This attribute was made redundant by the previous commit.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_101044) do
+ActiveRecord::Schema.define(version: 2018_10_16_161033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2018_10_15_101044) do
     t.text "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "accreditation_body"
   end
 
   add_foreign_key "branches", "suppliers"
