e9c0f7d34bb6d3387b9b9c067fd1a3c8af05fb09
Paul Battley <pbattley@gmail.com>
2018-10-16 12:09:15 +0100

    Use RuboCop to help remember the uuid table option
    This adds a Migration/RequireUUIDPrimaryKeys cop that checks that the
    option has been added to every create_table command.

---
+++ lib/cop/all.rb
@@ -0,0 +1,1 @@
+Dir[File.expand_path('**/*.rb', __dir__)].each { |f| require f }
---
+++ lib/cop/migration/require_uuid_primary_keys.rb
@@ -0,0 +1,25 @@
+module RuboCop
+  module Cop
+    module Migration
+      class RequireUUIDPrimaryKeys < RuboCop::Cop::Cop
+        MSG = 'Use UUIDs for database primary keys (with the `id: :uuid` option)'.freeze
+
+        def on_send(node)
+          return unless node.method_name == :create_table
+
+          add_offense(node, location: :expression) unless uuid_option_set?(node)
+        end
+
+        private
+
+        def uuid_option_set?(node)
+          return false unless node.last_argument.hash_type?
+
+          node.last_argument.pairs.any? do |pair|
+            pair.key.value == :id && pair.value.value == :uuid
+          end
+        end
+      end
+    end
+  end
+end
