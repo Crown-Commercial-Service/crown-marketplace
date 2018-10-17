c18f75ff3e7424e9be4678a4e51c40c597a0b8a4
Paul Battley <pbattley@gmail.com>
2018-10-17 16:08:23 +0100

    Enforce text over unlimited string column
    There's no performance advantage or functional difference between text
    and character varying (without limit) in PostgreSQL. As we're using text
    elsewhere for unconstrained fields, let's enforce consistency.

---
+++ lib/cop/migration/require_limit_on_string.rb
@@ -0,0 +1,36 @@
+module RuboCop
+  module Cop
+    module Migration
+      class RequireLimitOnString < RuboCop::Cop::Cop
+        MSG = 'Specify a limit when using the `string` column type. Use `text` if no limit is needed'.freeze
+
+        def on_send(node)
+          case node.method_name
+          when :add_column
+            check_options node, node.arguments[2], node.last_argument
+          when :column
+            check_options node, node.arguments[1], node.last_argument
+          when :string
+            check_options node, node.method_name, node.last_argument
+          end
+        end
+
+        private
+
+        def check_options(node, type, opts)
+          return unless type == :string
+
+          add_offense(node, location: :expression) unless length_option_set?(opts)
+        end
+
+        def length_option_set?(opts)
+          return false unless opts.hash_type?
+
+          opts.pairs.any? do |pair|
+            pair.key.value == :limit
+          end
+        end
+      end
+    end
+  end
+end
