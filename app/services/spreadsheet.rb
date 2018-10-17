c0703a77898b2aad8c1ea4b274b7ac58719f3e79
Ben Griffiths <bengriffiths@gmail.com>
2018-10-17 15:43:25 +0100

    Separate spreadsheet generation from branch iteration

--- app/services/spreadsheet.rb
+++ app/services/spreadsheet.rb
@@ -3,21 +3,25 @@ class Spreadsheet
     @branches = branches
   end
 
-  def to_xlsx
+  def spreadsheet(name)
     package = Axlsx::Package.new
     workbook = package.workbook
-    workbook.add_worksheet(name: 'Suppliers') do |sheet|
-      sheet.add_row ['Supplier name', 'Branch name', 'Contact name', 'Contact email', 'Telephone number']
-      @branches.each do |branch|
-        sheet.add_row [
-          branch.supplier.name,
-          branch.name,
-          branch.contact_name,
-          branch.contact_email,
-          branch.telephone_number
-        ]
-      end
+    workbook.add_worksheet(name: name) do |sheet|
+      yield sheet
     end
     package.to_stream.read
+  end
+
+  def to_xlsx
+    spreadsheet('Suppliers') do |sheet|
+      sheet.add_row ['Supplier name', 'Branch name', 'Contact name', 'Contact email', 'Telephone number']
+      @branches.each do |branch|
+        sheet.add_row [branch.supplier.name,
+                       branch.name,
+                       branch.contact_name,
+                       branch.contact_email,
+                       branch.telephone_number]
+      end
+    end
   end
 end
