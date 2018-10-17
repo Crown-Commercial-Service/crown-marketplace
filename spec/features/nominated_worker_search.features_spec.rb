950c1e7dfc69f34297b513c68238cf937d539d8a
Paul Battley <pbattley@gmail.com>
2018-10-17 12:03:17 +0100

    Extract features not unique to nominated path

--- spec/features/nominated_worker_search.features_spec.rb
+++ spec/features/nominated_worker_search.features_spec.rb
@@ -1,60 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature 'Nominated workers', type: :feature do
-  scenario 'Nominated worker choice should not be pre-selected' do
-    visit '/'
-    click_on 'Start now'
-
-    choose 'Hire a worker via an agency'
-    click_on 'Continue'
-
-    expect(page).not_to have_checked_field('Yes')
-    expect(page).not_to have_checked_field('No')
-  end
-
-  scenario 'Buyer is not looking for a nominated worker' do
-    visit '/'
-    click_on 'Start now'
-
-    choose 'Hire a worker via an agency'
-    click_on 'Continue'
-
-    choose 'No'
-    click_on 'Continue'
-
-    expect(page).to have_text('This system only allows you to search for nominated workers at the moment')
-  end
-
-  scenario 'Buyer was looking for a nominated worker but changed mind' do
-    visit '/'
-    click_on 'Start now'
-
-    choose 'Hire a worker via an agency'
-    click_on 'Continue'
-
-    choose 'Yes'
-    click_on 'Continue'
-
-    click_on 'Back'
-
-    expect(page).to have_checked_field('Yes')
-  end
-
-  scenario 'Buyer was not looking for a nominated worker but changed mind' do
-    visit '/'
-    click_on 'Start now'
-
-    choose 'Hire a worker via an agency'
-    click_on 'Continue'
-
-    choose 'No'
-    click_on 'Continue'
-
-    click_on 'Back'
-
-    expect(page).to have_checked_field('No')
-  end
-
   scenario 'Buyer finds suppliers within search range' do
     Geocoder::Lookup::Test.add_stub(
       'WC2B 6TE', [{ 'coordinates' => [51.5149666, -0.119098] }]
