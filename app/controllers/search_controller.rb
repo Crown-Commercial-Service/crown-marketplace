26609dbf727e556e763eff748966757fbdb4fc79
Chris Roos <chris@seagul.co.uk>
2018-10-17 17:25:25 +0100

    Extract SearchController#hire_via_agency_params
    To remove some duplication.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,15 +7,15 @@ class SearchController < ApplicationController
   def hire_via_agency_answer
     if params[:hire_via_agency] == 'yes'
       redirect_to nominated_worker_question_path(
-        params.permit(:hire_via_agency)
+        hire_via_agency_params
       )
     elsif params[:hire_via_agency] == 'no'
       redirect_to managed_service_provider_question_path(
-        params.permit(:hire_via_agency)
+        hire_via_agency_params
       )
     else
       redirect_to hire_via_agency_question_path(
-        params.permit(:hire_via_agency)
+        hire_via_agency_params
       ), flash: {
         error: 'Please choose an option'
       }
@@ -23,7 +23,7 @@ class SearchController < ApplicationController
   end
 
   def managed_service_provider_question
-    @back_path = hire_via_agency_question_path(params.permit(:hire_via_agency))
+    @back_path = hire_via_agency_question_path(hire_via_agency_params)
     @form_path = managed_service_provider_answer_path
   end
 
@@ -46,7 +46,7 @@ class SearchController < ApplicationController
   end
 
   def nominated_worker_question
-    @back_path = hire_via_agency_question_path(params.permit(:hire_via_agency))
+    @back_path = hire_via_agency_question_path(hire_via_agency_params)
     @form_path = nominated_worker_answer_path
   end
 
@@ -87,5 +87,11 @@ class SearchController < ApplicationController
 
   def non_nominated_worker_outcome
     @back_path = nominated_worker_question_path(params.permit(:nominated_worker, :hire_via_agency))
+  end
+
+  private
+
+  def hire_via_agency_params
+    params.permit(:hire_via_agency)
   end
 end
