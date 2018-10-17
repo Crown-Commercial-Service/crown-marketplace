8852bfb86ce23532737a8fa3a38807f97eaea184
Chris Lowis <chris.lowis@gofreerange.com>
2018-10-17 15:43:30 +0100

    Mark up telephone numbers with tel: scheme URLs
    This allows the numbers to be directly called if supported.

--- app/helpers/branches_helper.rb
+++ app/helpers/branches_helper.rb
@@ -2,4 +2,8 @@ module BranchesHelper
   def display_name_for_branch(branch)
     branch.name.present? ? branch.name : branch.town
   end
+
+  def tel_scheme_url(number)
+    "tel:#{number}"
+  end
 end
