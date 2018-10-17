7b0fb0dc682be455d126c95b0d7ee1bf03cc427f
Chris Lowis <chris.lowis@gofreerange.com>
2018-10-17 15:58:47 +0100

    Revert "Mark up telephone numbers with tel: scheme URLs"
    This reverts commit 8852bfb86ce23532737a8fa3a38807f97eaea184.
    
    This change went against the recommended advice[1] for phone numbers
    in the GOV.UK design system. I can't think of a reliable way to detect
    whether the client device can make calls, so it seems better to remove
    the link.
    
    [1] https://design-system.service.gov.uk/patterns/telephone-numbers/#don-t-display-telephone-numbers-as-links-on-devices-that-can-t-make-calls

--- app/helpers/branches_helper.rb
+++ app/helpers/branches_helper.rb
@@ -2,8 +2,4 @@ module BranchesHelper
   def display_name_for_branch(branch)
     branch.name.present? ? branch.name : branch.town
   end
-
-  def tel_scheme_url(number)
-    "tel:#{number}"
-  end
 end
