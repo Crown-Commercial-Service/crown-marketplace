35a9afde6146781df5337ac9e6eb6fda9d4a54bf
Ben Griffiths <bengriffiths@gmail.com>
2018-10-17 23:25:31 +0100

    Add shortlist spreadsheet format
    * required small changes to pass row numbers to #row so that
      we could correctly generate formulae.

--- app/services/spreadsheet.rb
+++ app/services/spreadsheet.rb
@@ -8,12 +8,37 @@ class Spreadsheet
       ['Supplier name', 'Branch name', 'Contact name', 'Contact email', 'Telephone number']
     end
 
-    def row(branch)
+    def row(branch, _row_num)
       [branch.supplier_name,
        branch.name,
        branch.contact_name,
        branch.contact_email,
        branch.telephone_number]
+    end
+  end
+
+  class Shortlist
+    def sheet_name
+      'Supplier shortlist'
+    end
+
+    def headers
+      ['Supplier name', 'Branch name', 'Contact name',
+       'Contact email', 'Telephone number', 'Rate',
+       'Daily quote',
+       'Costs of the worker', 'Supplier fee']
+    end
+
+    def row(branch, row_num)
+      [branch.supplier_name,
+       branch.name,
+       branch.contact_name,
+       branch.contact_email,
+       branch.telephone_number,
+       branch.rate,
+       '',
+       "=G#{row_num}/(1+F#{row_num})",
+       "=G#{row_num}-H#{row_num}"]
     end
   end
 
@@ -34,8 +59,8 @@ class Spreadsheet
   def to_xlsx
     spreadsheet(@format.sheet_name) do |sheet|
       sheet.add_row @format.headers
-      @branches.each do |branch|
-        sheet.add_row @format.row(branch)
+      @branches.each.with_index(2) do |branch, i|
+        sheet.add_row @format.row(branch, i)
       end
     end
   end
