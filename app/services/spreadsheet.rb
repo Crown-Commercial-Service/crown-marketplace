209488dd60c7fbebd0c05f2e951d5bc604aee872
Ben Griffiths <bengriffiths@gmail.com>
2018-10-17 23:27:12 +0100

    Format shortlist spreadsheet's columns

--- app/services/spreadsheet.rb
+++ app/services/spreadsheet.rb
@@ -15,6 +15,8 @@ class Spreadsheet
        branch.contact_email,
        branch.telephone_number]
     end
+
+    def style(workbook, sheet); end
   end
 
   class Shortlist
@@ -40,6 +42,18 @@ class Spreadsheet
        "=G#{row_num}/(1+F#{row_num})",
        "=G#{row_num}-H#{row_num}"]
     end
+
+    def style(workbook, sheet)
+      workbook.styles do |s|
+        percent = s.add_style(format_code: '00%')
+        money = s.add_style(format_code: '[$£-809]##,##0.00')
+        sheet.col_style 5, percent
+        sheet.col_style 6, money
+        sheet.col_style 7, money
+        sheet.col_style 8, money
+        sheet.column_widths(*Array.new(headers.length))
+      end
+    end
   end
 
   def initialize(branches, format = DataDownload.new)
@@ -51,17 +65,18 @@ class Spreadsheet
     package = Axlsx::Package.new
     workbook = package.workbook
     workbook.add_worksheet(name: name) do |sheet|
-      yield sheet
+      yield workbook, sheet
     end
     package.to_stream.read
   end
 
   def to_xlsx
-    spreadsheet(@format.sheet_name) do |sheet|
+    spreadsheet(@format.sheet_name) do |workbook, sheet|
       sheet.add_row @format.headers
       @branches.each.with_index(2) do |branch, i|
         sheet.add_row @format.row(branch, i)
       end
+      @format.style(workbook, sheet)
     end
   end
 end
