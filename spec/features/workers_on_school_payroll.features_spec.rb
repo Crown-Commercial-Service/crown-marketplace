86e58820c41f275832614400a9d36c7fb4a4de4f
Chris Roos <chris@seagul.co.uk>
2018-10-18 17:41:41 +0100

    Fix broken tests
    These were broken by the CSS changes in
    ff5f9a64cb647e7fff8a784b925ce462f0186b00.

--- spec/features/workers_on_school_payroll.features_spec.rb
+++ spec/features/workers_on_school_payroll.features_spec.rb
@@ -60,17 +60,17 @@ RSpec.feature 'Workers on school payroll', type: :feature do
 
     expect(page).not_to have_text('whitechapel'), 'suppliers without fixed term rates should not be displayed'
 
-    branches = page.all('.branch')
+    branches = page.all('.supplier-record')
     cheaper_branch = branches.first
     costlier_branch = branches.last
 
     expect(cheaper_branch.find('h2').text).to eq('westminster')
-    expect(cheaper_branch).to have_css('.distance', text: '1.1')
-    expect(cheaper_branch).to have_css('.markup-rate', text: '30.0%')
+    expect(cheaper_branch).to have_css('.supplier-record__distance', text: '1.1')
+    expect(cheaper_branch).to have_css('.supplier-record__markup-rate', text: '30.0%')
 
     expect(costlier_branch.find('h2').text).to eq('holborn')
-    expect(costlier_branch).to have_css('.distance', text: '0.0')
-    expect(costlier_branch).to have_css('.markup-rate', text: '35.0%')
+    expect(costlier_branch).to have_css('.supplier-record__distance', text: '0.0')
+    expect(costlier_branch).to have_css('.supplier-record__markup-rate', text: '35.0%')
 
     expect(page).not_to have_text('liverpool')
   end
