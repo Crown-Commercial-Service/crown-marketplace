6c7b6256db39e1bd81fd5c249540d6dec2d92af1
Ben Griffiths <bengriffiths@gmail.com>
2018-10-21 12:35:54 +0100

    Add Cognito authorization through auth_controller
    AWS Cognito provides OpenID Connect on top of OAuth2 to allow us use it
    for authentication in our app.
    
    We're using the omniauth and omniauth-oauth2 Gems to connect to Cognito.
    
    We're not currently verifying the signature of the JWT we receive from
    Cognito. We'll add that in a future commit.
    
    The 'GET /auth/cognito/callback' test in the Cognito request spec gives
    us some confidence that our Cognito OmniAuth strategy is working as
    expected by simulating the OAuth2 response with receive from Cognito.
    
    Notes:
    
    The Auth hash schema (which is the data made available in the
    'omniauth.auth' key) is described by Omniauth in
    https://github.com/omniauth/omniauth/wiki/Auth-Hash-Schema.
    
    We've excluded cognito_spec.rb from RSpec/FilePath because RuboCop wants
    us to name the folder omni_auth instead of omniauth.
    
    We set the OmniAuth logger to the Rails logger to avoid it writing
    directly to STDOUT.
    
    We've had to add `Dotenv.overload '.env.test' to the rails_helper.  This
    is necessary for when we're running the test suite using `rake`.
    Without this, the `rake` process loads any development environment
    variables first (which might contain real Cognito configuration).

---
+++ lib/omniauth/strategies/cognito.rb
@@ -0,0 +1,46 @@
+require 'omniauth-oauth2'
+require 'jwt'
+
+module OmniAuth
+  module Strategies
+    class Cognito < OmniAuth::Strategies::OAuth2
+      option :name, 'cognito'
+      option :client_options,
+             authorize_url: '/oauth2/authorize',
+             token_url: '/oauth2/token',
+             auth_scheme: :basic_auth
+
+      uid do
+        validated_id_token['sub'] if validated_id_token
+      end
+
+      info do
+        if validated_id_token
+          {
+            email: validated_id_token['email'],
+          }
+        end
+      end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
+      private
+
+      def id_token
+        access_token['id_token']
+      end
+
+      def validated_id_token
+        return nil unless id_token
+
+        @validated_id_token ||= JWT.decode(
+          id_token,
+          nil,
+          false
+        ).first
+      end
+    end
+  end
+end
