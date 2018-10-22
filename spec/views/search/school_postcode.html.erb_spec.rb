fd46c042d4889dda1ce6dc55a301d01ece016a65
Paul Battley <pbattley@gmail.com>
2018-10-22 15:35:20 +0100

    Use journey steps for the search controller

--- spec/views/search/school_postcode.html.erb_spec.rb
+++ spec/views/search/school_postcode.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'search/school_payroll_question.html.erb' do
+RSpec.describe 'search/school_postcode.html.erb' do
   it 'stores answer to hire via agency question in hidden field' do
     params[:hire_via_agency] = 'hire-via-agency'
     render
@@ -13,15 +13,9 @@ RSpec.describe 'search/school_payroll_question.html.erb' do
     expect(rendered).to have_css('input[name="nominated_worker"][value="nominated-worker"]', visible: false)
   end
 
-  it 'selects "yes" if school payroll is "yes"' do
-    params[:school_payroll] = 'yes'
+  it 'stores answer to school payroll question in hidden field' do
+    params[:school_payroll] = 'school-payroll'
     render
-    expect(rendered).to have_css('input[type="radio"][name="school_payroll"][value="yes"][checked]')
-  end
-
-  it 'selects "no" if school payroll is "no"' do
-    params[:school_payroll] = 'no'
-    render
-    expect(rendered).to have_css('input[type="radio"][name="school_payroll"][value="no"][checked]')
+    expect(rendered).to have_css('input[name="school_payroll"][value="school-payroll"]', visible: false)
   end
 end
