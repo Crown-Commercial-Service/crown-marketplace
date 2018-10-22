135bea230cf1dafb76cd712536c45e5bcca494fc
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-22 11:11:19 +0100

    Revert "Upgrade build AWS build machine to latest Ruby"
    This reverts commit 5aaf6e70bde2909b3fb850f56cb190061d43bee1.
    
    The ruby & ruby-dev packages on Ubuntu 14.04 are very old and don't give
    us the version of Ruby we need.

--- pipeline_pre_build_tests.sh
+++ pipeline_pre_build_tests.sh
@@ -4,7 +4,6 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key a
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
 apt update
 apt-get install -y build-essential
-apt-get install -y ruby ruby-dev
 apt install -y postgresql-9.6-postgis-2.3
 service postgresql start 9.6
 sudo -u postgres createuser --superuser root; sudo -u postgres createdb root
