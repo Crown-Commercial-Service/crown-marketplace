afa22f69aefe10798790563a75704b90d70cd678
Chris Lowis <chris.lowis@gofreerange.com>
2018-10-24 10:52:51 +0100

    Extract feedback email link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def metres_to_miles(metres)
     DistanceConvertor.metres_to_miles(metres)
   end
+
+  def feedback_email_link
+    if Marketplace.feedback_email_address.present?
+      mail_to(Marketplace.feedback_email_address, 'feedback', class: 'govuk-link')
+    else
+      'feedback'
+    end
+  end
 end
