d447b931c7f2189ab0c4cb3c06ef6f28c289bb59
Chris Roos <chris@seagul.co.uk>
2018-10-25 15:54:26 +0100

    Use GOV.UK error message component in worker_type

--- spec/views/search/worker_type.html.erb_spec.rb
+++ spec/views/search/worker_type.html.erb_spec.rb
@@ -1,11 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'search/worker_type.html.erb' do
+  before do
+    @form_path = '/'
+  end
+
   it 'stores answer to looking-for question in hidden field' do
     @back_path = '/'
-    @form_path = '/'
     params[:looking_for] = 'looking-for'
     render
     expect(rendered).to have_css('input[name="looking_for"][value="looking-for"]', visible: false)
+  end
+
+  it 'does not include any error message classes' do
+    render
+    expect(rendered).not_to have_css('.govuk-form-group--error')
+    expect(rendered).not_to have_css('.govuk-error-message')
+  end
+
+  context 'when the flash contains an error' do
+    before do
+      flash[:error] = 'error-message'
+      render
+    end
+
+    it 'adds the form group error class' do
+      expect(rendered).to have_css('.govuk-form-group.govuk-form-group--error')
+    end
+
+    it 'adds the message to the field with the error' do
+      expect(rendered).to have_css('.govuk-error-message', text: 'error-message')
+    end
   end
 end
