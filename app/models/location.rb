7ed98d185c12e8744036af0448a24d846af09211
Paul Battley <pbattley@gmail.com>
2018-10-25 15:00:12 +0100

    Simplify Location class
    From a usability point of view, we don't need to tell people that the
    postcode they entered was invalid: we simply need to inform them that it
    couldn't be located. This class now has an attribute `found?` which can
    be used for model validations.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,18 +1,12 @@
 class Location
-  attr_reader :postcode, :point, :error
+  attr_reader :postcode, :point
 
   def initialize(postcode)
     @postcode = UKPostcode.parse(postcode)
     @point = Geocoding.new.point(postcode: @postcode.to_s) if @postcode.valid?
   end
 
-  def valid?
-    if !@postcode.valid?
-      @error = 'Postcode is invalid'
-    elsif !@point
-      @error = "Couldn't find that postcode"
-    end
-
-    !@error
+  def found?
+    @point.present?
   end
 end
