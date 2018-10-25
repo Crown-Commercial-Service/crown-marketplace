b6d54b0df9ff85782ef3bbbc0ad8f7d581bfe392
Chris Roos <chris@seagul.co.uk>
2018-10-25 16:02:48 +0100

    Use GOV.UK error message component in school_postcode

--- spec/views/search/school_postcode.html.erb_spec.rb
+++ spec/views/search/school_postcode.html.erb_spec.rb
@@ -23,4 +23,25 @@ RSpec.describe 'search/school_postcode.html.erb' do
     render
     expect(rendered).to have_css('input[name="payroll_provider"][value="payroll-provider"]', visible: false)
   end
+
+  it 'does not include any error message classes' do
+    render
+    expect(rendered).not_to have_css('.govuk-form-group--error')
+    expect(rendered).not_to have_css('.govuk-error-message')
+  end
+
+  context 'when the flash contains an error' do
+    before do
+      flash[:error] = 'error-message'
+      render
+    end
+
+    it 'adds the form group error class' do
+      expect(rendered).to have_css('.govuk-form-group.govuk-form-group--error')
+    end
+
+    it 'adds the message to the field with the error' do
+      expect(rendered).to have_css('.govuk-error-message', text: 'error-message')
+    end
+  end
 end
