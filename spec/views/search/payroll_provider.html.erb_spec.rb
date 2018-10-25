e795e9338f2eb55e034196b1365cd5fc8cc00346
Chris Roos <chris@seagul.co.uk>
2018-10-25 15:59:28 +0100

    Use GOV.UK error message component in payroll_provider

--- spec/views/search/payroll_provider.html.erb_spec.rb
+++ spec/views/search/payroll_provider.html.erb_spec.rb
@@ -29,4 +29,25 @@ RSpec.describe 'search/payroll_provider.html.erb' do
     render
     expect(rendered).to have_css('input[type="radio"][name="payroll_provider"][value="agency"][checked]')
   end
+
+  it 'does not include any error message classes' do
+    render
+    expect(rendered).not_to have_css('.govuk-form-group--error')
+    expect(rendered).not_to have_css('.govuk-error-message')
+  end
+
+  context 'when the flash contains an error' do
+    before do
+      flash[:error] = 'error-message'
+      render
+    end
+
+    it 'adds the form group error class' do
+      expect(rendered).to have_css('.govuk-form-group.govuk-form-group--error')
+    end
+
+    it 'adds the message to the field with the error' do
+      expect(rendered).to have_css('.govuk-error-message', text: 'error-message')
+    end
+  end
 end
