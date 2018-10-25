a5ab553c411233811aea55d8d68283f61ed4efb2
Chris Roos <chris@seagul.co.uk>
2018-10-25 16:09:51 +0100

    Use GOV.UK error message component in managed_service_provider

---
+++ spec/views/search/managed_service_provider.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'search/managed_service_provider.html.erb' do
+  before do
+    @form_path = '/'
+  end
+
+  it 'does not include any error message classes' do
+    render
+    expect(rendered).not_to have_css('.govuk-form-group--error')
+    expect(rendered).not_to have_css('.govuk-error-message')
+  end
+
+  context 'when the flash contains an error' do
+    before do
+      flash[:error] = 'error-message'
+      render
+    end
+
+    it 'adds the form group error class' do
+      expect(rendered).to have_css('.govuk-form-group.govuk-form-group--error')
+    end
+
+    it 'adds the message to the field with the error' do
+      expect(rendered).to have_css('.govuk-error-message', text: 'error-message')
+    end
+  end
+end
