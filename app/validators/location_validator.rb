28822d87d3116ca1aa08e0b0cf0c8d24b6a83130
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-29 17:30:07 +0000

    No need to pass options to ActiveModel::Errors#add in validators
    We're only ever enabling or disabling these validators, so there's never
    any options to pass on. And in any case, it's not obvious to me that the
    options passed into the `validates` call in a model are appropriate to
    be passed into ActiveModel::Errors#add. The only option mentioned in the
    documentation for the latter is `strict` which would raise an exception
    instead of adding the error and I don't think we're ever going to want
    that.

--- app/validators/location_validator.rb
+++ app/validators/location_validator.rb
@@ -2,6 +2,6 @@ class LocationValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.found?
 
-    record.errors.add attribute, :invalid_location, options
+    record.errors.add attribute, :invalid_location
   end
 end
