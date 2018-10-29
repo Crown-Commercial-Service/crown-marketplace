058994ef37c159eb8caa5fd6755acd1327ae53ec
Chris Lowis <chris.lowis@gofreerange.com>
2018-10-29 11:31:41 +0000

    Use I18n helper in agency_worker_search feature spec
    The intention of this commit is to make the feature spec less brittle
    to changes in the copy used on the pages.

--- spec/features/agency_worker_search.features_spec.rb
+++ spec/features/agency_worker_search.features_spec.rb
@@ -5,40 +5,68 @@ RSpec.feature 'Agency workers', type: :feature do
     visit_home
     click_on 'Start now'
 
-    choose 'An individual worker'
-    click_on 'Continue'
+    choose a_managed_service_provider
+    click_on continue
 
-    expect(page).not_to have_checked_field('Yes')
-    expect(page).not_to have_checked_field('No')
+    expect(page).not_to have_checked_field(an_individual_worker)
+    expect(page).not_to have_checked_field(a_managed_service_provider)
   end
 
   scenario 'Buyer was looking for a nominated worker but changed mind' do
     visit_home
     click_on 'Start now'
 
-    choose 'An individual worker'
-    click_on 'Continue'
+    choose an_individual_worker
+    click_on continue
 
-    choose 'Yes'
-    click_on 'Continue'
+    choose yes
+    click_on continue
 
-    click_on 'Back'
+    click_on back
 
-    expect(page).to have_checked_field('Yes')
+    expect(page).to have_checked_field(yes)
   end
 
   scenario 'Buyer was looking for an agency supplied worker but changed mind' do
     visit_home
     click_on 'Start now'
 
-    choose 'An individual worker'
-    click_on 'Continue'
+    choose an_individual_worker
+    click_on continue
 
-    choose 'No'
-    click_on 'Continue'
+    choose no
+    click_on continue
 
-    click_on 'Back'
+    click_on back
 
-    expect(page).to have_checked_field('No')
+    expect(page).to have_checked_field(no)
+  end
+
+  def a_managed_service_provider
+    I18n.t('search.looking_for.answer_managed_service_provider')
+  end
+
+  def continue
+    I18n.t('search.looking_for.submit')
+  end
+
+  def an_individual_worker
+    I18n.t('search.looking_for.answer_worker')
+  end
+
+  def a_managed_service_provider
+    I18n.t('search.looking_for.answer_managed_service_provider')
+  end
+
+  def yes
+    I18n.t('search.worker_type.answer_agency_supplied')
+  end
+
+  def back
+    I18n.t('layouts.application.back')
+  end
+
+  def no
+    I18n.t('search.worker_type.answer_nominated_worker')
   end
 end
