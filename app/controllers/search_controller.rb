00bc4108da8c67c5e89294e3feb13a32bd46b0b8
Ben Griffiths <bengriffiths@gmail.com>
2018-10-29 14:53:28 +0000

    Extract teacher supply journey class

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -21,27 +21,13 @@ class SearchController < ApplicationController
                    end
       render @journey.template
     else
-      redirect_to next_step_path
+      redirect_to @journey.next_step_path
     end
   end
 
   private
 
-  def next_step_path
-    case @journey.next_slug
-    when 'results'
-      branches_path(params: @journey.params)
-    when 'master-vendor-managed-service'
-      master_vendors_path(@journey.params)
-    when 'neutral-vendor-managed-service'
-      neutral_vendors_path(@journey.params)
-    else
-      search_question_path(slug: @journey.next_slug, params: @journey.params)
-    end
-  end
-
   def build_journey
-    first_step_class = Steps::LookingFor
-    Journey.new(first_step_class, params[:slug], params)
+    TeacherSupplyJourney.new(params[:slug], params)
   end
 end
