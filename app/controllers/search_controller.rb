b1af1a55830ba06842c3d34bb9c63b01f0bc4720
Chris Roos <chris@seagul.co.uk>
2018-10-29 11:46:55 +0000

    Explicitly assign @journey in SearchController
    We want to use this instance variable in the templates. We're assigning
    it explicitly (instead of as a side effect of calling `#journey`) to
    make it more idiomatic Rails.
    
    Co-authored-by: James Mead <3169+floehopper@users.noreply.github.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,24 +1,26 @@
 class SearchController < ApplicationController
   def question
-    @form_path = search_answer_path(slug: journey.current_slug)
-    @back_path = if journey.previous_slug.present?
-                   search_question_path(slug: journey.previous_slug, params: journey.params)
+    @journey = build_journey
+    @form_path = search_answer_path(slug: @journey.current_slug)
+    @back_path = if @journey.previous_slug.present?
+                   search_question_path(slug: @journey.previous_slug, params: @journey.params)
                  else
                    homepage_path
                  end
-    render journey.template
+    render @journey.template
   end
 
   def answer
-    if journey.invalid?
-      @form_path = search_answer_path(slug: journey.current_slug)
-      @back_path = if journey.previous_slug.present?
-                     search_question_path(slug: journey.previous_slug, params: journey.params)
+    @journey = build_journey
+    if @journey.invalid?
+      @form_path = search_answer_path(slug: @journey.current_slug)
+      @back_path = if @journey.previous_slug.present?
+                     search_question_path(slug: @journey.previous_slug, params: @journey.params)
                    else
                      homepage_path
                    end
-      flash.now[:error] = journey.error
-      render journey.template
+      flash.now[:error] = @journey.error
+      render @journey.template
     else
       redirect_to next_step_path
     end
@@ -27,20 +29,20 @@ class SearchController < ApplicationController
   private
 
   def next_step_path
-    case journey.next_slug
+    case @journey.next_slug
     when 'results'
-      branches_path(params: journey.params)
+      branches_path(params: @journey.params)
     when 'master-vendor-managed-service'
-      master_vendors_path(journey.params)
+      master_vendors_path(@journey.params)
     when 'neutral-vendor-managed-service'
-      neutral_vendors_path(journey.params)
+      neutral_vendors_path(@journey.params)
     else
-      search_question_path(slug: journey.next_slug, params: journey.params)
+      search_question_path(slug: @journey.next_slug, params: @journey.params)
     end
   end
 
-  def journey
+  def build_journey
     first_step_class = Steps::LookingFor
-    @journey ||= Journey.new(first_step_class, params[:slug], params)
+    Journey.new(first_step_class, params[:slug], params)
   end
 end
