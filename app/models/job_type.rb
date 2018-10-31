6a055ecd69d48b6c8389d1bf74eb396857be7e20
Paul Battley <pbattley@gmail.com>
2018-10-31 15:40:10 +0000

    Read job type and term data from CSV
    This is easier to manipulate than long embedded strings in the code.

--- app/models/job_type.rb
+++ app/models/job_type.rb
@@ -4,17 +4,5 @@ class JobType
   attr_accessor :code, :description
 end
 
-JobType.define(
-  ['code',        'description'],
-  ['qt',          'Qualified teacher: Non-SEN roles'],
-  ['qt_sen',      'Qualified teacher: SEN roles'],
-  ['uqt',         'Unqualified teacher: Non-SEN roles'],
-  ['uqt_sen',     'Unqualified teacher: SEN roles'],
-  ['support',     'Educational support staff: Non-SEN roles '\
-                  '(incl. cover supervisor and teaching assistant)'],
-  ['support_sen', 'Educational support staff: SEN roles '\
-                  '(incl. cover supervisor and teaching assistant)'],
-  ['senior',      'Other roles: headteacher and senior leadership positions'],
-  ['admin',       'Other roles: admin & clerical staff, IT staff, '\
-                  'finance staff, cleaners, etc.']
-)
+require 'csv'
+JobType.define(*CSV.read(Rails.root.join('data', 'job_types.csv')))
--- app/models/term.rb
+++ app/models/term.rb
@@ -4,11 +4,5 @@ class Term
   attr_accessor :code, :description, :rate_term
 end
 
-Term.define(
-  ['code',    'description',         'rate_term'],
-  ['0_1',     'Up to 1 week',        'one_week'],
-  ['1_4',     '1 week to 4 weeks',   'twelve_weeks'],
-  ['4_8',     '4 weeks to 8 weeks',  'twelve_weeks'],
-  ['8_12',    '8 weeks to 12 weeks', 'twelve_weeks'],
-  ['12_plus', 'Over 12 weeks',       'more_than_twelve_weeks']
-)
+require 'csv'
+Term.define(*CSV.read(Rails.root.join('data', 'terms.csv')))
