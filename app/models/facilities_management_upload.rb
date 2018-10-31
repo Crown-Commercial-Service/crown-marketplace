f00b5ee9895ac66e904f21ad10e5cf6b6cef85b6
James Mead <3169+floehopper@users.noreply.github.com>
2018-10-31 18:47:17 +0000

    Add FacilitiesManagementSupplier model
    Note that this is closely based (via copy & paste) on the Upload model
    which is used for supply teachers. I can imagine we might want to DRY
    this up at some point, but I'm happy doing this for now.

---
+++ app/models/facilities_management_upload.rb
@@ -0,0 +1,30 @@
+class FacilitiesManagementUpload
+  def self.create!(suppliers)
+    error = all_or_none(FacilitiesManagementSupplier) do
+      FacilitiesManagementSupplier.destroy_all
+
+      suppliers.map do |supplier_data|
+        create_supplier!(supplier_data)
+      end
+    end
+    raise error if error
+  end
+
+  def self.all_or_none(transaction_class)
+    error = nil
+    transaction_class.transaction do
+      yield
+    rescue ActiveRecord::RecordInvalid => e
+      error = e
+      raise ActiveRecord::Rollback
+    end
+    error
+  end
+
+  def self.create_supplier!(data)
+    FacilitiesManagementSupplier.create!(
+      id: data['supplier_id'],
+      name: data['supplier_name']
+    )
+  end
+end
---
+++ spec/models/facilities_management_upload_spec.rb
@@ -0,0 +1,115 @@
+require 'rails_helper'
+
+RSpec.describe FacilitiesManagementUpload, type: :model do
+  describe 'create' do
+    let(:supplier_name) { Faker::Company.unique.name }
+    let(:supplier_id) { SecureRandom.uuid }
+
+    let(:suppliers) do
+      [
+        {
+          'supplier_name' => supplier_name,
+          'supplier_id' => supplier_id
+        }
+      ]
+    end
+
+    context 'when supplier list is empty' do
+      let(:suppliers) { [] }
+
+      it 'does not create any suppliers' do
+        expect do
+          described_class.create!(suppliers)
+        end.not_to change(FacilitiesManagementSupplier, :count)
+      end
+    end
+
+    context 'when supplier does not exist' do
+      it 'creates supplier' do
+        expect do
+          described_class.create!(suppliers)
+        end.to change(FacilitiesManagementSupplier, :count).by(1)
+      end
+
+      it 'assigns ID to supplier' do
+        described_class.create!(suppliers)
+
+        supplier = FacilitiesManagementSupplier.last
+        expect(supplier.id).to eq(supplier_id)
+      end
+
+      it 'assigns attributes to supplier' do
+        described_class.create!(suppliers)
+
+        supplier = FacilitiesManagementSupplier.last
+        expect(supplier.name).to eq(supplier_name)
+      end
+    end
+
+    context 'when suppliers already exist' do
+      let!(:first_supplier) { create(:facilities_management_supplier) }
+      let!(:second_supplier) { create(:facilities_management_supplier) }
+
+      it 'destroys all existing suppliers' do
+        described_class.create!(suppliers)
+
+        expect(FacilitiesManagementSupplier.find_by(id: first_supplier.id)).to be_nil
+        expect(FacilitiesManagementSupplier.find_by(id: second_supplier.id)).to be_nil
+      end
+
+      context 'and data for one supplier is invalid' do
+        let(:suppliers) do
+          [
+            {
+              'supplier_name' => '',
+            }
+          ]
+        end
+
+        it 'leaves existing data intact' do
+          ignoring_exception(ActiveRecord::RecordInvalid) do
+            described_class.create!(suppliers)
+          end
+
+          expect(FacilitiesManagementSupplier.find_by(id: first_supplier.id)).to eq(first_supplier)
+          expect(FacilitiesManagementSupplier.find_by(id: second_supplier.id)).to eq(second_supplier)
+        end
+      end
+    end
+
+    context 'when data for one supplier is invalid' do
+      let(:suppliers) do
+        [
+          {
+            'supplier_name' => supplier_name,
+          },
+          {
+            'supplier_name' => '',
+          }
+        ]
+      end
+
+      it 'raises ActiveRecord::RecordInvalid exception' do
+        expect do
+          described_class.create!(suppliers)
+        end.to raise_error(ActiveRecord::RecordInvalid)
+      end
+
+      it 'does not create any suppliers' do
+        expect do
+          ignoring_exception(ActiveRecord::RecordInvalid) do
+            described_class.create!(suppliers)
+          end
+        end.not_to change(FacilitiesManagementSupplier, :count)
+      end
+    end
+  end
+
+  private
+
+  def ignoring_exception(klass)
+    yield
+  rescue klass
+    nil
+  end
+end
