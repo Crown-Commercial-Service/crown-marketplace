6d29b85d941db3a4368554635b16b2dc4847bae2
Paul Battley <pbattley@gmail.com>
2018-11-01 15:02:31 +0000

    Add NUTS 1 region to FM region
    This lets us group the massive list.

--- app/models/facilities_management_region.rb
+++ app/models/facilities_management_region.rb
@@ -3,6 +3,14 @@ class FacilitiesManagementRegion
 
   attr_accessor :code, :name
 
+  def nuts1_code
+    code[0, 3]
+  end
+
+  def nuts1_region
+    Nuts1Region.find_by(code: nuts1_code)
+  end
+
   def nuts2_code
     code[0, 4]
   end
@@ -12,11 +20,19 @@ class FacilitiesManagementRegion
   end
 
   def nuts3_code
-    code.length > 4 ? code : nil
+    nuts3? ? code : nil
   end
 
   def nuts3_region
-    nuts3_code ? Nuts3Region.find_by(code: nuts3_code) : nil
+    nuts3? ? Nuts3Region.find_by(code: nuts3_code) : nil
+  end
+
+  def nuts3?
+    code.length > 4
+  end
+
+  def nuts2?
+    !nuts3?
   end
 end
 
