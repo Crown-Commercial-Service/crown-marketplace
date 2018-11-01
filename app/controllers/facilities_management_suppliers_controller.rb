7eb676feb887760b0ff296315407250593389c70
Chris Lowis <chris.lowis@gofreerange.com>
2018-11-01 15:28:32 +0000

    Remove link to external suppliers page
    Now we have the same information on this page as contained in the
    external link, we can remove the link.

--- app/controllers/facilities_management_suppliers_controller.rb
+++ app/controllers/facilities_management_suppliers_controller.rb
@@ -3,16 +3,12 @@ class FacilitiesManagementSuppliersController < ApplicationController
     case params[:value_band]
     when 'under1_5m'
       @lot = '1a'
-      @url = 'https://ccs-agreements.cabinetoffice.gov.uk/suppliers?sm_field_contract_id=%22RM3830%3A1a%22'
     when 'under7m'
       @lot = '1a'
-      @url = 'https://ccs-agreements.cabinetoffice.gov.uk/suppliers?sm_field_contract_id=%22RM3830%3A1a%22'
     when 'under50m'
       @lot = '1b'
-      @url = 'https://ccs-agreements.cabinetoffice.gov.uk/suppliers?sm_field_contract_id=%22RM3830%3A1b%22'
     when 'over50m'
       @lot = '1c'
-      @url = 'https://ccs-agreements.cabinetoffice.gov.uk/suppliers?sm_field_contract_id=%22RM3830%3A1c%22'
     end
 
     @suppliers = FacilitiesManagementSupplier.available_in_lot(@lot)
