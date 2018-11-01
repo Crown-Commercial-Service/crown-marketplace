ab33c3ef16cdd19a7d6224256189aa7f7cd3f59f
Paul Battley <pbattley@gmail.com>
2018-11-01 16:53:48 +0000

    Support Array parameters in JourneyStep
    The behaviour of params.permit is different with arrays.

--- app/models/journey_step.rb
+++ app/models/journey_step.rb
@@ -4,17 +4,29 @@ class JourneyStep
   include ActiveModel::Validations
   extend ActiveModel::Translation
 
-  def self.attribute(name)
-    params << name
+  def self.attribute(name, type = nil)
+    if type == Array
+      array_params << name
+    else
+      single_params << name
+    end
     attr_accessor name
   end
 
   def self.params
-    @params ||= []
+    single_params + [array_params.map { |k| [k, []] }.to_h]
   end
 
-  def self.from_params(params)
-    new(params.permit(*@params))
+  def self.single_params
+    @single_params ||= []
+  end
+
+  def self.array_params
+    @array_params ||= []
+  end
+
+  def self.from_params(raw)
+    new(raw.permit(*params))
   end
 
   def slug
