b347b99c8389571e7d7606748b1587333a27c7a3
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-01 11:34:48 +0000

    Create regional availabilities for FM supplier in upload

--- app/models/facilities_management_upload.rb
+++ app/models/facilities_management_upload.rb
@@ -22,12 +22,24 @@ class FacilitiesManagementUpload
   end
 
   def self.create_supplier!(data)
-    FacilitiesManagementSupplier.create!(
+    supplier = FacilitiesManagementSupplier.create!(
       id: data['supplier_id'],
       name: data['supplier_name'],
       contact_name: data['contact_name'],
       contact_email: data['contact_email'],
       telephone_number: data['contact_phone']
     )
+
+    lots = data.fetch('lots', [])
+    lots.each do |lot|
+      lot_number = lot['lot_number']
+      regions = lot.fetch('regions', [])
+      regions.each do |region|
+        supplier.regional_availabilities.create!(
+          lot_number: lot_number,
+          region_code: region
+        )
+      end
+    end
   end
 end
