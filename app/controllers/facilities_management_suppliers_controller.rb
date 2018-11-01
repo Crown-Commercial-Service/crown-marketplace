822f93eee359fc3f7ade1515d70551714a1fab8d
Chris Lowis <chris.lowis@gofreerange.com>
2018-11-01 15:10:40 +0000

    Combine FM suppliers into a single controller action
    We'd like to dynamically populate the list of suppliers from the
    database. It'll be easier to make this change if we have a single
    controller action and view. The current `lot1*_suppliers` views differ
    only by the URL they link to (which we will remove when we have the
    supplier information in the page), and the presence of some additional
    "direct award" guidance for Lot 1a. This commit combines them and
    simplifies the preceding code path.

--- app/controllers/facilities_management_suppliers_controller.rb
+++ app/controllers/facilities_management_suppliers_controller.rb
@@ -1,17 +1,22 @@
 class FacilitiesManagementSuppliersController < ApplicationController
-  def lot1a_suppliers
-    @back_path = source_journey.current_step_path
-    @suppliers = FacilitiesManagementSupplier.available_in_lot('1a')
-  end
+  def index
+    case params[:value_band]
+    when 'under1_5m'
+      @lot = '1a'
+      @url = 'https://ccs-agreements.cabinetoffice.gov.uk/suppliers?sm_field_contract_id=%22RM3830%3A1a%22'
+    when 'under7m'
+      @lot = '1a'
+      @url = 'https://ccs-agreements.cabinetoffice.gov.uk/suppliers?sm_field_contract_id=%22RM3830%3A1a%22'
+    when 'under50m'
+      @lot = '1b'
+      @url = 'https://ccs-agreements.cabinetoffice.gov.uk/suppliers?sm_field_contract_id=%22RM3830%3A1b%22'
+    when 'over50m'
+      @lot = '1c'
+      @url = 'https://ccs-agreements.cabinetoffice.gov.uk/suppliers?sm_field_contract_id=%22RM3830%3A1c%22'
+    end
 
-  def lot1b_suppliers
+    @suppliers = FacilitiesManagementSupplier.available_in_lot(@lot)
     @back_path = source_journey.current_step_path
-    @suppliers = FacilitiesManagementSupplier.available_in_lot('1b')
-  end
-
-  def lot1c_suppliers
-    @back_path = source_journey.current_step_path
-    @suppliers = FacilitiesManagementSupplier.available_in_lot('1c')
   end
 
   private
--- app/models/facilities_management_journey.rb
+++ app/models/facilities_management_journey.rb
@@ -17,12 +17,8 @@ class FacilitiesManagementJourney < Journey
 
   def next_step_path
     case next_slug
-    when 'lot1a-suppliers'
-      facilities_management_lot1a_suppliers_path(journey: self.class.journey_name, params: params)
-    when 'lot1b-suppliers'
-      facilities_management_lot1b_suppliers_path(journey: self.class.journey_name, params: params)
-    when 'lot1c-suppliers'
-      facilities_management_lot1c_suppliers_path(journey: self.class.journey_name, params: params)
+    when 'suppliers'
+      facilities_management_suppliers_path(journey: self.class.journey_name, params: params)
     else
       super
     end
