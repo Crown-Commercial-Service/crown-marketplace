66dd104df8abc746003b85f4d792a6d500231f85
Chris Lowis <chris.lowis@gofreerange.com>
2018-11-01 15:24:50 +0000

    Display supplier contact details
    This is just the basic supplier name and contact details. I haven't
    attempted to style to match the design at this stage.

--- spec/views/facilities_management_suppliers/index.html.erb_spec.rb
+++ spec/views/facilities_management_suppliers/index.html.erb_spec.rb
@@ -5,9 +5,28 @@ RSpec.describe 'facilities_management_suppliers/index.html.erb' do
   let(:supplier2) { build(:facilities_management_supplier) }
   let(:suppliers) { [supplier1, supplier2] }
 
-  it 'displays the number of suppliers in the lot' do
+  before do
     assign(:suppliers, suppliers)
     render
+  end
+
+  it 'displays the number of suppliers in the lot' do
     expect(rendered).to include('2 suppliers')
+  end
+
+  it 'displays the supplier name' do
+    expect(rendered).to include(supplier1.name)
+  end
+
+  it 'displays the supplier contact name' do
+    expect(rendered).to include(supplier1.contact_name)
+  end
+
+  it 'displays the supplier contact email' do
+    expect(rendered).to include(supplier1.contact_email)
+  end
+
+  it 'displays the supplier telephone number' do
+    expect(rendered).to include(supplier1.telephone_number)
   end
 end
