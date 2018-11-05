10ace9d7e1e579267d4a74a0ecc02b16d53a90f9
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-05 21:09:23 +0000

    Extract FacilitiesManagementLot#direct_award_possible?
    It's not 100% obvious that this is entirely a concern of the
    FacilitiesManagementLot class, but I think it's better to extract it
    somewhere rather than have a magic lot number in the template.

--- app/models/facilities_management_lot.rb
+++ app/models/facilities_management_lot.rb
@@ -2,6 +2,10 @@ class FacilitiesManagementLot
   include StaticRecord
 
   attr_accessor :number, :description
+
+  def direct_award_possible?
+    number == '1a'
+  end
 end
 
 FacilitiesManagementLot.load_csv('facilities_management_lots.csv')
