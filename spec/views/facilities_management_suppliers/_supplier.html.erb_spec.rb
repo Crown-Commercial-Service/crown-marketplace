9718f31de225ee72096ccdafc5eb8f5a236c3510
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-05 17:04:29 +0000

    Display services offered by each supplier on FM supplier index page
    Note that services are grouped by work package. This is very unlikely to
    be the final layout/markup, because each supplier has *loads* of
    services which makes the page virtually unusable. However, we can
    iterate on this in subsequent commits.

--- spec/views/facilities_management_suppliers/_supplier.html.erb_spec.rb
+++ spec/views/facilities_management_suppliers/_supplier.html.erb_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'facilities_management_suppliers/_supplier.html.erb' do
   let(:supplier) { build(:facilities_management_supplier) }
+  let(:services) { {} }
 
   before do
-    render 'facilities_management_suppliers/supplier', supplier: supplier
+    allow(supplier).to receive(:services_by_work_package_in_lot).and_return(services)
+    render 'facilities_management_suppliers/supplier', supplier: supplier, lot_number: '1a'
   end
 
   it 'displays the supplier name' do
@@ -21,5 +23,39 @@ RSpec.describe 'facilities_management_suppliers/_supplier.html.erb' do
 
   it 'displays the supplier telephone number' do
     expect(rendered).to have_text(supplier.telephone_number)
+  end
+
+  context 'when supplier has multiple services in one work package' do
+    let(:work_package) { FacilitiesManagementWorkPackage.new(name: 'work-package') }
+    let(:service1) { FacilitiesManagementService.new(name: 'service-1') }
+    let(:service2) { FacilitiesManagementService.new(name: 'service-2') }
+
+    let(:services) do
+      {
+        work_package => [service1, service2]
+      }
+    end
+
+    it 'displays both services within the work package' do
+      expect(rendered).to have_text(/work-package\s+service-1\s+service-2/)
+    end
+  end
+
+  context 'when supplier has one service in two different work packages' do
+    let(:work_package1) { FacilitiesManagementWorkPackage.new(name: 'work-package-1') }
+    let(:work_package2) { FacilitiesManagementWorkPackage.new(name: 'work-package-2') }
+    let(:service1) { FacilitiesManagementService.new(name: 'service-1') }
+    let(:service2) { FacilitiesManagementService.new(name: 'service-2') }
+
+    let(:services) do
+      {
+        work_package1 => [service1],
+        work_package2 => [service2]
+      }
+    end
+
+    it 'displays each service within the its work package' do
+      expect(rendered).to have_text(/work-package-1\s+service-1\s+work-package-2\s+service-2/)
+    end
   end
 end
