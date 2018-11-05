7c0e9470ceb53b51dc1f771a610d882ed23c1e97
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-05 21:09:23 +0000

    Extract FacilitiesManagementLot#direct_award_possible?
    It's not 100% obvious that this is entirely a concern of the
    FacilitiesManagementLot class, but I think it's better to extract it
    somewhere rather than have a magic lot number in the template.

--- spec/models/facilities_management_lot_spec.rb
+++ spec/models/facilities_management_lot_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe FacilitiesManagementLot, type: :model do
     expect(first_lot.number).to eq('1a')
     expect(first_lot.description).to eq('Total contract value up to £7M')
   end
+
+  describe '#direct_award_possible?' do
+    let(:result) { lot.direct_award_possible? }
+
+    context 'when number is 1a' do
+      let(:lot) { described_class.find_by(number: '1a') }
+
+      it 'returns truth-y' do
+        expect(result).to be_truthy
+      end
+    end
+
+    context 'when number is not 1a' do
+      let(:lot) { described_class.find_by(number: '1b') }
+
+      it 'returns false-y' do
+        expect(result).to be_falsey
+      end
+    end
+  end
 end
