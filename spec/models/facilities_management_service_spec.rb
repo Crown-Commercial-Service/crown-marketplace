37d02fa583c41c3fb168c3576e7b8b0738c70b74
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-06 11:36:02 +0000

    Add FacilitiesManagementService#mandatory? method & backing data

--- spec/models/facilities_management_service_spec.rb
+++ spec/models/facilities_management_service_spec.rb
@@ -15,9 +15,33 @@ RSpec.describe FacilitiesManagementService, type: :model do
     expect(first_service.work_package_code).to eq('A')
   end
 
+  it 'populates mandatory attribute of first service' do
+    expect(first_service.mandatory).to eq('true')
+  end
+
   it 'looks up work package based on its code' do
     work_package = first_service.work_package
     expect(work_package.code).to eq('A')
     expect(work_package.name).to eq('Contract Management')
+  end
+
+  describe '#mandatory?' do
+    let(:service) { described_class.new(mandatory: mandatory) }
+
+    context 'when mandatory is "true"' do
+      let(:mandatory) { 'true' }
+
+      it 'returns truth-y' do
+        expect(service).to be_mandatory
+      end
+    end
+
+    context 'when mandatory is "false"' do
+      let(:mandatory) { 'false' }
+
+      it 'returns false-y' do
+        expect(service).not_to be_mandatory
+      end
+    end
   end
 end
