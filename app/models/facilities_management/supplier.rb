c3e876205ceedd9ba69da65451cc671bd9542dcc
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-06 18:44:27 +0000

    Use explicit module definition to DRY up FM models
    This diff is best viewed with the `--ignore-all-space ` option, because
    of all the indentation changes.

--- app/models/facilities_management/supplier.rb
+++ app/models/facilities_management/supplier.rb
@@ -1,46 +1,48 @@
-class FacilitiesManagement::Supplier < ApplicationRecord
-  has_many :regional_availabilities,
-           class_name: 'FacilitiesManagement::RegionalAvailability',
-           foreign_key: :facilities_management_supplier_id,
-           inverse_of: :supplier,
-           dependent: :destroy
+module FacilitiesManagement
+  class Supplier < ApplicationRecord
+    has_many :regional_availabilities,
+             class_name: 'FacilitiesManagement::RegionalAvailability',
+             foreign_key: :facilities_management_supplier_id,
+             inverse_of: :supplier,
+             dependent: :destroy
 
-  has_many :service_offerings,
-           class_name: 'FacilitiesManagement::ServiceOffering',
-           foreign_key: :facilities_management_supplier_id,
-           inverse_of: :supplier,
-           dependent: :destroy
+    has_many :service_offerings,
+             class_name: 'FacilitiesManagement::ServiceOffering',
+             foreign_key: :facilities_management_supplier_id,
+             inverse_of: :supplier,
+             dependent: :destroy
 
-  validates :name, presence: true
+    validates :name, presence: true
 
-  def self.available_in_lot(lot_number)
-    joins(:regional_availabilities)
-      .merge(FacilitiesManagement::RegionalAvailability.for_lot(lot_number))
-      .includes(:service_offerings)
-      .uniq
-  end
+    def self.available_in_lot(lot_number)
+      joins(:regional_availabilities)
+        .merge(RegionalAvailability.for_lot(lot_number))
+        .includes(:service_offerings)
+        .uniq
+    end
 
-  def self.delete_all_with_dependents
-    FacilitiesManagement::RegionalAvailability.delete_all
-    FacilitiesManagement::ServiceOffering.delete_all
-    delete_all
-  end
+    def self.delete_all_with_dependents
+      RegionalAvailability.delete_all
+      ServiceOffering.delete_all
+      delete_all
+    end
 
-  def self.available_in_lot_and_regions(lot_number, region_codes)
-    where(id: FacilitiesManagement::RegionalAvailability
-                .supplier_ids_for_lot_and_regions(lot_number, region_codes))
-      .joins(:regional_availabilities)
-      .merge(FacilitiesManagement::RegionalAvailability
-               .for_lot_and_regions(lot_number, region_codes))
-      .includes(:service_offerings)
-      .uniq
-  end
+    def self.available_in_lot_and_regions(lot_number, region_codes)
+      where(id: RegionalAvailability
+                  .supplier_ids_for_lot_and_regions(lot_number, region_codes))
+        .joins(:regional_availabilities)
+        .merge(RegionalAvailability
+                 .for_lot_and_regions(lot_number, region_codes))
+        .includes(:service_offerings)
+        .uniq
+    end
 
-  def services_by_work_package_in_lot(lot_number)
-    service_offerings_in_lot(lot_number).map(&:service).group_by(&:work_package)
-  end
+    def services_by_work_package_in_lot(lot_number)
+      service_offerings_in_lot(lot_number).map(&:service).group_by(&:work_package)
+    end
 
-  def service_offerings_in_lot(lot_number)
-    service_offerings.select { |so| so.lot_number == lot_number }
+    def service_offerings_in_lot(lot_number)
+      service_offerings.select { |so| so.lot_number == lot_number }
+    end
   end
 end
