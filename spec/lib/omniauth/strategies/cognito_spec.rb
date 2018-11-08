a0f2dfd71c7625273c8a721628fbeaa97352f93b
Chris Roos <chris@seagul.co.uk>
2018-11-08 22:50:18 +0000

    Verify claims in the Cognito JWT
    Note that we're only verifying the issuer at the moment.

--- spec/lib/omniauth/strategies/cognito_spec.rb
+++ spec/lib/omniauth/strategies/cognito_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe OmniAuth::Strategies::Cognito do
     allow(strategy).to receive(:validated_id_token).and_return(cognito_id_token)
     expect(strategy.info[:email]).to eq('email-of-authenticated-user')
   end
+
+  it 'verifies claims in cognito token' do
+    allow(strategy).to receive(:id_token).and_return('id-token')
+
+    allow(Cognito::UserPool).to receive(:new).and_return('user-pool')
+
+    encoded_token = instance_double(Cognito::EncodedToken)
+    allow(Cognito::EncodedToken).to receive(:new).and_return(encoded_token)
+
+    token = instance_double(Cognito::Token)
+    allow(encoded_token).to receive(:decode).and_return(token)
+    allow(token).to receive(:verify!).and_return('verified-token')
+
+    expect(strategy.send(:validated_id_token)).to eq('verified-token')
+  end
 end
