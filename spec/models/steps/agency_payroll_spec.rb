12505088580826ccb7f5162caba0bddd40fce300
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-08 13:44:36 +0000

    Add model specs for ST steps with custom validation error messages
    My main aim here is to add some kind of check that the custom validation
    error message is present. This will be particularly relevant when I move
    these step classes into the SupplyTeachers namespace.

---
+++ spec/models/steps/agency_payroll_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Steps::AgencyPayroll, type: :model do
+  subject(:step) do
+    described_class.new(
+      postcode: postcode,
+      term: Term.all.first,
+      job_type: JobType.all.first
+    )
+  end
+
+  let(:model_key) { 'activemodel.errors.models.steps/agency_payroll' }
+
+  let(:postcode) { Faker::Address.unique.postcode }
+
+  before do
+    Geocoder::Lookup::Test.add_stub(
+      postcode, [{ 'coordinates' => [51.5149666, -0.119098] }]
+    )
+  end
+
+  after do
+    Geocoder::Lookup::Test.reset
+  end
+
+  it { is_expected.to be_valid }
+
+  context 'when location is not valid' do
+    before do
+      step.postcode = 'XY1 2AB'
+    end
+
+    it { is_expected.not_to be_valid }
+
+    it 'obtains the error message from an I18n translation' do
+      step.valid?
+      expect(step.errors[:location]).to include(
+        I18n.t("#{model_key}.attributes.location.invalid_location")
+      )
+    end
+  end
+end
---
+++ spec/models/steps/payroll_provider_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Steps::PayrollProvider, type: :model do
+  subject(:step) { described_class.new(payroll_provider: 'school') }
+
+  let(:model_key) { 'activemodel.errors.models.steps/payroll_provider' }
+
+  it { is_expected.to be_valid }
+
+  context 'when payroll_provider is not valid' do
+    before do
+      step.payroll_provider = 'unacceptable-value'
+    end
+
+    it { is_expected.not_to be_valid }
+
+    it 'obtains the error message from an I18n translation' do
+      step.valid?
+      expect(step.errors[:payroll_provider]).to include(
+        I18n.t("#{model_key}.attributes.payroll_provider.inclusion")
+      )
+    end
+  end
+end
