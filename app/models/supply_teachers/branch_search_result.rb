7f38dc250e6d00016e68254c2f6facede0d6bfff
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-08 14:55:38 +0000

    Use explicit module definition for BranchSearchResult
    This diff is best viewed with the `--ignore-all-space ` option, because
    of all the indentation changes.

--- app/models/supply_teachers/branch_search_result.rb
+++ app/models/supply_teachers/branch_search_result.rb
@@ -1,18 +1,20 @@
-class SupplyTeachers::BranchSearchResult
-  attr_reader :supplier_name
-  attr_reader :name
-  attr_reader :contact_name
-  attr_reader :telephone_number
-  attr_reader :contact_email
-  attr_accessor :rate
-  attr_accessor :distance
+module SupplyTeachers
+  class BranchSearchResult
+    attr_reader :supplier_name
+    attr_reader :name
+    attr_reader :contact_name
+    attr_reader :telephone_number
+    attr_reader :contact_email
+    attr_accessor :rate
+    attr_accessor :distance
 
-  def initialize(supplier_name:, name:, contact_name:,
-                 telephone_number:, contact_email:)
-    @supplier_name = supplier_name
-    @name = name
-    @contact_name = contact_name
-    @telephone_number = telephone_number
-    @contact_email = contact_email
+    def initialize(supplier_name:, name:, contact_name:,
+                   telephone_number:, contact_email:)
+      @supplier_name = supplier_name
+      @name = name
+      @contact_name = contact_name
+      @telephone_number = telephone_number
+      @contact_email = contact_email
+    end
   end
 end
