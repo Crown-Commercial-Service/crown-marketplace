443d9ba99471ad78a8062e6ff654fbf8ea82bf74
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-08 08:41:03 +0000

    Move FM journey steps into FacilitiesManagement namespace
    Note that I had to add one explicit require statement to fix a constant
    lookup which was causing specs to fail intermittently (order dependent).
    I suspect we are missing a bunch of these elsewhere, but I plan to
    address that separately.

---
+++ app/models/facilities_management/steps/supplier_region.rb
@@ -0,0 +1,23 @@
+module FacilitiesManagement
+  module Steps
+    class SupplierRegion
+      include JourneyStep
+
+      attribute :region_codes, Array
+      validates :region_codes, length: { minimum: 1 }
+
+      def initialize(*)
+        self.region_codes = []
+        super
+      end
+
+      def regions
+        FacilitiesManagement::Region.where(code: region_codes)
+      end
+
+      def next_step_class
+        Suppliers
+      end
+    end
+  end
+end
---
+++ app/models/facilities_management/steps/value_band.rb
@@ -0,0 +1,29 @@
+module FacilitiesManagement
+  module Steps
+    class Suppliers
+      include JourneyStep
+    end
+
+    class ValueBand
+      include JourneyStep
+
+      attribute :value_band
+      validates :value_band, inclusion: {
+        in: ['under1_5m', 'under7m', 'under50m', 'over50m']
+      }
+
+      def next_step_class
+        case value_band
+        when 'under1_5m'
+          SupplierRegion
+        when 'under7m'
+          SupplierRegion
+        when 'under50m'
+          Suppliers
+        when 'over50m'
+          Suppliers
+        end
+      end
+    end
+  end
+end
