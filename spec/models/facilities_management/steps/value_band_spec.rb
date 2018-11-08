d0ecf1a11c8490823bd1c0c9798ec8b993fa5f4f
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-08 12:43:42 +0000

    Fix I18n translation keys for FM steps in new namespace
    This problem wasn't caught by any spec failures, so I've added a couple
    of model specs which hopefully give us a better chance of catching
    similar problems in future.

---
+++ spec/models/facilities_management/steps/value_band_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+module FacilitiesManagement
+  RSpec.describe Steps::ValueBand, type: :model do
+    subject(:step) { described_class.new(value_band: 'under1_5m') }
+
+    let(:model_key) { 'activemodel.errors.models.facilities_management/steps/value_band' }
+
+    it { is_expected.to be_valid }
+
+    context 'when value band is not in list of acceptable values' do
+      before do
+        step.value_band = 'unacceptable-value'
+      end
+
+      it { is_expected.not_to be_valid }
+
+      it 'obtains the error message from an I18n translation' do
+        step.valid?
+        expect(step.errors[:value_band]).to include(
+          I18n.t("#{model_key}.attributes.value_band.inclusion")
+        )
+      end
+    end
+  end
+end
