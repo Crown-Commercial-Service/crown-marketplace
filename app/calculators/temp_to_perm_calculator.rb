14569a9bec5fcce6c2c91a1d06f6820662a08402
Chris Lowis <chris.lowis@gofreerange.com>
2018-11-08 14:48:52 +0000

    Add temp-to-perm fee calculator
    This calculator implements some of the logic for the calculation in
    Schedule 3 of the supply teacher agency framework as explained using
    prose and screenshots of Excel in Trello[1].
    
    This calculator does not currently include any logic for capping the
    fee based on the notice period, that will have to be implemented
    later.
    
    The explanation mentions that bank holidays must be excluded in the
    working days calculation, but does not specify which bank holidays to
    include where those differ by country. On the basis of other
    conversations about sign-in, I've assumed we will use English bank
    holidays as provided by the `gb_eng` option in the Holidays gem.
    
    In case it's not obvious to anyone reading this, and it wasn't to me,
    the calculation of the daily_supplier_fee gives the agency's
    proportion of the mark-up. That is, for example, if you pay £100 and
    the markup is 15% then the teacher receives £86.96 and the agency
    receives £13.04 (£86.96 * 1.15 = £100).
    
    [1] https://trello.com/c/0DH6ybk5/154-buyers-can-estimate-the-cost-of-hiring-a-temporary-worker-as-permanent#comment-5bd9ab593fd0b289d574289a

---
+++ app/calculators/temp_to_perm_calculator.rb
@@ -0,0 +1,39 @@
+require 'holidays'
+
+module TempToPermCalculator
+  class Calculator
+    # rubocop:disable Metrics/ParameterLists
+    def initialize(
+      day_rate:,
+      days_per_week:,
+      contract_start_date:,
+      hire_date:,
+      markup_rate:,
+      school_holidays:
+    )
+      @day_rate = day_rate
+      @days_per_week = days_per_week
+      @contract_start_date = contract_start_date
+      @hire_date = hire_date
+      @markup_rate = markup_rate
+      @school_holidays = school_holidays
+    end
+    # rubocop:enable Metrics/ParameterLists
+
+    def fee
+      daily_supplier_fee * working_days * (@days_per_week / 5.0)
+    end
+
+    def daily_supplier_fee
+      @day_rate - (@day_rate / (1 + @markup_rate))
+    end
+
+    def working_days
+      (@contract_start_date..@hire_date).select do |day|
+        day != @hire_date &&
+          day.on_weekday? &&
+          Holidays.on(day, :gb_eng).empty?
+      end.count - @school_holidays
+    end
+  end
+end
