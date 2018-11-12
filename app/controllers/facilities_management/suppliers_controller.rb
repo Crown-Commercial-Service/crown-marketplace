38b1072b7144b12bbeb23301ce7423b42da7793e
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-12 08:51:09 +0000

    Use explicit module declaration to DRY up FM controllers
    I've added an explicit require statement for
    FacilitiesManagement::Journey, because Rails' auto-loading picks up the
    top-level Journey class otherwise. My attention was drawn to this issue
    by some order-dependent spec failures.
    
    This diff is best viewed with the `--ignore-all-space` option, because
    of all the indentation changes.

--- app/controllers/facilities_management/suppliers_controller.rb
+++ app/controllers/facilities_management/suppliers_controller.rb
@@ -1,29 +1,33 @@
-class FacilitiesManagement::SuppliersController < ApplicationController
-  def index
-    @journey = FacilitiesManagement::Journey.new(params[:slug], params)
-    @back_path = @journey.previous_step_path
+require 'facilities_management/journey'
 
-    case params[:value_band]
-    when 'under1_5m'
-      small_lot
-    when 'under7m'
-      small_lot
-    when 'under50m'
-      big_lot '1b'
-    when 'over50m'
-      big_lot '1c'
+module FacilitiesManagement
+  class SuppliersController < ApplicationController
+    def index
+      @journey = Journey.new(params[:slug], params)
+      @back_path = @journey.previous_step_path
+
+      case params[:value_band]
+      when 'under1_5m'
+        small_lot
+      when 'under7m'
+        small_lot
+      when 'under50m'
+        big_lot '1b'
+      when 'over50m'
+        big_lot '1c'
+      end
     end
-  end
 
-  private
+    private
 
-  def small_lot
-    @lot = FacilitiesManagement::Lot.find_by(number: '1a')
-    @suppliers = FacilitiesManagement::Supplier.available_in_lot_and_regions(@lot.number, params[:region_codes])
-  end
+    def small_lot
+      @lot = Lot.find_by(number: '1a')
+      @suppliers = Supplier.available_in_lot_and_regions(@lot.number, params[:region_codes])
+    end
 
-  def big_lot(lot_number)
-    @lot = FacilitiesManagement::Lot.find_by(number: lot_number)
-    @suppliers = FacilitiesManagement::Supplier.available_in_lot(@lot.number)
+    def big_lot(lot_number)
+      @lot = Lot.find_by(number: lot_number)
+      @suppliers = Supplier.available_in_lot(@lot.number)
+    end
   end
 end
