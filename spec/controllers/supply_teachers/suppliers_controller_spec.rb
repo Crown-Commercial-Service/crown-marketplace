494a835b7f4963140b5194bb84cbcb87f27b4595
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-12 08:52:09 +0000

    Use explicit module declaration to DRY up ST controller specs

--- spec/controllers/supply_teachers/suppliers_controller_spec.rb
+++ spec/controllers/supply_teachers/suppliers_controller_spec.rb
@@ -1,67 +1,69 @@
 require 'rails_helper'
 
-RSpec.describe SupplyTeachers::SuppliersController, type: :controller do
-  describe 'GET master_vendors' do
-    let(:supplier) { build(:supplier) }
-    let(:suppliers) { [supplier] }
+module SupplyTeachers
+  RSpec.describe SuppliersController, type: :controller do
+    describe 'GET master_vendors' do
+      let(:supplier) { build(:supplier) }
+      let(:suppliers) { [supplier] }
 
-    before do
-      allow(SupplyTeachers::Supplier).to receive(:with_master_vendor_rates).and_return(suppliers)
-      get :master_vendors, params: {
-        journey: 'supply-teachers',
-        looking_for: 'managed_service_provider',
-        managed_service_provider: 'master_vendor'
-      }
+      before do
+        allow(Supplier).to receive(:with_master_vendor_rates).and_return(suppliers)
+        get :master_vendors, params: {
+          journey: 'supply-teachers',
+          looking_for: 'managed_service_provider',
+          managed_service_provider: 'master_vendor'
+        }
+      end
+
+      it 'renders the master_vendors template' do
+        expect(response).to render_template('master_vendors')
+      end
+
+      it 'assigns suppliers with master vendor rates to suppliers' do
+        expect(assigns(:suppliers)).to eq(suppliers)
+      end
+
+      it 'sets the back path to the managed-service-provider question' do
+        expected_path = journey_question_path(
+          journey: 'supply-teachers',
+          slug: 'managed-service-provider',
+          looking_for: 'managed_service_provider',
+          managed_service_provider: 'master_vendor'
+        )
+        expect(assigns(:back_path)).to eq(expected_path)
+      end
     end
 
-    it 'renders the master_vendors template' do
-      expect(response).to render_template('master_vendors')
-    end
+    describe 'GET neutral_vendors' do
+      let(:supplier) { build(:supplier) }
+      let(:suppliers) { [supplier] }
 
-    it 'assigns suppliers with master vendor rates to suppliers' do
-      expect(assigns(:suppliers)).to eq(suppliers)
-    end
+      before do
+        allow(Supplier).to receive(:with_neutral_vendor_rates).and_return(suppliers)
+        get :neutral_vendors, params: {
+          journey: 'supply-teachers',
+          looking_for: 'managed_service_provider',
+          managed_service_provider: 'neutral_vendor'
+        }
+      end
 
-    it 'sets the back path to the managed-service-provider question' do
-      expected_path = journey_question_path(
-        journey: 'supply-teachers',
-        slug: 'managed-service-provider',
-        looking_for: 'managed_service_provider',
-        managed_service_provider: 'master_vendor'
-      )
-      expect(assigns(:back_path)).to eq(expected_path)
-    end
-  end
+      it 'renders the neutral_vendors template' do
+        expect(response).to render_template('neutral_vendors')
+      end
 
-  describe 'GET neutral_vendors' do
-    let(:supplier) { build(:supplier) }
-    let(:suppliers) { [supplier] }
+      it 'assigns suppliers with neutral vendor rates to suppliers' do
+        expect(assigns(:suppliers)).to eq(suppliers)
+      end
 
-    before do
-      allow(SupplyTeachers::Supplier).to receive(:with_neutral_vendor_rates).and_return(suppliers)
-      get :neutral_vendors, params: {
-        journey: 'supply-teachers',
-        looking_for: 'managed_service_provider',
-        managed_service_provider: 'neutral_vendor'
-      }
-    end
-
-    it 'renders the neutral_vendors template' do
-      expect(response).to render_template('neutral_vendors')
-    end
-
-    it 'assigns suppliers with neutral vendor rates to suppliers' do
-      expect(assigns(:suppliers)).to eq(suppliers)
-    end
-
-    it 'sets the back path to the managed-service-provider question' do
-      expected_path = journey_question_path(
-        journey: 'supply-teachers',
-        slug: 'managed-service-provider',
-        looking_for: 'managed_service_provider',
-        managed_service_provider: 'neutral_vendor'
-      )
-      expect(assigns(:back_path)).to eq(expected_path)
+      it 'sets the back path to the managed-service-provider question' do
+        expected_path = journey_question_path(
+          journey: 'supply-teachers',
+          slug: 'managed-service-provider',
+          looking_for: 'managed_service_provider',
+          managed_service_provider: 'neutral_vendor'
+        )
+        expect(assigns(:back_path)).to eq(expected_path)
+      end
     end
   end
 end
