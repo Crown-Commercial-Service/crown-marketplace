16e0cd83df2ab72b5d7b104d6d1905ff4f613cc9
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-12 08:52:09 +0000

    Use explicit module declaration to DRY up ST controller specs

--- spec/controllers/supply_teachers/branches_controller_spec.rb
+++ spec/controllers/supply_teachers/branches_controller_spec.rb
@@ -1,105 +1,107 @@
 require 'rails_helper'
 
-RSpec.describe SupplyTeachers::BranchesController, type: :controller do
-  describe 'GET index' do
-    let(:first_branch) { build(:branch) }
-    let(:second_branch) { build(:branch) }
-    let(:branches) { [first_branch, second_branch] }
+module SupplyTeachers
+  RSpec.describe BranchesController, type: :controller do
+    describe 'GET index' do
+      let(:first_branch) { build(:branch) }
+      let(:second_branch) { build(:branch) }
+      let(:branches) { [first_branch, second_branch] }
 
-    context 'with a valid postcode' do
-      let(:postcode) { 'W1A 1AA' }
-      let(:params) do
-        {
-          journey: 'supply-teachers',
-          looking_for: 'worker',
-          worker_type: 'nominated',
-          postcode: postcode,
-          slug: 'nominated-worker-results'
-        }
+      context 'with a valid postcode' do
+        let(:postcode) { 'W1A 1AA' }
+        let(:params) do
+          {
+            journey: 'supply-teachers',
+            looking_for: 'worker',
+            worker_type: 'nominated',
+            postcode: postcode,
+            slug: 'nominated-worker-results'
+          }
+        end
+
+        before do
+          allow(Branch).to receive(:search).and_return(branches)
+
+          Geocoder::Lookup::Test.add_stub(
+            postcode, [{ 'coordinates' => [51.5149666, -0.119098] }]
+          )
+          get :index, params: params
+        end
+
+        after do
+          Geocoder::Lookup::Test.reset
+        end
+
+        it 'assigns back_path to school-postcode question path' do
+          expect(assigns(:back_path)).to eq(
+            journey_question_path(params.merge(slug: 'school-postcode'))
+          )
+        end
+
+        it 'assigns BranchSearchResults to @branches' do
+          expect(assigns(:branches).map(&:class).uniq).to eq(
+            [BranchSearchResult]
+          )
+        end
+
+        it 'expects branches to be assigned to @branches' do
+          expect(assigns(:branches).map(&:name)).to eq([first_branch.name, second_branch.name])
+        end
+
+        it 'responds to html' do
+          expect(response.content_type).to eq 'text/html'
+        end
+
+        it 'responds to requests for spreadsheets' do
+          allow(Spreadsheet).to receive(:new).and_return(instance_double('Spreadsheet', to_xlsx: 'spreadsheet-data'))
+
+          get :index, params: params.merge(format: 'xlsx')
+
+          expect(response.content_type).to eq 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        end
       end
 
-      before do
-        allow(SupplyTeachers::Branch).to receive(:search).and_return(branches)
+      context 'when postcode parsing fails' do
+        let(:params) do
+          {
+            journey: 'supply-teachers',
+            postcode: 'nonsense',
+            worker_type: 'nominated',
+            looking_for: 'worker'
+          }
+        end
 
-        Geocoder::Lookup::Test.add_stub(
-          postcode, [{ 'coordinates' => [51.5149666, -0.119098] }]
-        )
-        get :index, params: params
+        before do
+          get :index, params: params
+        end
+
+        it 'renders school-postcode question' do
+          expect(response).to render_template('journey/school_postcode')
+        end
       end
 
-      after do
-        Geocoder::Lookup::Test.reset
-      end
+      context 'when postcode geocoding fails' do
+        let(:postcode) { valid_fake_postcode }
 
-      it 'assigns back_path to school-postcode question path' do
-        expect(assigns(:back_path)).to eq(
-          journey_question_path(params.merge(slug: 'school-postcode'))
-        )
-      end
+        before do
+          Geocoder::Lookup::Test.add_stub(
+            postcode, [{ 'coordinates' => nil }]
+          )
+          get :index, params: {
+            journey: 'supply-teachers',
+            postcode: postcode,
+            worker_type: 'nominated',
+            looking_for: 'worker'
+          }
+        end
 
-      it 'assigns BranchSearchResults to @branches' do
-        expect(assigns(:branches).map(&:class).uniq).to eq(
-          [SupplyTeachers::BranchSearchResult]
-        )
-      end
+        after do
+          Geocoder::Lookup::Test.reset
+        end
 
-      it 'expects branches to be assigned to @branches' do
-        expect(assigns(:branches).map(&:name)).to eq([first_branch.name, second_branch.name])
-      end
-
-      it 'responds to html' do
-        expect(response.content_type).to eq 'text/html'
-      end
-
-      it 'responds to requests for spreadsheets' do
-        allow(Spreadsheet).to receive(:new).and_return(instance_double('Spreadsheet', to_xlsx: 'spreadsheet-data'))
-
-        get :index, params: params.merge(format: 'xlsx')
-
-        expect(response.content_type).to eq 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-      end
-    end
-
-    context 'when postcode parsing fails' do
-      let(:params) do
-        {
-          journey: 'supply-teachers',
-          postcode: 'nonsense',
-          worker_type: 'nominated',
-          looking_for: 'worker'
-        }
-      end
-
-      before do
-        get :index, params: params
-      end
-
-      it 'renders school-postcode question' do
-        expect(response).to render_template('journey/school_postcode')
-      end
-    end
-
-    context 'when postcode geocoding fails' do
-      let(:postcode) { valid_fake_postcode }
-
-      before do
-        Geocoder::Lookup::Test.add_stub(
-          postcode, [{ 'coordinates' => nil }]
-        )
-        get :index, params: {
-          journey: 'supply-teachers',
-          postcode: postcode,
-          worker_type: 'nominated',
-          looking_for: 'worker'
-        }
-      end
-
-      after do
-        Geocoder::Lookup::Test.reset
-      end
-
-      it 'renders school-postcode question' do
-        expect(response).to render_template('journey/school_postcode')
+        it 'renders school-postcode question' do
+          expect(response).to render_template('journey/school_postcode')
+        end
       end
     end
   end
--- spec/controllers/supply_teachers/uploads_controller_spec.rb
+++ spec/controllers/supply_teachers/uploads_controller_spec.rb
@@ -1,42 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe SupplyTeachers::UploadsController, type: :controller do
-  describe 'POST create' do
-    let(:suppliers) { [] }
+module SupplyTeachers
+  RSpec.describe UploadsController, type: :controller do
+    describe 'POST create' do
+      let(:suppliers) { [] }
 
-    before do
-      allow(SupplyTeachers::Upload).to receive(:create!)
-    end
-
-    it 'creates suppliers and their associated data from JSON' do
-      post :create, body: suppliers.to_json
-
-      expect(SupplyTeachers::Upload).to have_received(:create!).with(suppliers)
-    end
-
-    it 'responds with HTTP created status' do
-      post :create, body: suppliers.to_json
-
-      expect(response).to have_http_status(:created)
-    end
-
-    context 'when model validation error occurs' do
       before do
-        allow(SupplyTeachers::Upload).to receive(:create!).and_raise(ActiveRecord::RecordInvalid)
+        allow(Upload).to receive(:create!)
       end
 
-      it 'raises ActiveRecord::RecordInvalid' do
-        expect do
-          post :create, body: suppliers.to_json
-        end.to raise_error(ActiveRecord::RecordInvalid)
-      end
-    end
+      it 'creates suppliers and their associated data from JSON' do
+        post :create, body: suppliers.to_json
 
-    context 'when JSON is invalid' do
-      it 'raises JSON::ParserError' do
-        expect do
-          post :create, body: '{'
-        end.to raise_error(JSON::ParserError)
+        expect(Upload).to have_received(:create!).with(suppliers)
+      end
+
+      it 'responds with HTTP created status' do
+        post :create, body: suppliers.to_json
+
+        expect(response).to have_http_status(:created)
+      end
+
+      context 'when model validation error occurs' do
+        before do
+          allow(Upload).to receive(:create!).and_raise(ActiveRecord::RecordInvalid)
+        end
+
+        it 'raises ActiveRecord::RecordInvalid' do
+          expect do
+            post :create, body: suppliers.to_json
+          end.to raise_error(ActiveRecord::RecordInvalid)
+        end
+      end
+
+      context 'when JSON is invalid' do
+        it 'raises JSON::ParserError' do
+          expect do
+            post :create, body: '{'
+          end.to raise_error(JSON::ParserError)
+        end
       end
     end
   end
