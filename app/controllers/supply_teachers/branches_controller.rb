b6d7acb50a952507e1dc32995c8def01754b9329
James Mead <3169+floehopper@users.noreply.github.com>
2018-11-12 08:50:39 +0000

    Use explicit module declaration to DRY up ST controllers
    I've added explicit require statements for SupplyTeachers::Journey,
    because Rails' auto-loading picks up the top-level Journey class
    otherwise. My attention was drawn to this issue by some order-dependent
    spec failures.
    
    This diff is best viewed with the `--ignore-all-space` option, because
    of all the indentation changes.

--- app/controllers/supply_teachers/branches_controller.rb
+++ app/controllers/supply_teachers/branches_controller.rb
@@ -1,28 +1,32 @@
-class SupplyTeachers::BranchesController < ApplicationController
-  def index
-    @journey = SupplyTeachers::Journey.new(params[:slug], params)
-    @back_path = @journey.previous_step_path
+require 'supply_teachers/journey'
 
-    if @journey.valid?
-      render_branches
-    else
-      @form_path = @journey.form_path
-      render "journey/#{@journey.template}"
+module SupplyTeachers
+  class BranchesController < ApplicationController
+    def index
+      @journey = Journey.new(params[:slug], params)
+      @back_path = @journey.previous_step_path
+
+      if @journey.valid?
+        render_branches
+      else
+        @form_path = @journey.form_path
+        render "journey/#{@journey.template}"
+      end
     end
-  end
 
-  private
+    private
 
-  def render_branches
-    step = @journey.current_step
-    @location = step.location
-    @branches = step.branches
+    def render_branches
+      step = @journey.current_step
+      @location = step.location
+      @branches = step.branches
 
-    respond_to do |format|
-      format.html
-      format.xlsx do
-        spreadsheet = Spreadsheet.new(@branches, with_calculations: params[:calculations].present?)
-        render xlsx: spreadsheet.to_xlsx, filename: 'branches'
+      respond_to do |format|
+        format.html
+        format.xlsx do
+          spreadsheet = Spreadsheet.new(@branches, with_calculations: params[:calculations].present?)
+          render xlsx: spreadsheet.to_xlsx, filename: 'branches'
+        end
       end
     end
   end
