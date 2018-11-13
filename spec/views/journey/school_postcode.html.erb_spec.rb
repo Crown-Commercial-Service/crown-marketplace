aba7d20b491d6a65c84e9408216bb2d8fcb31214
Chris Roos <chris@seagul.co.uk>
2018-11-13 17:25:00 +0000

    Dynamically populate hidden form fields for school_postcode

--- spec/views/journey/school_postcode.html.erb_spec.rb
+++ spec/views/journey/school_postcode.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'journey/school_postcode.html.erb' do
   let(:step) { SupplyTeachers::Steps::SchoolPostcode.new }
   let(:errors) { ActiveModel::Errors.new(step) }
-  let(:journey) { instance_double('Journey', errors: errors) }
+  let(:journey) { instance_double('Journey', errors: errors, params: params) }
 
   before do
     view.extend(ApplicationHelper)
@@ -12,22 +12,15 @@ RSpec.describe 'journey/school_postcode.html.erb' do
     assign(:form_path, '/')
   end
 
-  it 'stores answer to looking-for question in hidden field' do
-    params[:looking_for] = 'looking-for'
-    render
-    expect(rendered).to have_css('input[name="looking_for"][value="looking-for"]', visible: false)
-  end
+  context 'when there are previous questions/answers stored in the params' do
+    before do
+      params[:question] = 'answer'
+    end
 
-  it 'stores answer to worker-type question in hidden field' do
-    params[:worker_type] = 'worker-type'
-    render
-    expect(rendered).to have_css('input[name="worker_type"][value="worker-type"]', visible: false)
-  end
-
-  it 'stores answer to payroll-provider question in hidden field' do
-    params[:payroll_provider] = 'payroll-provider'
-    render
-    expect(rendered).to have_css('input[name="payroll_provider"][value="payroll-provider"]', visible: false)
+    it 'stores them in hidden fields' do
+      render
+      expect(rendered).to have_css('input[type="hidden"][name="question"][value="answer"]', visible: false)
+    end
   end
 
   it 'does not display the error summary' do
