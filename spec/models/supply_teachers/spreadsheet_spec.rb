8881087ce24a57d952b7c7e1d585d965e60975ee
Paul Battley <pbattley@gmail.com>
2018-11-14 16:50:40 +0000

    Use known phone number when testing spreadsheet
    Spacing in UK phone numbers is quite complex, and the random phone
    numbers generated by Faker occasionally have the wrong spacing. This was
    being corrected by the telephone number formatting, causing intermittent
    failures.

--- spec/models/supply_teachers/spreadsheet_spec.rb
+++ spec/models/supply_teachers/spreadsheet_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SupplyTeachers::Spreadsheet do
 
   let(:telephone_number) { '0121 496 0123' }
   let(:branch1) { build(:branch_search_result, telephone_number: telephone_number) }
-  let(:branch2) { build(:branch_search_result) }
+  let(:branch2) { build(:branch_search_result, telephone_number: '029 2018 0999') }
 
   describe 'the generated worksheet' do
     let(:worksheet) { workbook.worksheets.first }
