a0efafb7ef72c79683e126814c938e015c024901
Ben Griffiths <bengriffiths@gmail.com>
2018-11-15 13:34:54 +0000

    Adds a step to choose MC services from a lot.
    Displays only those services that are present in the params[:lot]

---
+++ app/models/management_consultancy/steps/choose_services.rb
@@ -0,0 +1,23 @@
+module ManagementConsultancy
+  module Steps
+    class ChooseServices
+      include JourneyStep
+
+      attribute :services, Array
+      validates :services, length: { minimum: 1 }
+
+      def initialize(*)
+        self.services = []
+        super
+      end
+
+      def services_for_lot(lot)
+        ManagementConsultancy::Service.where(lot_number: lot).sort_by(&:code)
+      end
+
+      def next_step_class
+        Suppliers
+      end
+    end
+  end
+end
