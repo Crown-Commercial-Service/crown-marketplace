f2553a1e304a808992340adf44a14668ed78aee6
Paul Battley <pbattley@gmail.com>
2018-11-15 11:48:48 +0000

    Add contact details to MC supplier list view
    This does not include any text when a value is missing, as is the case
    with the other frameworks, pending clarification of whether we're going
    to remove it everywhere.

--- app/controllers/management_consultancy/suppliers_controller.rb
+++ app/controllers/management_consultancy/suppliers_controller.rb
@@ -2,6 +2,8 @@ require 'management_consultancy/journey'
 
 module ManagementConsultancy
   class SuppliersController < ApplicationController
+    helper :telephone_number
+
     def index
       @journey = Journey.new(params[:slug], params)
       @back_path = @journey.previous_step_path
