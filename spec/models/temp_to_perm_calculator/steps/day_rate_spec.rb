bdc2dd5a648968b015327efa671d6d4b7ba3c31c
Chris Roos <chris@seagul.co.uk>
2018-11-19 17:46:57 +0000

    Use explicit namespacing in temp_to_perm_calculator specs
    As per the same changes we made in bf9f7131b4c99b0e10d2c84bff9d6630cc4f5f91.

--- spec/models/temp_to_perm_calculator/steps/day_rate_spec.rb
+++ spec/models/temp_to_perm_calculator/steps/day_rate_spec.rb
@@ -1,35 +1,31 @@
 require 'rails_helper'
 
-module TempToPermCalculator
-  module Steps
-    RSpec.describe DayRate, type: :model do
-      subject(:step) do
-        described_class.new(
-          day_rate: day_rate
-        )
-      end
+RSpec.describe TempToPermCalculator::Steps::DayRate, type: :model do
+  subject(:step) do
+    described_class.new(
+      day_rate: day_rate
+    )
+  end
 
-      let(:day_rate) { 500 }
+  let(:day_rate) { 500 }
 
-      it { is_expected.to be_valid }
+  it { is_expected.to be_valid }
 
-      describe '#next_step_class' do
-        it 'is MarkupRate' do
-          expect(step.next_step_class).to eq(MarkupRate)
-        end
-      end
-
-      context 'with a missing day_rate' do
-        let(:day_rate) { nil }
-
-        it { is_expected.to be_invalid }
-      end
-
-      context 'with a non-numeric day_rate' do
-        let(:day_rate) { 'abc' }
-
-        it { is_expected.to be_invalid }
-      end
+  describe '#next_step_class' do
+    it 'is MarkupRate' do
+      expect(step.next_step_class).to eq(TempToPermCalculator::Steps::MarkupRate)
     end
+  end
+
+  context 'with a missing day_rate' do
+    let(:day_rate) { nil }
+
+    it { is_expected.to be_invalid }
+  end
+
+  context 'with a non-numeric day_rate' do
+    let(:day_rate) { 'abc' }
+
+    it { is_expected.to be_invalid }
   end
 end
--- spec/models/temp_to_perm_calculator/steps/fee_spec.rb
+++ spec/models/temp_to_perm_calculator/steps/fee_spec.rb
@@ -1,13 +1,9 @@
 require 'rails_helper'
 
-module TempToPermCalculator
-  module Steps
-    RSpec.describe Fee, type: :model do
-      subject(:step) { described_class.new }
+RSpec.describe TempToPermCalculator::Steps::Fee, type: :model do
+  subject(:step) { described_class.new }
 
-      it 'is the final step' do
-        expect(step.final?).to be true
-      end
-    end
+  it 'is the final step' do
+    expect(step.final?).to be true
   end
 end
