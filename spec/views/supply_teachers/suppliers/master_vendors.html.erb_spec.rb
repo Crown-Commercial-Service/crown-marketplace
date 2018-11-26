d527123b05e899e45d720b2bce1ca162d88dd93c
Paul Battley <pbattley@gmail.com>
2018-11-26 17:03:50 +0000

    Add spec for master vendors template

---
+++ spec/views/supply_teachers/suppliers/master_vendors.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'supply_teachers/suppliers/master_vendors.html.erb' do
+  let(:suppliers) { [] }
+
+  before do
+    assign(:suppliers, suppliers)
+  end
+
+  it 'displays the number of suppliers' do
+    render
+    expect(rendered).to have_text(/0 suppliers/)
+  end
+end
