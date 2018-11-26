fb8f62c78b8d009dbea626a752d08d51ae5e329b
Chris Roos <chris@seagul.co.uk>
2018-11-26 13:33:09 +0000

    Use "Sign in" instead of "Log in"
    As per https://www.gov.uk/guidance/style-guide/a-to-z-of-gov-uk-style#sign-in-or-log-in.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,25 +77,25 @@ end
 
 def visit_supply_teachers_home
   visit '/'
-  click_on 'Log in with beta credentials'
+  click_on 'Sign in with beta credentials'
   click_on I18n.t('home.index.supply_teachers_link')
 end
 
 def visit_temp_to_perm_calculator_home
   visit '/'
-  click_on 'Log in with beta credentials'
+  click_on 'Sign in with beta credentials'
   click_on I18n.t('home.index.temp_to_perm_calculator_link')
 end
 
 def visit_facilities_management_home
   visit '/'
-  click_on 'Log in with beta credentials'
+  click_on 'Sign in with beta credentials'
   click_on I18n.t('home.index.facilities_management_link')
 end
 
 def visit_management_consultancy_home
   visit '/'
-  click_on 'Log in with beta credentials'
+  click_on 'Sign in with beta credentials'
   click_on I18n.t('home.index.management_consultancy_link')
 end
 
