7e1ba0a9996c741e1591e7668c2a4ed152023141
Paul Battley <pbattley@gmail.com>
2018-11-27 14:04:05 +0000

    Move Cognito module to app directories
    This module doesn't perform initialisation itself.

--- config/initializers/cognito.rb
+++ config/initializers/cognito.rb
@@ -13,21 +13,3 @@ cognito_options = {
 }
 
 Rails.application.config.middleware.use OmniAuth::Strategies::Cognito, client_id, client_secret, cognito_options
-
-module Cognito
-  def self.pool_site
-    ENV.fetch('COGNITO_USER_POOL_SITE')
-  end
-
-  def self.client_id
-    ENV.fetch('COGNITO_CLIENT_ID')
-  end
-
-  def self.logout_path(redirect)
-    if pool_site.present?
-      "#{pool_site}/logout?client_id=#{client_id}&logout_uri=#{redirect}"
-    else
-      redirect
-    end
-  end
-end
