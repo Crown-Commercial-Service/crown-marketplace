0c9d68b76376352cceebff12b9b9675ae12a4ea3
Chris Roos <chris@seagul.co.uk>
2018-11-27 16:04:59 +0000

    Inline Calculator#working_days_from_contract_start
    I'm not sure this method is adding much clarity.

--- app/calculators/temp_to_perm_calculator/calculator.rb
+++ app/calculators/temp_to_perm_calculator/calculator.rb
@@ -53,7 +53,7 @@ module TempToPermCalculator
     end
 
     def ideal_hire_date
-      working_days_from_contract_start(WORKING_DAYS_BEFORE_WHICH_EARLY_HIRE_FEE_CAN_BE_CHARGED)
+      working_days_after(@contract_start_date, WORKING_DAYS_BEFORE_WHICH_EARLY_HIRE_FEE_CAN_BE_CHARGED)
     end
 
     def ideal_notice_date
@@ -67,15 +67,11 @@ module TempToPermCalculator
     private
 
     def earliest_date_late_notice_fee_can_be_charged
-      working_days_from_contract_start(WORKING_DAYS_AFTER_WHICH_LATE_NOTICE_FEE_CAN_BE_CHARGED)
+      working_days_after(@contract_start_date, WORKING_DAYS_AFTER_WHICH_LATE_NOTICE_FEE_CAN_BE_CHARGED)
     end
 
     def latest_date_early_hire_fee_can_be_charged
-      working_days_from_contract_start(WORKING_DAYS_BEFORE_WHICH_EARLY_HIRE_FEE_CAN_BE_CHARGED - 1)
-    end
-
-    def working_days_from_contract_start(number_of_days)
-      working_days_after(@contract_start_date)
+      working_days_after(@contract_start_date, WORKING_DAYS_BEFORE_WHICH_EARLY_HIRE_FEE_CAN_BE_CHARGED - 1)
     end
 
     def working_days_after(date, number_of_days)
