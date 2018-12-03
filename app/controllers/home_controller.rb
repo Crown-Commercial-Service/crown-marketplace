8f0080049e7f994081dab68af918db38bb31f836
Paul Battley <pbattley@gmail.com>
2018-12-03 16:35:15 +0000

    Remove vestigial home controller actions
    These are served by namespaced controllers instead.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,6 @@ class HomeController < ApplicationController
 
   def index; end
 
-  def supply_teachers; end
-
-  def facilities_management; end
-
-  def management_consultancy; end
-
   def gateway
     redirect_to homepage_path if logged_in?
   end
