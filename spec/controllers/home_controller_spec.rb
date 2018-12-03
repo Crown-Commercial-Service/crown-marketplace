897dd75a74b4b72f1ba3a95e54c184e072a7252e
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-03 17:48:55 +0000

    Make HomeController spec more idiomatic
    It's more idiomatic to have context related differences in `let` or
    `before` blocks, than in the examples themselves.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller, auth: true do
+  let(:logged_in) { true }
+
+  before do
+    allow(controller).to receive(:logged_in?).and_return(logged_in)
+  end
+
   describe 'GET gateway' do
     context 'when not signed in' do
+      let(:logged_in) { false }
+
       it 'renders the gateway page' do
-        allow(controller).to receive(:logged_in?).and_return(false)
         get :gateway
         expect(response).to render_template(:gateway)
       end
     end
 
     context 'when signed in' do
+      let(:logged_in) { true }
+
       it 'redirects to the start page' do
-        allow(controller).to receive(:logged_in?).and_return(true)
         get :gateway
         expect(response).to redirect_to(homepage_path)
       end
