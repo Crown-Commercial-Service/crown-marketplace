348ded51323917db47bd3266c54bb831bd4f3a66
Paul Battley <pbattley@gmail.com>
2018-12-05 17:26:41 +0000

    Test home controller in logged-out state

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,29 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe HomeController, type: :controller, auth: true do
-  let(:logged_in) { true }
-
+RSpec.describe HomeController, type: :controller do
   before do
-    allow(controller).to receive(:logged_in?).and_return(logged_in)
+    ensure_not_logged_in
   end
 
   describe 'GET status' do
-    context 'when not signed in' do
-      let(:logged_in) { false }
-
-      it 'displays status information about the app' do
-        get :status
-        expect(response).to render_template(:status, layout: false)
-      end
-    end
-
-    context 'when signed in' do
-      let(:logged_in) { true }
-
-      it 'displays status information about the app' do
-        get :status
-        expect(response).to render_template(:status, layout: false)
-      end
+    it 'displays status information about the app' do
+      get :status
+      expect(response).to render_template(:status, layout: false)
     end
   end
 end
