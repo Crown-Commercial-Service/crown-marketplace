21d69bb1f435e8443346dcd5c32bf790eb60acaa
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-06 15:01:29 +0000

    Move date-related methods from ST HomeController -> ContractStart step
    I need access to the parsed contract_start_date and hire_date in order
    to do some higher level validation. Moving these methods into the step
    will achieve this without adding any duplication.
    
    I've chosen to obtain all the inputs to the Calculator constructor from
    the ContractStart step attributes for consistency.
    
    I had to rescue ArgumentError exceptions in the step model versions of
    the methods, because at this point we can't guarantee anything about the
    values of the day, month & year attributes for each date.
    
    I had to add the `looking_for` parameter to the controller spec to make
    it more realistic, so that it was properly constructing the journey i.e.
    it has the relevant value for `previous_step`.

--- app/controllers/supply_teachers/home_controller.rb
+++ app/controllers/supply_teachers/home_controller.rb
@@ -6,32 +6,15 @@ module SupplyTeachers
 
     def fee
       journey = Journey.new(params[:slug], params)
+      previous_step = journey.previous_step
       @back_path = journey.previous_step_path
 
       @calculator = TempToPermCalculator::Calculator.new(
-        day_rate: params[:day_rate].to_i,
-        days_per_week: params[:days_per_week].to_i,
-        contract_start_date: contract_start_date,
-        hire_date: hire_date,
-        markup_rate: params[:markup_rate].to_f / 100.0
-      )
-    end
-
-    private
-
-    def contract_start_date
-      Date.new(
-        params[:contract_start_year].to_i,
-        params[:contract_start_month].to_i,
-        params[:contract_start_day].to_i
-      )
-    end
-
-    def hire_date
-      Date.new(
-        params[:hire_date_year].to_i,
-        params[:hire_date_month].to_i,
-        params[:hire_date_day].to_i
+        day_rate: previous_step.day_rate.to_i,
+        days_per_week: previous_step.days_per_week.to_i,
+        contract_start_date: previous_step.contract_start_date,
+        hire_date: previous_step.hire_date,
+        markup_rate: previous_step.markup_rate.to_f / 100.0
       )
     end
   end
