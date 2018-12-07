14ca554863cf4955d07705f23e65be3b01a51898
Chris Roos <chris@seagul.co.uk>
2018-12-07 16:15:01 +0000

    Simplify feature for temp-to-perm calculator
    By using a daily supplier fee of £10 and 5 days of early hire fee. These
    are also the figures we're using in the calculator_spec.

--- spec/features/supply_teachers/temp_to_perm_calculator.feature_spec.rb
+++ spec/features/supply_teachers/temp_to_perm_calculator.feature_spec.rb
@@ -7,8 +7,8 @@ RSpec.feature 'Temp to Perm fee calculator', type: :feature do
     choose I18n.t('supply_teachers.journey.looking_for.answer_calculate_temp_to_perm_fee')
     click_on I18n.t('common.submit')
 
-    fill_in 'contract_start_date_day', with: 12
-    fill_in 'contract_start_date_month', with: 11
+    fill_in 'contract_start_date_day', with: 3
+    fill_in 'contract_start_date_month', with: 9
     fill_in 'contract_start_date_year', with: 2018
 
     fill_in 'hire_date_day', with: 19
@@ -17,13 +17,13 @@ RSpec.feature 'Temp to Perm fee calculator', type: :feature do
 
     fill_in 'days_per_week', with: 5
 
-    fill_in 'day_rate', with: 600
+    fill_in 'day_rate', with: 110
 
-    fill_in 'markup_rate', with: 30
+    fill_in 'markup_rate', with: 10
 
     click_on I18n.t('common.submit')
 
-    expect(page).to have_text('Based on the information provided you could be charged £7,615.38')
+    expect(page).to have_text('Based on the information provided you could be charged £50')
   end
 
   scenario 'Hiring a worker after 12 weeks of the start of their contract but without enough notice period' do
