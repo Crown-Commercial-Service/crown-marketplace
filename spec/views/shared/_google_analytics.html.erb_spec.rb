bba62de18edbcb697720ec54a99afc90d9b9402c
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-10 14:57:24 +0000

    Improve example names for google_analytics partial view spec

--- spec/views/shared/_google_analytics.html.erb_spec.rb
+++ spec/views/shared/_google_analytics.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'shared/_google_analytics.html.erb' do
-  context 'when GA_TRACKING_ID is missing' do
+  context 'when Google Analytics tracking ID is missing' do
     before do
       allow(Marketplace).to receive(:ga_tracking_id).and_return(nil)
       render partial: 'shared/google_analytics'
@@ -12,7 +12,7 @@ RSpec.describe 'shared/_google_analytics.html.erb' do
     end
   end
 
-  context 'when GA_TRACKING_ID is set' do
+  context 'when Google Analytics tracking ID is set' do
     before do
       allow(Marketplace).to receive(:ga_tracking_id).and_return('UA-999-9')
       render partial: 'shared/google_analytics'
