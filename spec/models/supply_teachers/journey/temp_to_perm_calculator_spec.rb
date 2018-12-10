566d837668c423f71a60044555ee5c8f47dca566
Chris Roos <chris@seagul.co.uk>
2018-12-10 12:07:01 +0000

    Validate presence of hire date
    As per the guidance in the GOV.UK Design System[1]: "Check the entire
    date, not the day, month and year separately."
    
    The `hire_date` is a `Date` constructed from the day, month and year
    parts. If it's present then we know it was a valid date. If it's absent
    then it's either missing or an invalid date.
    
    [1]: https://design-system.service.gov.uk/components/date-input/#error-messages

--- spec/models/supply_teachers/journey/temp_to_perm_calculator_spec.rb
+++ spec/models/supply_teachers/journey/temp_to_perm_calculator_spec.rb
@@ -317,52 +317,19 @@ RSpec.describe SupplyTeachers::Journey::TempToPermCalculator, type: :model do
     end
   end
 
-  context 'with a missing hire_date_year' do
+  context 'when hire_date is missing' do
+    let(:hire_date_day) { nil }
+    let(:hire_date_month) { nil }
     let(:hire_date_year) { nil }
 
     it { is_expected.to be_invalid }
-  end
 
-  context 'with a missing hire_date_month' do
-    let(:hire_date_month) { nil }
-
-    it { is_expected.to be_invalid }
-  end
-
-  context 'with a missing hire_date_day' do
-    let(:hire_date_day) { nil }
-
-    it { is_expected.to be_invalid }
-  end
-
-  context 'with a non-numeric hire_date_year' do
-    let(:hire_date_year) { 'abc' }
-
-    it { is_expected.to be_invalid }
-  end
-
-  context 'with a non-numeric hire_date_month' do
-    let(:hire_date_month) { 'abc' }
-
-    it { is_expected.to be_invalid }
-  end
-
-  context 'with a non-numeric hire_date_day' do
-    let(:hire_date_day) { 'abc' }
-
-    it { is_expected.to be_invalid }
-  end
-
-  context 'with a nonsense hire_date_day' do
-    let(:hire_date_day) { 123 }
-
-    it { is_expected.to be_invalid }
-  end
-
-  context 'with a nonsense hire_date_month' do
-    let(:hire_date_month) { 13 }
-
-    it { is_expected.to be_invalid }
+    it 'obtains the error message from an I18n translation' do
+      step.valid?
+      expect(step.errors[:hire_date]).to include(
+        I18n.t("#{model_key}.attributes.hire_date.blank")
+      )
+    end
   end
 
   context 'with a missing days_per_week' do
