a93de34ad4f9d6d7c330cc89bb56c8593ecc6319
Chris Roos <chris@seagul.co.uk>
2018-12-10 14:03:59 +0000

    Validate presence/validity of holiday_1_end_date
    As per the guidance in the GOV.UK Design System[1]: "Check the entire
    date, not the day, month and year separately."
    
    Note that if any/all of the day, month or year have been entered but
    `holiday_1_end_date` is empty then we can assume it's invalid because it
    implies that parsing the date has failed.
    
    [1]: https://design-system.service.gov.uk/components/date-input/#error-messages

--- spec/models/supply_teachers/journey/temp_to_perm_calculator_spec.rb
+++ spec/models/supply_teachers/journey/temp_to_perm_calculator_spec.rb
@@ -490,4 +490,35 @@ RSpec.describe SupplyTeachers::Journey::TempToPermCalculator, type: :model do
       )
     end
   end
+
+  context 'when holiday_1_end_date is absent' do
+    let(:holiday_1_end_date_day) { nil }
+    let(:holiday_1_end_date_month) { nil }
+    let(:holiday_1_end_date_year) { nil }
+
+    it { is_expected.to be_valid }
+  end
+
+  context 'when holiday_1_end_date is present and a real date' do
+    let(:holiday_1_end_date_day) { 1 }
+    let(:holiday_1_end_date_month) { 12 }
+    let(:holiday_1_end_date_year) { 2018 }
+
+    it { is_expected.to be_valid }
+  end
+
+  context 'when holiday_1_end_date is present but invalid' do
+    let(:holiday_1_end_date_day) { 40 }
+    let(:holiday_1_end_date_month) { 12 }
+    let(:holiday_1_end_date_year) { 2018 }
+
+    it { is_expected.to be_invalid }
+
+    it 'obtains the error message from an I18n translation' do
+      step.valid?
+      expect(step.errors[:holiday_1_end_date]).to include(
+        I18n.t("#{model_key}.attributes.holiday_1_end_date.invalid")
+      )
+    end
+  end
 end
