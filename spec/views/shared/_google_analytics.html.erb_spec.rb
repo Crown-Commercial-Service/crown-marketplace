a032a6420a3679281790295a1dda5dbbc0ec8ed9
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-10 14:59:14 +0000

    Make google_analytics partial view spec more idiomatic

--- spec/views/shared/_google_analytics.html.erb_spec.rb
+++ spec/views/shared/_google_analytics.html.erb_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'shared/_google_analytics.html.erb' do
+  before do
+    allow(Marketplace).to receive(:ga_tracking_id).and_return(ga_tracking_id)
+  end
+
   context 'when Google Analytics tracking ID is missing' do
-    before do
-      allow(Marketplace).to receive(:ga_tracking_id).and_return(nil)
-      render partial: 'shared/google_analytics'
-    end
+    let(:ga_tracking_id) { nil }
 
     it 'does not render anything' do
+      render partial: 'shared/google_analytics'
+
       expect(rendered).to be_blank
     end
   end
 
   context 'when Google Analytics tracking ID is set' do
-    before do
-      allow(Marketplace).to receive(:ga_tracking_id).and_return('UA-999-9')
-      render partial: 'shared/google_analytics'
-    end
+    let(:ga_tracking_id) { 'UA-999-9' }
 
     it 'displays the error summary' do
+      render partial: 'shared/google_analytics'
+
       expect(rendered).to match(/script/)
       expect(rendered).to match(/UA-999-9/)
     end
