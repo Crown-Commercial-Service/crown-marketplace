7b6f5a982e80bed182181d6e33275756c6db61f9
Chris Roos <chris@seagul.co.uk>
2018-12-10 12:07:01 +0000

    Validate presence of hire date
    As per the guidance in the GOV.UK Design System[1]: "Check the entire
    date, not the day, month and year separately."
    
    The `hire_date` is a `Date` constructed from the day, month and year
    parts. If it's present then we know it was a valid date. If it's absent
    then it's either missing or an invalid date.
    
    [1]: https://design-system.service.gov.uk/components/date-input/#error-messages

--- app/models/supply_teachers/journey/temp_to_perm_calculator.rb
+++ app/models/supply_teachers/journey/temp_to_perm_calculator.rb
@@ -21,18 +21,9 @@ module SupplyTeachers
               numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 
     attribute :hire_date_day
-    validates :hire_date_day,
-              presence: true,
-              numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
     attribute :hire_date_month
-    validates :hire_date_month,
-              presence: true,
-              numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12 }
-
     attribute :hire_date_year
-    validates :hire_date_year,
-              presence: true,
-              numericality: { only_integer: true }
+    validates :hire_date, presence: true
 
     attribute :holiday_1_start_date_day
     validates :holiday_1_start_date_day,
--- spec/views/supply_teachers/journey/temp_to_perm_calculator.html.erb_spec.rb
+++ spec/views/supply_teachers/journey/temp_to_perm_calculator.html.erb_spec.rb
@@ -85,14 +85,14 @@ RSpec.describe 'supply_teachers/journey/temp_to_perm_calculator.html.erb' do
     end
   end
 
-  context 'when the journey has an error in hire_date_day' do
+  context 'when the journey has an error in hire_date' do
     before do
-      errors.add(:hire_date_day, 'error-message')
+      errors.add(:hire_date, 'error-message')
     end
 
     it 'links the fieldset to the error message' do
       render
-      expect(rendered).to have_css('fieldset[aria-describedby="hire_date_day-error"]')
+      expect(rendered).to have_css('fieldset[aria-describedby="hire_date-error"]')
     end
 
     it 'displays the error summary' do
@@ -107,86 +107,27 @@ RSpec.describe 'supply_teachers/journey/temp_to_perm_calculator.html.erb' do
 
     it 'adds the message to the field with the error' do
       render
-      expect(rendered).to have_css('#hire_date_day-error.govuk-error-message', text: 'error-message')
+      expect(rendered).to have_css('#hire_date-error.govuk-error-message', text: 'error-message')
     end
 
-    it 'adds error class to the field in order to highlight it' do
+    it 'adds error class to the day field in order to highlight it' do
       render
       expect(rendered).to have_css('#hire_date_day.govuk-input--error')
+    end
+
+    it 'adds error class to the month field to highlight it' do
+      render
+      expect(rendered).to have_css('#hire_date_month.govuk-input--error')
+    end
+
+    it 'adds error class to the year field to highlight it' do
+      render
+      expect(rendered).to have_css('#hire_date_year.govuk-input--error')
     end
 
     it 'adds an error prefix to the page title' do
       render
       expect(view.content_for(:page_title_prefix)).to match(t('layouts.application.error_prefix'))
-    end
-
-    context 'and the journey has an error in hire_date_month' do
-      before do
-        errors.add(:hire_date_month, 'error-message')
-      end
-
-      it 'still links the fieldset to the hire_date_day error message' do
-        render
-        expect(rendered).to have_css('fieldset[aria-describedby~="hire_date_day-error"]')
-      end
-
-      it 'also links the fieldset to the hire_date_month error message' do
-        render
-        expect(rendered).to have_css('fieldset[aria-describedby~="hire_date_month-error"]')
-      end
-
-      it 'adds the message to the hire_date_month field' do
-        render
-        expect(rendered).to have_css('#hire_date_month-error.govuk-error-message', text: 'error-message')
-      end
-
-      it 'still adds error class to the hire_date_day field to highlight it' do
-        render
-        expect(rendered).to have_css('#hire_date_day.govuk-input--error')
-      end
-
-      it 'also adds error class to the hire_date_month field to highlight it' do
-        render
-        expect(rendered).to have_css('#hire_date_month.govuk-input--error')
-      end
-    end
-  end
-
-  context 'when the journey has an error in hire_date_month' do
-    before do
-      errors.add(:hire_date_month, 'error-message')
-    end
-
-    it 'adds error class to the hire_date_month field to highlight it' do
-      render
-      expect(rendered).to have_css('#hire_date_month.govuk-input--error')
-    end
-  end
-
-  context 'when the journey has an error in hire_date_year' do
-    before do
-      errors.add(:hire_date_year, 'error-message')
-    end
-
-    it 'adds error class to the hire_date_year field to highlight it' do
-      render
-      expect(rendered).to have_css('#hire_date_year.govuk-input--error')
-    end
-  end
-
-  context 'when the journey has an error in hire_date' do
-    before do
-      errors.add(:hire_date, 'error-message')
-    end
-
-    it 'links the fieldset to the error message' do
-      render
-      expect(rendered).to have_css('fieldset[aria-describedby="hire_date-error"]')
-    end
-
-    it 'adds the message to the field with the error' do
-      render
-      expect(rendered).to have_css('#hire_date-error.govuk-error-message', text: 'error-message')
     end
   end
 
