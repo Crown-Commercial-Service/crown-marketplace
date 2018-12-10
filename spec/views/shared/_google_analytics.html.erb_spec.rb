eb61c7acc176859f52f2291eeba376f66a54e9dc
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-10 15:02:43 +0000

    Rename ga_tracking_id -> google_analytics_tracking_id
    To make its purpose clearer.

--- spec/views/shared/_google_analytics.html.erb_spec.rb
+++ spec/views/shared/_google_analytics.html.erb_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'shared/_google_analytics.html.erb' do
   before do
-    allow(Marketplace).to receive(:ga_tracking_id).and_return(ga_tracking_id)
+    allow(Marketplace).to receive(:google_analytics_tracking_id)
+      .and_return(google_analytics_tracking_id)
   end
 
   context 'when Google Analytics tracking ID is missing' do
-    let(:ga_tracking_id) { nil }
+    let(:google_analytics_tracking_id) { nil }
 
     it 'does not render anything' do
       render partial: 'shared/google_analytics'
@@ -16,7 +17,7 @@ RSpec.describe 'shared/_google_analytics.html.erb' do
   end
 
   context 'when Google Analytics tracking ID is set' do
-    let(:ga_tracking_id) { 'UA-999-9' }
+    let(:google_analytics_tracking_id) { 'UA-999-9' }
 
     it 'renders the Google Analytics tracking code' do
       render partial: 'shared/google_analytics'
