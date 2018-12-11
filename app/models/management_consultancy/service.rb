f40a6d12cd950f8edc63385821b5a4669f464084
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-11 14:26:47 +0000

    Add inclusion validation for MC ServiceOffering#service_code

--- app/models/management_consultancy/service.rb
+++ app/models/management_consultancy/service.rb
@@ -3,6 +3,10 @@ module ManagementConsultancy
     include StaticRecord
 
     attr_accessor :code, :name, :lot_number
+
+    def self.all_codes
+      all.map(&:code)
+    end
   end
 
   Service.load_csv('management_consultancy/services.csv')
