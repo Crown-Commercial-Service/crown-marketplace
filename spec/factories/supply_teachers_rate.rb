b8f9bf4c3c45a89d8ebfe01c6c5de1c1c8d980ac
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-12 16:55:55 +0000

    Namespace ST rate factory traits

--- spec/factories/supply_teachers_rate.rb
+++ spec/factories/supply_teachers_rate.rb
@@ -1,28 +1,28 @@
 FactoryBot.define do
-  trait :direct_provision do
+  trait :supply_teachers_direct_provision do
     lot_number { 1 }
   end
 
-  trait :master_vendor do
+  trait :supply_teachers_master_vendor do
     lot_number { 2 }
   end
 
-  trait :neutral_vendor do
+  trait :supply_teachers_neutral_vendor do
     lot_number { 3 }
   end
 
   factory :supply_teachers_rate, aliases: [:direct_provision_rate], class: SupplyTeachers::Rate do
     association :supplier, factory: :supply_teachers_supplier
-    direct_provision
+    supply_teachers_direct_provision
     job_type { 'nominated' }
     mark_up { 0.5 }
   end
 
   factory :supply_teachers_master_vendor_rate, parent: :supply_teachers_rate do
-    master_vendor
+    supply_teachers_master_vendor
   end
 
   factory :supply_teachers_neutral_vendor_rate, parent: :supply_teachers_rate do
-    neutral_vendor
+    supply_teachers_neutral_vendor
   end
 end
