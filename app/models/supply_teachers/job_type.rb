3993beb8663d2480b3a526897cb307b527b84ba3
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-13 15:48:34 +0000

    Use SupplyTeachers::JobType vs SupplyTeachers::Rate::JOB_TYPES
    This removes some duplication, although it brings the overloading of
    the meaning of SupplyTeachers::Rate#job_type into starker contrast.
    
    I've had to add the non-role-related job types to the CSV data for
    JobType and then add a new field to distinguish them from the
    role-related job types. This isn't ideal, but I think it makes the code
    more explicit and consistent across the piece and so is a small step
    forward.

--- app/models/supply_teachers/job_type.rb
+++ app/models/supply_teachers/job_type.rb
@@ -2,7 +2,23 @@ module SupplyTeachers
   class JobType
     include StaticRecord
 
-    attr_accessor :code, :description
+    attr_accessor :code, :description, :role
+
+    def self.[](code)
+      find_by(code: code).description
+    end
+
+    def self.find_role_by(code:)
+      find_by(code: code, role: 'true')
+    end
+
+    def self.roles
+      where(role: 'true')
+    end
+
+    def self.all_codes
+      all.map(&:code)
+    end
   end
 
   JobType.load_csv('supply_teachers/job_types.csv')
--- spec/models/supply_teachers/job_type_spec.rb
+++ spec/models/supply_teachers/job_type_spec.rb
@@ -4,13 +4,48 @@ RSpec.describe SupplyTeachers::JobType, type: :model do
   subject(:job_types) { described_class.all }
 
   let(:first_job_type) { job_types.first }
+  let(:non_role_job_type) { described_class.find_by(code: 'fixed_term') }
 
   it 'loads job_types from CSV' do
-    expect(job_types.count).to eq(8)
+    expect(job_types.count).to eq(11)
   end
 
   it 'populates attributes of first job_type' do
     expect(first_job_type.code).to eq('qt')
     expect(first_job_type.description).to eq('Qualified teacher: non-SEN roles')
+  end
+
+  describe '.[]' do
+    it 'looks up job type description by code' do
+      expect(described_class[first_job_type.code]).to eq(first_job_type.description)
+    end
+  end
+
+  describe '.find_role_by' do
+    it 'looks up role job type by code' do
+      expect(described_class.find_role_by(code: first_job_type.code)).to eq(first_job_type)
+    end
+
+    it 'does not find non-role job type' do
+      expect(described_class.find_role_by(code: non_role_job_type.code)).to be_nil
+    end
+  end
+
+  describe '.roles' do
+    it 'returns all role job types' do
+      expect(described_class.roles.count).to eq(8)
+      expect(described_class.roles).to include(first_job_type)
+    end
+
+    it 'does not include non-role job type' do
+      expect(described_class.roles).not_to include(non_role_job_type)
+    end
+  end
+
+  describe '.all_codes' do
+    it 'returns codes for all services' do
+      expect(described_class.all_codes.count).to eq(job_types.count)
+      expect(described_class.all_codes.first).to eq(first_job_type.code)
+    end
   end
 end
