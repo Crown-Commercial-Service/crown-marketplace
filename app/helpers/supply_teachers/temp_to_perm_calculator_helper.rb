bdfce3da8d6a51a4a086c7e7c0dbbfd339bd614c
Chris Roos <chris@seagul.co.uk>
2018-12-13 15:20:00 +0000

    Extract display_notice_period_given helper method

--- app/helpers/supply_teachers/temp_to_perm_calculator_helper.rb
+++ app/helpers/supply_teachers/temp_to_perm_calculator_helper.rb
@@ -3,4 +3,11 @@ module SupplyTeachers::TempToPermCalculatorHelper
     t('supply_teachers.home.temp_to_perm_fee.notice_period_required',
       days: calculator.days_notice_required)
   end
+
+  def display_notice_period_given(calculator)
+    t('supply_teachers.home.temp_to_perm_fee.notice_period_given',
+      days: calculator.days_notice_given,
+      notice_date: calculator.notice_date.to_s(:long_with_day),
+      hire_date: calculator.hire_date.to_s(:long_with_day))
+  end
 end
