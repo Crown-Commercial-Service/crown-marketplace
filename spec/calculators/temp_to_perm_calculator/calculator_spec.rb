20119874044c7e6f7c047b64f8999947a5ee6848
Chris Roos <chris@seagul.co.uk>
2018-12-13 14:28:17 +0000

    Extract temp-to-perm dates to a shared context
    To remove some duplication.

--- spec/calculators/temp_to_perm_calculator/calculator_spec.rb
+++ spec/calculators/temp_to_perm_calculator/calculator_spec.rb
@@ -16,21 +16,7 @@ RSpec.describe TempToPermCalculator::Calculator do
     )
   end
 
-  let(:start_of_1st_week)  { Date.parse('Mon 3 Sep 2018') }
-  let(:start_of_2nd_week)  { Date.parse('Mon 10 Sep 2018') }
-  let(:start_of_3rd_week)  { Date.parse('Mon 17 Sep 2018') }
-  let(:start_of_4th_week)  { Date.parse('Mon 24 Sep 2018') }
-  let(:start_of_5th_week)  { Date.parse('Mon 1 Oct 2018') }
-  let(:start_of_6th_week)  { Date.parse('Mon 8 Oct 2018') }
-  let(:start_of_7th_week)  { Date.parse('Mon 15 Oct 2018') }
-  let(:start_of_8th_week)  { Date.parse('Mon 22 Oct 2018') }
-  let(:start_of_9th_week)  { Date.parse('Mon 29 Oct 2018') }
-  let(:start_of_10th_week) { Date.parse('Mon 5 Nov 2018') }
-  let(:start_of_11th_week) { Date.parse('Mon 12 Nov 2018') }
-  let(:start_of_12th_week) { Date.parse('Mon 19 Nov 2018') }
-  let(:start_of_13th_week) { Date.parse('Mon 26 Nov 2018') }
-  let(:start_of_14th_week) { Date.parse('Mon 3 Dec 2018') }
-  let(:start_of_15th_week) { Date.parse('Mon 10 Dec 2018') }
+  include_context 'with friendly dates'
 
   let(:contract_start_date) { start_of_1st_week }
   let(:hire_date) { start_of_1st_week }
