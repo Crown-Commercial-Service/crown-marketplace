e57a1dda2ed17906f8d76f2172ec04c503b5416f
Chris Roos <chris@seagul.co.uk>
2018-12-13 10:47:51 +0000

    Add feature for happy path of temp-to-perm-calculator

--- spec/features/supply_teachers/temp_to_perm_calculator.feature_spec.rb
+++ spec/features/supply_teachers/temp_to_perm_calculator.feature_spec.rb
@@ -1,6 +1,35 @@
 require 'rails_helper'
 
 RSpec.feature 'Temp to Perm fee calculator', type: :feature do
+  scenario 'Making a worker permanent after 12 weeks and giving at least 4 weeks notice' do
+    visit_supply_teachers_start
+
+    choose I18n.t('supply_teachers.journey.looking_for.answer_calculate_temp_to_perm_fee')
+    click_on I18n.t('common.submit')
+
+    fill_in 'contract_start_date_day', with: 3
+    fill_in 'contract_start_date_month', with: 9
+    fill_in 'contract_start_date_year', with: 2018
+
+    fill_in 'hire_date_day', with: 26
+    fill_in 'hire_date_month', with: 11
+    fill_in 'hire_date_year', with: 2018
+
+    fill_in 'days_per_week', with: 5
+
+    fill_in 'day_rate', with: 110
+
+    fill_in 'markup_rate', with: 10
+
+    fill_in 'notice_date_day', with: 29
+    fill_in 'notice_date_month', with: 10
+    fill_in 'notice_date_year', with: 2018
+
+    click_on I18n.t('common.submit')
+
+    expect(page).to have_text('Based on the information provided you could be charged £0')
+  end
+
   scenario 'Hiring a worker within 12 weeks of the start of their contract' do
     visit_supply_teachers_start
 
