62086390cce71ea9fd2856caf944e00c5e7279ee
Chris Roos <chris@seagul.co.uk>
2018-12-13 15:24:58 +0000

    Extract display_suppliers_daily_fee helper method

--- app/helpers/supply_teachers/temp_to_perm_calculator_helper.rb
+++ app/helpers/supply_teachers/temp_to_perm_calculator_helper.rb
@@ -15,4 +15,11 @@ module SupplyTeachers::TempToPermCalculatorHelper
     t('supply_teachers.home.temp_to_perm_fee.lack_of_notice_chargeable_days',
       days: calculator.chargeable_working_days_based_on_lack_of_notice)
   end
+
+  def display_suppliers_daily_fee(calculator)
+    t('supply_teachers.home.temp_to_perm_fee.daily_supplier_fee',
+      fee: number_to_currency(calculator.daily_supplier_fee),
+      markup_rate: number_to_percentage(calculator.markup_rate * 100, precision: 1),
+      day_rate: number_to_currency(calculator.day_rate))
+  end
 end
