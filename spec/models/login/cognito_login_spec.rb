3a4604722fe1201e9197aa5a36e4c9b1313d3084
Paul Battley <pbattley@gmail.com>
2018-12-17 13:46:30 +0000

    Prevent Cognito sign-in working in ST framework
    Although this was already hidden, there was a back door whereby one
    could sign into another framework via Cognito and jump sideways to
    supply teachers.
    
    This makes Cognito user permission for supply teachers contingent on the
    same environment variable used to remove the option from the interface.

--- spec/models/login/cognito_login_spec.rb
+++ spec/models/login/cognito_login_spec.rb
@@ -21,8 +21,28 @@ RSpec.describe Login::CognitoLogin, type: :model do
   it { is_expected.to have_attributes(email: email) }
 
   context 'when the framework is supply_teachers' do
-    it 'permits access' do
-      expect(login.permit?(:supply_teachers)).to be true
+    context 'when Cognito is enabled for supply teachers' do
+      before do
+        allow(Marketplace)
+          .to receive(:supply_teachers_cognito_enabled?)
+          .and_return(true)
+      end
+
+      it 'permits access' do
+        expect(login.permit?(:supply_teachers)).to be true
+      end
+    end
+
+    context 'when Cognito is not enabled for supply teachers' do
+      before do
+        allow(Marketplace)
+          .to receive(:supply_teachers_cognito_enabled?)
+          .and_return(false)
+      end
+
+      it 'denies access' do
+        expect(login.permit?(:supply_teachers)).to be false
+      end
     end
   end
 
