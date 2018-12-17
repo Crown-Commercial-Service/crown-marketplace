c645c6e85132488adae99cd507e8529c35c8eb0f
Paul Battley <pbattley@gmail.com>
2018-12-17 11:17:50 +0000

    Give dev & test users a Cognito sign-in option
    As no one can get DfE accounts, they need to be able to use Cognito to
    sign in to test the service.
    
    This is implemented as a "secret" (not really secret, because the code
    is open!) route:
    
        /supply-teachers/cognito
    
    This adds a "Sign in with beta credentials" link to the gateway page
    even when SUPPLY_TEACHERS_COGNITO_ENABLED is not set.

--- app/controllers/supply_teachers/gateway_controller.rb
+++ app/controllers/supply_teachers/gateway_controller.rb
@@ -3,6 +3,9 @@ module SupplyTeachers
     require_permission :none, only: :index
 
     def index
+      @cognito_enabled =
+        Marketplace.supply_teachers_cognito_enabled? ||
+        params[:cognito_enabled]
       redirect_to supply_teachers_path if logged_in?
     end
   end
