994be475ac64c6b52490e841981e1c6971644148
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-18 15:17:47 +0000

    Ensure instances of SupplyTeachers::Term have unique codes
    Given that we're using these identifiers to lookup instances, we should
    ensure they are unique.

--- spec/models/supply_teachers/term_spec.rb
+++ spec/models/supply_teachers/term_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe SupplyTeachers::Term, type: :model do
   subject(:terms) { described_class.all }
 
   let(:first_term) { terms.first }
+  let(:all_codes) { terms.map(&:code) }
 
   it 'loads terms from CSV' do
     expect(terms.count).to eq(5)
@@ -12,5 +13,9 @@ RSpec.describe SupplyTeachers::Term, type: :model do
   it 'populates attributes of first term' do
     expect(first_term.code).to eq('0_1')
     expect(first_term.description).to eq('Up to 1 week')
+  end
+
+  it 'only has unique codes' do
+    expect(all_codes.uniq).to contain_exactly(*all_codes)
   end
 end
