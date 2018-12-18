420cae3063cb9a431498b0e65d7b7b8cf337be0c
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-18 16:05:25 +0000

    Consistently use Virtus to implement StaticRecord boolean attributes

--- app/models/facilities_management/service.rb
+++ app/models/facilities_management/service.rb
@@ -1,15 +1,19 @@
 module FacilitiesManagement
   class Service
+    include Virtus.model
     include StaticRecord
 
-    attr_accessor :code, :name, :work_package_code, :mandatory
+    attribute :code, String
+    attribute :name, String
+    attribute :work_package_code, String
+    attribute :mandatory, Axiom::Types::Boolean
 
     def work_package
       WorkPackage.find_by(code: work_package_code)
     end
 
     def mandatory?
-      mandatory == 'true'
+      mandatory
     end
 
     def self.all_codes
