1fa850a1ef82dd960cc74af43cc12a42625b4c9b
Laura Porter <laura@dxw.com>
2019-01-15 15:20:14 +0000

    (fix) Add a fallback sign out route if there is no current session
    Trello card: https://trello.com/c/nTwYhW9n/909-exception-when-signing-out-via-cognito
    
    This PR fixes the "second sign out error" as detailed in the card. The issue was arising when a user logged out of Cognito, got an error (but was successfully signed out), hit the Back button in their browser, and clicked sign out again.
    
    Because they had successfully signed out of Cognito, their session had been cleared. The auth controller was relying on a session to know where to redirect the user. This provides a fallback so that if there is no session pesent, and no current_login, a user hitting Sign Out will still be redirected to a valid page instead of seeing a 500 error.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -7,6 +7,8 @@ class AuthController < ApplicationController
   end
 
   def sign_out
+    return redirect_to supply_teachers_gateway_url if current_login.nil?
+
     redirect_to current_login.logout_url(self)
     delete_current_login
   end
