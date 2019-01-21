febf709b6d582eb0c3f3bbb2ce6507e477c92bfd
Robbie Paul <robbie.paul@dxw.com>
2019-01-21 17:06:50 +0000

    (refactor) Calculate the supplier markup in a separate class

---
+++ spec/models/supply_teachers/supplier_mark_up_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe SupplyTeachers::SupplierMarkUp, type: :model do
+  let(:supplier_mark_up) { described_class.new(daily_rate: daily_rate, markup_rate: markup_rate) }
+  let(:daily_rate) { '220' }
+  let(:markup_rate) { 0.44 }
+
+  describe '#worker_cost' do
+    it 'calculates the worker cost out of the daily rate' do
+      expect(supplier_mark_up.worker_cost).to be_within(0.1).of(152.78)
+    end
+  end
+
+  describe '#supplier_fee' do
+    it 'calculates the supplier’s fee out of the daily rate' do
+      expect(supplier_mark_up.supplier_fee).to be_within(0.1).of(67.22)
+    end
+  end
+end
