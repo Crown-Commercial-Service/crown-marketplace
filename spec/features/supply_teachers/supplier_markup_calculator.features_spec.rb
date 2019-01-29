7aaee4b011b4bd8c175e030f186a3fc98f0d8e9c
Robbie Paul <robbie.paul@dxw.com>
2019-01-29 12:30:03 +0000

    (spec) Supplier calculator AJAX is now tested
    Poltergeist has issues finding the GOV.UK radio buttons by label, it
    thinks they are hidden so we have to define `visible: false`

--- spec/features/supply_teachers/supplier_markup_calculator.features_spec.rb
+++ spec/features/supply_teachers/supplier_markup_calculator.features_spec.rb
@@ -65,4 +65,30 @@ RSpec.feature 'Supplier mark-up calculator', type: :feature do
       expect(page).to have_css('.supplier-record__agency-fee', text: '£34.62')
     end
   end
+
+  scenario 'Buyer can calculate the agency mark-up via AJAX', js: true do
+    visit_supply_teachers_start
+
+    choose I18n.t('supply_teachers.journey.looking_for.answer_worker'), visible: false
+    click_on I18n.t('common.submit')
+
+    choose 'Yes', visible: false
+    click_on I18n.t('common.submit')
+
+    choose 'Yes', visible: false
+    click_on I18n.t('common.submit')
+
+    fill_in 'postcode', with: 'WC2B 6TE'
+    choose '4 weeks to 8 weeks', visible: false
+    choose 'Qualified teacher: SEN roles', visible: false
+    click_on I18n.t('common.submit')
+
+    branches = page.all('.supplier-record')
+
+    within branches.first do
+      fill_in 'Enter daily rate', with: '200'
+      expect(page).to have_css('.supplier-record__worker-cost', text: '£153.85')
+      expect(page).to have_css('.supplier-record__agency-fee', text: '£46.15')
+    end
+  end
 end
