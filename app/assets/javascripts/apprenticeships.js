d7adedee3d3a9bda1544a500a6563dcfeeb514e0
Darren Harridence <darren.harridence@btinternet.com>
2019-02-27 16:46:57 +0000

    search filter HTML upgrades

---
+++ app/assets/javascripts/apprenticeships.js
@@ -0,0 +1,24 @@
+function initSearchResults(){
+    alert('fired');
+}
+
+
+
+
+
+
+
+
+
+
+
+function initCustomFnc() {
+    
+
+
+
+    if($('#results-filters').length){//it exists
+        initSearchResults();
+    }
+}
+
