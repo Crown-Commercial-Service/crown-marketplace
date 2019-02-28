33b6545ca2c12b82b557b5e26f28bbe7d3cd07d7
milomia <m-hunt3@sky.com>
2019-02-28 15:47:45 +0000

    update screens for v2

--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,10 @@ Rails.application.routes.draw do
     get '/search', to: 'home#search'
     get '/search_results', to: 'home#search_results'
     get '/supplier_search', to: 'home#supplier_search'
+    get '/find_apprentices', to: 'home#find_apprentices'
+    get '/find_apprentices2', to: 'home#find_apprentices2'
+    get '/find_apprentices3', to: 'home#find_apprentices3'
+    get '/outline', to: 'home#outline'
     get '/requirements', to: 'home#requirements'
     get '/requirement', to: 'home#requirement'
   end
