42338e3dd487cadfd73a6629ff3f44527a107f69
Darren Harridence <darren.harridence@btinternet.com>
2019-02-28 14:16:10 +0000

    checkboxes accordian

--- app/assets/javascripts/apprenticeships.js
+++ app/assets/javascripts/apprenticeships.js
@@ -1,5 +1,20 @@
-function initSearchResults(){
-    alert('fired');
+function initSearchResults(id){
+    var accord = id.find('.CCS-AT-checkbox-accordian'); 
+
+    accord.each(function(index) {
+        var link = $(this).find('.CCS-AT-show');
+        var wrap = $(this);//.find('.CCS-AT-checkboxes-wrap');
+
+        link.attr('aria-expanded', 'false').click(function(e){
+            e.preventDefault();//if 'a' tag
+
+            $(this).attr('aria-expanded',$(this).attr('aria-expanded')==='true'?'false':'true' )
+            .find('span').text(function(i, text){
+                return text === "Show" ? "Hide" : "Show";
+            });
+            wrap.toggleClass('show');//could use '+' css selector on aria attr val, but this supports legacy browsers
+        });
+    });
 }
 
 
@@ -16,9 +31,9 @@ function initCustomFnc() {
     
 
 
-
-    if($('#results-filters').length){//it exists
-        initSearchResults();
+    var filt = $('#ccs-at-results-filters');
+    if(filt.length){//it exists
+        initSearchResults(filt);
     }
 }
 
