4b4a74ead25aa1cb907dcf84426fc171cb2cc39b
milomia <m-hunt3@sky.com>
2019-03-01 13:41:37 +0000

    update home_helper and controller

--- app/controllers/apprenticeships/home_controller.rb
+++ app/controllers/apprenticeships/home_controller.rb
@@ -9,6 +9,71 @@ module Apprenticeships
 
     def search_results; end
 
+    def training_provider
+      require 'ostruct'
+      @provider = OpenStruct.new(
+        title: 'ABC Training LTD',
+        email: 'emailaddress@websiteurl.ac.uk',
+        phone: '01234 567890',
+        website: 'http://websiteurl.ac.uk',
+        address: 'Maidstone Campus, Tonbridge Road, Maidstone, Kent, ME16 8AQ',
+        about_apprenticeship: 'This professionally accredited course aims to equip you with the knowledge, technical and practical skills and techniques required for a successful career as a building services engineer, able to respond to environmental and ethical considerations, while talking into consideration relevant social and economic implications too.
+                ABC Training LTD has a long and prestigious engineering heritage and is ranked 2nd for \'Mechanical Engineering\', 10th for \'Civil Engineering\' and 30th for \'Electronic and Electrical Engineering\' by the Guardian University Guide 2018. We enjoy a strong portfolio of industry-related research, particularly in the areas of low carbon building technology, sustainable construction materials and engineering education',
+        apprenticeship: 'Building services design engineer',
+        ofsted_apprenticeship: '2 - Good',
+        level: 6,
+        duration: '24 months',
+        funding_band: 'At funding band maximum',
+        ability_to_bespoke: 'Yes',
+        experience_in_this_apprenticeship: 'Yes',
+        day_release: true,
+        block_release: true,
+        at_your_location: false,
+        classroom_based: true,
+        online: true,
+        workplace_based: false,
+        blended: true,
+        classroom_sharing_open: false,
+        classroom_sharing_closed: true,
+        classroom_sharing_public: false,
+        training_availability_open: false,
+        training_availability_closed: true,
+        training_availability_public: false,
+        overall_ofsted: '1 - Outstanding',
+        experience_in_delivering_apprenticeships: "Yes",
+        recruitment_support: "Yes",
+        about_provider: 'University of the Year for Student Experience; latest Times and Sunday Times league table
+                         Ranked No.13 UK University; Guardian University Guide 2018
+                         Gold for outstanding teaching and learning; Teaching Excellence Framework 2017
+                         ABC Training LTD apprenticeships are pleased to offer the opportunity to upskill employees with a range of higher and degree apprenticeships enable people of all ages to earn a wage while studying for a higher-level qualification.
+                         With ABC Training LTD, apprentices can gain professional qualifications as well as academic qualifications.',
+        apprenticeships_standards: ['Construction Building: Woodmachining',
+                                    'Construction Technical and Professional: Building Control',
+                                    'Construction Building: Trowel Occupations',
+                                    'Facilities Management: Building Services',
+                                    'Building Services Engineering Installer',
+                                    'Building Services Engineering Ductwork Craftsperson']
+      )
+    end
+
+    def training_provider_list
+      @list = OpenStruct.new(
+        standard: 'Building services design engineer',
+        level: 6,
+        help_with_recruiting: 'Yes',
+        funding_band: 'Above band maximum',
+        minimum_number: 30,
+        minimum_ofsted: '2 - Good',
+        training_in_multiple_locations: 'No',
+        need_to_customise: 'No',
+        delivery_method: 'Classroom based
+                          Blended',
+        time_demand: 'Day release',
+        classroom_sharing: 'Closed',
+        training_availability: 'Scheduled'
+      )
+    end
+
     private
 
     def set_back_path
--- app/helpers/apprenticeships/home_helper.rb
+++ app/helpers/apprenticeships/home_helper.rb
@@ -1,39 +1,39 @@
 module Apprenticeships::HomeHelper
 
-  # TODO: TBD where these will sit
+  # TODO: TBD where this will sit
   def levels
     ['Level 2 (15)', 'Level 3 (24)', 'Level 4 (10)', 'Level 5 (2)', 'Level 6 (3)', 'Level 7 (1)']
   end
 
+  # TODO: TBD where this will sit
   def routes
     ['Agriculture, environmental and animal care', 'Business administration', 'Catering and hospitality', 'Construction',
      'Creative and design', 'Digital', 'Engineering and manufacturing', 'Hair and beauty', 'Health and science',
      'Legal, finance and accounting', 'Protective services', 'Sales, marketing and procurement', 'Transport']
   end
 
+  # TODO: TBD what options we have and where these will sit
   def sort_option
     ['Best match', 'Level', 'Provider(s)']
   end
 
-  def supplier_sort_option
-    ['Overall ofsted rating (high to low)', 'Distance (closest first)', 'Overall ofset rating(low to high)']
-  end
-
+  # TODO: TBD where this will sit
   def results
     [
-      { name: 'Building Services Design Technician', level: 3, length:36, max_funding: 12000, providers: 12 },
-      { name: 'Building Services Design Engineer', level: 6, length:60, max_funding: 27000, providers: 8 },
-      { name: 'Vehicle Body and Paint: Body Building', level: 2, length:36, max_funding: 9000, providers: 16 },
-      { name: 'Construction Building: Trowel Occupations', level: 3, length: 36, max_funding: 11000, providers: 3 },
-      { name: 'Construction Building: Trowel Occupations', level: 3, length: 48 , max_funding: 182000, providers: 22 },
-      { name: 'Construction Building: Woodmachining', level: 3, length: 24, max_funding: 8000, providers: 44 },
-      { name: 'Construction Building: Woodmachining', level: 3, length: 24, max_funding: 8000, providers: 44 },
-      { name: 'Construction Site Engineering Technician', level: 3, length: 24, max_funding: 8000, providers: 12 },
-      { name: 'Marine Engineer', level: 3, length: 24, max_funding: 8000, providers: 1 }
+      { name: 'Building Services Design Technician', level: 3, providers: 12 },
+      { name: 'Building Services Design Engineer', level: 6, providers: 8 },
+      { name: 'Vehicle Body and Paint: Body Building', level: 3, providers: 16 },
+      { name: 'Construction Building: Trowel Occupations', level: 2, providers: 3 },
+      { name: 'Construction Building: Trowel Occupations', level: 3, providers: 22 },
+      { name: 'Building Services Engineering Technology and Project Management Technician', level: 3, providers: 2 },
+      { name: 'Construction Building: Maintenance Operations', level: 2, providers: 18 },
+      { name: 'Construction Building: Woodmachining', level: 2, providers: 44 },
+      { name: 'Construction Site Engineering Technician', level: 4, providers: 12 },
+      { name: 'Marine Engineer', level: 3, providers: 1 }
     ]
   end
 
-  def ofsted_rating
+ def ofsted_rating
     [ '1 - Outstanding (0)', '2 - Good (3)', '3 - Requires improvements (5)']
   end
 
@@ -43,7 +43,7 @@ module Apprenticeships::HomeHelper
 
   def provider_experience
     [ 'Experience in apprenticeships','Experience in delivering this apprenticeship course']
-  end 
+  end
 
   def delivery_method
     [ 'Day Release(2)','Online(3)','Workplace Based(2)','Evenings (0)', 'Flexible around working pattern(1)']
@@ -60,8 +60,7 @@ module Apprenticeships::HomeHelper
   def training_availability
     ['Scheduled(5)','On demand(2)','Roll-on/Roll-off(3)' ]
   end
-
-  def supplier_results
+ def supplier_results
     [
       { name: 'ABC Training LTD National', rating: 2, distance: 7 },
       { name: 'DWK Group', rating: 2, distance: 22 },
@@ -91,4 +90,15 @@ module Apprenticeships::HomeHelper
      'Transport and logistics',
   ]
   end
-end 
+
+
+  def option_availability(option)
+    option ? '✓' : 'x'
+  end
+
+  def list_from_array(array)
+    array.collect do |item|
+      content_tag(:li, item)
+    end.join.html_safe
+  end
+end
