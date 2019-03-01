c922b0b476edaa38c5092520818654a99c661988
Tariq Hamid <tariq@hamid.com>
2019-03-01 21:11:17 +0000

    puts print statements replaced by Rails.Logger.info
    https://pushback.atlassian.net/browse/FM-38

--- app/services/static_data_loader.rb
+++ app/services/static_data_loader.rb
@@ -22,17 +22,17 @@ class StaticDataLoader
 
   def self.call_rake(static_data_class, class_name, query)
     if File.split($PROGRAM_NAME).last == 'rake'
-      puts 'Guess what, I`m running this from Rake'
+      Rails.logger.info('Guess what, I`m running this from Rake')
     else
       begin
-        puts 'No, this is not a Rake task'
+        Rails.logger.info('No, this is not a Rake task')
         Rails.application.load_tasks
         Rake::Task['db:static'].execute
         # reload the data a second time
         static_data_class.load_db(query)
       rescue StandardError => error
         message = class_name + " data is missing! Please run 'rake db:static' to load static data."
-        puts "\e[5;37;41m\n" + message + "\033[0m\n"
+        Rails.logger.info("\e[5;37;41m\n" + message + "\033[0m\n")
         raise error
       end
     end
