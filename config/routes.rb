07e753e0a3f1ef467f7b980f1476a6ad97572465
Iulia R Ionita <iulia@ionita.me>
2019-03-04 15:53:07 +0000

    Added all_suppliers page in supply_teachers

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     get '/temp-to-perm-fee', to: 'home#temp_to_perm_fee'
     get '/master-vendors', to: 'suppliers#master_vendors', as: 'master_vendors'
     get '/neutral-vendors', to: 'suppliers#neutral_vendors', as: 'neutral_vendors'
+    get '/all-suppliers', to: 'suppliers#all_suppliers', as: 'all_suppliers'
     get '/agency-payroll-results', to: 'branches#index', slug: 'agency-payroll-results'
     get '/fixed-term-results', to: 'branches#index', slug: 'fixed-term-results', as: 'fixed_term_results'
     get '/nominated-worker-results', to: 'branches#index', slug: 'nominated-worker-results'
