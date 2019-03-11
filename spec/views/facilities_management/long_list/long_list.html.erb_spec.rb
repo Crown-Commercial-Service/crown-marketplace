346917d820f6393d925e2a1f4e0f23e58a201c87
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-03-11 13:30:27 +0000

    FM update unit tests

--- spec/views/facilities_management/long_list/long_list.html.erb_spec.rb
+++ spec/views/facilities_management/long_list/long_list.html.erb_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
+require 'json'
+require 'facilities_management/fm_supplier_data'
+RSpec.describe 'Long List', type: :view do
+  it 'Counts suppliers for all lot types with a location of UKC1 and a service of C-20' do
+    fm_supplier_data = FMSupplierData.new
+    supplier_count = fm_supplier_data.long_list_supplier_count("('UKC1')", "('C-20')")
+    assert(supplier_count >= 0)
+  end
 
-RSpec.describe 'long_list/long_list.html.erbb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'counts lot1a suppliers with a location of UKC1 and a service of C-20' do
+    fm_supplier_data = FMSupplierData.new
+    lot1a = fm_supplier_data.long_list_suppliers_lot1a("('UKC1')", "('C-20')")
+    assert(!lot1a.nil?)
+  end
+
+  it 'counts lot1b suppliers with a location of UKC1 and a service of C-20' do
+    fm_supplier_data = FMSupplierData.new
+    lot1b = fm_supplier_data.long_list_suppliers_lot1b("('UKC1')", "('C-20')")
+    assert(!lot1b.nil?)
+  end
+
+  it 'counts lot1c suppliers with a location of UKC1 and a service of C-20' do
+    fm_supplier_data = FMSupplierData.new
+    lot1c = fm_supplier_data.long_list_suppliers_lot1c("('UKC1')", "('C-20')")
+    assert(!lot1c.nil?)
+  end
 end
