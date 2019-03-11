04628bcf75226258dce9ccf41ffe28057dfc59b8
Iulia R Ionita <iulia@ionita.me>
2019-03-11 12:13:44 +0000

    Added option to get to the All Suppliers page on the Looking for page. Also removed all the unneeded data from this page

--- db/migrate/20190304163411_add_address_1_address_2_county_and_region_to_supply_teachers_branches.rb
+++
@@ -1,8 +0,0 @@
-class AddAddress1Address2CountyAndRegionToSupplyTeachersBranches < ActiveRecord::Migration[5.2]
-  def change
-    add_column :supply_teachers_branches, :address_1, :string
-    add_column :supply_teachers_branches, :address_2, :string
-    add_column :supply_teachers_branches, :county, :string
-    add_column :supply_teachers_branches, :region, :string
-  end
-end
