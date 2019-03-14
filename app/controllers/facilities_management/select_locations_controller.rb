f04b7a658536fa1d3ec5c9d6bd89083c1095f136
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-03-14 09:09:01 +0000

    FM25-28 Refactored inline error message to be reusable

--- app/controllers/facilities_management/select_locations_controller.rb
+++ app/controllers/facilities_management/select_locations_controller.rb
@@ -1,6 +1,12 @@
 class FacilitiesManagement::SelectLocationsController < ApplicationController
   require_permission :facilities_management, only: :select_location
+
   def select_location
+    # Inline error text for this page
+    @inline_error_summary_title = 'There was a problem'
+    @inline_error_summary_body_href = '#'
+    @inline_summary_error_text = 'You must select at least one location before clicking the save continue button'
+    # Get nuts regions
     h = {}
     Nuts1Region.all.each { |x| h[x.code] = x.name }
     @regions = h
