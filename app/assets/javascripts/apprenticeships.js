15c51904d239b9f122bd75eb4168c0afcf6d7299
Darren Harridence <darren.harridence@btinternet.com>
2019-03-15 17:36:25 +0000

    AT-70 dynamic JS checkbox labels WIP

--- app/assets/javascripts/apprenticeships.js
+++ app/assets/javascripts/apprenticeships.js
@@ -1,9 +1,41 @@
+function ifChecked(sWrap, h){//count and show checked checkboxes totals 
+    var howmany; 
+
+    if(sWrap === false){//called from Clear All link
+        howmany = 0; 
+    }else{
+        howmany = sWrap.find('.govuk-checkboxes__input:checked').length;
+    }
+
+    if(howmany > 0){
+        var txt = h.data('txt');
+        h.find('.ccs-filter-no').text(howmany).end().find('.ccs-filter-txt').text(txt).end().addClass('ccs-hint--show');
+    }else{
+        h.removeClass('ccs-hint--show').find('.ccs-filter-no .ccs-filter-txt').empty();
+    }
+}
+
+function whenChecked(sWrap, h){//recount checked boxes totals when checkbox states changes
+    var allcheckbxs = sWrap.find('.govuk-checkboxes__input'); 
+
+    allcheckbxs.change(function(){
+        ifChecked(sWrap, h); 
+    });
+}
+
 function initSearchResults(id){
     var accord = id.find('.ccs-at-checkbox-accordian'); 
 
     accord.each(function(index) {
-        var link = $(this).find('.ccs-at-btn-toggle');
         var shopWrap = $(this);
+
+        var hint = shopWrap.find('.ccs-govuk-hint--selected');
+        if(hint.length){
+            ifChecked(shopWrap, hint); //1. ccs-at-checkbox-accordian  2. ccs-govuk-hint--selected
+            whenChecked(shopWrap, hint);
+        }        
+
+        var link = $(this).find('.ccs-at-btn-toggle');
 
         link.attr('aria-expanded', 'false').click(function(e){
             e.preventDefault();//if 'a' tag used
@@ -16,7 +48,6 @@ function initSearchResults(id){
         });
     }); 
 
-
     var checkboxs = accord.find('.govuk-checkboxes__input');
 
     checkboxs.keypress(function(e){//select checkbox with keyboard
@@ -28,6 +59,11 @@ function initSearchResults(id){
     $('#ccs-clear-filters').click(function(e){
         e.preventDefault();
         checkboxs.prop('checked', false); // AND resubmit form to 'self' ????
+
+        var hint = id.find('.ccs-govuk-hint--selected');
+        if(hint.length){
+            ifChecked(false, hint);
+        }
     });
 }
 
@@ -47,6 +83,4 @@ function initCustomFnc() {
     if(filt.length){
         initSearchResults(filt);
     }
-
 }
-
