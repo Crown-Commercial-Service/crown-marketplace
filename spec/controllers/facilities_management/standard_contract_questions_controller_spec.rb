d9368830e7b5cc8cb03f2108978c644b623194a2
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-03-15 14:30:59 +0000

    FM-51 added contract length and extentions

---
+++ spec/controllers/facilities_management/standard_contract_questions_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe FacilitiesManagement::StandardContractQuestionsController, type: :controller do
+
+  describe "GET #standard_contract_questions" do
+    it "returns http success" do
+      get :standard_contract_questions
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
