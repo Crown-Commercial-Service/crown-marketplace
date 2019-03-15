2624866f6945d11ac8429cfce5cf985e5bbe2ef9
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-03-15 11:11:09 +0000

    FM25-28 filtering changes and supplier list bug fix.

--- app/assets/javascripts/common/common.js
+++ app/assets/javascripts/common/common.js
@@ -59,8 +59,20 @@ const pageUtils = {
     getCodes: ((arr) => {
         let result = [];
         arr.forEach((value, index, array) => {
-            result.push(value.code.replace('-','.'));
+            result.push(value.code.replace('-', '.'));
         });
+        return result;
+    }),
+
+    generateGuid: (() => {
+        let result, i, j;
+        result = '';
+        for (j = 0; j < 32; j++) {
+            if (j == 8 || j == 12 || j == 16 || j == 20)
+                result = result + '-';
+            i = Math.floor(Math.random() * 16).toString(16).toUpperCase();
+            result = result + i;
+        }
         return result;
     })
 };
