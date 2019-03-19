090032eef16b939c832bd3ea97606fab4bb46265
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-03-19 15:57:22 +0000

    FM-51 added contract extensions

--- spec/views/facilities_management/standard_contract_questions/standard_contract_questions.html.erb_spec.rb
+++ spec/views/facilities_management/standard_contract_questions/standard_contract_questions.html.erb_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
-RSpec.describe "standard_contract_questions/standard_contract_questions.html.erb", type: :view do
+RSpec.describe 'standard_contract_questions/standard_contract_questions.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
