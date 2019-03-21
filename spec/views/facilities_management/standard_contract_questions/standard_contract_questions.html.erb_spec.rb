f351c14c6affd00d28af7c1828cbeead7829508a
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-03-21 08:51:36 +0000

    FM51 - remove unused boiler plate

--- spec/views/facilities_management/standard_contract_questions/standard_contract_questions.html.erb_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-RSpec.describe 'standard_contract_questions/standard_contract_questions.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
