7b5cb8cd33238a05bda707761c6939fb21490853
Tariq Hamid <tariq@hamid.com>
2019-03-21 11:30:30 +0000

    refactoring of postcode test

--- spec/services/postcode_checker_spec.rb
+++ spec/services/postcode_checker_spec.rb
@@ -5,24 +5,24 @@ require 'rails_helper'
 RSpec.describe PostcodeChecker do
   describe 'POST postcode' do
     it 'Postcode not in London' do
-      expect(PostcodeChecker.inLondon?('G32 0RP')).to be false
+      expect(described_class.in_london?('G32 0RP')).to be false
     end
 
     it 'Postcode in London' do
-      expect(PostcodeChecker.inLondon?('SW1P 2BA')).to be true
+      expect(described_class.in_london?('SW1P 2BA')).to be true
     end
 
     it 'Bad postcode not in London' do
-      expect(PostcodeChecker.inLondon?('X11 1XX')).to be false
+      expect(described_class.in_london?('X11 1XX')).to be false
     end
 
     it 'nuts code in Westminster' do
-      place = PostcodeChecker.location_info('SW1P 2BA')
+      place = described_class.location_info('SW1P 2BA')
       expect(place['nuts']).to eq 'Westminster'
     end
 
     it 'admin district is Westminster' do
-      place = PostcodeChecker.location_info('SW1P 2BA')
+      place = described_class.location_info('SW1P 2BA')
       expect(place['admin_district']).to eq 'Westminster'
     end
   end
