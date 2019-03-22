887606a938087f5e4b7e9cbe448d0034bfc040d9
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-03-22 08:40:53 +0000

    FM52 Add new address files

--- app/controllers/facilities_management/buildings_controller.rb
+++ app/controllers/facilities_management/buildings_controller.rb
@@ -1,6 +1,9 @@
 class FacilitiesManagement::BuildingsController < ApplicationController
-  require_permission :facilities_management, only: :buildings
+  require_permission :facilities_management, only: [:buildings, :new_building]
 
   def buildings
+  end
+
+  def new_building
   end
 end
