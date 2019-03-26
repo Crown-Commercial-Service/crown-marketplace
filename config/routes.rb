c406b2371750e9b2d43f16e163c1873eb6e5f1cd
Chris Gannon <cjgannon@gmail.com>
2019-03-26 10:23:10 +0000

    Add average daily rate to supplier results page
    Also add rate card model

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ Rails.application.routes.draw do
     get '/', to: 'home#index'
     get '/gateway', to: 'gateway#index'
     get '/suppliers', to: 'suppliers#index'
-    get '/supplier/:id', to: 'suppliers#show', as: 'supplier'
+    get '/suppliers/:id', to: 'suppliers#show', as: 'supplier'
     get '/suppliers/download', to: 'suppliers#download', as: 'suppliers_download'
     get '/start', to: 'journey#start', as: 'journey_start'
     get '/:slug', to: 'journey#question', as: 'journey_question'
