cb697b0a002fbb64c3667208490d83ecc60e53f6
Darren Harridence <darren.harridence@btinternet.com>
2019-03-27 16:13:10 +0000

    New ccs patterns directory created

---
+++ app/controllers/ccs_patterns/home_controller.rb
@@ -0,0 +1,17 @@
+module CcsPatterns
+  class HomeController < FrameworkController
+    require_permission :none, only: [:index, :dynamic_accordian]
+    before_action :set_back_path, except: :index
+
+    def index; end
+
+    def dynamic_accordian; end
+
+
+    private
+
+    def set_back_path
+      @back_path = :back
+    end
+  end
+end
