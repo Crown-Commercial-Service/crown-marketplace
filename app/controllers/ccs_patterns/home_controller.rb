03ee045abb793fb6da3ca3eb06ad53e6759b3157
Darren Harridence <darren.harridence@btinternet.com>
2019-04-02 16:31:23 +0100

    styles for global page AT 116

--- app/controllers/ccs_patterns/home_controller.rb
+++ app/controllers/ccs_patterns/home_controller.rb
@@ -1,13 +1,15 @@
 module CcsPatterns
   class HomeController < FrameworkController
-    require_permission :none, only: %i[index dynamic_accordian supplier_results_v1 small_checkboxes]
+    require_permission :none, only: %i[index dynamic_accordian supplier_results_v1 supplier_results_v2 small_checkboxes]
     before_action :set_back_path, except: :index
 
     def index; end
 
     def dynamic_accordian; end
 
-    def supplier_results_v1; end
+    def supplier_results_v1; end 
+
+    def supplier_results_v2; end
 
     def small_checkboxes; end
 
