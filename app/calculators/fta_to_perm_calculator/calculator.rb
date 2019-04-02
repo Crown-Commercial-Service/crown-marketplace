d40253087e30c3fdafcafc6991edbd1f7b5a22d3
Iulia R Ionita <iulia@ionita.me>
2019-04-02 14:23:14 +0100

    Initial FTA-to-perm calculator done (no tests)

---
+++ app/calculators/fta_to_perm_calculator/calculator.rb
@@ -0,0 +1,22 @@
+require 'holidays'
+
+module FTAToPermCalculator
+  class Calculator
+
+    attr_reader :within_6_months, :fixed_term_contract_fee, :current_contract_length
+
+    def initialize(
+      within_6_months:,
+      fixed_term_contract_fee:,
+      current_contract_length:
+    )
+      @within_6_months = within_6_months
+      @fixed_term_contract_fee = fixed_term_contract_fee
+      @current_contract_length = current_contract_length
+    end
+
+    def fee
+      @within_6_months ? (@fixed_term_contract_fee/@current_contract_length) * 12 - @fixed_term_contract_fee : 0
+    end
+  end
+end
