d95cccf3a8420a8c52c54db6d82eceb5a905b1da
milomia <m-hunt3@sky.com>
2019-04-03 10:46:27 +0100

    remove byebug

--- app/controllers/facilities_management/calculator_controller.rb
+++ app/controllers/facilities_management/calculator_controller.rb
@@ -4,7 +4,6 @@ require 'facilities_management/fm_supplier_data'
 class FacilitiesManagement::CalculatorController < ApplicationController
   require_permission :facilities_management, only: :calculator
   def calculator
-    byebug
     @calc = FMCalculator::Calculator.new('G1', 23000, 125, 'Y', 'Y', 'Y', 'N')
     x1 = @calc.sumunitofmeasure('G1', 23000, 125, 'Y', 'Y', 'Y', 'N')
     render json: x1
