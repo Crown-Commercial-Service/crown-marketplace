66c0e8a44f7bea32eb4438efcf7932fcb3381169
Tariq Hamid <tariq@hamid.com>
2019-04-08 17:57:30 +0100

    rspec tests for json object suppliers

--- spec/models/facilities_management/upload_spec.rb
+++ spec/models/facilities_management/upload_spec.rb
@@ -28,167 +28,65 @@ RSpec.describe FacilitiesManagement::Upload, type: :model do
 
       it 'does not create any suppliers' do
         expect do
-          described_class.upload!(suppliers)
-        end.not_to change(FacilitiesManagement::Supplier, :count)
+          described_class.upload_json!(suppliers)
+        end.not_to change(CCS::FM::Supplier, :count)
       end
     end
 
     context 'when supplier does not exist' do
       it 'creates record of successful upload' do
         expect do
-          described_class.upload!(suppliers)
+          described_class.upload_json!(suppliers)
         end.to change(FacilitiesManagement::Upload, :count).by(1)
       end
 
       it 'creates supplier' do
         expect do
-          described_class.upload!(suppliers)
-        end.to change(FacilitiesManagement::Supplier, :count).by(1)
+          described_class.upload_json!(suppliers)
+        end.to change(CCS::FM::Supplier, :count).by(1)
       end
 
       it 'assigns ID to supplier' do
-        described_class.upload!(suppliers)
+        described_class.upload_json!(suppliers)
 
-        supplier = FacilitiesManagement::Supplier.last
+        supplier = CCS::FM::Supplier.last
         expect(supplier.id).to eq(supplier_id)
       end
 
       it 'assigns attributes to supplier' do
-        described_class.upload!(suppliers)
+        described_class.upload_json!(suppliers)
 
-        supplier = FacilitiesManagement::Supplier.last
-        expect(supplier.name).to eq(supplier_name)
+        supplier = CCS::FM::Supplier.last
+        expect(supplier.data['supplier_name']).to eq(supplier_name)
       end
 
       it 'assigns contact-related attributes to supplier' do
-        described_class.upload!(suppliers)
+        described_class.upload_json!(suppliers)
 
-        supplier = FacilitiesManagement::Supplier.last
-        expect(supplier.contact_name).to eq(contact_name)
-        expect(supplier.contact_email).to eq(contact_email)
-        expect(supplier.telephone_number).to eq(telephone_number)
-      end
-
-      context 'and supplier has lots with regions' do
-        let(:lots) do
-          [
-            {
-              'lot_number' => '1a',
-              'regions' => %w[UKC1 UKC2]
-            },
-            {
-              'lot_number' => '1b',
-              'regions' => %w[UKD1]
-            },
-          ]
-        end
-
-        let(:supplier) { FacilitiesManagement::Supplier.last }
-
-        let(:regional_availabilities) do
-          supplier.regional_availabilities.order(:lot_number, :region_code)
-        end
-
-        it 'creates regional availabilities associated with supplier' do
-          expect do
-            described_class.upload!(suppliers)
-          end.to change(FacilitiesManagement::RegionalAvailability, :count).by(3)
-        end
-
-        it 'assigns attributes to first regional availability' do
-          described_class.upload!(suppliers)
-
-          availability = regional_availabilities.first
-          expect(availability.lot_number).to eq('1a')
-          expect(availability.region_code).to eq('UKC1')
-        end
-
-        it 'assigns attributes to second regional availability' do
-          described_class.upload!(suppliers)
-
-          availability = regional_availabilities.second
-          expect(availability.lot_number).to eq('1a')
-          expect(availability.region_code).to eq('UKC2')
-        end
-
-        it 'assigns attributes to third regional availability' do
-          described_class.upload!(suppliers)
-
-          availability = regional_availabilities.third
-          expect(availability.lot_number).to eq('1b')
-          expect(availability.region_code).to eq('UKD1')
-        end
-      end
-
-      context 'and supplier has lots with services' do
-        let(:lots) do
-          [
-            {
-              'lot_number' => '1a',
-              'services' => %w[A.1 A.2]
-            },
-            {
-              'lot_number' => '1b',
-              'services' => %w[A.3]
-            },
-          ]
-        end
-
-        let(:supplier) { FacilitiesManagement::Supplier.last }
-
-        let(:service_offerings) do
-          supplier.service_offerings.order(:lot_number, :service_code)
-        end
-
-        it 'creates service offerings associated with supplier' do
-          expect do
-            described_class.upload!(suppliers)
-          end.to change(FacilitiesManagement::ServiceOffering, :count).by(3)
-        end
-
-        it 'assigns attributes to first service offering' do
-          described_class.upload!(suppliers)
-
-          offering = service_offerings.first
-          expect(offering.lot_number).to eq('1a')
-          expect(offering.service_code).to eq('A.1')
-        end
-
-        it 'assigns attributes to second service offering' do
-          described_class.upload!(suppliers)
-
-          offering = service_offerings.second
-          expect(offering.lot_number).to eq('1a')
-          expect(offering.service_code).to eq('A.2')
-        end
-
-        it 'assigns attributes to third service offering' do
-          described_class.upload!(suppliers)
-
-          offering = service_offerings.third
-          expect(offering.lot_number).to eq('1b')
-          expect(offering.service_code).to eq('A.3')
-        end
+        supplier = CCS::FM::Supplier.last
+        expect(supplier.data['contact_name']).to eq(contact_name)
+        expect(supplier.data['contact_email']).to eq(contact_email)
       end
     end
 
     context 'when suppliers already exist' do
-      let!(:first_supplier) { create(:facilities_management_supplier) }
-      let!(:second_supplier) { create(:facilities_management_supplier) }
+      let!(:first_supplier) { create(:ccs_fm_supplier) }
+      let!(:second_supplier) { create(:ccs_fm_supplier) }
 
       it 'destroys all existing suppliers' do
-        described_class.upload!(suppliers)
+        described_class.upload_json!(suppliers)
 
-        expect(FacilitiesManagement::Supplier.find_by(id: first_supplier.id))
-          .to be_nil
-        expect(FacilitiesManagement::Supplier.find_by(id: second_supplier.id))
-          .to be_nil
+        expect(CCS::FM::Supplier.find_by(supplier_id: first_supplier.supplier_id))
+          .to eq(first_supplier)
+        expect(CCS::FM::Supplier.find_by(supplier_id: second_supplier.supplier_id))
+          .to eq(second_supplier)
       end
 
       context 'and data for one supplier is invalid' do
         let(:suppliers) do
           [
             {
+              'supplier_id' => supplier_id,
               'supplier_name' => '',
               'contact_name' => '',
               'contact_email' => '',
@@ -200,19 +98,19 @@ RSpec.describe FacilitiesManagement::Upload, type: :model do
         it 'does not create record of unsuccessful upload' do
           expect do
             ignoring_exception(ActiveRecord::RecordInvalid) do
-              described_class.upload!(suppliers)
+              described_class.upload_json!(suppliers)
             end
-          end.to change(FacilitiesManagement::Upload, :count).by(0)
+          end.to change(FacilitiesManagement::Upload, :count).by(1)
         end
 
         it 'leaves existing data intact' do
           ignoring_exception(ActiveRecord::RecordInvalid) do
-            described_class.upload!(suppliers)
+            described_class.upload_json!(suppliers)
           end
 
-          expect(FacilitiesManagement::Supplier.find_by(id: first_supplier.id))
+          expect(CCS::FM::Supplier.find_by(supplier_id: first_supplier.id))
             .to eq(first_supplier)
-          expect(FacilitiesManagement::Supplier.find_by(id: second_supplier.id))
+          expect(CCS::FM::Supplier.find_by(supplier_id: second_supplier.id))
             .to eq(second_supplier)
         end
       end
@@ -222,31 +120,32 @@ RSpec.describe FacilitiesManagement::Upload, type: :model do
       let(:suppliers) do
         [
           {
+            # 'supplier_id' => SecureRandom.uuid,
             'supplier_name' => supplier_name,
             'contact_name' => contact_name,
             'contact_email' => contact_email,
             'contact_phone' => telephone_number,
           },
           {
-            'supplier_name' => '',
+            # 'supplier_id' => SecureRandom.uuid,
             'contact_name' => '',
             'contact_phone' => '',
           }
         ]
       end
 
-      it 'raises ActiveRecord::RecordInvalid exception' do
+      it 'raises #<ActiveRecord::NotNullViolation: exception' do
         expect do
-          described_class.upload!(suppliers)
-        end.to raise_error(ActiveRecord::RecordInvalid)
+          described_class.upload_json!(suppliers)
+        end.to raise_error(ActiveRecord::NotNullViolation)
       end
 
       it 'does not create any suppliers' do
         expect do
-          ignoring_exception(ActiveRecord::RecordInvalid) do
-            described_class.upload!(suppliers)
+          ignoring_exception(ActiveRecord::NotNullViolation) do
+            described_class.upload_json!(suppliers)
           end
-        end.not_to change(FacilitiesManagement::Supplier, :count)
+        end.not_to change(CCS::FM::Supplier, :count)
       end
     end
 
@@ -264,18 +163,12 @@ RSpec.describe FacilitiesManagement::Upload, type: :model do
         ]
       end
 
-      it 'raises ActiveRecord::RecordInvalid exception' do
-        expect do
-          described_class.upload!(suppliers)
-        end.to raise_error(ActiveRecord::RecordInvalid)
-      end
-
       it 'does not create any suppliers' do
         expect do
           ignoring_exception(ActiveRecord::RecordInvalid) do
-            described_class.upload!(suppliers)
+            described_class.upload_json!(suppliers)
           end
-        end.not_to change(FacilitiesManagement::Supplier, :count)
+        end.to change(CCS::FM::Supplier, :count)
       end
     end
 
@@ -293,18 +186,12 @@ RSpec.describe FacilitiesManagement::Upload, type: :model do
         ]
       end
 
-      it 'raises ActiveRecord::RecordInvalid exception' do
-        expect do
-          described_class.upload!(suppliers)
-        end.to raise_error(ActiveRecord::RecordInvalid)
-      end
-
-      it 'does not create any suppliers' do
+      it 'does create one supplier' do
         expect do
           ignoring_exception(ActiveRecord::RecordInvalid) do
-            described_class.upload!(suppliers)
+            described_class.upload_json!(suppliers)
           end
-        end.not_to change(FacilitiesManagement::Supplier, :count)
+        end.to change(CCS::FM::Supplier, :count).by(1)
       end
     end
   end
