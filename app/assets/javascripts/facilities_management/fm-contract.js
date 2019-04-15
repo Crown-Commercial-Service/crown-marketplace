d5198e7b1a41e3a1f62e549a89bfa2a31356e327
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-04-15 10:39:11 +0100

    FM 101 added validation and caching of contract start date

--- app/assets/javascripts/facilities_management/fm-contract.js
+++ app/assets/javascripts/facilities_management/fm-contract.js
@@ -1,12 +1,51 @@
 $(() => {
 
+    const validateDate = (() => {
+        const day = $('#day').val();
+        const month = $('#month').val();
+        const year = $('#year').val();
+
+        const isValid = fm.services.isDateValid(day, month, year);
+
+        if (isValid === false) {
+            $('#fm-contract-start-error-message').removeClass('govuk-visually-hidden');
+            $('#day').addClass('govuk-input--error');
+            $('#month').addClass('govuk-input--error');
+            $('#year').addClass('govuk-input--error');
+            $('#fm-contract-date-form-group-error-container').addClass('govuk-form-group--error')
+        } else {
+            $('#fm-contract-start-error-message').addClass('govuk-visually-hidden');
+            $('#day').removeClass('govuk-input--error');
+            $('#month').removeClass('govuk-input--error');
+            $('#year').removeClass('govuk-input--error');
+            $('#fm-contract-date-form-group-error-container').removeClass('govuk-form-group--error');
+        }
+
+        return isValid;
+
+    });
 
 
+    $('#day').on('change', ((e) => {
+        validateDate();
+    }));
 
+    $('#month').on('change', ((e) => {
+        validateDate();
+    }));
 
+    $('#year').on('change', ((e) => {
+        validateDate();
+    }));
 
-
-
-
+    $('#fm-save-contract-start-date-link').on('click', ((e) => {
+        e.preventDefault();
+        if (validateDate() === true) {
+            const day = $('#day').val();
+            const month = $('#month').val();
+            const year = $('#year').val();
+            pageUtils.setCachedData('fm-contract-start-date', day + '-' + month + '-' + year);
+        }
+    }));
 
 });
