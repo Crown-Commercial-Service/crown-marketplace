d0350ec114e83427afbcff6b1ee5ed70e2824a2a
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-04-15 10:39:11 +0100

    FM 101 added validation and caching of contract start date

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,4 +24,5 @@
 //= require facilities_management/fm-buildings.js
 //= require facilities_management/fm-address.js
 //= require facilities_management/fm-units-of-measurement.js
+//= require facilities_management/fm-contract.js
 //= require global/global.js
