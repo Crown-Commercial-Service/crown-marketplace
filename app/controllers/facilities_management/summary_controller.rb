b51dff91e47f395515084c4635f550041aa77bd7
Tariq Hamid <tariq@hamid.com>
2019-04-16 22:04:18 +0100

    improve rake

--- app/controllers/facilities_management/summary_controller.rb
+++ app/controllers/facilities_management/summary_controller.rb
@@ -7,8 +7,6 @@ module FacilitiesManagement
       # :nocov:
 
     def index
-      @idx = 'Hello Nika'
-      @idx2 = 'Hello Tariq'
 
       @select_fm_locations = '/facilities-management/select-locations'
       @select_fm_services = '/facilities-management/select-services'
@@ -18,12 +16,36 @@ module FacilitiesManagement
       @inline_error_summary_body_href = '#'
       @inline_summary_error_text = 'You must select at least one longList before clicking the save continue button'
 
+
+      # Get nuts regions
+      # h = {}
+      # Nuts1Region.all.each { |x| h[x.code] = x.name }
+      # @regions = h
+      h = {}
+      FacilitiesManagement::Region.all.each { |x| h[x.code] = x.name }
+      @subregions = h
+      @subregions.select! { | k, v | @posted_locations.include? k }
+
       @supplier_count = $tsi[session.id, :supplier_count]
-      @suppliers_lot1a = $tsi[session.id, :suppliers_lot1a]
-      @suppliers_lot1b = $tsi[session.id, :suppliers_lot1b]
-      @suppliers_lot1c = $tsi[session.id, :suppliers_lot1c]
+
+      @choice = params[:lot]
+      case @choice # a_variable is the variable we want to compare
+      when '1a'
+        @suppliers_lot1a = $tsi[session.id, :suppliers_lot1a]
+      when '1b'
+        @suppliers_lot1b = $tsi[session.id, :suppliers_lot1b]
+      when '1c'
+        @suppliers_lot1c = $tsi[session.id, :suppliers_lot1c]
+      else
+        @suppliers_lot1a = $tsi[session.id, :suppliers_lot1a]
+        @suppliers_lot1b = $tsi[session.id, :suppliers_lot1b]
+        @suppliers_lot1c = $tsi[session.id, :suppliers_lot1c]
+      end
     end
 
+    # @supplier_count = [ @suppliers_lot1a, @suppliers_lot1b, @suppliers_lot1c ].max
+    # puts @supplier_count
+    puts 'SummaryController >> index'
   end
 
 end
