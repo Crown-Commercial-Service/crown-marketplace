5f17327a9b5e312d005200d2742a69e2fdbffcb7
Iulia R Ionita <iulia@ionita.me>
2019-04-16 17:12:15 +0100

    Added approved action

--- app/controllers/supply_teachers/admin/uploads_controller.rb
+++ app/controllers/supply_teachers/admin/uploads_controller.rb
@@ -12,7 +12,6 @@ module SupplyTeachers
 
       def create
         @upload = Upload.new(upload_params)
-        @upload.state = 'created'
 
         if @upload.save
           redirect_to supply_teachers_admin_uploads_path
@@ -21,10 +20,41 @@ module SupplyTeachers
         end
       end
 
+      def show
+        @upload = Upload.find(params[:id])
+      end
+
+      def approve
+        suppliers = JSON.parse(File.read(data_file))
+
+        SupplyTeachers::Upload.upload!(suppliers)
+
+        redirect_to supply_teachers_admin_uploads_path
+      rescue ActiveRecord::RecordInvalid => e
+        summary = {
+          record: e.record,
+          record_class: e.record.class.to_s,
+          errors: e.record.errors
+        }
+        render show: summary, status: :unprocessable_entity
+      end
+
+      def reject
+
+      end
+
       private
 
       def upload_params
         params.require(:supply_teachers_admin_upload).permit(:current_accredited_suppliers, :geographical_data_all_suppliers, :lot_1_and_lot_2_comparisons, :master_vendor_contacts, :neutral_vendor_contacts, :pricing_for_tool, :supplier_lookup)
+      end
+
+      def data_file
+        if Rails.env.production?
+          './lib/tasks/supply_teachers/output/data.json'
+        else
+          './lib/tasks/supply_teachers/output/anonymous.json'
+        end
       end
     end
   end
