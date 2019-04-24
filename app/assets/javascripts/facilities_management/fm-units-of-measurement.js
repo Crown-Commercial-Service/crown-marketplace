562d71bb49c2f4fd64ccf97364475a9030def42b
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-04-24 13:02:52 +0100

    FM initial lift pages and fix for drop index

--- app/assets/javascripts/facilities_management/fm-units-of-measurement.js
+++ app/assets/javascripts/facilities_management/fm-units-of-measurement.js
@@ -12,10 +12,17 @@ $(() => {
         let building_id = $('#fm-building-uom-id').attr('value');
         let service_code = $('#fm-service-uom-code').attr('value');
         let uom_value = $('#fm-uom-input').val();
+        let isLift = $('#fm-is-lift').attr('value');
 
         if (uom_value && uom_value.length > 0) {
             pageUtils.toggleInlineErrorMessage(false);
-            fm.services.save_uom(building_id, service_code, uom_value);
+            if (isLift === true) {
+
+                //fm-current-building
+
+            } else {
+                fm.services.save_uom(building_id, service_code, uom_value);
+            }
         } else {
             pageUtils.toggleInlineErrorMessage(true);
         }
