6e5cf30d525e5e473b9cb95fb97f14955c76659c
Tariq Hamid <tariq@hamid.com>
2019-04-25 00:16:05 +0100

    flush db

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-bundle exec rails db:static
+bundle exec rails db:drop
+
+bundle exec rails db:setup
 
 bundle exec rails db:migrate
+
+bundle exec rails db:static
 
 bundle exec rails server
