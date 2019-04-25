eaf14c5dcb46501892e11050fa4c3a227a9cfc38
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-04-25 12:39:27 +0100

    FM added lift info to the buildings list page

--- app/services/facilities_management/fm_service_data.rb
+++ app/services/facilities_management/fm_service_data.rb
@@ -66,7 +66,7 @@ where trim(replace(subcode, '-', '.')) not in (select v.service_code from fm_uom
 
   def uom_values(email_address)
     query = "select building_id, service_code, uom_value from fm_uom_values where
-	user_id = '" + Base64.encode64(email_address) + "'"
+	user_id = '" + Base64.encode64(email_address) + "' and service_code not in ('C.5'); "
     ActiveRecord::Base.connection.execute(query)
   rescue StandardError => e
     Rails.logger.warn "Couldn't get unit of measurement values: #{e}"
@@ -75,12 +75,14 @@ where trim(replace(subcode, '-', '.')) not in (select v.service_code from fm_uom
   def save_lift_data(email_address, building_id, json_data)
     query = "INSERT INTO fm_lifts (user_id, building_id, lift_data) VALUES('" + Base64.encode64(email_address) + "', '" + building_id + "', '" + json_data.to_json + "');"
     ActiveRecord::Base.connection.execute(query)
+    add_uom_value(email_address, building_id, 'C.5', 'Saved')
   rescue StandardError => e
     Rails.logger.warn "Couldn't save lift data : #{e}"
   end
 
-  def get_lift_data(email_address, building_id)
-    query = "select lift_data from fm_lifts where user_id = '" + Base64.encode64(email_address) + "' and building_id = '" + building_id + "';"
+  def get_lift_data(email_address)
+    query = "select building_id, lift_data::jsonb->'lift_data'->>'lifts-qty' lift_qty, lift_data::jsonb->'lift_data'->>'total-floor-count' total_floor_count from fm_lifts
+   where user_id =  '" + Base64.encode64(email_address) + "';"
     ActiveRecord::Base.connection.execute(query)
   rescue StandardError => e
     Rails.logger.warn "Couldn't retrieve lift data : #{e}"
