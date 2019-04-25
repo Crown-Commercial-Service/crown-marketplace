2b79a2f28095b39f021bcc873f7c0bcd56a4e598
Iulia R Ionita <iulia@ionita.me>
2019-04-25 17:17:40 +0100

    Added tests for upload model

---
+++ spec/factories/supply_teachers_admin_upload.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :supply_teachers_admin_upload, class: SupplyTeachers::Admin::Upload do
+    supplier_lookup { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/supplier_lookup_test.csv')) }
+  end
+end
