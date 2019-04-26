1fafcdfc095897cc525367494429abf01d25c0e0
Iulia R Ionita <iulia@ionita.me>
2019-04-26 16:54:01 +0100

    Updated scpecs

--- spec/models/supply_teachers/admin/upload_spec.rb
+++ spec/models/supply_teachers/admin/upload_spec.rb
@@ -3,16 +3,29 @@ require 'rails_helper'
 RSpec.describe SupplyTeachers::Admin::Upload, type: :model do
   subject { create(:supply_teachers_admin_upload) }
 
-  describe '#aasm state' do
+  describe '#default scope' do
+    it 'orders by descending created_at' do
+      expect(described_class.all.to_sql).to eq described_class.all.order(created_at: :desc).to_sql
+    end
+  end
 
+  describe '#aasm state' do
+    before do
+      allow(subject).to receive(:cleanup_input_files)
+      allow(subject).to receive(:start_upload)
+    end
     describe 'initial state' do
       it { is_expected.to have_state(:in_progress) }
+      it { is_expected.not_to have_received(:cleanup_input_files) }
+      it { is_expected.not_to have_received(:start_upload) }
     end
 
     describe 'in_review' do
       before { subject.review }
 
       it { is_expected.to have_state(:in_review) }
+      it { is_expected.not_to have_received(:cleanup_input_files) }
+      it { is_expected.not_to have_received(:start_upload) }
     end
 
     describe 'failed' do
@@ -20,6 +33,8 @@ RSpec.describe SupplyTeachers::Admin::Upload, type: :model do
         before { subject.fail }
 
         it { is_expected.to have_state(:failed) }
+        it { is_expected.to have_received(:cleanup_input_files) }
+        it { is_expected.not_to have_received(:start_upload) }
       end
 
       context 'when uploading' do
@@ -28,6 +43,8 @@ RSpec.describe SupplyTeachers::Admin::Upload, type: :model do
           subject.fail
         end
         it { is_expected.to have_state(:failed) }
+        it { is_expected.to have_received(:cleanup_input_files) }
+        it { is_expected.not_to have_received(:start_upload) }
       end
     end
 
@@ -37,6 +54,8 @@ RSpec.describe SupplyTeachers::Admin::Upload, type: :model do
         subject.upload
       end
       it { is_expected.to have_state(:uploading) }
+      it { is_expected.not_to have_received(:cleanup_input_files) }
+      it { is_expected.to have_received(:start_upload) }
     end
 
     describe 'approved' do
@@ -45,6 +64,8 @@ RSpec.describe SupplyTeachers::Admin::Upload, type: :model do
         subject.approve
       end
       it { is_expected.to have_state(:approved) }
+      it { is_expected.not_to have_received(:cleanup_input_files) }
+      it { is_expected.not_to have_received(:start_upload) }
     end
 
     describe 'rejected' do
@@ -53,6 +74,8 @@ RSpec.describe SupplyTeachers::Admin::Upload, type: :model do
         subject.reject
       end
       it { is_expected.to have_state(:rejected) }
+      it { is_expected.to have_received(:cleanup_input_files) }
+      it { is_expected.not_to have_received(:start_upload) }
     end
 
     describe 'canceled' do
@@ -62,6 +85,8 @@ RSpec.describe SupplyTeachers::Admin::Upload, type: :model do
           subject.cancel
         end
         it { is_expected.to have_state(:canceled) }
+        it { is_expected.to have_received(:cleanup_input_files) }
+        it { is_expected.not_to have_received(:start_upload) }
       end
       context 'when in progress' do
         before do
@@ -69,6 +94,53 @@ RSpec.describe SupplyTeachers::Admin::Upload, type: :model do
           subject.cancel
         end
         it { is_expected.to have_state(:canceled) }
+        it { is_expected.to have_received(:cleanup_input_files) }
+        it { is_expected.not_to have_received(:start_upload) }
+      end
+    end
+  end
+
+  describe '#available_for_cp' do
+    let(:upload) { create(:supply_teachers_admin_upload) }
+
+    context 'when no previous upload exists' do
+      it 'returns true for supplier_lookup' do
+        expect(upload.send(:available_for_cp, :supplier_lookup)).to eq false
+      end
+    end
+
+    context 'when previous approved upload exists' do
+      before do
+        subject.aasm.current_state = :uploading
+        subject.approve!
+      end
+
+      it 'returns true for supplier_lookup' do
+        expect(upload.send(:available_for_cp, :supplier_lookup)).to eq true
+      end
+
+      it 'returns false for current_accredited_suppliers' do
+        expect(upload.send(:available_for_cp, :current_accredited_suppliers)).to eq false
+      end
+
+      it 'returns false for geographical_data_all_suppliers' do
+        expect(upload.send(:available_for_cp, :geographical_data_all_suppliers)).to eq false
+      end
+
+      it 'returns false for lot_1_and_lot_2_comparisons' do
+        expect(upload.send(:available_for_cp, :lot_1_and_lot_2_comparisons)).to eq false
+      end
+
+      it 'returns false for master_vendor_contacts' do
+        expect(upload.send(:available_for_cp, :master_vendor_contacts)).to eq false
+      end
+
+      it 'returns false for neutral_vendor_contacts' do
+        expect(upload.send(:available_for_cp, :neutral_vendor_contacts)).to eq false
+      end
+
+      it 'returns false for pricing_for_tool' do
+        expect(upload.send(:available_for_cp, :pricing_for_tool)).to eq false
       end
     end
   end
@@ -112,14 +184,6 @@ RSpec.describe SupplyTeachers::Admin::Upload, type: :model do
         expect(described_class.previous_uploaded_file_object(:supplier_lookup)).to eq nil
       end
     end
-  end
-
-  describe '#cleanup_input_files' do
-
-  end
-
-  describe '#default scope' do
-
   end
 
 end
