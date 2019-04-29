68afc8e96ed6a979079356eb23b09250767644a9
Iulia R Ionita <iulia@ionita.me>
2019-04-29 14:31:59 +0100

    Added cache attr accessors to permit params

--- app/controllers/supply_teachers/admin/uploads_controller.rb
+++ app/controllers/supply_teachers/admin/uploads_controller.rb
@@ -38,7 +38,7 @@ module SupplyTeachers
       private
 
       def upload_params
-        params.require(:supply_teachers_admin_upload).permit(:current_accredited_suppliers, :geographical_data_all_suppliers, :lot_1_and_lot_2_comparisons, :master_vendor_contacts, :neutral_vendor_contacts, :pricing_for_tool, :supplier_lookup)
+        params.require(:supply_teachers_admin_upload).permit(:current_accredited_suppliers, :geographical_data_all_suppliers, :lot_1_and_lot_2_comparisons, :master_vendor_contacts, :neutral_vendor_contacts, :pricing_for_tool, :supplier_lookup, :current_accredited_suppliers_cache, :geographical_data_all_suppliers_cache, :lot_1_and_lot_2_comparisons_cache, :master_vendor_contacts_cache, :neutral_vendor_contacts_cache, :pricing_for_tool_cache, :supplier_lookup_cache)
       end
     end
   end
