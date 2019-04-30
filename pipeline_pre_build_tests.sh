e82bc4c64737df43fb6dbe3393dbed912881346d
Iulia R Ionita <iulia@ionita.me>
2019-04-30 17:19:47 +0100

    Switched to version 2.4 of postgis

--- pipeline_pre_build_tests.sh
+++ pipeline_pre_build_tests.sh
@@ -4,7 +4,7 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key a
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
 apt update
 apt-get install -y build-essential
-apt install -y postgresql-9.6-postgis-2.3
+apt install -y postgresql-9.6-postgis-2.4
 service postgresql start 9.6
 sudo -u postgres createuser --superuser root; sudo -u postgres createdb root
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
