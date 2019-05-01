022081f33d4634f575ffb87707e50b61d998c002
Iulia R Ionita <iulia@ionita.me>
2019-05-01 16:10:17 +0100

    Revert "Feature/st 69 admin"

--- lib/user_constraint.rb
+++
@@ -1,8 +0,0 @@
-class UserConstraint
-  def matches?(request)
-    return false unless request.session[:login]
-
-    login = Login.from_session(request.session[:login])
-    login.present?
-  end
-end
