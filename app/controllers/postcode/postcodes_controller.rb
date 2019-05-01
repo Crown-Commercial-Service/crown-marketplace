35fccf5989512795ff48976144aba082c0372f54
Tariq Hamid <tariq@hamid.com>
2019-05-01 17:44:50 +0100

    controller action that starts rake db:postcodes

--- app/controllers/postcode/postcodes_controller.rb
+++ app/controllers/postcode/postcodes_controller.rb
@@ -16,12 +16,52 @@ module Postcode
     def show
       result = if params[:id] == 'in_london'
                  PostcodeChecker.in_london? params[:postcode]
+               elsif params[:id] == 'upload'
+                 upload(params[:access_key], params[:secret_access_key], params[:bucket], params[:region])
                else
                  PostcodeChecker.location_info(params[:id])
                end
       render json: { status: 200, result: result }
     rescue StandardError
       render json: { status: 404, error: 'Postcode not found' }
+    end
+
+    private
+
+    def upload(access_key, secret_access_key, bucket, region)
+      aws_secrets = {
+        AccessKeyId: access_key,
+        SecretAccessKey: secret_access_key,
+        bucket: bucket,
+        region: region
+      }
+
+      File.open(Rails.root.to_s + '/../aws-secrets.json', 'w') { |file| file.write(aws_secrets.to_json) }
+
+      rake
+
+      { status: 200, result: "Uploading postcodes from AWS bucket #{bucket}, region: #{region}" }
+    rescue IOError => e
+      #some error occur, dir not writable etc.
+      { status: 404, error: e.to_s }
+    rescue StandardError => e
+      { status: 404, error: e.to_s }
+    end
+
+    def rake
+      if File.split($PROGRAM_NAME).last == 'rake'
+        Rails.logger.info('Guess what, I`m running this from Rake')
+      else
+        begin
+          Rails.logger.info('No, this is not a Rake task')
+          Rails.application.load_tasks
+          Rake::Task['db:postcode'].execute
+        rescue StandardError
+          message = self.class.name + " data is missing! Please run 'rake db:postcode' to load postcode data."
+          Rails.logger.info("\e[5;37;41m\n" + message + "\033[0m\n")
+          raise error
+        end
+      end
     end
   end
 end
