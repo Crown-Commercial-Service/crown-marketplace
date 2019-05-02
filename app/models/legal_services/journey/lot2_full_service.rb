514ea5a084d88db46c854ba48d9aec58019146a4
milomia <m-hunt3@sky.com>
2019-05-02 15:21:13 +0100

    more stash changes

--- app/models/legal_services/journey/lot2_full_service.rb
+++ app/models/legal_services/journey/lot2_full_service.rb
@@ -2,11 +2,7 @@ module LegalServices
   class Journey::Lot2FullService
     include Steppable
     def next_step_class
-<<<<<<< Updated upstream
       Journey::Lot2FullService
-=======
-      Journey::LegalJurisdiction
->>>>>>> Stashed changes
     end
   end
 end
