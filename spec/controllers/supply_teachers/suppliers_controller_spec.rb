6dba42950a7b15238ae9537e4e8e5d1254e6443c
Iulia R Ionita <iulia@ionita.me>
2019-05-09 11:53:17 +0100

    Fixed tests

--- spec/controllers/supply_teachers/suppliers_controller_spec.rb
+++ spec/controllers/supply_teachers/suppliers_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe SupplyTeachers::SuppliersController, type: :controller, auth: tru
     before do
       allow(SupplyTeachers::Supplier)
         .to receive(:with_master_vendor_rates)
-        .and_return(suppliers)
+              .and_return(suppliers)
       get :master_vendors, params: {
         journey: 'supply-teachers',
         looking_for: 'managed_service_provider',
@@ -46,7 +46,7 @@ RSpec.describe SupplyTeachers::SuppliersController, type: :controller, auth: tru
     before do
       allow(SupplyTeachers::Supplier)
         .to receive(:with_neutral_vendor_rates)
-        .and_return(suppliers)
+              .and_return(suppliers)
       get :neutral_vendors, params: {
         journey: 'supply-teachers',
         looking_for: 'managed_service_provider',
@@ -74,8 +74,8 @@ RSpec.describe SupplyTeachers::SuppliersController, type: :controller, auth: tru
   end
 
   describe 'GET all suppliers' do
-    let(:branch) { create(:supply_teachers_branch) }
-    let(:branch1) { create(:supply_teachers_branch) }
+    let(:branch) { create(:supply_teachers_branch,  name: 'Branch A') }
+    let(:branch1) { create(:supply_teachers_branch, name: 'Branch B') }
 
     before do
       get :all_suppliers, params: {
