22c5a829c6afb7fe1b02956cab80c1c847914c10
milomia <m-hunt3@sky.com>
2019-05-14 15:19:29 +0100

    LS40 fix routing

--- app/models/legal_services/journey/choose_services_area2.rb
+++ app/models/legal_services/journey/choose_services_area2.rb
@@ -2,11 +2,11 @@ module LegalServices
   class Journey::ChooseServicesArea2
     include Steppable
 
-    attribute :services_area
-    validates :services_area, inclusion: ['yes', 'no']
+    attribute :services_area2
+    validates :services_area2, inclusion: ['yes', 'no', 'other']
 
     def next_step_class
-      case services_area
+      case services_area2
       when 'yes'
         Journey::Lot1RegionalService
       when 'no'
