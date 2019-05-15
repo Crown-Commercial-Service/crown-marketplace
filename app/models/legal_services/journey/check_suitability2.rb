0c82881b27524bcbdbb18c6b1ed2ee67425938b8
milomia <m-hunt3@sky.com>
2019-05-15 11:54:20 +0100

    LS-40 Routing changes

--- app/models/legal_services/journey/check_suitability2.rb
+++ app/models/legal_services/journey/check_suitability2.rb
@@ -8,7 +8,7 @@ module LegalServices
     def next_step_class
       case central_government
       when 'yes'
-        Journey::CheckSuitability3
+        Journey::Lot1RegionalService
       else
         Journey::Sorry
       end
