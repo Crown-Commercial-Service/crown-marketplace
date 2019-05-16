20268fd1852939db52314a64c1d06bf4f5bbd27e
milomia <m-hunt3@sky.com>
2019-05-16 11:21:36 +0100

    LS-40 bug fixes

--- app/models/legal_services/journey/regional_legal_service.rb
+++ app/models/legal_services/journey/regional_legal_service.rb
@@ -1,8 +1,8 @@
 module LegalServices
   class Journey::RegionalLegalService
     include Steppable
-    attribute :lot, Array
-    validates :lot, length: { minimum: 1 }
+    attribute :lot1_region, Array
+    validates :lot1_region, length: { minimum: 1 }
 
     def next_step_class
       Journey::SupplierResults
