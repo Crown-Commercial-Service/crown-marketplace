ea111101c71defb64adbcf72df7ba0578ab45afe
Iulia R Ionita <iulia@ionita.me>
2019-05-17 10:50:07 +0100

    carrierwave config fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage    = :aws
-    config.aws_bucket = ENV.fetch('CCS_APP_API_DATA_BUCKET')
+    config.aws_bucket = ENV['CCS_APP_API_DATA_BUCKET']
     config.aws_acl = 'public-read'
   end
 end
