a70390765abd91b8144f6d7eadf5026eb5485939
Iulia R Ionita <iulia@ionita.me>
2019-05-17 15:10:59 +0100

    Commented out some carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,15 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage    = :aws
     config.aws_bucket = ENV['CCS_APP_API_DATA_BUCKET']
-    config.aws_acl = 'public-read'
-
-    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
-
-    config.aws_attributes = -> {
-      {
-        expires: 1.year.from_now.httpdate,
-        cache_control: 'max-age=315576000'
-      }
-    }
+    # config.aws_acl = 'public-read'
+    #
+    # config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+    #
+    # config.aws_attributes = -> {
+    #   {
+    #     expires: 1.year.from_now.httpdate,
+    #     cache_control: 'max-age=315576000'
+    #   }
+    # }
   end
 end
