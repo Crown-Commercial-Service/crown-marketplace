c103114cb1c8733c322b35b6632807cb7e8a8882
Iulia R Ionita <iulia@ionita.me>
2019-05-17 12:20:53 +0100

    Added url expiration and increased cache control on carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,12 @@ if Rails.env.production?
     config.storage    = :aws
     config.aws_bucket = ENV['CCS_APP_API_DATA_BUCKET']
     config.aws_acl = 'public-read'
-    config.aws_attributes = { cache_control: 'max-age=604800' }
+
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+
+    config.aws_attributes = {
+      expires: 1.year.from_now.httpdate,
+      cache_control: 'max-age=31536000'
+    }
   end
 end
