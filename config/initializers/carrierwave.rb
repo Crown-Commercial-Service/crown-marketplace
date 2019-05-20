980db71fbe164450749592a790fe572d70195215
Iulia R Ionita <iulia@ionita.me>
2019-05-20 14:36:50 +0100

    Switched from fog to carrierwave-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,18 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      region: 'eu-west-2',
-      use_iam_profile: true
+    config.storage    = :aws
+    config.aws_bucket = ENV['CCS_APP_API_DATA_BUCKET']
+    config.aws_acl    = 'private'
+    # The maximum period for authenticated_urls is only 7 days.
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+    config.aws_attributes = -> { {
+      expires: 1.week.from_now.httpdate,
+      cache_control: 'max-age=#{365.days.to_i}'
+    } }
+
+    config.aws_credentials = {
+      region:            'eu-west-2',
+      stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
     }
-    config.fog_directory  = ENV['CCS_APP_API_DATA_BUCKET']
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
 end
