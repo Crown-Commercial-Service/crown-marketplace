7c63deeef36a65a11c6dfabb608314bdfeaaa190
Iulia R Ionita <iulia@ionita.me>
2019-05-20 13:10:04 +0100

    Switched back to fog-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = :fog
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       region: 'eu-west-2',
