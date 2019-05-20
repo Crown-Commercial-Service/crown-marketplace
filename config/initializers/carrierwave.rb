804dda44ad1099c6464492a1a07011698d8cc644
Iulia R Ionita <iulia@ionita.me>
2019-05-20 12:41:11 +0100

    Switched from fog-aws to fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,11 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
+    config.fog_provider = :fog
     config.fog_credentials = {
       provider: 'AWS',
       region: 'eu-west-2',
-      use_iam_profile: true,
-      aws_access_key_id: '',
-      aws_secret_access_key: ''
+      use_iam_profile: true
     }
-    config.fog_public = true
     config.fog_directory  = ENV['CCS_APP_API_DATA_BUCKET']
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
