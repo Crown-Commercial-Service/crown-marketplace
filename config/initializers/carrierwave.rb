7d3fe422919cec419ac665f9b18ab4f8f8fee33d
Iulia R Ionita <iulia@ionita.me>
2019-05-20 16:35:38 +0100

    Added aws_acl back to carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,5 +2,6 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage    = :aws
     config.aws_bucket = ENV['CCS_APP_API_DATA_BUCKET']
+    config.aws_acl    = 'private'
   end
 end
