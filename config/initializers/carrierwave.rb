dff1cde4f20aa3dbb7bf6c6511b36d1871398242
Iulia R Ionita <iulia@ionita.me>
2019-05-20 16:12:01 +0100

    Removed some of the carrierwave configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,5 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage    = :aws
     config.aws_bucket = ENV['CCS_APP_API_DATA_BUCKET']
-    config.aws_acl    = 'private'
-
-    config.aws_attributes = -> { {
-      expires: 1.year.from_now.httpdate,
-      cache_control: "max-age=#{365.days.to_i}"
-    } }
-
-    config.aws_credentials = {
-      region:            'eu-west-2',
-      stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
-    }
   end
 end
