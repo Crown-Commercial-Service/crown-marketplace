156bbbc6dbf04214ac21cc6ef3c8986770b6fa68
Darren Harridence <darren.harridence@btinternet.com>
2019-05-20 16:07:42 +0100

    msg journey update

--- app/models/legal_services/journey/lot2_full_service.rb
+++ app/models/legal_services/journey/lot2_full_service.rb
@@ -1,11 +1,17 @@
 module LegalServices
   class Journey::Lot2FullService
     include Steppable
-    attribute :lot2_full_service, Array
-    validates :lot2_full_service, length: { minimum: 1 }
-    include Steppable
+    attribute :lot2_full_service
+    validate :validate_lot2_full_service
+
     def next_step_class
       Journey::LegalJurisdiction
     end
+
+    private
+    def validate_lot2_full_service
+      errors.add(:lot2_full_service, :too_short) if lot2_full_service.nil?
+    end
+
   end
 end
