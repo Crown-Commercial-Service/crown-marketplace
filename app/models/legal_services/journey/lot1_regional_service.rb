e45b2da86cea293d3de8389f374e66708d5ff30a
milomia <m-hunt3@sky.com>
2019-05-22 12:10:50 +0100

    error message for lot1_regional_service in LS-85

--- app/models/legal_services/journey/lot1_regional_service.rb
+++ app/models/legal_services/journey/lot1_regional_service.rb
@@ -1,8 +1,16 @@
 module LegalServices
   class Journey::Lot1RegionalService
     include Steppable
+    attribute :lot1_regional_service
+    validate :validate_lot1_regional_service
+
     def next_step_class
       Journey::RegionalLegalService
+    end
+
+    private
+    def validate_lot1_regional_service
+      errors.add(:lot1_regional_service, :too_short) if lot1_regional_service.nil?
     end
   end
 end
