cd09738e00601d67f680fec7b57b698a18d963c0
Iulia R Ionita <iulia@ionita.me>
2019-05-23 16:58:35 +0100

    Changes to ST supplier data rake task: all tasks are now methods

--- lib/tasks/supply_teachers/scripts/exclude_suppliers_without_accreditation.rb
+++ lib/tasks/supply_teachers/scripts/exclude_suppliers_without_accreditation.rb
@@ -2,8 +2,11 @@
 
 require 'json'
 
-suppliers = JSON.parse($stdin.read)
-suppliers_with_accreditation = suppliers.select { |s| s['accreditation'] }
-# rubocop:disable Rails/Output
-puts JSON.pretty_generate(suppliers_with_accreditation)
-# rubocop:enable Rails/Output
+def exclude_suppliers_without_accreditation
+  suppliers = JSON.parse(File.read('./lib/tasks/supply_teachers/output/data_merged.json'))
+  suppliers_with_accreditation = suppliers.select { |s| s['accreditation'] }
+
+  File.open('./lib/tasks/supply_teachers/output/data_only_accredited.json.tmp', 'w') do |f|
+    f.puts JSON.pretty_generate(suppliers_with_accreditation)
+  end
+end
