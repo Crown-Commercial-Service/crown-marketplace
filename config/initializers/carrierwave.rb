414e8b2587a64dc2657650f54307d8db9d39f527
Iulia R Ionita <iulia@ionita.me>
2019-05-30 15:31:41 +0100

    Added cache control and expiration to carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,16 @@ if Rails.env.production?
     config.aws_bucket = ENV['CCS_APP_API_DATA_BUCKET']
     config.aws_acl    = 'private'
 
+    # The maximum period for authenticated_urls is only 7 days.
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+    # Set custom options such as cache control to leverage browser caching.
+    # You can use either a static Hash or a Proc.
+    config.aws_attributes = -> { {
+      expires: 1.year.from_now.httpdate,
+      cache_control: "max-age=#{365.days.to_i}"
+    } }
+
     config.aws_credentials = {
       region: ENV['COGNITO_AWS_REGION']
     }
