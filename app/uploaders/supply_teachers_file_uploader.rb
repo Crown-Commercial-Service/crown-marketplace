3c8b10b45a1046ad5bddccb5036f78236b575ebf
Iulia R Ionita <iulia@ionita.me>
2019-06-03 17:04:59 +0100

    Fixed tests

--- app/uploaders/supply_teachers_file_uploader.rb
+++ app/uploaders/supply_teachers_file_uploader.rb
@@ -17,6 +17,6 @@ class SupplyTeachersFileUploader < CarrierWave::Uploader::Base
   end
 
   def cache_dir
-    './storage/supply_teachers/tmp/uploads'
+    Rails.root.join('storage', 'supply_teachers', 'tmp', 'uploads')
   end
 end
