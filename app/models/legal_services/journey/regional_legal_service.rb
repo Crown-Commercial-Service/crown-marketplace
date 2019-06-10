a45fac27e6be519726f2b08502fac04d14ec5659
milomia <m-hunt3@sky.com>
2019-06-10 13:40:12 +0100

    feature/LS-101 rake changes

--- app/models/legal_services/journey/regional_legal_service.rb
+++ app/models/legal_services/journey/regional_legal_service.rb
@@ -1,9 +1,8 @@
 module LegalServices
   class Journey::RegionalLegalService
     include Steppable
-    byebug
     attribute :region1, Array
-    validates :region1, length: {minimum: 1}
+    validates :region1, length: { minimum: 1 }
 
     def next_step_class
       Journey::SupplierResults
