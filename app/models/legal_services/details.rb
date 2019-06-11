6dfa79fc3c5e51831d1130844ea8732f49f893db
milomia <m-hunt3@sky.com>
2019-06-11 16:06:43 +0100

    wip

--- app/models/legal_services/details.rb
+++ app/models/legal_services/details.rb
@@ -2,19 +2,29 @@ module LegalServices
   class Details
     include StaticRecord
 
-    attr_accessor :firm, :email, :phone, :address, :sme, :DUNS_number, :lot1_prospectus, :lot2_prospectus, :lo3_propsectus, :lot4_prospectuss, :about
+    header =  {
+                  :firm =>'firm',
+                  :email_address => 'Generic_Contact_Email_address',
+                  :phone_number => 'Generic_Contact_Phone_number',
+                  :website => 'Generic_Contact_Website_URL',
+                  :address => 'Generic_Contact_Postal_address',
+                  :SME => 'Is_an_SME',
+                  :DUNS_number => 'DUNS_Number',
+                  :lot1_prospectus => 'Lot_1_Prospectus_Link',
+                  :lot2_prospectus => 'Lot_2_Prospectus_Link',
+                  :lot3_prospectus => 'Lot_3_Prospectus_Link',
+                  :lot4_prospectus => 'Lot_4_Prospectus_Link',
+                  :about => 'About_the_supplier' 
+                }
+
+    attr_accessor header[:firm], header[:email_address], header[:phone_number], header[:website], header[:address], header[:SME], header[:DUNS_number], header[:lot1_prospectus], header[:lot2_prospectus], header[:lot3_prospectus], header[:lot4_prospectus], header[:about]
 
     def self.for_region(region, child_law, court_of_protection)
       where(region: region, child_law: child_law, court_of_protection: court_of_protection)
     end
 
-    def self.for_service(service)
-      case service
-      when 'child_law'
-        where(child_law: 'x')
-      when 'court_of_protection'
-        where(court_of_protection: 'x')
-      end
+    def self.for_sme(firm)
+        where(firm: firm)
     end
 
     def full_description
