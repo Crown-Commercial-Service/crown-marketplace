850e41028fc24f5627dc4e6b8768c57b1724822c
milomia <m-hunt3@sky.com>
2019-06-14 20:49:32 +0100

    rake changes

--- app/models/legal_services/journey/regional_legal_service.rb
+++ app/models/legal_services/journey/regional_legal_service.rb
@@ -3,9 +3,6 @@ module LegalServices
     include Steppable
     attribute :region1, Array
     validates :region1, length: { minimum: 1 }
-    
-    @regions = [['North East','NE'], ['North West','NW'],['Yorkshire and the Humber','YH'],['East Midlands','EM'], ['West Midlands','WM'],['East of England','EE'], ['Greater London','GL'], ['South East','SE'],['South West','SW'],['Wales','WA'],['Scotland','SC'],['Northern Ireland','NI']]
-
     def next_step_class
       Journey::Suppliers
     end
