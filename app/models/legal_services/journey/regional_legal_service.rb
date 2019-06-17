19fbe3af1e8ccd2d81d6779f7fd928238e38929b
milomia <m-hunt3@sky.com>
2019-06-17 15:51:21 +0100

    happy journey for service selection differs from unhappy

--- app/models/legal_services/journey/regional_legal_service.rb
+++ app/models/legal_services/journey/regional_legal_service.rb
@@ -1,7 +1,7 @@
 module LegalServices
   class Journey::RegionalLegalService
     include Steppable
-    attribute :region1, Array
+    attribute :region1,  Array
     validates :region1, length: { minimum: 1 }
     def next_step_class
       Journey::Suppliers
