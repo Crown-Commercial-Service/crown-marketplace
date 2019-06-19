e8b7788024a9a713d8d8d251904ae33fb0760084
milomia <m-hunt3@sky.com>
2019-06-19 11:48:57 +0100

    feature/LS-84 download link

--- app/models/legal_services/journey/regional_legal_service.rb
+++ app/models/legal_services/journey/regional_legal_service.rb
@@ -2,9 +2,15 @@ module LegalServices
   class Journey::RegionalLegalService
     include Steppable
     attribute :region_all, Array
-    validates :region_all, length: { minimum: 1 }
+    validate :validate_regional_legal_service
     def next_step_class
       Journey::Suppliers
+    end
+    private
+
+    def validate_regional_legal_service
+      byebug
+      errors.add(:region_all, :too_short) if region_all.nil?
     end
   end
 end
