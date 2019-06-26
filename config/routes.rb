9676c1957e6f4011cd427e1c8cb648cbd8fc07c9
Darren Harridence <darren.harridence@btinternet.com>
2019-06-26 16:32:50 +0100

    WIP password reset

--- config/routes.rb
+++ config/routes.rb
@@ -151,6 +151,7 @@ Rails.application.routes.draw do
     get '/cog-email', to: 'home#cog_email'
     get '/cog-register-domain-not-on-whitelist', to: 'home#cog_register_domain_not_on_whitelist'
     get '/cog-forgot-password-request', to: 'home#cog_forgot_password_request'
+    get '/cog-forgot-password-reset', to: 'home#cog_forgot_password_reset'
   end
 
   namespace 'legal_services', path: 'legal-services' do
