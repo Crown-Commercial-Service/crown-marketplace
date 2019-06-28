05c2449e5781ea601350d1f55f2ff3fa2656dfa1
Iulia R Ionita <iulia@ionita.me>
2019-06-28 17:01:18 +0100

    Login now works on all tools when password change is required. Also sidekiq now uses the current user to check for auth

---
+++ app/controllers/facilities_management/users_controller.rb
@@ -0,0 +1,17 @@
+module FacilitiesManagement
+  class UsersController < Base::UsersController
+    private
+
+    def new_challenge_path
+      legal_services_users_challenge_path(challenge_name: @challenge.new_challenge_name, session: @challenge.new_session, username: @challenge.cognito_uuid)
+    end
+
+    def fail_challenge_path
+      legal_services_users_challenge_path(challenge_name: params[:challenge_name], session: params[:session], username: params[:username])
+    end
+
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || legal_services_journey_start_path
+    end
+  end
+end
