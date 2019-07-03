1a1a04ab9124766c090c175b4ad3342c6de9729e
Chris Gannon <cjgannon@gmail.com>
2019-07-03 11:24:04 +0100

    Add supplier spreadsheet creator for MC

--- app/controllers/management_consultancy/suppliers_controller.rb
+++ app/controllers/management_consultancy/suppliers_controller.rb
@@ -1,7 +1,7 @@
 module ManagementConsultancy
   class SuppliersController < FrameworkController
     helper :telephone_number
-    before_action :fetch_suppliers, only: [:index]
+    before_action :fetch_suppliers, only: %i[index download]
 
     def index
       @journey = Journey.new(params[:slug], params)
@@ -14,7 +14,16 @@ module ManagementConsultancy
       @supplier = Supplier.find(params[:id])
     end
 
-    def download; end
+    def download
+      respond_to do |format|
+        format.html
+        format.xlsx do
+          spreadsheet_builder = ManagementConsultancy::SupplierSpreadsheetCreator.new(@all_suppliers, params)
+          spreadsheet = spreadsheet_builder.build
+          render xlsx: spreadsheet.to_stream.read, filename: 'shortlist.xlsx', format: 'application/vnd.openxmlformates-officedocument.spreadsheetml.sheet'
+        end
+      end
+    end
 
     private
 
