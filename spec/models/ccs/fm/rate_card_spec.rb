331208c4e67775060b3b7fef99240417a81642ff
Tariq Hamid <tariq@hamid.com>
2019-07-04 13:08:17 +0100

    add test

--- spec/models/ccs/fm/rate_card_spec.rb
+++ spec/models/ccs/fm/rate_card_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe CCS::FM::RateCard, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
   it 'can contain entries' do
-    expect(CCS::FM::RateCard.count).to be > -1
+    expect(CCS::FM::RateCard.count).to be > 0
+  end
+
+  it 'can retrieve latest entry' do
+    current = CCS::FM::RateCard.last
+    expect(current.data['Prices'].count).to be > 0
   end
 end
