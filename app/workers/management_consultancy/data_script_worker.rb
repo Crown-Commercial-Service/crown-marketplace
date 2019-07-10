6b98886a30322df39ecaddbacc7841f661557405
Chris Gannon <cjgannon@gmail.com>
2019-07-10 16:50:58 +0100

    Update MC data workers to use S3 on non-dev environments

--- app/workers/management_consultancy/data_script_worker.rb
+++ app/workers/management_consultancy/data_script_worker.rb
@@ -13,10 +13,10 @@ module ManagementConsultancy
       Rake::Task['mc:data'].invoke
 
       begin
-        if File.zero?(Rails.root.join('storage', 'management_consultancy', 'current_data', 'output', 'errors.out'))
+        if File.zero?(error_file_path)
           upload.review!
         else
-          file = File.open(Rails.root.join('storage', 'management_consultancy', 'current_data', 'output', 'errors.out'))
+          file = File.open(error_file_path)
           fail_upload(upload, 'There is an error with your files: ' + file.read)
         end
       rescue StandardError => e
@@ -29,6 +29,14 @@ module ManagementConsultancy
     def fail_upload(upload, fail_reason)
       upload.fail!
       upload.update(fail_reason: fail_reason)
+    end
+
+    def error_file_path
+      file_path = 'storage/management_consultancy/current_data/output/errors.out'
+      return file_path if Rails.env.development?
+
+      object = Aws::S3::Resource.new(region: ENV['COGNITO_AWS_REGION'])
+      object.bucket(ENV['CCS_APP_API_DATA_BUCKET']).object(s3_path(file_path))
     end
   end
 end
