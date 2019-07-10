597226fd602c776bc955f537006ee9412bf0f96f
Tariq Hamid <tariq@hamid.com>
2019-07-10 15:27:19 +0100

    refactor CalculatorDirectAward

---
+++ spec/calculators/fm_calculator/calculator_direct_award_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe FMCalculator::CalculatorDirectAward do
+  subject(:calculator) do
+    described_class.new
+  end
+
+  describe 'FM Direct Award CalculatorDirect'
+  it 'calculate' do
+    # p 'TTTT'
+    # described_class.test
+    calculator.test
+  end
+end
