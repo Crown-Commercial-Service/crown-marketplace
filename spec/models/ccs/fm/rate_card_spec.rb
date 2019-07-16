5694ff46190964f99ee4226194f72644c08ba00f
Tariq Hamid <tariq@hamid.com>
2019-07-16 12:17:06 +0100

    refactor report tests

--- spec/models/ccs/fm/rate_card_spec.rb
+++ spec/models/ccs/fm/rate_card_spec.rb
@@ -20,4 +20,23 @@ RSpec.describe CCS::FM::RateCard, type: :model do
     expect(is_this_the_last_dated_record.id).to eq(actual_last_record.first.id)
     expect(is_this_the_last_dated_record.updated_at).to eq(actual_last_record.first.updated_at)
   end
+
+  it 'contains rates for services' do
+    rate_card = CCS::FM::RateCard.last
+
+    # sheet_name == 'Prices'
+    # data['Prices'][rate_card['Supplier']][rate_card['Service Ref']] = rate_card
+    prices = rate_card.data['Prices'].keys.map { |k| rate_card.data['Prices'][k]['C.1'] }
+    expect(prices.count).to be > 0
+
+    # sheet_name == 'Discount'
+    # data['Discount'][rate_card['Supplier']][rate_card['Ref']] = rate_card
+    discount = rate_card.data['Discount'].keys.map { |k| rate_card.data['Discount'][k]['C.1'] }
+    expect(discount.count).to be > 0
+
+    # sheet_name == 'Variances'
+    # data['Variances'][rate_card['Supplier']] = rate_card
+    variances = rate_card.data['Variances'].keys.map { |k| rate_card.data['Discount'][k] }
+    expect(variances.count).to be > 0
+  end
 end
