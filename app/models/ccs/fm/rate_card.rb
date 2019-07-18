dbdb5f04a9f949446b334ab03d1c763ac6b84b60
Tariq Hamid <tariq@hamid.com>
2019-07-18 12:23:12 +0100

    fix test for latest RateCard

--- app/models/ccs/fm/rate_card.rb
+++ app/models/ccs/fm/rate_card.rb
@@ -2,6 +2,12 @@ module CCS
   module FM
     class RateCard < ApplicationRecord
       # Event.where("payload->>'kind' = ?", "user_renamed")
+
+      # rubocop:disable Rails/FindBy
+      def self.latest
+        where(updated_at: CCS::FM::RateCard.select('max(updated_at)')).first
+      end
+      # rubocop:enable Rails/FindBy
     end
   end
 end
