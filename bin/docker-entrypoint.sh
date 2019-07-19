cd55b447b7af9627b1ba1f107c4af646ed5bd8e8
Tariq Hamid <tariq@hamid.com>
2019-07-19 15:33:13 +0100

    do not attempt to load postcodes

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 bundle exec rails db:migrate
 
 bundle exec rails db:static
-bundle exec rails db:postcode
+# bundle exec rails db:postcode
 
 bundle exec sidekiq -C ./config/sidekiq.yml -d -L ./log/sidekiq.log -e production if ENV['APP_RUN_SIDEKIQ']
 
