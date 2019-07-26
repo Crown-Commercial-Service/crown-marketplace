950882f1e9e6723ef2a12d57709812352b87f124
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-07-26 08:12:25 +0100

    Rubocop errors fixed

--- db/migrate/20190724080329_fm_rates.rb
+++ db/migrate/20190724080329_fm_rates.rb
@@ -1,14 +1,14 @@
 class FmRates < ActiveRecord::Migration[5.2]
   def change
-    if !table_exists?(:fm_rates)
-      create_table 'fm_rates', id: false do |t|
-        t.text 'code', limit: 255
-        t.decimal 'framework'
-        t.decimal 'benchmark'
-        t.index ['code'], name: 'fm_rates_code_key', unique: true
-        t.datetime 'created_at', default: -> { "CURRENT_TIMESTAMP" }
-        t.datetime 'updated_at', default: -> { "CURRENT_TIMESTAMP" }
-      end
+    return if table_exists?(:fm_rates)
+
+    create_table 'fm_rates', id: false do |t|
+      t.text 'code', limit: 255
+      t.decimal 'framework'
+      t.decimal 'benchmark'
+      t.index ['code'], name: 'fm_rates_code_key', unique: true
+      t.datetime 'created_at', default: -> { 'CURRENT_TIMESTAMP' }
+      t.datetime 'updated_at', default: -> { 'CURRENT_TIMESTAMP' }
     end
   end
 end
