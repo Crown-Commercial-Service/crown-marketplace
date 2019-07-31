218f1991b4e2e221395639ac5a36bf1516f04b37
Iulia R Ionita <iulia@ionita.me>
2019-07-31 16:33:41 +0100

    Tests done

--- spec/services/cognito/confirm_sign_up_spec.rb
+++ spec/services/cognito/confirm_sign_up_spec.rb
@@ -1,11 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Cognito::ConfirmSignUp do
+  let(:email) { 'user@test.com' }
+  let(:user) { create(:user, email: email, confirmed_at: nil) }
+  let(:confirmation_code) { '123456' }
+  let(:aws_client) { instance_double(Aws::CognitoIdentityProvider::Client) }
+
+  describe '#validations' do
+    let(:response) { described_class.new(user.email, confirmation_code) }
+
+    before do
+      allow(Aws::CognitoIdentityProvider::Client).to receive(:new).and_return(aws_client)
+      allow(aws_client).to receive(:confirm_sign_up).and_return(JSON[{ user_sub: '1234'.to_json }])
+    end
+
+    context 'when confirmation_code shorter than 6 characters' do
+      let(:cofirmation_code) { '123' }
+
+      it 'is invalid' do
+        expect(response.valid?).to eq false
+      end
+    end
+
+    context 'when confirmation_code contains other characters than numbers' do
+      let(:confirmation_code) { 'invalid' }
+
+      it 'is invalid' do
+        expect(response.valid?).to eq false
+      end
+    end
+
+    context 'when confirmation_code is nil' do
+      let(:confirmaton_code) { '' }
+
+      it 'is invalid' do
+        expect(response.valid?).to eq false
+      end
+    end
+
+    context 'when email is nil' do
+      let(:email) { '' }
+
+      it 'is invalid' do
+        expect(response.valid?).to eq false
+      end
+    end
+  end
+
   describe '#call' do
-    let(:email) { 'user@test.com' }
-    let(:user) { create(:user, email: email, confirmed_at: nil) }
-    let(:confirmation_code) { '12345' }
-    let(:aws_client) { instance_double(Aws::CognitoIdentityProvider::Client) }
 
     context 'when success' do
       let(:response) { described_class.call(user.email, confirmation_code) }
