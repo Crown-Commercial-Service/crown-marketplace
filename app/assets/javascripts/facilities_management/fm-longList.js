8812ea5d50e8630f4548c84b247041de1ff508e1
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-08-08 12:11:02 +0100

    IE11 fixes

--- app/assets/javascripts/facilities_management/fm-longList.js
+++ app/assets/javascripts/facilities_management/fm-longList.js
@@ -1,11 +1,11 @@
-$(() => {
+$(function () {
 
     let selectedLocations = pageUtils.getCachedData('fm-locations');
     let selectedServices = pageUtils.getCachedData('fm-services');
     let supplierCount = parseInt($('#fm-long-list-supplier-count').innerText);
     let visibleSuppliers = [];
 
-    const init = (() => {
+    const init = (function () {
 
         const regionCheckBoxes = $('#fm-region-check-boxes');
         const serviceCheckBoxes = $('#fm-service-check-boxes');
@@ -13,17 +13,16 @@ $(() => {
         /* Load selected locations to the filters */
         selectedLocations = pageUtils.sortByName(selectedLocations);
 
-        for (let value of selectedLocations) {
-            //selectedLocations.forEach((value, index, array) => {
+        for (let x = 0; x < selectedLocations.length; x++) {
             let checkbox = '<div class="govuk-checkboxes__item">' +
                 '<input class="govuk-checkboxes__input" checked id="' + value.code + '" name="fm-regions-checkbox" type="checkbox" value="' + value.name + '">' +
                 '<label class="govuk-label govuk-checkboxes__label govuk-!-font-size-16 CCS-fm-supplier-filter-check-box-label"  for="' + value.code + '">' + value.name + '</label></div>';
 
             regionCheckBoxes.append(checkbox);
-            $('#' + value.code).on('click', (e) => {
+            $('#' + value.code).on('click', function (e) {
                 updateCounts();
                 if (!e.target.checked) {
-                    let filtered = selectedLocations.filter((obj, index, arr) => {
+                    let filtered = selectedLocations.filter(function (obj, index, arr) {
                         if (obj.code !== value.code) {
                             return true;
                         } else {
@@ -46,20 +45,21 @@ $(() => {
 
         }
 
-
         /* Load selected services to the filters */
         selectedServices = pageUtils.sortByName(selectedServices);
 
-        selectedServices.forEach((value, index, array) => {
+        for (let x = 0; x < selectedServices.length; x++) {
+            let value = selectedServices[x];
+
             let checkbox = '<div class="govuk-checkboxes__item">' +
                 '<input class="govuk-checkboxes__input" checked id="' + value.code + '" name="fm-services-checkbox" type="checkbox" value="' + value.name + '">' +
                 '<label class="govuk-label govuk-checkboxes__label govuk-!-font-size-16 CCS-fm-supplier-filter-check-box-label" for="' + value.code + '">' + value.name + '</label></div>';
 
             serviceCheckBoxes.append(checkbox);
-            $('#' + value.code).on('click', (e) => {
+            $('#' + value.code).on('click', function (e) {
                 updateCounts();
                 if (!e.target.checked) {
-                    let filtered = selectedServices.filter((service, index, arr) => {
+                    let filtered = selectedServices.filter(function (service, index, arr) {
                         if (service.code !== value.code) {
                             return true;
                         } else {
@@ -84,125 +84,133 @@ $(() => {
                 pageUtils.setCachedData('fm-services', selectedServices);
                 filterSuppliers();
             });
+
+
+            filterSuppliers();
+            updateCounts();
+
+        }
+        ;
+
+
+        const updateCounts = (function () {
+            let regionCount = $("input[name='fm-regions-checkbox']:checked").length;
+            let serviceCount = $("input[name='fm-services-checkbox']:checked").length;
+            $('#region-count').text(regionCount + " selected");
+            $('#service-count').text(serviceCount + " selected");
+            supplierCount = visibleSuppliers.length;
+            if (supplierCount >= 0) {
+                $('#fm-long-list-supplier-count').text(supplierCount);
+            }
+
         });
 
-        filterSuppliers();
-        updateCounts();
+        /* Click handler for the filter toggle button */
+        $('#filter-toggle-btn').on('click', function (e) {
+            e.preventDefault();
 
-    });
+            let filterPane = $('#fm-filter-pane');
+            let btn = $('#filter-toggle-btn');
+            let isHidden = filterPane.attr('hidden') ? false : true;
 
-    const updateCounts = (() => {
-        let regionCount = $("input[name='fm-regions-checkbox']:checked").length;
-        let serviceCount = $("input[name='fm-services-checkbox']:checked").length;
-        $('#region-count').text(regionCount + " selected");
-        $('#service-count').text(serviceCount + " selected");
-        supplierCount = visibleSuppliers.length;
-        if (supplierCount >= 0) {
-            $('#fm-long-list-supplier-count').text(supplierCount);
-        }
+            let longListSection = $("#CCS-fm-suppliers-long-list");
 
-    });
+            if (isHidden === true) {
+                filterPane.attr('hidden', true);
+                btn.text('Show filters');
+                longListSection.removeClass('govuk-grid-column-two-thirds')
+            } else {
+                filterPane.attr('hidden', false);
+                btn.text('Hide filters');
+                longListSection.addClass('govuk-grid-column-two-thirds')
+            }
 
-    /* Click handler for the filter toggle button */
-    $('#filter-toggle-btn').on('click', (e) => {
-        e.preventDefault();
-
-        let filterPane = $('#fm-filter-pane');
-        let btn = $('#filter-toggle-btn');
-        let isHidden = filterPane.attr('hidden') ? false : true;
-
-        let longListSection = $("#CCS-fm-suppliers-long-list");
-
-        if (isHidden === true) {
-            filterPane.attr('hidden', true);
-            btn.text('Show filters');
-            longListSection.removeClass('govuk-grid-column-two-thirds')
-        } else {
-            filterPane.attr('hidden', false);
-            btn.text('Hide filters');
-            longListSection.addClass('govuk-grid-column-two-thirds')
-        }
-
-    });
-
-    $('#fm-suppliers-long-list-clear-filters').on('click', (e) => {
-        e.preventDefault();
-
-        $('input[name="fm-regions-checkbox"]').prop("checked", false);
-        $('input[name="fm-services-checkbox"]').prop("checked", false);
-
-        let tableRows = $('tbody  > tr');
-        tableRows.each(function (rowIndex, row) {
-            let id = row.id;
-            $('#' + id).attr('hidden', true);
         });
 
-        visibleSuppliers = [];
-        updateCounts();
-    });
+        $('#fm-suppliers-long-list-clear-filters').on('click', function (e) {
+            e.preventDefault();
 
-    /* Click handler for Print button */
-    $('#FM-print-supplier-list').on('click', (e) => {
-        e.preventDefault();
-        window.print();
-    });
+            $('input[name="fm-regions-checkbox"]').prop("checked", false);
+            $('input[name="fm-services-checkbox"]').prop("checked", false);
+
+            let tableRows = $('tbody  > tr');
+            tableRows.each(function (rowIndex, row) {
+                let id = row.id;
+                $('#' + id).attr('hidden', true);
+            });
+
+            visibleSuppliers = [];
+            updateCounts();
+        });
+
+        /* Click handler for Print button */
+        $('#FM-print-supplier-list').on('click', function (e) {
+            e.preventDefault();
+            window.print();
+        });
 
 
-    const filterSuppliers = () => {
+        const filterSuppliers = function () {
 
-        let tableRows = $('tbody  > tr');
-        visibleSuppliers = [];
-        for (let row of tableRows) {
-            if (row.id) {
-                let id = '#' + row.id;
-                let name = $(id).attr('name');
-                let operationalAreas = $(id).attr('regioncode');
+            let tableRows = $('tbody  > tr');
+            visibleSuppliers = [];
 
-                if (operationalAreas) {
-                    operationalAreas = JSON.parse(operationalAreas);
-                    let serviceOfferings = JSON.parse($(id).attr('servicecode'));
+            for (let x = 0; x < tableRows.length; x++) {
+                let row = tableRows[x];
 
-                    let isServiceOfferingSelected = selectedServices.some(selectedService => {
-                        return serviceOfferings.includes(selectedService.code.replace('-', '.'));
-                    });
+                if (row.id) {
+                    let id = '#' + row.id;
+                    let name = $(id).attr('name');
+                    let operationalAreas = $(id).attr('regioncode');
 
-                    let isOperationalAreaSelected = selectedLocations.some(selectedLocation => {
-                        return operationalAreas.includes(selectedLocation.code);
-                    });
+                    if (operationalAreas) {
+                        operationalAreas = JSON.parse(operationalAreas);
+                        let serviceOfferings = JSON.parse($(id).attr('servicecode'));
 
-                    if (isServiceOfferingSelected === true && isOperationalAreaSelected === true) {
-                        $(id).attr('hidden', false);
-                        if (name && !visibleSuppliers.includes(name)) {
-                            visibleSuppliers.push(name);
+                        let isServiceOfferingSelected = selectedServices.some(function (selectedService) {
+                            return serviceOfferings.includes(selectedService.code.replace('-', '.'));
+                        });
+
+                        let isOperationalAreaSelected = selectedLocations.some(function (selectedLocation) {
+                            return operationalAreas.includes(selectedLocation.code);
+                        });
+
+                        if (isServiceOfferingSelected === true && isOperationalAreaSelected === true) {
+                            $(id).attr('hidden', false);
+                            if (name && !visibleSuppliers.includes(name)) {
+                                visibleSuppliers.push(name);
+                            }
+                        } else {
+                            $(id).attr('hidden', true);
                         }
-                    } else {
-                        $(id).attr('hidden', true);
                     }
                 }
             }
-        }
 
+            updateCounts();
+        };
+
+
+        $('#fm-suppliers-continue-button').on('click', function (e) {
+            e.preventDefault();
+            let regionCount = $("input[name='fm-regions-checkbox']:checked").length;
+            let serviceCount = $("input[name='fm-services-checkbox']:checked").length;
+
+            if (regionCount > 0 && serviceCount > 0) {
+                pageUtils.toggleInlineErrorMessage(false);
+                // location.href = '/facilities-management/standard-contract/questions';
+                $('#fm-long-list-form').submit()
+            } else {
+                pageUtils.toggleInlineErrorMessage(true);
+                $("html, body").animate({scrollTop: 0}, "1");
+                $('html, body').stop(true, true);
+            }
+
+        });
+
+        init();
         updateCounts();
-    };
 
-
-    $('#fm-suppliers-continue-button').on('click', (e) => {
-        e.preventDefault();
-        let regionCount = $("input[name='fm-regions-checkbox']:checked").length;
-        let serviceCount = $("input[name='fm-services-checkbox']:checked").length;
-
-        if (regionCount > 0 && serviceCount > 0) {
-            pageUtils.toggleInlineErrorMessage(false);
-            // location.href = '/facilities-management/standard-contract/questions';
-            $('#fm-long-list-form').submit()
-        } else {
-            pageUtils.toggleInlineErrorMessage(true);
-            $("html, body").animate({scrollTop: 0}, "1");
-            $('html, body').stop(true, true);
-        }
 
     });
-
-    init();
-    updateCounts();
 });
