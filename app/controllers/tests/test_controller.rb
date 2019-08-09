fd888ff61774312bdb33481204cb5f01a140f0d1
Tariq Hamid <tariq@hamid.com>
2019-08-09 17:13:31 +0100

    select all params

--- app/controllers/tests/test_controller.rb
+++ app/controllers/tests/test_controller.rb
@@ -1,45 +1,28 @@
 module Tests
-  class TestController < FrameworkController
-    skip_before_action :authenticate_user!
-    before_action :set_back_path, except: :index
+  class TestController  < ActionController::Base
+    ## skip_before_action :authenticate_user!
+    # skip_before_action :authorize_user
+    skip_before_action :verify_authenticity_token, :only => [:index]
+
     respond_to :html, :json
 
+
+
     def index
+      # params.permit!
+
       render layout: false
     end
-
-    def dynamic_accordian; end
-
-    def supplier_results_v1; end
-
-    def supplier_results_v2; end
-
-    def small_checkboxes; end
-
-    def titles_checkboxes; end
-
-    def numbered_pagination; end
-
-    def table_5050; end
-
-    def supplier_detail; end
-
-    def errors_find_apprentices; end
-
-    def errors_find_apprentices2; end
-
-    def errors_find_apprentices3; end
-
-    def errors_find_apprentices4; end
-
-    def cog_sign_in; end
-
-    def cog_sign_in_password_prompt_change; end
 
     private
 
     def set_back_path
       @back_path = :back
     end
+
+    # def post_params(*args)
+    #   params.require(:post).permit(*args)
+    # end
+
   end
 end
