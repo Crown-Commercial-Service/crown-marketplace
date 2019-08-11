a950b98b3e64ee602c73ca95ec07302f790633b5
Tariq Hamid <tariq@hamid.com>
2019-08-11 22:03:10 +0100

    create test calculation

--- app/controllers/tests/test_controller.rb
+++ app/controllers/tests/test_controller.rb
@@ -9,6 +9,8 @@ module Tests
     def index
       # params.permit!
 
+      calculate params
+
       render layout: false
     end
 
@@ -16,6 +18,55 @@ module Tests
 
     def set_back_path
       @back_path = :back
+    end
+
+    def calculate(vals)
+      id = SecureRandom.uuid
+
+      b =
+        {
+          'id' => id,
+          'gia' => vals['gia'],
+          'name' => 'ccs',
+          'region' => 'London',
+          'address' =>
+            {
+              'fm-address-town' => 'London',
+              'fm-address-line-1' => '151 Buckingham Palace Road',
+              'fm-address-postcode' => 'SW1W 9SZ'
+            },
+          'isLondon' => 'No',
+          'services' =>
+            [
+              { 'code' => 'G-1', 'name' => 'Airport and aerodrome maintenance services' },
+              { 'code' => 'M-1', 'name' => 'CAFM system' },
+              # { 'code' => 'N-1', 'name' => 'Helpdesk services' },
+              { 'code' => 'O-1', 'name' => 'Management of billable works' }
+            ],
+          'fm-building-type' => 'General office - Customer Facing'
+        }
+
+      all_buildings =
+        [
+          OpenStruct.new(building_json: b)
+        ]
+      uom_vals =
+        [
+          {
+            'user_id' => 'dGVzdEBleGFtcGxlLmNvbQ==\n',
+            'service_code' => 'G.1',
+            'uom_value' => '125',
+            'building_id' => id,
+            'title_text' => "What's the number of building users (occupants) in this building?",
+            'example_text' => "For example, 56. What's the maximum capacity of this building."
+          }
+        ]
+      # data
+      dummy_supplier_name = 'Hickle-Schinner'
+
+      report = FacilitiesManagement::SummaryReport.new(start_date, 'test@example.com', data2)
+      # prices = rate_card.data['Prices'].keys.map { |k| rate_card.data['Prices'][k]['C.1'] }
+      report.calculate_services_for_buildings all_buildings, uom_vals, rates, rate_card, dummy_supplier_name
     end
   end
 end
