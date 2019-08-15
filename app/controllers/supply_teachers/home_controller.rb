d60b69cedb822df9ce5d4e1cb505e3e4831e68a6
Iulia R Ionita <iulia@ionita.me>
2019-08-15 11:41:22 +0100

    Fixed-term-fee casting to float rather than integer

--- app/controllers/supply_teachers/home_controller.rb
+++ app/controllers/supply_teachers/home_controller.rb
@@ -31,7 +31,7 @@ module SupplyTeachers
       @back_path = journey.previous_step_path
 
       @calculator = FTAToPermCalculator::Calculator.new(
-        fixed_term_contract_fee: @previous_step.try(:fixed_term_fee).try(:to_i),
+        fixed_term_contract_fee: @previous_step.try(:fixed_term_fee).try(:to_f),
         current_contract_length: @previous_step.try(:current_contract_length).try(:to_f),
       )
     end
