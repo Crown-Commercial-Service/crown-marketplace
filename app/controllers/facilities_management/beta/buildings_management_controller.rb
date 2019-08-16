5f390730586fe6911f35a3eac4864d5b68c7eb39
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-08-16 14:03:15 +0100

    Addedd more routing for buildings management

--- app/controllers/facilities_management/beta/buildings_management_controller.rb
+++ app/controllers/facilities_management/beta/buildings_management_controller.rb
@@ -4,6 +4,31 @@ module FacilitiesManagement
     before_action :authorize_user, only: %i[buildings_management].freeze
 
     def buildings_management
+      @error_msg = ''
+    end
+
+    def building
+      @error_msg = ''
+    end
+
+    def building_type
+      @error_msg = ''
+    end
+
+    def building_gross_internal_area
+      @error_msg = ''
+    end
+
+    def building_details_summary
+      @error_msg = ''
+    end
+
+    def building_address
+      @error_msg = ''
+    end
+
+    def building_security_type
+      @error_msg = ''
     end
   end
 end
