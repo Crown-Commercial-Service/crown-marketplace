949c6cfba0a81119ad98a0fa055f47882defeebe
Tariq Hamid <tariq@hamid.com>
2019-08-19 17:21:03 +0100

    do a query via code

--- app/models/ccs/fm/supplier.rb
+++ app/models/ccs/fm/supplier.rb
@@ -8,9 +8,11 @@ module CCS
 
     class Supplier < ApplicationRecord
       # usage:
+      # CCS::FM::Supplier.where("data->'supplier_name' = 'Shields, Ratke and Parisian'")
       # CCS::FM::Supplier.supplier_name('Shields, Ratke and Parisian')
       def self.supplier_name(name)
-        select { |s| s['data']['supplier_name'] == name }.first
+        # p "data->'supplier_name' = '#{name}'"
+        where("data->>'supplier_name' = '#{name}'")
       end
 
       # usage:
@@ -22,40 +24,15 @@ module CCS
       end
 
       def self.selected_suppliers(for_lot, for_regions, for_services)
-        CCS::FM::Supplier.all.select do |s|
+        suppliers = CCS::FM::Supplier.all.select do |s|
           s.data['lots'].find do |l|
-            # ([2, 6, 13, 99, 27] & [2, 6]).any?
-            (for_regions - l['regions']).empty? && (for_services - l['services']).empty? if l['lot_number'] == for_lot
+            (l['lot_number'] == for_lot) &&
+              (for_regions & l['regions']).any? &&
+              (for_services & l['services']).any?
           end
         end
       end
 
-      # usage:
-      # CCS::FM::Supplier.long_list_suppliers_lot(["UKM21", "UKC1"], ["C.1", "L.1"], "1a")
-      def self.long_list_suppliers_lot(locations, services, for_lot)
-        vals = selected_suppliers(for_lot, locations, services)
-
-        result =
-          vals.map do |s|
-            selected_lot = s.data['lots'].select do |x|
-              x['lot_number'] = for_lot
-            end
-            first_selected_lot = selected_lot&.first
-            { 'name' => s.data['supplier_name'],
-              'service_code' => first_selected_lot['services'],
-              'region_code' => first_selected_lot['regions'] }
-          end
-        result
-      end
-
-      #  (@suppliers_lot1a.map { |s| s['name'] } << @suppliers_lot1b.map { |s| s['name'] } << @suppliers_lot1c.map { |s| s['name'] }).flatten.uniq.count
-      def self.supplier_count(locations, services)
-        vals_1a = selected_suppliers('1a', locations, services)
-        vals_1b = selected_suppliers('1b', locations, services)
-        vals_1c = selected_suppliers('1c', locations, services)
-
-        (vals_1a + vals_1b + vals_1c).uniq.count
-      end
     end
   end
 end
