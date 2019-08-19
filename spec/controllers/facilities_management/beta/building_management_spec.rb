285a404dc26632999c6c9cd4813e89b419270a21
Julian Young <julianyoung@outlook.com>
2019-08-19 17:36:46 +0100

    Added RSpec

---
+++ spec/controllers/facilities_management/beta/building_management_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+
+describe 'MyBehavior' do
+  before do # Do nothing
+  end
+
+  after do # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      pending 'Not implemented'
+    end
+  end
+end
