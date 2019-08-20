eafe0d3a1226c900a5b450a2d5c9c25d3f42d212
Julian Young <julianyoung@outlook.com>
2019-08-20 12:36:01 +0100

    Resolved test failures

--- spec/controllers/facilities_management/beta/buildings_management_controller_spec.rb
+++ spec/controllers/facilities_management/beta/buildings_management_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe FacilitiesManagement::Beta::BuildingsManagementController do
-  describe 'GET index' do
+  describe 'GET #{buildings_management}' do
     it 'returns http success' do
-      get :index
+      get :buildings_management
       expect(response).to have_http_status(:found)
     end
   end
