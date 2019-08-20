7ef8dc30b28599765bebdec1219c1b96f5529e9f
Julian Young <julianyoung@outlook.com>
2019-08-20 12:44:23 +0100

    Remove spec issue

--- spec/controllers/facilities_management/beta/buildings_management_controller_spec.rb
+++ spec/controllers/facilities_management/beta/buildings_management_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe FacilitiesManagement::Beta::BuildingsManagementController do
-  describe 'GET #{buildings_management}' do
+  describe 'GET #buildings_management' do
     it 'returns http success' do
       get :buildings_management
       expect(response).to have_http_status(:found)
