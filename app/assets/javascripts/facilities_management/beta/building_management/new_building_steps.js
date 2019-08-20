20d9dff1e1785488101ba5d077ac4cbdf6552205
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-08-20 14:55:22 +0100

    Save building initial commit

---
+++ app/assets/javascripts/facilities_management/beta/building_management/new_building_steps.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    let step = $('#fm-manage-building-step').val();
+
+
+    if (step === 1) {
+
+    }
+
+
+    if (step === 2) {
+
+    }
+
+    if (step === 3) {
+
+    }
+
+    if (step === 4) {
+
+    }
+});
