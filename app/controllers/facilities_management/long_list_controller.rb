8536b091c611757fbe5f2f3e275a1209adc232c6
Tariq Hamid <tariq@hamid.com>
2019-08-20 13:34:41 +0100

    filter suppliers and services correctly

--- app/controllers/facilities_management/long_list_controller.rb
+++ app/controllers/facilities_management/long_list_controller.rb
@@ -9,6 +9,7 @@ class FacilitiesManagement::LongListController < ApplicationController
   before_action :authenticate_user!, only: :long_list
   before_action :authorize_user, only: :long_list
 
+  # rubocop:disable Metrics/AbcSize
   def long_list
     @select_fm_locations = '/facilities-management/select-locations'
     @select_fm_services = '/facilities-management/select-services'
@@ -20,25 +21,30 @@ class FacilitiesManagement::LongListController < ApplicationController
     @locations = '(' + @posted_locations.map { |x| "'\"#{x}\"'" }.join(',') + ')'
     @services = '(' + @posted_services.map { |x| "'\"#{x}\"'" }.join(',') + ')'
 
-    @suppliers_lot1a = CCS::FM::Supplier.long_list_suppliers_lot(@posted_locations, @posted_services, '1a')
-    @suppliers_lot1b = CCS::FM::Supplier.long_list_suppliers_lot(@posted_locations, @posted_services, '1b')
-    @suppliers_lot1c = CCS::FM::Supplier.long_list_suppliers_lot(@posted_locations, @posted_services, '1c')
+    # fm_supplier_data = FMSupplierData.new
+    # @supplier_count = fm_supplier_data.long_list_supplier_count(@locations, @services)
+    # @suppliers_lot1a = fm_supplier_data.long_list_suppliers_lot(@locations, @services, '1a')
+    # @suppliers_lot1b = fm_supplier_data.long_list_suppliers_lot(@locations, @services, '1b')
+    # @suppliers_lot1c = fm_supplier_data.long_list_suppliers_lot(@locations, @services, '1c')
 
-    @supplier_count = CCS::FM::Supplier.supplier_count(@posted_locations, @posted_services)
+    @suppliers_lot1a  = CCS::FM::Supplier.long_list_suppliers_lot(@posted_locations, @posted_services, '1a')
+    @suppliers_lot1b  = CCS::FM::Supplier.long_list_suppliers_lot(@posted_locations, @posted_services, '1b')
+    @suppliers_lot1c  = CCS::FM::Supplier.long_list_suppliers_lot(@posted_locations, @posted_services, '1c')
+    @supplier_count = (@suppliers_lot1a.map { |s| s['name'] } << @suppliers_lot1b.map { |s| s['name'] } << @suppliers_lot1c.map { |s| s['name'] }).flatten.uniq.count
 
     set_current_choices
   end
+  # rubocop:enable Metrics/AbcSize
 
   private
 
   def set_current_choices
     TransientSessionInfo[session.id] = nil
+    # TransientSessionInfo[session.id, 'supplier_count'] = @supplier_count
     TransientSessionInfo[session.id, 'posted_locations'] = @posted_locations
     TransientSessionInfo[session.id, 'posted_services'] = @posted_services
     TransientSessionInfo[session.id, 'locations'] = @locations
     TransientSessionInfo[session.id, 'services'] = @services
-
-    TransientSessionInfo[session.id, 'env'] = request.query_parameters['env']
   end
 
   protected
--- spec/models/ccs/fm/supplier_spec.rb
+++ spec/models/ccs/fm/supplier_spec.rb
@@ -1,27 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CCS::FM::Supplier, type: :model do
-
-
-  # SELECT distinct data->>'supplier_name' as "name", lots->>'services' as  "service_code",
-  # lots->>'regions' as "region_code" from fm_suppliers,
-  # jsonb_array_elements(fm_suppliers.data -> 'lots') lots,
-  # jsonb_array_elements(lots -> 'regions') regions,
-  # jsonb_array_elements(lots->'services') services  
-  # where regions in ('"UKC1"','"UKM21"') 
-  # and services in ('"C.1"') 
-  # and  lots -> 'lot_number' in ( '"1a"' )
-  # group by name, service_code, region_code order by data->>'supplier_name'
   it 'can select suppliers for a given lot, regions and services' do
-
     for_lot = '1a'
-    for_regions = []
-    for_services = []
-    vals = CCS::FM::Supplier.selected_suppliers(for_lot, ['UKC1','UKM21'], ['C.1'])
-
-    p vals.count
+    for_regions = ['UKC1', 'UKM21']
+    for_services = ['C.1', 'L.1']
+    vals = CCS::FM::Supplier.selected_suppliers(for_lot, for_regions, for_services)
 
     expect(vals.count).to be > 0
   end
-
 end
