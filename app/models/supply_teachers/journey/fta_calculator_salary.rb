6d5eb51b8d88e77df78f7c4c37508f32a68e1b9d
Iulia R Ionita <iulia@ionita.me>
2019-08-21 15:33:56 +0100

    Added missing validations

--- app/models/supply_teachers/journey/fta_calculator_salary.rb
+++ app/models/supply_teachers/journey/fta_calculator_salary.rb
@@ -4,9 +4,16 @@ module SupplyTeachers
 
     attribute :salary
     validates :salary, presence: true
+    validate :valid_salary
 
     def next_step_class
       Journey::SchoolPostcodeAgencySuppliedWorker
+    end
+
+    private
+
+    def valid_salary
+      errors.add(:salary, :invalid) if salary.to_f <= 0
     end
   end
 end
