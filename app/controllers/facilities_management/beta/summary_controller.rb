0513c986022e5e54e71b7333ff5b9f65653c6143
Tariq Hamid <tariq@hamid.com>
2019-08-26 18:07:01 +0100

    direct award guidance page with lorem ipsum

--- app/controllers/facilities_management/beta/summary_controller.rb
+++ app/controllers/facilities_management/beta/summary_controller.rb
@@ -1,7 +1,11 @@
 module FacilitiesManagement
   class Beta::SummaryController < FacilitiesManagement::HomeController
     def guidance
-      render plain: 'guidance test'
+      # render plain: 'guidance test'
+    end
+
+    def suppliers
+      render plain: 'shortlisted DA suppliers'
     end
   end
 end
