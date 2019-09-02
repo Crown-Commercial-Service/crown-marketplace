e32d70071ff6c4be0eee3f7668d4afc9d5ba33c9
Chris Gannon <cjgannon@gmail.com>
2019-09-02 12:34:18 +0100

    Change LS datascript worker to use file urls

--- lib/tasks/legal_services/scripts/add_suppliers.rb
+++ lib/tasks/legal_services/scripts/add_suppliers.rb
@@ -3,7 +3,7 @@ require 'json'
 
 def add_suppliers(upload_id)
   upload = LegalServices::Admin::Upload.find(upload_id)
-  suppliers_workbook = Roo::Spreadsheet.open upload.suppliers.file
+  suppliers_workbook = Roo::Spreadsheet.open upload.suppliers.url
 
   headers = {
     name: 'Supplier Name',
