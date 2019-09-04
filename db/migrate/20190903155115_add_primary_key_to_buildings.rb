d3061f8ce12f983ff644d6a3b999ab2ae353516d
Julian Young <julianyoung@outlook.com>
2019-09-04 13:18:00 +0100

    Responses to Rubocop

--- db/migrate/20190903155115_add_primary_key_to_buildings.rb
+++ db/migrate/20190903155115_add_primary_key_to_buildings.rb
@@ -1,5 +1,9 @@
 class AddPrimaryKeyToBuildings < ActiveRecord::Migration[5.2]
-  def change
-    execute "ALTER TABLE public.facilities_management_buildings ADD CONSTRAINT facilities_management_buildings_pk PRIMARY KEY (id);"
+  def up
+    execute 'ALTER TABLE public.facilities_management_buildings ADD CONSTRAINT facilities_management_buildings_pk PRIMARY KEY (id);'
+  end
+
+  def down
+    execute 'ALTER TABLE public.facilities_management_buildings DROP CONSTRAINT facilities_management_buildings_pk;'
   end
 end
