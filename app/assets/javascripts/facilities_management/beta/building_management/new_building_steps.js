a229e995ba74cfa838818c78f9931fd7720f147e
Julian Young <julianyoung@outlook.com>
2019-09-04 15:53:21 +0100

    Correction of JSON data being sent to the server Addition of building status checks.

--- app/assets/javascripts/facilities_management/beta/building_management/new_building_steps.js
+++ app/assets/javascripts/facilities_management/beta/building_management/new_building_steps.js
@@ -40,7 +40,7 @@ $(function () {
                 dataType: 'json',
                 type: 'post',
                 contentType: 'application/json',
-                data: JSON.stringify(giaValue),
+                data: giaValue,
                 processData: false,
                 success: function (data, textStatus, jQxhr) {
                     location.href = redirectURI
@@ -66,7 +66,7 @@ $(function () {
                 dataType: 'json',
                 type: 'post',
                 contentType: 'application/json',
-                data: JSON.stringify(radioValue),
+                data: radioValue,
                 processData: false,
                 success: function (data, textStatus, jQxhr) {
                     location.href = redirectURI
