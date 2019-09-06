35ec4dbe8f1fee614de85e7500866516ed1a5cd1
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-09-06 13:36:44 +0100

    Character counter and hide other selection

--- app/assets/javascripts/facilities_management/beta/building_management/building_security_type.js
+++ app/assets/javascripts/facilities_management/beta/building_management/building_security_type.js
@@ -1,13 +1,29 @@
 $(function () {
+
     $("input:radio[name=fm-building-security-type-radio]").on('click', function (e) {
         $('#inline-error-message').addClass('govuk-visually-hidden');
+        if (e.target.id !== 'fm-building-security-type-other') {
+            $('#fm-bm-sec-other-container').addClass('govuk-visually-hidden');
+        }
     });
+
+    $('#fm-building-security-type-other').on('click', function (e) {
+
+        if (e.target.checked) {
+            $('#fm-bm-sec-other-container').removeClass('govuk-visually-hidden');
+        }
+
+    });
+
     $('#fm-building-security-type-more-detail').on('keyup', function (e) {
         let value = e.target.value;
         $('#fm-building-security-type-other').val(value);
+
+        let charsLeft = FM.calcCharsLeft(value, 150);
+        $('#fm-bm-bs-char-count').text('You have ' + charsLeft + ' characters remaining');
     });
 
-    $('#fm-bm-security-type-footer #fm-bm-cancel-and-return').on('click', function(e){
+    $('#fm-bm-security-type-footer #fm-bm-cancel-and-return').on('click', function (e) {
         $('#fm-bm-cancel-and-return-form').submit();
     });
 
