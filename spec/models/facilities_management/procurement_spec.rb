ed65286ead26c47492e20bd565e77fd3c317f5db
Chris Gannon <cjgannon@gmail.com>
2019-09-12 14:10:02 +0100

    Restore schema and correct invalid tests of FM Procurement model

--- spec/models/facilities_management/procurement_spec.rb
+++ spec/models/facilities_management/procurement_spec.rb
@@ -1,27 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe FacilitiesManagement::Procurement, type: :model do
-  let(:user) { build(:user) }
   subject(:procurement) { build(:facilities_management_procurement, user: user) }
+
+  let(:user) { build(:user) }
 
   it { is_expected.to be_valid }
 
   describe '#name' do
     context 'when the name is more than 100 characters' do
-      it 'should not be valid' do
+      it 'is expected to not be valid' do
         procurement.name = (0...101).map { ('a'..'z').to_a[rand(26)] }.join
 
-        expect(subject).to_not be_valid
+        expect(procurement).not_to be_valid
       end
     end
 
     context 'when the name is not unique' do
-      let(:second_procurement) {  build(:facilities_management_procurement, name: subject.name, user: user) }
+      let(:second_procurement) {  build(:facilities_management_procurement, name: procurement.name, user: user) }
 
-      it 'should not be valid' do
-        subject.save
+      it 'expected to not be valid' do
+        procurement.save
 
-        expect(second_procurement).to_not be_valid
+        expect(second_procurement).not_to be_valid
       end
     end
   end
