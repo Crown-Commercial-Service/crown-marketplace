4742547f179c23aae520864a8051536888c69f1a
Tariq Hamid <tariq@hamid.com>
2019-09-16 15:30:10 +0100

    remove javascript href links

--- app/assets/javascripts/facilities_management/fm-buyer-account.js
+++ app/assets/javascripts/facilities_management/fm-buyer-account.js
@@ -3,10 +3,6 @@ $(function () {
         location.href = 'select-services?env=public-beta'
     });
 
-    $('#fm-manage-procurements-link').on('click', function (e) {
-        location.href = 'beta/procurements?env=public-beta'
-    });
-
     $('#fm-manage-buildings-link').on('click', function (e) {
         location.href = 'buildings-list'
     });
