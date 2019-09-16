dac45bf34c08d48612766a3361fcde189d65a110
Julian Young <julianyoung@outlook.com>
2019-09-16 16:46:25 +0100

    Fixing fussy rubocop errors

--- db/migrate/20190724080344_change_timestamp_defaults_fm_regions.rb
+++ db/migrate/20190724080344_change_timestamp_defaults_fm_regions.rb
@@ -3,7 +3,7 @@ class ChangeTimestampDefaultsFmRegions < ActiveRecord::Migration[5.2]
     change_column_null :fm_regions, :created_at, true
     change_column_null :fm_regions, :updated_at, true
 
-    change_column_default :fm_regions, :created_at, 'now()'
-    change_column_default :fm_regions, :updated_at, 'now()'
+    change_column_default :fm_regions, :created_at, from: '', to: 'now()'
+    change_column_default :fm_regions, :updated_at, from: '', to: 'now()'
   end
 end
