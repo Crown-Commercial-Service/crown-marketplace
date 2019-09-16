09f89ff7bbb4b57927d76c5fff2f3b0e47ea6cea
Julian Young <julianyoung@outlook.com>
2019-09-16 16:46:25 +0100

    Fixing fussy rubocop errors

--- db/migrate/20190724080343_fm_regions.rb
+++ db/migrate/20190724080343_fm_regions.rb
@@ -2,7 +2,6 @@ class FmRegions < ActiveRecord::Migration[5.2]
   def change
     return if table_exists?('fm_regions')
 
-
     create_table :fm_regions, id: false do |t|
       t.text 'code', limit: 255
       t.text 'name', limit: 255
@@ -10,7 +9,7 @@ class FmRegions < ActiveRecord::Migration[5.2]
       t.index ['code'], name: 'fm_regions_code_key', unique: true
     end
 
-    change_column_default :fm_regions, :created_at, 'now()'
-    change_column_default :fm_regions, :updated_at, 'now()'
+    change_column_default :fm_regions, :created_at, from: '', to: 'now()'
+    change_column_default :fm_regions, :updated_at, from: '', to: 'now()'
   end
 end
