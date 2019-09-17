76d0de448c2993b2fae3b7993a54b06dbb95bf8c
Julian Young <julianyoung@outlook.com>
2019-09-17 12:22:21 +0100

    Added migration to drop_london postcodes  - a redundant table

--- db/migrate/20190917111950_drop_table_london_postcodes.rb
+++ db/migrate/20190917111950_drop_table_london_postcodes.rb
@@ -3,7 +3,6 @@ class DropTableLondonPostcodes < ActiveRecord::Migration[5.2]
     drop_table :london_postcodes
   end
 
-  # rubocop:disable Rails/CreateTableWithTimestamps
   def down
     create_table :london_postcodes, id: false, force: :cascade do |t|
       t.text :postcode
@@ -12,5 +11,4 @@ class DropTableLondonPostcodes < ActiveRecord::Migration[5.2]
       t.text 'Last updated'
     end
   end
-  # rubocop:enable Rails/CreateTableWithTimestamps
 end
