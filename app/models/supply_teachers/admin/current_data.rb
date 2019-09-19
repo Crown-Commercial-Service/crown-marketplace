d11c39a2497a7802d85154338f67956e3e6f874b
Iulia R Ionita <iulia@ionita.me>
2019-09-19 15:58:50 +0100

    Added validation to only allow one CurrentData

--- app/models/supply_teachers/admin/current_data.rb
+++ app/models/supply_teachers/admin/current_data.rb
@@ -3,7 +3,7 @@ module SupplyTeachers
     class CurrentData < ApplicationRecord
       self.table_name = 'supply_teachers_admin_current_data'
 
-      ATTRIBUTES = %i[current_accredited_suppliers geographical_data_all_suppliers lot_1_and_lot_2_comparisons master_vendor_contacts neutral_vendor_contacts pricing_for_tool supplier_lookup].freeze
+      validate :only_one_record, on: :create
 
       # input files
       mount_uploader :current_accredited_suppliers, SupplyTeachersFileUploader
@@ -16,6 +16,12 @@ module SupplyTeachers
 
       # output file
       mount_uploader :data, SupplyTeachersJsonFileUploader
+
+      private
+
+      def only_one_record
+        errors.add(:base, 'You can only have one Current Data.') if CurrentData.all.count >= 1
+      end
     end
   end
 end
