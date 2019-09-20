09e23b4e84539b982560cdfc6bcea5caca6f22b4
Man From Mons <julianyoung@outlook.com>
2019-09-20 15:49:10 +0100

    Addition of HTML layouts and templates and CSS. No javascript applied to this new layout No movement of other templates into the beta path Next commit will refactor javascript, existing templates into beta and any new required templates

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,14 @@
 ActiveRecord::Schema.define(version: 2019_09_19_101129) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "hstore"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "postgis"
 
   create_table "facilities_management_buildings", id: :uuid, default: nil, force: :cascade do |t|
-    t.string "user_id", null: false
+    t.text "user_id", null: false
     t.jsonb "building_json", null: false
+    t.datetime "created_at", default: -> { "now()" }
     t.datetime "updated_at", null: false
     t.string "status", default: "Incomplete", null: false
     t.string "updated_by", null: false
@@ -78,27 +78,22 @@ ActiveRecord::Schema.define(version: 2019_09_19_101129) do
   end
 
   create_table "fm_cache", id: false, force: :cascade do |t|
-    t.string "user_id", null: false
-    t.string "key", null: false
-    t.string "value"
+    t.text "user_id", null: false
+    t.text "key", null: false
+    t.text "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id", "key"], name: "fm_cache_user_id_idx"
   end
 
   create_table "fm_lifts", id: false, force: :cascade do |t|
-    t.string "user_id", null: false
-    t.string "building_id", null: false
+    t.text "user_id", null: false
+    t.text "building_id", null: false
     t.jsonb "lift_data", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index "((lift_data -> 'floor-data'::text))", name: "fm_lifts_lift_json", using: :gin
     t.index ["user_id", "building_id"], name: "fm_lifts_user_id_idx"
-  end
-
-  create_table "fm_procurements", id: false, force: :cascade do |t|
-    t.string "user_id", null: false
-    t.datetime "date_created", null: false
-    t.datetime "last_updated", null: false
-    t.jsonb "procurement"
-    t.string "updated_by", null: false
-    t.index ["user_id"], name: "fm_procurements_user_id_idx"
   end
 
   create_table "fm_rate_cards", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -111,24 +106,29 @@ ActiveRecord::Schema.define(version: 2019_09_19_101129) do
   end
 
   create_table "fm_rates", id: false, force: :cascade do |t|
-    t.string "code", limit: 255
+    t.text "code"
     t.decimal "framework"
     t.decimal "benchmark"
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }
     t.index ["code"], name: "fm_rates_code_key", unique: true
   end
 
   create_table "fm_regions", id: false, force: :cascade do |t|
-    t.string "code", limit: 255
-    t.string "name", limit: 255
+    t.text "code"
+    t.text "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["code"], name: "fm_regions_code_key", unique: true
   end
 
-  create_table "fm_security_types", id: false, force: :cascade do |t|
-    t.uuid "id", default: -> { "gen_random_uuid()" }, null: false
-    t.string "title", null: false
-    t.string "description"
-    t.integer "sort_order", null: false
-    t.index ["id"], name: "fm_security_types_id_idx"
+  create_table "fm_security_types", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "title", null: false
+    t.text "description"
+    t.integer "sort_order"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["id"], name: "index_fm_security_types_on_id"
   end
 
   create_table "fm_static_data", id: false, force: :cascade do |t|
@@ -157,10 +157,12 @@ ActiveRecord::Schema.define(version: 2019_09_19_101129) do
   end
 
   create_table "fm_uom_values", id: false, force: :cascade do |t|
-    t.string "user_id"
-    t.string "service_code"
-    t.string "uom_value"
-    t.string "building_id"
+    t.text "user_id"
+    t.text "service_code"
+    t.text "uom_value"
+    t.text "building_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id", "service_code", "building_id"], name: "fm_uom_values_user_id_idx"
   end
 
@@ -218,13 +220,6 @@ ActiveRecord::Schema.define(version: 2019_09_19_101129) do
   create_table "legal_services_uploads", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "london_postcodes", id: false, force: :cascade do |t|
-    t.text "postcode"
-    t.text "In Use"
-    t.text "region"
-    t.text "Last updated"
   end
 
   create_table "management_consultancy_admin_uploads", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
