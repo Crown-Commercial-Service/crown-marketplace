24208138e5d3e7c3f00a47519edb61c9795174d1
Iulia R Ionita <iulia@ionita.me>
2019-09-23 15:39:55 +0100

    Temporarily added rescue NoMethodError in omniauth_dfe in order to view full backtrace of error

--- config/initializers/omniauth_dfe.rb
+++ config/initializers/omniauth_dfe.rb
@@ -30,6 +30,10 @@ if Marketplace.dfe_signin_enabled?
       else
         @app.call(env)
       end
+    rescue NoMethodError => e
+      logger.error 'OMNIAUTH DFE ERROR:'
+      logger.error e.message
+      logger.error e.backtrace.join("\n")
     end
   end
 
