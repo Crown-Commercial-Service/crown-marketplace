8144773d809d952bcbcabdfc8905d6392b58a976
Chris Gannon <cjgannon@gmail.com>
2019-09-23 09:16:41 +0100

    Add a procurement router service for procurement updates

---
+++ app/services/facilities_management/procurement_router.rb
@@ -0,0 +1,28 @@
+class FacilitiesManagement::ProcurementRouter
+  include Rails.application.routes.url_helpers
+
+  STEPS = ['a', 'b'].freeze
+
+  def initialize(id:, step: nil)
+    @id = id
+    @step = step
+  end
+
+  def route
+    return facilities_management_beta_procurement_path(id: @id) if next_step.nil?
+
+    edit_facilities_management_beta_procurement_path(id: @id, step: next_step)
+  end
+
+  private
+
+  def next_step
+    return nil if @step.nil?
+
+    return nil unless STEPS.include? @step
+
+    return nil if @step == STEPS.last
+
+    STEPS[STEPS.find_index(@step) + 1]
+  end
+end
