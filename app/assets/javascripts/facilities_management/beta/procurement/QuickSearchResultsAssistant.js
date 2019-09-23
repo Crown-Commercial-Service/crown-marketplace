1a724174b8033093e437ff2ea86fc7cc5b3b9980
Man From Mons <julianyoung@outlook.com>
2019-09-23 18:27:17 +0100

    Trying to override Rails forms validation to slide in GOVUK conventions - FAILED Trying to customise the QuickSearchResultsAssistant initialise to a per-page basis - FAILED

--- app/assets/javascripts/facilities_management/beta/procurement/QuickSearchResultsAssistant.js
+++ app/assets/javascripts/facilities_management/beta/procurement/QuickSearchResultsAssistant.js
@@ -1,13 +1,19 @@
 const QuickSearchResultsAssistant = {
     visibleSuppliers : [],
-    helper : new FilterComponent("procurement"),
+    helper : null,
 
-    init: function () {
+    init: function (classification, action, module_name) {
+        this.helper = new FilterComponent(classification);
         this.helper.AddSection("region");
         this.helper.AddSection("service");
         this.helper.UpdateCounts();
         this.helper.SynchroniseFilterToggleButton( this.onToggleButtonClick.bind(this));
         this.helper.ConnectCheckboxes(this.onCheckboxChanged.bind(this));
+        this.form = $("#" + action + "_" + module_name + "_" + classification).first();
+        this.form.on('submit', function (x) { return this.validateForm.bind(this);});
+        this.form.onSubmit = function () {
+            return this.validateForm.bind(this);
+        };
     },
     FilterSuppliers : function(filterEvent) {
         let tableSource = filterEvent.FilterTarget.jqueryObject;
@@ -74,9 +80,59 @@ const QuickSearchResultsAssistant = {
                 targetSection.addClass('govuk-grid-column-two-thirds')
             }
         }
+    },
+    validateForm : function () {
+        let submitForm = true;
+
+        if ( null != this.form ) {
+            let elements = this.form.elements;
+            if ( elements.length > 0 ) {
+                elements.forEach ( function ( element ) {
+                    let jElem = $(element);
+                    let elementValue = jElem.val();
+                    if (jElem.prop('required')) {
+                        if ('' + elementValue == '') {
+                            this.toggleRequiredError(jElem);
+                            submitForm = false;
+                        }
+                    }
+                    if (jElem.prop('maxlength')) {
+                        let maxLength = parseInt(jElem.prop('maxlength'));
+                        if ( ('' + elementValue).length > maxLength) {
+                            this.toggleLengthError(jElem);
+                            submitForm = false;
+                        }
+                    }
+                });
+            }
+        }
+
+        return submitForm;
+    },
+    toggleRequiredError : function (jQueryElement) {
+        let jqueryElementForInputGroup = jQueryElement.parentsUntil (".govuk-form-group");
+        let jqueryElementForRequiredMessage = jQueryElement.siblings("div[data-validation='required'");
+        if (jqueryElementForInputGroup.hasClass("govuk-form-group--error")) {
+            jqueryElementForInputGroup.removeClass("govuk-form-group--error");
+            jqueryElementForRequiredMessage.addClass("govuk-visually-hidden");
+        } else {
+            jqueryElementForInputGroup.addClass("govuk-form-group--error");
+            jqueryElementForRequiredMessage.removeClass("govuk-visually-hidden");
+        }
+    },
+    toggleLengthError : function (jQueryElement) {
+        let jqueryElementForInputGroup = jQueryElement.parentsUntil (".govuk-form-group");
+        let jqueryElementForRequiredMessage = jQueryElement.siblings("div[data-validation='maxlength'");
+        if (jqueryElementForInputGroup.hasClass("govuk-form-group--error")) {
+            jqueryElementForInputGroup.removeClass("govuk-form-group--error");
+            jqueryElementForRequiredMessage.addClass("govuk-visually-hidden");
+        } else {
+            jqueryElementForInputGroup.addClass("govuk-form-group--error");
+            jqueryElementForRequiredMessage.removeClass("govuk-visually-hidden");
+        }
     }
 };
 
 $(function () {
-    QuickSearchResultsAssistant.init();
+    QuickSearchResultsAssistant.init("procurement", "new", "facilities_management");
 }());
