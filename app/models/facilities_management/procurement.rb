9e12f24ba2028edc469d5424bf2d1f91430a3235
Chris Gannon <cjgannon@gmail.com>
2019-09-25 17:00:54 +0100

    Add routing rules around quick search procurements

--- app/models/facilities_management/procurement.rb
+++ app/models/facilities_management/procurement.rb
@@ -1,5 +1,7 @@
 module FacilitiesManagement
   class Procurement < ApplicationRecord
+    include AASM
+
     belongs_to :user,
                foreign_key: :user_id,
                inverse_of: :procurements
@@ -7,5 +9,14 @@ module FacilitiesManagement
     validates :name, presence: true
     validates :name, uniqueness: { scope: :user }
     validates :name, length: 1..100
+
+    aasm do
+      state :quick_search, initial: true
+      state :detailed_search
+
+      event :start_detailed_search do
+        transitions from: :quick_search, to: :detailed_search
+      end
+    end
   end
 end
