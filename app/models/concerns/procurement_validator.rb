d50938b92a4a81e768ad6c5ad1195c89f885b9d4
Iulia R Ionita <iulia@ionita.me>
2019-09-26 13:54:59 +0100

    Small comments change

--- app/models/concerns/procurement_validator.rb
+++ app/models/concerns/procurement_validator.rb
@@ -7,11 +7,11 @@ module ProcurementValidator
     validates :name, uniqueness: { scope: :user }, on: :create
     validates :name, length: 1..100, on: :create
 
-    # validations on :contract_name
+    # validations on :contract_name step
     validates :contract_name, presence: true, on: :contract_name
     validates :contract_name, length: 1..100, on: :contract_name
 
-    # validations on :annual_cost
+    # validations on :annual_cost step
     validates :estimated_annual_cost, presence: true, on: :annual_cost
   end
 end
