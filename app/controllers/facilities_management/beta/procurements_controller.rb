7060aa679a5bfb85e51e9a576710ec3c9fb03970
Chris Gannon <cjgannon@gmail.com>
2019-10-01 13:51:28 +0100

    Fix validation on FM procurement controller

--- app/controllers/facilities_management/beta/procurements_controller.rb
+++ app/controllers/facilities_management/beta/procurements_controller.rb
@@ -45,7 +45,7 @@ module FacilitiesManagement
 
       def update
         @procurement.assign_attributes(procurement_params)
-        if @procurement.save(context: params[:step].try(:to_sym))
+        if @procurement.save(context: params[:facilities_management_procurement][:step].try(:to_sym))
           @procurement.start_detailed_search! if @procurement.quick_search? && params['start_detailed_search'].present?
           @procurement.reload
 
@@ -53,6 +53,8 @@ module FacilitiesManagement
 
           redirect_to FacilitiesManagement::ProcurementRouter.new(id: @procurement.id, procurement_state: @procurement.aasm_state, step: @current_step).route
         else
+          set_step_param
+
           render :edit
         end
       end
@@ -105,6 +107,10 @@ module FacilitiesManagement
 
       def find_services(service_codes)
         @services = Service.where(code: service_codes)
+      end
+
+      def set_step_param
+        params[:step] = params[:facilities_management_procurement][:step] if @procurement.detailed_search?
       end
     end
   end
