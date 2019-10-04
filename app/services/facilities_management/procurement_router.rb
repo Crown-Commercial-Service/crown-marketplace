1beb4fd9020cc76fd3fd25be10488b6d7e1ee938
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-10-04 14:18:44 +0100

    Contract date calculations

--- app/services/facilities_management/procurement_router.rb
+++ app/services/facilities_management/procurement_router.rb
@@ -1,7 +1,7 @@
 class FacilitiesManagement::ProcurementRouter
   include Rails.application.routes.url_helpers
 
-  STEPS = %w[contract_name estimated_annual_cost tupe].freeze
+  STEPS = %w[contract_name estimated_annual_cost tupe contract_dates].freeze
 
   def initialize(id:, procurement_state:, step: nil)
     @id = id
