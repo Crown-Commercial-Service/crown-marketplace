2f2db838133c818104a5a05f49885c1edcf69405
Man From Mons <julianyoung@outlook.com>
2019-10-11 20:46:39 +0100

    Validation Rules and Error messages for contract_dates validation business-logic

---
+++ spec/models/facilities_management/procurement_dates_spec.rb
@@ -0,0 +1,229 @@
+require 'rails_helper'
+
+RSpec.describe FacilitiesManagement::Procurement, type: :model do
+  subject(:procurement) { build(:facilities_management_procurement, name: 'contract_dates_test', user: user) }
+
+  let(:user) { build(:user) }
+
+  def log_error
+    puts $stdout, "Messages: #{procurement.errors.to_json}"
+    puts $stdout, "Details: #{procurement.errors.details.to_json}"
+  end
+
+  describe 'contract data should not be invalid because call of period is not > 0' do
+    context 'when the initial_call_off_period is not supplied' do
+      it 'will be valid' do
+        procurement.save
+        expect(procurement.valid?).to eq true
+      end
+    end
+
+    context 'when the initial_call_off_period is set to Nil' do
+      it 'will be valid' do
+        procurement.initial_call_off_period = nil
+        procurement.save
+        expect(procurement.valid?).to eq true
+      end
+    end
+
+    context 'when initial_call_off_period is an empty string or whitespace' do
+      it 'will be valid ' do
+        procurement.initial_call_off_period = ''
+        procurement.save
+        expect(procurement.valid?).to eq true
+        procurement.initial_call_off_period = '     '
+        procurement.save
+        expect(procurement.valid?).to eq true
+      end
+    end
+
+    context 'when initial_call_off_period is < 0' do
+      it 'will be invalid' do
+        procurement.initial_call_off_period = -2
+        procurement.save
+        log_error
+        expect(procurement.errors.details[:initial_call_off_period][0][:error]).to eq :greater_than_or_equal_to
+        expect(procurement.valid?).to eq false
+      end
+    end
+
+    context 'when initial_call_off_period is == 0' do
+      it 'will be valid' do
+        procurement.initial_call_off_period = 0
+        procurement.save
+        expect(procurement.valid?).to eq true
+      end
+    end
+  end
+
+  describe 'when a call off period is specified, other validation rules will come into effect, ' do
+    context 'when initial_call_off_period is > 0' do
+      it 'will be invalid' do
+        procurement.initial_call_off_period = 1
+        procurement.save
+        log_error
+        expect(procurement.errors.details[:mobilisation_period][0][:error]).to eq :not_a_number
+        expect(procurement.valid?).to eq false
+      end
+    end
+
+    context 'when mobilisation period is nil or whitespace' do
+      it 'and will be invalid when the value is nil' do
+        procurement.initial_call_off_period = 1
+        procurement.mobilisation_period = nil
+        procurement.save
+        log_error
+        expect(procurement.errors.details[:mobilisation_period][0][:error]).to eq :not_a_number
+        expect(procurement.valid?).to eq false
+      end
+
+      it 'and will be invalid when the value is whitespace' do
+        procurement.initial_call_off_period = 1
+        procurement.mobilisation_period = '    '
+        procurement.save
+        log_error
+        expect(procurement.errors.details[:mobilisation_period][0][:error]).to eq :not_a_number
+        expect(procurement.valid?).to eq false
+      end
+    end
+
+    context 'when mobilisation period is zero' do
+      it 'and will be valid' do
+        procurement.initial_call_off_period = 1
+        procurement.mobilisation_period = 0
+        procurement.save
+        expect(procurement.valid?).to eq true
+      end
+    end
+
+    describe 'and when mob period is > 0, then further rules come into play:' do
+      context 'when mob period is 1' do
+        it 'will be invalid' do
+          procurement.initial_call_off_period = 1
+          procurement.mobilisation_period = 1
+          procurement.save
+          log_error
+          expect(procurement.errors.details[:initial_call_off_start_date][0][:error]).to eq :blank
+          expect(procurement.valid?).to eq false
+        end
+      end
+
+      context 'when the contract_start_date needs to be populated' do
+        it 'will be invalid if the date is nil' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = false
+          procurement.mobilisation_period = 1
+          procurement.initial_call_off_start_date = nil
+          procurement.save
+          log_error
+          expect(procurement.errors.details[:initial_call_off_start_date][0][:error]).to eq :blank
+          expect(procurement.valid?).to eq false
+        end
+        it 'will be invalid if the date is the wrong format or empty' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = false
+          procurement.mobilisation_period = 1
+          procurement.initial_call_off_start_date = ''
+          procurement.save
+          log_error
+          expect(procurement.errors.details[:initial_call_off_start_date][0][:error]).to eq :blank
+          expect(procurement.valid?).to eq false
+        end
+        it 'will be invalid if the date is before now' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = false
+          procurement.mobilisation_period = 1
+          procurement.initial_call_off_start_date = DateTime.current - 100
+          procurement.initial_call_off_end_date = DateTime.current - 10
+          procurement.save
+          log_error
+          expect(procurement.errors.details[:initial_call_off_start_date][0][:error]).to eq :after
+          expect(procurement.valid?).to eq false
+        end
+        it 'will be valid if the date is after now' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = false
+          procurement.mobilisation_period = 1
+          procurement.initial_call_off_start_date = DateTime.current + 100
+          procurement.initial_call_off_end_date = DateTime.current + 200
+          procurement.save
+          expect(procurement.valid?).to eq true
+        end
+      end
+
+      context 'when TUPE is specified' do
+        it 'will be valid when mob period is 1 and TUPE is false' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = false
+          procurement.mobilisation_period = 1
+          procurement.initial_call_off_start_date = DateTime.current + 100
+          procurement.initial_call_off_end_date = DateTime.current + 200
+          procurement.save
+          expect(procurement.valid?).to eq true
+        end
+        it 'will be invalid when mob period is 1 and TUPE is true' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = true
+          procurement.mobilisation_period = 1
+          procurement.initial_call_off_start_date = DateTime.current + 100
+          procurement.initial_call_off_end_date = DateTime.current + 200
+          procurement.save
+          log_error
+          expect(procurement.errors.details[:mobilisation_period][0][:error]).to eq :greater_than_or_equal_to
+          expect(procurement.valid?).to eq false
+        end
+        it 'will be valid when mob period is 4 and TUPE is true' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = true
+          procurement.mobilisation_period = 4
+          procurement.initial_call_off_start_date = DateTime.current + 100
+          procurement.initial_call_off_end_date = DateTime.current + 200
+          procurement.save
+          log_error
+          expect(procurement.valid?).to eq true
+        end
+      end
+    end
+
+    describe 'there are a set of rules that all depend on the gateway values being valid' do
+      context 'when the call-off end date is empty' do
+        it 'will be invalid' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = true
+          procurement.mobilisation_period = 4
+          procurement.initial_call_off_start_date = DateTime.current + 100
+          procurement.save
+          log_error
+          expect(procurement.errors.details[:initial_call_off_end_date][0][:error]).to eq :not_a_date
+          expect(procurement.valid?).to eq false
+        end
+      end
+
+      context 'when the call-off end date is less than the start date' do
+        it 'will be invalid' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = true
+          procurement.mobilisation_period = 4
+          procurement.initial_call_off_start_date = DateTime.current + 100
+          procurement.initial_call_off_end_date = DateTime.current - 100
+          procurement.save
+          log_error
+          expect(procurement.errors.details[:initial_call_off_end_date][0][:error]).to eq :after_or_equal_to
+          expect(procurement.valid?).to eq false
+        end
+      end
+
+      context 'when the call-off end date is more than/= to the start date' do
+        it 'will be valid' do
+          procurement.initial_call_off_period = 1
+          procurement.tupe = true
+          procurement.mobilisation_period = 4
+          procurement.initial_call_off_start_date = DateTime.current + 100
+          procurement.initial_call_off_end_date = DateTime.current + 200
+          procurement.save
+          expect(procurement.valid?).to eq true
+        end
+      end
+    end
+  end
+end
