6380da071bffcb09a1ee0de2cbd82b1a761a6bf0
Tariq Hamid <tariq@hamid.com>
2019-10-20 16:59:33 +0100

    values per building

--- app/models/ccs/fm/service.rb
+++ app/models/ccs/fm/service.rb
@@ -17,7 +17,7 @@ module CCS
       # end
 
       def self.direct_award_services
-        %w[C.11 C.12 C.13 C.1 C.2 C.3 C.4 C.5 C.6 C.7 E.1 E.2 E.3 E.4 E.5 E.6 E.7 E.8 G.15 G.1 G.2 G.3 G.4 G.5 G.6 G.7 H.4 H.5 I.1 I.2 I.3 I.4 J.1 J.2 J.3 J.4 J.5 J.6 K.1 K.2 K.3 K.7 M.1 N.1]
+        %w[A.1	A.2	A.3	A.4	A.5	A.6	A.7	A.8	A.9	A.10	A.11	A.12	A.13	A.14	A.15	A.16	A.17	C.11	C.12	C.13	C.1	C.2	C.3	C.4	C.5	C.6	C.7	E.1	E.2	E.3	E.4	E.5	E.6	E.7	E.8	G.15	G.1	G.2	G.3	G.4	G.5	G.6	G.7	H.4	H.5	I.1	I.2	I.3	I.4	J.1	J.2	J.3	J.4	J.5	J.6	K.1	K.2	K.3	K.7	M.1	N.1]
       end
     end
   end
