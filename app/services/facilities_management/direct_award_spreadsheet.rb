cb1b51f356584a232567a3aefc1a48fa84a34a4b
Tariq Hamid <tariq@hamid.com>
2019-10-21 17:23:56 +0100

    show service prices

--- app/services/facilities_management/direct_award_spreadsheet.rb
+++ app/services/facilities_management/direct_award_spreadsheet.rb
@@ -20,18 +20,28 @@ class FacilitiesManagement::DirectAwardSpreadsheet
       sheet.add_row
       sheet.add_row ['Table 1. Baseline service costs for year 1']
       new_row = ['Service Reference', 'Service Name', 'Total']
-      keys = []
       @data.keys.sort.each.with_index do |_k, idx|
         new_row << 'Building ' + (idx + 1).to_s
-        keys << idx
       end
       sheet.add_row new_row
+
+      sorted_building_keys = @data.keys.sort
 
       @data.keys.collect { |k| @data[k].keys }
            .flatten.uniq
            .sort_by { |code| [code[0..code.index('.') - 1], code[code.index('.') + 1..-1].to_i] }.each do |s|
 
-        sheet.add_row [s, FacilitiesManagement::Service.where(code: s).first.name]
+        new_row = [s, FacilitiesManagement::Service.where(code: s).first.name]
+
+        new_row2 = []
+        sum = 0
+        sorted_building_keys.each do |k|
+          new_row2 << @data[k][s]
+          sum += @data[k][s]
+        end
+
+        new_row = (new_row << sum << new_row2).flatten
+        sheet.add_row new_row
       end
     end
 
