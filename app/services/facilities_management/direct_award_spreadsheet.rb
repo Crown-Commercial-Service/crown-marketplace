5be8b6e76ac2c687a2df961967436d5535952839
Tariq Hamid <tariq@hamid.com>
2019-10-21 16:41:32 +0100

    show services

--- app/services/facilities_management/direct_award_spreadsheet.rb
+++ app/services/facilities_management/direct_award_spreadsheet.rb
@@ -1,5 +1,6 @@
 class FacilitiesManagement::DirectAwardSpreadsheet
-  def initialize(data)
+  def initialize(supplier_name, data)
+    @supplier_name = supplier_name
     @data = data
     create_spreadsheet
   end
@@ -10,14 +11,33 @@ class FacilitiesManagement::DirectAwardSpreadsheet
 
   private
 
+  # rubocop:disable Metrics/AbcSize
   def create_spreadsheet
     @package = Axlsx::Package.new
     @workbook = @package.workbook
 
     @workbook.add_worksheet(name: 'Contract Price Matrix') do |sheet|
+      sheet.add_row
+      sheet.add_row ['Table 1. Baseline service costs for year 1']
+      new_row = ['Service Reference', 'Service Name', 'Total']
+      keys = []
+      @data.keys.sort.each.with_index do |_k, idx|
+        new_row << 'Building ' + (idx + 1).to_s
+        keys << idx
+      end
+      sheet.add_row new_row
+
+      @data.keys.collect { |k| @data[k].keys }
+           .flatten.uniq
+           .sort_by { |code| [code[0..code.index('.') - 1], code[code.index('.') + 1..-1].to_i] }.each do |s|
+
+        sheet.add_row [s, FacilitiesManagement::Service.where(code: s).first.name]
+      end
     end
 
     @workbook.add_worksheet(name: 'Contract Rate Card') do |sheet|
+      sheet.add_row [@supplier_name]
     end
   end
+  # rubocop:enable Metrics/AbcSize
 end
