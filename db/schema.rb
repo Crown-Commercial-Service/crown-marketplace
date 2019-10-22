a51b555fdf1cf1497509326c82ffcb1940164672
Tariq Hamid <tariq@hamid.com>
2019-10-22 10:53:32 +0100

    schema,rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_103501) do
+ActiveRecord::Schema.define(version: 2019_10_18_183421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2019_10_17_103501) do
     t.index ["user_id"], name: "idx_buildings_user_id"
   end
 
+  create_table "facilities_management_procurement_building_services", force: :cascade do |t|
+    t.uuid "facilities_management_procurement_building_id", null: false
+    t.string "code", limit: 10
+    t.string "name", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["facilities_management_procurement_building_id"], name: "index_fm_procurements_on_fm_procurement_building_id"
+  end
+
   create_table "facilities_management_procurement_buildings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "facilities_management_procurement_id", null: false
     t.text "service_codes", default: [], array: true
@@ -42,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_10_17_103501) do
     t.string "county", limit: 255
     t.string "postcode", limit: 20
     t.boolean "active"
+    t.uuid "building_id"
     t.index ["facilities_management_procurement_id"], name: "index_fm_procurements_on_fm_procurement_id"
   end
 
@@ -498,6 +508,7 @@ ActiveRecord::Schema.define(version: 2019_10_17_103501) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "facilities_management_procurement_building_services", "facilities_management_procurement_buildings"
   add_foreign_key "facilities_management_procurement_buildings", "facilities_management_procurements"
   add_foreign_key "facilities_management_procurements", "users"
   add_foreign_key "facilities_management_regional_availabilities", "facilities_management_suppliers"
