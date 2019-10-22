cb8f1987f1ed3715e75aac617c449f037d8a8a76
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-10-22 15:23:22 +0100

    fix migration

--- db/migrate/20190816162029_add_last_updated_tofacilities_management_buildings.rb
+++ db/migrate/20190816162029_add_last_updated_tofacilities_management_buildings.rb
@@ -1,13 +1,9 @@
 class AddLastUpdatedTofacilitiesManagementBuildings < ActiveRecord::Migration[5.2]
   def change
-    unless column_exists? :facilities_management_buildings, :updated_at
-      add_column :facilities_management_buildings, :updated_at, :datetime
-    end
+    add_column :facilities_management_buildings, :updated_at, :datetime unless column_exists? :facilities_management_buildings, :updated_at
 
-    unless column_exists? :facilities_management_buildings, :status
-      add_column :facilities_management_buildings, :status, :string, null: false, default: 'Incomplete'
-    end
+    add_column :facilities_management_buildings, :status, :string, null: false, default: 'Incomplete' unless column_exists? :facilities_management_buildings, :status
 
-    change_column_default :facilities_management_buildings, :updated_at, from: nil, to: 'now()'
+    change_column_default :facilities_management_buildings, :updated_at, from: nil, to: 'now()' if column_exists? :facilities_management_buildings, :updated_at
   end
 end
