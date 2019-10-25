49e89a1c39e5c670fc1467bb51adb6d9dbf793b3
Man From Mons <julianyoung@outlook.com>
2019-10-26 00:17:13 +0100

    Layouts and estimated annual cost and additional model has errors helper

--- app/helpers/facilities_management/beta/procurements_helper.rb
+++ app/helpers/facilities_management/beta/procurements_helper.rb
@@ -4,7 +4,11 @@ module FacilitiesManagement::Beta::ProcurementsHelper
   end
 
   def does_form_for_current_step_require_special_client_validation?(params)
-    %i[contract_dates estimated_annual_cost pension_funds].include? params[:step].try(:to_sym)
+    %i[contract_dates estimated_annual_cost].include? params[:step].try(:to_sym)
+  end
+
+  def format_date(date_object)
+    date_object&.strftime '%d %B %Y'
   end
 
   def initial_call_off_period_start_date
@@ -14,25 +18,27 @@ module FacilitiesManagement::Beta::ProcurementsHelper
   def initial_call_off_period_end_date
     return '' if @procurement.initial_call_off_start_date.blank?
 
-    Date.parse(@procurement.initial_call_off_start_date.to_s).next_year(@procurement.initial_call_off_period) - 1
+    end_date = Date.parse(@procurement.initial_call_off_start_date.to_s).next_year(@procurement.initial_call_off_period) - 1
+    format_date end_date
   end
 
   def mobilisation_period
     result = ''
     period = @procurement.mobilisation_period
     result = "#{period} #{'week'.pluralize(period)}" if @procurement.mobilisation_period_required == true
-    result = '' if @procurement.mobilisation_period_required.blank?
-    result = 'None' if @procurement.mobilisation_period_required == false
+    result = 'None' unless @procurement.mobilisation_period_required == true
+    result = 'None' if @procurement.mobilisation_period_required.blank?
     result
   end
 
   def mobilisation_start_date
-    start_date = Date.parse(@procurement.initial_call_off_start_date.to_s) - 1
-    start_date - (@procurement.mobilisation_period * 7)
+    start_date = Date.parse(@procurement.initial_call_off_start_date.to_s) - (@procurement.mobilisation_period * 7)
+    format_date start_date
   end
 
   def mobilisation_end_date
-    Date.parse(@procurement.initial_call_off_start_date.to_s) - 1
+    end_date = Date.parse(@procurement.initial_call_off_start_date.to_s) - 1
+    format_date end_date
   end
 
   def initial_call_off_period(period)
@@ -43,47 +49,34 @@ module FacilitiesManagement::Beta::ProcurementsHelper
     "#{format_date start_date} to #{format_date end_date}"
   end
 
+  def extension_one_description
+    extension_one_start_date = @procurement.initial_call_off_start_date.next_year(@procurement.initial_call_off_period)
+    extension_one_end_date = extension_one_start_date.next_year(@procurement.optional_call_off_extensions_1) - 1
+    format_extension extension_one_start_date, extension_one_end_date
+  end
+
+  def extension_two_description
+    extension_one_start_date = @procurement.initial_call_off_start_date.next_year(@procurement.initial_call_off_period)
+    start_date = extension_one_start_date.next_year(@procurement.optional_call_off_extensions_1)
+    end_date = (start_date - 1).next_year(@procurement.optional_call_off_extensions_2)
+    format_extension start_date, end_date
+  end
+
+  def extension_three_description
+    extension_one_start_date = @procurement.initial_call_off_start_date.next_year(@procurement.initial_call_off_period)
+    start_date = extension_one_start_date.next_year(@procurement.optional_call_off_extensions_1 + +@procurement.optional_call_off_extensions_2)
+    end_date = (start_date - 1).next_year(@procurement.optional_call_off_extensions_3)
+    format_extension start_date, end_date
+  end
+
+  def extension_four_description
+    extension_one_start_date = @procurement.initial_call_off_start_date.next_year(@procurement.initial_call_off_period)
+    start_date = extension_one_start_date.next_year(@procurement.optional_call_off_extensions_1 + @procurement.optional_call_off_extensions_2 + @procurement.optional_call_off_extensions_3)
+    end_date = (start_date - 1).next_year(@procurement.optional_call_off_extensions_4)
+    format_extension start_date, end_date
+  end
+
   def any_service_codes(procurement_buildings)
     procurement_buildings.map(&:service_codes).flatten.reject(&:blank?).any?
-  end
-
-  def building_services_status?(status)
-    return t('common.incomplete') unless status == true
-
-    t('common.complete')
-  end
-
-  PROCUREMENT_STATE = { da_draft: 'DA draft',
-                        further_competition: 'Further competition',
-                        results: 'Results',
-                        quick_search: 'Quick search',
-                        detailed_search: 'Detailed search',
-                        closed: 'closed' }.freeze
-
-  def procurement_state(procurement_state)
-    return procurement_state.humanize unless PROCUREMENT_STATE.key?(procurement_state.to_sym)
-
-    PROCUREMENT_STATE[procurement_state.to_sym]
-  end
-
-  def mobilisation_period_description
-    format_extension(mobilisation_start_date, mobilisation_end_date)
-  end
-
-  def initial_call_off_period_description
-    format_extension(@procurement.initial_call_off_start_date, initial_call_off_period_end_date)
-  end
-
-  def to_lower_case(str)
-    return str if !/[[:upper:]]/.match(str[0]).nil? && !/[[:upper:]]/.match(str[1]).nil?
-
-    str[0] = str[0].downcase
-    str
-  end
-
-  def sort_by_pension_fund_created_at
-    # problem was for pension funds with duplicated names,the validation has an error so there is no created_at
-    parts = @procurement.procurement_pension_funds.partition { |o| o.created_at.nil? }
-    parts.last.sort_by(&:created_at) + parts.first
   end
 end
--- app/models/concerns/procurement_validator.rb
+++ app/models/concerns/procurement_validator.rb
@@ -1,35 +1,24 @@
-# rubocop:disable Metrics/ModuleLength
 module ProcurementValidator
   extend ActiveSupport::Concern
 
   # rubocop:disable Metrics/BlockLength
   included do
+    # validations on :name step
+    validates :name, presence: true, on: :name
+    validates :name, uniqueness: { scope: :user }, on: :name
+    validates :name, length: 1..100, on: :name
+
     # validations on :contract_name step
-    before_validation :remove_excess_whitespace_from_name, on: :contract_name
-    validates :contract_name, presence: true, on: %i[contract_name]
-    validates :contract_name, uniqueness: { scope: :user }, on: :contract_name
+    validates :contract_name, presence: true, on: :contract_name
     validates :contract_name, length: 1..100, on: :contract_name
 
     # validations on :estimated_annual_cost step
-    validates :estimated_cost_known, inclusion: { in: [true, false] }, on: %i[estimated_annual_cost]
+    validates :estimated_cost_known, inclusion: { in:[true,false] }, on: :estimated_annual_cost
     validates :estimated_annual_cost, presence: true, if: -> { estimated_cost_known? }, on: :estimated_annual_cost
-    validates :estimated_annual_cost, numericality: { allow_nil: false, only_integer: true, greater_than_or_equal_to: 1 }, if: -> { estimated_cost_known? }, on: :estimated_annual_cost
+    validates :estimated_annual_cost, numericality: { allow_nil: false, only_integer: true,
+                                                      greater_than_or_equal_to: 1 }, on: :estimated_annual_cost
     # validations on :procurement_buildings step
-    validate :at_least_one_active_procurement_building, on: %i[procurement_buildings]
-
-    validate :service_codes_not_empty, on: %i[services]
-
-    validates :tupe, inclusion: { in: [true, false] }, on: %i[tupe]
-
-    validates :payment_method, inclusion: { in: ['bacs', 'card'] }, on: %i[payment_method]
-
-    validates :using_buyer_detail_for_invoice_details, inclusion: { in: [true, false] }, on: %i[invoicing_contact_details]
-
-    validates :using_buyer_detail_for_authorised_detail, inclusion: { in: [true, false] }, on: %i[authorised_representative]
-
-    validates :using_buyer_detail_for_notices_detail, inclusion: { in: [true, false] }, on: %i[notices_contact_details]
-
-    validates :local_government_pension_scheme, inclusion: { in: [true, false] }, on: %i[local_government_pension_scheme]
+    validate :at_least_one_active_procurement_building, on: :procurement_buildings
 
     validates :tupe, inclusion: { in: [true, false] }, on: :tupe
 
@@ -47,46 +36,39 @@ module ProcurementValidator
     #   optional_call_off_extensions_4 (int)
     # they are prepared and layed-out in logical sequence
     # they are tested in the appropriate rspec
-    validates :initial_call_off_period, presence: true, on: %i[contract_dates]
-    validates :initial_call_off_period, numericality: { allow_nil: false, only_integer: true, greater_than_or_equal_to: 1 }, if: -> { initial_call_off_period.present? }, on: :contract_dates
-    validates :initial_call_off_period, numericality: { allow_nil: false, only_integer: true, less_than_or_equal_to: 7 }, if: -> { initial_call_off_period.present? }, on: :contract_dates
-    validates :initial_call_off_start_date, presence: true, if: :initial_call_off_period_expects_a_date?, on: :contract_dates
-    validates :initial_call_off_start_date, date: { allow_nil: false, after_or_equal_to: proc { Time.zone.today } }, if: :initial_call_off_period_expects_a_date?, on: :contract_dates
-    validates :mobilisation_period, numericality: { allow_nil: true, only_integer: true, greater_than: -1 }, if: -> { initial_call_off_period.present? ? initial_call_off_period.positive? : false }, on: :contract_dates
-    validates :mobilisation_period, numericality: { allow_nil: true, only_integer: true, greater_than_or_equal_to: 4 }, if: -> { tupe? && (initial_call_off_period.present? ? initial_call_off_period.positive? : false) }, on: :contract_dates
+    validates :initial_call_off_period, presence: true, on: :contract_dates
+    validates :initial_call_off_period, numericality: { allow_nil: false, only_integer: true,
+                                                        greater_than_or_equal_to: 1 },
+                                        if: -> { initial_call_off_period.present? }, on: :contract_dates
+    validates :initial_call_off_period, numericality: { allow_nil: false, only_integer: true,
+                                                        less_than_or_equal_to: 7 },
+                                        if: -> { initial_call_off_period.present? }, on: :contract_dates
+    validates :initial_call_off_start_date, presence: true,
+                                            if: :initial_call_off_period_expects_a_date?,
+                                            on: :contract_dates
+    validates :initial_call_off_start_date, date: { allow_nil: false, after_or_equal_to: proc { Time.zone.today } },
+                                            if: :initial_call_off_period_expects_a_date?,
+                                            on: :contract_dates
+    validates :mobilisation_period, numericality: { allow_nil: true, only_integer: true,
+                                                    greater_than: -1 },
+                                    if: -> { initial_call_off_period.present? ? initial_call_off_period.positive? : false },
+                                    on: :contract_dates
+    validates :mobilisation_period, numericality: { allow_nil: true, only_integer: true,
+                                                    greater_than_or_equal_to: 4 },
+                                    if: -> { tupe? && (initial_call_off_period.present? ? initial_call_off_period.positive? : false) },
+                                    on: :contract_dates
 
-    validates :optional_call_off_extensions_1, numericality: { allow_nil: true, only_integer: true, greater_than_or_equal_to: 1 }, on: :contract_dates
-    validates :optional_call_off_extensions_2, numericality: { allow_nil: true, only_integer: true, greater_than_or_equal_to: 1 }, on: :contract_dates
-    validates :optional_call_off_extensions_3, numericality: { allow_nil: true, only_integer: true, greater_than_or_equal_to: 1 }, on: :contract_dates
-    validates :optional_call_off_extensions_4, numericality: { allow_nil: true, only_integer: true, greater_than_or_equal_to: 1 }, on: :contract_dates
+    validates :optional_call_off_extensions_1, numericality: { allow_nil: true }, on: :contract_dates
+    validates :optional_call_off_extensions_2, numericality: { allow_nil: true }, on: :contract_dates
+    validates :optional_call_off_extensions_3, numericality: { allow_nil: true }, on: :contract_dates
+    validates :optional_call_off_extensions_4, numericality: { allow_nil: true }, on: :contract_dates
     validate :optional_call_off_extensions_too_long, on: :contract_dates
     #
     # End of validation rules for contract-dates
     #############################################
-    validates :security_policy_document_required, inclusion: { in: [true, false] }, on: :security_policy_document
-    validates :security_policy_document_name, presence: true, if: :security_policy_document_required?, on: :security_policy_document
-    validates :security_policy_document_version_number, presence: true, if: :security_policy_document_required?, on: :security_policy_document
-    validate :security_policy_document_date_valid?, on: :security_policy_document
-
-    # Additional validations for the 'Continue' button on the 'Detailed search summary' page - validating on :all
-    validate :presence_of_about_the_contract, on: :all
-    validate :at_least_one_building, on: :all
-    validate :all_services_valid, on: :all
-    validate :validate_contract_period_questions, on: :all
-    validate :validate_mobilisation_and_tupe, on: :all
-    validate :at_least_one_service_per_building, on: :all
-
-    # Validation for the contract_details page
-    validate :validate_contract_details, on: :contract_details
+    validate :service_codes_not_empty, on: :services
 
     private
-
-    #############################################
-    # Start of validation methods for contract-dates
-
-    def remove_excess_whitespace_from_name
-      self.contract_name = contract_name&.split&.join(' ')
-    end
 
     #############################################
     # Start of validation methods for contract-dates
@@ -117,58 +99,6 @@ module ProcurementValidator
     def service_codes_not_empty
       errors.add(:service_codes, :invalid) if defined?(service_codes) && service_codes.empty?
     end
-
-    def security_policy_document_date_valid?
-      errors.add(:security_policy_document_date, :not_a_date) unless all_security_policy_document_date_fields_valid?
-    end
-
-    def all_security_policy_document_date_fields_valid?
-      return true if security_policy_document_date_yyyy.blank? && security_policy_document_date_mm.blank? && security_policy_document_date_dd.blank?
-
-      return false if security_policy_document_date_yyyy.to_i < 1970
-
-      Date.valid_date? security_policy_document_date_yyyy.to_i, security_policy_document_date_mm.to_i, security_policy_document_date_dd.to_i
-    end
-
-    def presence_of_about_the_contract
-      errors.add(:contract_name, :not_present) if contract_name.blank?
-      errors.add(:estimated_cost_known, :not_present) if estimated_cost_known.nil?
-      errors.add(:tupe, :not_present) if tupe.nil?
-    end
-
-    def validate_contract_period_questions
-      errors.add(:initial_call_off_period, :not_present) if initial_call_off_period.blank?
-    end
-
-    def at_least_one_building
-      errors.add(:procurement_buildings, :not_present) if active_procurement_buildings.none?
-    end
-
-    def at_least_one_service_per_building
-      errors.add(:base, :services_not_present) if active_procurement_buildings.none? || procurement_building_services.none?
-    end
-
-    def all_services_valid
-      active_procurement_buildings.each do |pb|
-        pb.errors.add(:base, :services_invalid) unless pb.valid?(:procurement_building_services)
-      end
-    end
-
-    def validate_mobilisation_and_tupe
-      errors.add(:mobilisation_period, :not_valid_with_tupe) if (!mobilisation_period || mobilisation_period < 4) && tupe == true
-    end
-
-    # rubocop:disable Metrics/CyclomaticComplexity, Metrics/AbcSize
-    def validate_contract_details
-      errors.add(:payment_method, :not_present_contract_details) if payment_method.nil?
-      errors.add(:using_buyer_detail_for_invoice_details, :not_present_contract_details) if using_buyer_detail_for_invoice_details.nil?
-      errors.add(:using_buyer_detail_for_authorised_detail, :not_present_contract_details) if using_buyer_detail_for_authorised_detail.nil?
-      errors.add(:using_buyer_detail_for_notices_detail, :not_present_contract_details) if using_buyer_detail_for_notices_detail.nil?
-      errors.add(:security_policy_document_required, :not_present_contract_details) if security_policy_document_required.nil?
-      errors.add(:local_government_pension_scheme, :not_present_contract_details) if local_government_pension_scheme.nil?
-      errors.any?
-    end
   end
-  # rubocop:enable Metrics/BlockLength, Metrics/AbcSize, Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/BlockLength
 end
-# rubocop:enable Metrics/ModuleLength
