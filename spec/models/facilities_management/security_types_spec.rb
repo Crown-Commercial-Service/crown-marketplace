a71d5c92f8aa5566e023fe9fc74cf7a020b89e51
Man From Mons <julianyoung@outlook.com>
2019-10-25 17:44:55 +0100

    Responses to codacy - use of described_class

--- spec/models/facilities_management/security_types_spec.rb
+++ spec/models/facilities_management/security_types_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe FacilitiesManagement::SecurityTypes, type: :model do
   describe '#SecurityTypes' do
     context 'when constructing, expect a result' do
       it 'is not nil' do
-        s = FacilitiesManagement::SecurityTypes.new
+        s = described_class.new
         expect(s).not_to eq nil
       end
     end
 
     context 'when accessing the database' do
       it 'returns a collection or rows' do
-        expect(FacilitiesManagement::SecurityTypes.first.blank?).to eq false
+        expect(described_class.first.blank?).to eq false
       end
     end
   end
