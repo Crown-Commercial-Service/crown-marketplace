a8b482e51cfe8107e366c35f4c657c930c959f77
Neil Hathaway <neil.hathaway@crowncommercial.gov.uk>
2019-10-31 15:40:55 +0000

    Schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_152610) do
+ActiveRecord::Schema.define(version: 2019_10_28_101010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
