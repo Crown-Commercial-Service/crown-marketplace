17d12b690f1860144af3f6c301bfeaadcce1e572
ManFromMons <julianyoung@outlook.com>
2019-11-04 11:18:23 +0000

    Repaired route tests

--- spec/controllers/facilities_management/beta/procurement_buildings_services_controller_spec.rb
+++ spec/controllers/facilities_management/beta/procurement_buildings_services_controller_spec.rb
@@ -1,17 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe FacilitiesManagement::Beta::ProcurementBuildingsServicesController, type: :controller do
-  describe 'GET #index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe 'GET #edit' do
     it 'returns http success' do
-      get :edit
-      expect(response).to have_http_status(:success)
+      get :edit, params: { id: 9897987 }
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
