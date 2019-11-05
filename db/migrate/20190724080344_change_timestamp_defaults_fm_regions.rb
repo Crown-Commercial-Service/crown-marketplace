f82093b86f9fb29a0a0d7eabb31132b84e17eec0
Iulia R Ionita <iulia@ionita.me>
2019-11-05 10:34:48 +0000

    Small fix migration

--- db/migrate/20190724080344_change_timestamp_defaults_fm_regions.rb
+++ db/migrate/20190724080344_change_timestamp_defaults_fm_regions.rb
@@ -4,14 +4,14 @@ class ChangeTimestampDefaultsFmRegions < ActiveRecord::Migration[5.2]
       change_column_null :fm_regions, :created_at, true
       change_column_default :fm_regions, :created_at, from: '', to: 'now()'
     else
-      add_column :fm_regions, :created_at, :datetime, from: '', to: 'now()', default: true
+      add_column :fm_regions, :created_at, :datetime, from: '', to: 'now()', null: true
     end
 
     if column_exists?(:fm_regions, :updated_at)
       change_column_null :fm_regions, :updated_at, true
       change_column_default :fm_regions, :updated_at, from: '', to: 'now()'
     else
-      add_column :fm_regions, :updated_at, :datetime, from: '', to: 'now()', default: true
+      add_column :fm_regions, :updated_at, :datetime, from: '', to: 'now()', null: true
     end
   end
 end
