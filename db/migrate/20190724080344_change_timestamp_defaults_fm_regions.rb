76337e1bcc893788f2ce8d5a0f8b442b7a323a4b
Iulia R Ionita <iulia@ionita.me>
2019-11-05 10:32:49 +0000

    Rubocop fix

--- db/migrate/20190724080344_change_timestamp_defaults_fm_regions.rb
+++ db/migrate/20190724080344_change_timestamp_defaults_fm_regions.rb
@@ -1,15 +1,17 @@
 class ChangeTimestampDefaultsFmRegions < ActiveRecord::Migration[5.2]
   def change
-    unless column_exists?(:fm_regions, :created_at)
-      add_column :fm_regions, :created_at, :datetime, null: false
+    if column_exists?(:fm_regions, :created_at)
+      change_column_null :fm_regions, :created_at, true
+      change_column_default :fm_regions, :created_at, from: '', to: 'now()'
+    else
+      add_column :fm_regions, :created_at, :datetime, from: '', to: 'now()', default: true
     end
-    unless column_exists?(:fm_regions, :updated_at)
-      add_column :fm_regions, :updated_at, :datetime, null: false
-    end
-    change_column_null :fm_regions, :created_at, true
-    change_column_null :fm_regions, :updated_at, true
 
-    change_column_default :fm_regions, :created_at, from: '', to: 'now()'
-    change_column_default :fm_regions, :updated_at, from: '', to: 'now()'
+    if column_exists?(:fm_regions, :updated_at)
+      change_column_null :fm_regions, :updated_at, true
+      change_column_default :fm_regions, :updated_at, from: '', to: 'now()'
+    else
+      add_column :fm_regions, :updated_at, :datetime, from: '', to: 'now()', default: true
+    end
   end
 end
