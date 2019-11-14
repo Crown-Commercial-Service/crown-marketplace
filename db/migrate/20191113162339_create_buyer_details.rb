ebeea41c72e923a70f4516853872e0d7f3c609bb
Iulia R Ionita <iulia@ionita.me>
2019-11-14 10:26:04 +0000

    Changed database table

--- db/migrate/20191113162339_create_buyer_details.rb
+++ db/migrate/20191113162339_create_buyer_details.rb
@@ -1,10 +1,8 @@
 class CreateBuyerDetails < ActiveRecord::Migration[5.2]
   def change
-    # rubocop:disable Rails/ReversibleMigration
     drop_table :facilities_management_buyer if table_exists?(:facilities_management_buyer)
-    # rubocop:enable Rails/ReversibleMigration
 
-    create_table :facilities_management_buyer_details, id: :uuid do |t|
+    create_table :facilities_management_buyer_details do |t|
       t.references :user
       t.string :full_name, limit: 255
       t.string :job_title, limit: 255
@@ -16,6 +14,9 @@ class CreateBuyerDetails < ActiveRecord::Migration[5.2]
       t.string :organisation_address_county, limit: 255
       t.string :organisation_address_postcode, limit: 255
       t.boolean :central_government
+      t.boolean :wider_public_sector
+      t.boolean :active
+      t.string :email, limit: 255
 
       t.timestamps
     end
