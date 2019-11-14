e21cd6edb24920081d14a9e5fbc7a6a2d8de495b
Iulia R Ionita <iulia@ionita.me>
2019-11-14 14:49:04 +0000

    Address pickup now fully working and data persisted

--- app/assets/javascripts/facilities_management/beta/buyer/buyer_details.js
+++ app/assets/javascripts/facilities_management/beta/buyer/buyer_details.js
@@ -38,12 +38,24 @@ $(function () {
     });
 
     $('#buyer-details-postcode-lookup-results').on('change', function (e) {
+        let selectedOption = $("select#buyer-details-postcode-lookup-results > option:selected");
         selectedAddress = void 0;
-        selectedAddress = $("select#buyer-details-postcode-lookup-results > option:selected").val();
+        selectedAddress = selectedOption.val();
+        let add1 = selectedOption.data('add1');
+        let add2 = selectedOption.data('add2');
+        let town = selectedOption.data('town');
+        let county = selectedOption.data('county');
+        let postcode = selectedOption.data('postcode');
 
+        $('#organisation-address-line-1').val(add1);
+        $('#organisation-address-line-2').val(add2);
+        $('#organisation-address-town').val(town);
+        $('#organisation-address-county').val(county);
+        $('#organisation-address-postcode').val(postcode);
         $('#fm-post-code-results-container').addClass('govuk-visually-hidden');
         $('#selected-address-container').removeClass('govuk-visually-hidden');
         $('#selected-address-label').text(selectedAddress);
+        $('#selected-address-postcode').text(postcode);
         $('#organisation_address').text(selectedAddress);
     });
 
--- app/controllers/facilities_management/beta/buyer_details_controller.rb
+++ app/controllers/facilities_management/beta/buyer_details_controller.rb
@@ -6,6 +6,31 @@ module FacilitiesManagement
       end
 
       def update
+        @buyer_detail = current_user.buyer_detail
+
+        if @buyer_detail.update(buyer_detail_params)
+          redirect_to facilities_management_beta_path
+        else
+          render :edit
+        end
+      end
+
+      private
+
+      def buyer_detail_params
+        params.require(:facilities_management_buyer_detail)
+              .permit(
+                :full_name,
+                :job_title,
+                :telephone_number,
+                :organisation_name,
+                :organisation_address_line_1,
+                :organisation_address_line_2,
+                :organisation_address_town,
+                :organisation_address_county,
+                :organisation_address_postcode,
+                :central_government
+              )
       end
     end
   end
