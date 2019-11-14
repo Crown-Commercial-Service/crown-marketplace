e98bd9f736a7f036e729edf193fcf63af128704d
Iulia R Ionita <iulia@ionita.me>
2019-11-14 16:00:01 +0000

    Added some unit tests

--- app/controllers/facilities_management/beta/buyer_account_controller.rb
+++ app/controllers/facilities_management/beta/buyer_account_controller.rb
@@ -2,7 +2,6 @@ require 'facilities_management/buyer_details'
 module FacilitiesManagement
   module Beta
     class BuyerAccountController < FrameworkController
-
       def buyer_account
         @current_login_email = current_user.email.to_s
         @buyer_detail = FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user)
