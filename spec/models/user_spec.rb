1aeef3c457ea3a0c2dd71cf694ab091ea061fd2e
ManFromMons <julianyoung@outlook.com>
2019-11-15 12:07:29 +0100

    Added RSpecs to the new user method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,54 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe '#fm_buyer_details_incomplete?' do
+    let(:confirmed_at) { Time.zone.now }
+
+    context 'when user is buyer without buyer details' do
+      it 'will return true' do
+        user.roles = %i[buyer fm_access]
+        expect(user.fm_buyer_details_incomplete?).to eq true
+      end
+    end
+
+    context 'when user isn\'t a buyer' do
+      it 'will return false' do
+        user.roles = %i[fm_access]
+        expect(user.fm_buyer_details_incomplete?).to eq false
+      end
+    end
+
+    context 'when user is a buyer and has empty details' do
+      it 'will return true' do
+        user.roles = %i[buyer fm_access]
+        user.buyer_detail = nil
+        expect(user.fm_buyer_details_incomplete?).to eq true
+      end
+    end
+
+    context 'when user is a buyer and has incomplete details' do
+      it 'will return true' do
+        user.roles = %i[buyer fm_access]
+        user.buyer_detail = FacilitiesManagement::BuyerDetail.new
+        user.buyer_detail.full_name = 'Test name'
+        expect(user.fm_buyer_details_incomplete?).to eq true
+      end
+    end
+
+    context 'when user is a buyer and has complete details' do
+      it 'will return true' do
+        user.roles = %i[buyer fm_access]
+        user.buyer_detail = FacilitiesManagement::BuyerDetail.new
+        user.buyer_detail.full_name = 'Test name'
+        user.buyer_detail.job_title = 'Job title'
+        user.buyer_detail.telephone_number = '3434'
+        user.buyer_detail.organisation_name = 'org name'
+        user.buyer_detail.organisation_address_postcode = 'postcode'
+        user.buyer_detail.central_government = true
+
+        expect(user.fm_buyer_details_incomplete?).to eq false
+      end
+    end
+  end
 end
