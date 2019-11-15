782744d73cef34dd5edb81c12303154418276a54
ManFromMons <julianyoung@outlook.com>
2019-11-15 13:00:45 +0100

    Corrected URL routing issues, removed the client-side validation in the buyer-details screen, repaired the fm/beta URL redirecting to supply-teachers issue and the buyer_details validation issue with the central_government field

--- app/controllers/facilities_management/beta/buyer_account_controller.rb
+++ app/controllers/facilities_management/beta/buyer_account_controller.rb
@@ -1,6 +1,8 @@
 module FacilitiesManagement
   module Beta
     class BuyerAccountController < FrameworkController
+      before_action :redirect_if_needed
+
       def buyer_account
         @current_login_email = current_user.email.to_s
         @buyer_detail = FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user)
@@ -8,6 +10,12 @@ module FacilitiesManagement
 
       def buyer_details
         @current_login_email = current_user.email.to_s
+      end
+
+      private
+
+      def redirect_if_needed
+        redirect_to facilities_management_beta_start_path unless user_signed_in?
       end
     end
   end
