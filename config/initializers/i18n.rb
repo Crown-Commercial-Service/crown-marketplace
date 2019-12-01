6147f35c28bbfa328de55cbd023f5d6e7d4d03b6
ManFromMons <julianyoung@outlook.com>
2019-12-01 05:25:41 +0100

    Problem resolved :  using form_with and not form_for causes a different code-path on the client to be executed on form-submission - this code-path wasn't reloading the page Also:   Layout repairs and additions   Minor bugs repaired   Errors messages, styles and validations rendering completely - with a minor issue   Validation Logic improvements and robustness in the summary display   Translation cleanup   Code-cleanup and robocop
    Tests are not verified against this new code...

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -9,17 +9,22 @@ if ['1.7.0'].include?(I18n::VERSION)
         #
         def lookup(locale, key, scope = [], options = {})
           init_translations unless initialized?
+
           keys = I18n.normalize_keys(locale, key, scope, options[:separator])
 
-          puts "I18N keys: #{keys}"  if ENV['I18N_DEBUG'] == '1'
+          Rails.logger.debug "I18N keys: #{keys}" if ENV['I18N_DEBUG']
+          process_keys
+        end
 
-          keys.inject(translations) do |result, _key|
-            _key = _key.to_sym
-            return nil unless result.is_a?(Hash) && result.has_key?(_key)
-            result = result[_key]
-            result = resolve(locale, _key, result, options.merge(:scope => nil)) if result.is_a?(Symbol)
+        def process_keys
+          keys.inject(translations) do |result, inner_key|
+            inner_key = inner_key.to_sym
+            return nil unless result.is_a?(Hash) && result.key?(inner_key)
 
-            puts "\t\t => " + result.to_s + "\n" if ENV['I18N_DEBUG'] == '1' && (result.class == String)
+            result = result[inner_key]
+            result = resolve(locale, inner_key, result, options.merge(scope: nil)) if result.is_a?(Symbol)
+
+            Rails.logger.debug "\t\t => " + result.to_s + "\n" if ENV['I18N_DEBUG'] && (result.class == String)
 
             result
           end
@@ -29,10 +34,10 @@ if ['1.7.0'].include?(I18n::VERSION)
   end
 
 else
-  puts "\n--------------------------------------------------------------------------------"
-  puts "WARNING: you're using version #{I18n::VERSION} of the i18n gem."
-  puts "         Please double check that your monkey-patch still works!"
-  puts "         see: \"#{__FILE__}\""
-  puts "         see: http://www.unixgods.org/~tilo/Rails/which_l10n_strings_is_rails_trying_to_lookup.html"
-  puts "--------------------------------------------------------------------------------\n"
+  Rails.Logger.debug '\n--------------------------------------------------------------------------------'
+  Rails.Logger.debug "WARNING: you are using version #{I18n::VERSION} of the i18n gem."
+  Rails.Logger.debug '         Please double check that your monkey-patch still works!'
+  Rails.Logger.debug "         see: '#{__FILE__}'"
+  Rails.Logger.debug '         see: http://www.unixgods.org/~tilo/Rails/which_l10n_strings_is_rails_trying_to_lookup.html'
+  Rails.Logger.debug '--------------------------------------------------------------------------------\n'
 end
