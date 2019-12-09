581443f7a0d88309e6d60895d891f4dd1767644e
Clarence Brown <clarence.brown@crowncommercial.gov.uk>
2019-12-09 15:16:58 +0000

    Added logic to reject special characters in relevant text fields

--- app/models/facilities_management/buyer_detail.rb
+++ app/models/facilities_management/buyer_detail.rb
@@ -2,17 +2,17 @@ module FacilitiesManagement
   class BuyerDetail < ApplicationRecord
     belongs_to :user, foreign_key: :user_id, inverse_of: :buyer_detail
 
-    validates :full_name, presence: true, on: :update
-    validates :job_title, presence: true, on: :update
+    validates :full_name, presence: true, format: { with: /\A^([a-zA-Z\- ]*)$\z/ }, on: :update
+    validates :job_title, presence: true, format: { with: /\A^([a-zA-Z\- ]*)$\z/ }, on: :update
     validates :telephone_number, presence: true, on: :update
     validates :telephone_number, numericality: { greater_than: 0, message: :blank }, on: :update
-    validates :organisation_name, presence: true, on: :update
-    validates :organisation_address_postcode, presence: true, on: :update
+    validates :organisation_name, presence: true, format: { with: /\A^([a-zA-Z\- ]*)$\z/ }, on: :update
+    validates :organisation_address_postcode, presence: true, format: { with: /\A^([a-zA-Z (0-9)]*)$\z/ }, on: :update
     validates :central_government, inclusion: { in: [true, false], message: :blank }, on: :update
 
-    validates :organisation_address_postcode, presence: true, on: :update_address
-    validates :organisation_address_town, presence: true, on: :update_address
-    validates :organisation_address_line_1, presence: true, on: :update_address
+    validates :organisation_address_postcode, presence: true, format: { with: /\A^([a-zA-Z (0-9)]*)$\z/ }, on: :update_address
+    validates :organisation_address_town, presence: true, format: { with: /\A^([a-zA-Z\- ]*)$\z/ }, on: :update_address
+    validates :organisation_address_line_1, presence: true, format: { with: /\A^([a-zA-Z\- (0-9)]*)$\z/ }, on: :update_address
 
     def full_organisation_address
       "#{organisation_address_line_1}#{', ' + organisation_address_line_2 if organisation_address_line_2.present?}#{', ' + organisation_address_town}#{', ' + organisation_address_county if organisation_address_county.present?}#{', ' + organisation_address_postcode}"
