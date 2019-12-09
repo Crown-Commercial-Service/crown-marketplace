383e5f83e8188f81cd2a5abb8c80e5a7a15016df
James Kavanagh <james.kavanagh@crowncommercial.gov.uk>
2019-12-09 14:39:44 +0000

    Updated fix for more recent development

--- app/models/facilities_management/procurement_building_service.rb
+++ app/models/facilities_management/procurement_building_service.rb
@@ -145,7 +145,7 @@ module FacilitiesManagement
       return unless this_service[:context].key?(:volume)
 
       this_service[:context][:volume].each do |question|
-        validates_numericality_of(question.to_sym, greater_than: 0, only_integer: true, message: :invalid) if send(this_service[:context][:volume].first).present?
+        validates_numericality_of(question.to_sym, greater_than: 0, only_integer: true, message: :invalid) if this_service[:context][:volume].first.present?
       end
     end
 
