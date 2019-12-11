8ade2f11b342cf2588cc4d27371e1a0f842b28ed
Chee-Ng <chee@Envy-x360.localdomain>
2019-12-11 13:58:05 +0000

    Fixed buyer detail error for postcode

--- app/models/facilities_management/buyer_detail.rb
+++ app/models/facilities_management/buyer_detail.rb
@@ -1,5 +1,8 @@
 module FacilitiesManagement
   class BuyerDetail < ApplicationRecord
+
+    attr_accessor :error_postcode
+
     belongs_to :user, foreign_key: :user_id, inverse_of: :buyer_detail
 
     validates :full_name, presence: true, on: :update
@@ -8,6 +11,7 @@ module FacilitiesManagement
     validates :telephone_number, numericality: { greater_than: 0, message: :blank }, on: :update
     validates :organisation_name, presence: true, on: :update
     validates :organisation_address_postcode, presence: true, on: :update
+    validate  :validate_postcode_exist, on: :update
     validates :central_government, inclusion: { in: [true, false], message: :blank }, on: :update
 
     validates :organisation_address_postcode, presence: true, on: :update_address
@@ -17,5 +21,13 @@ module FacilitiesManagement
     def full_organisation_address
       "#{organisation_address_line_1}#{', ' + organisation_address_line_2 if organisation_address_line_2.present?}#{', ' + organisation_address_town}#{', ' + organisation_address_county if organisation_address_county.present?}#{', ' + organisation_address_postcode}"
     end
+
+    def validate_postcode_exist
+        if organisation_address_line_1 == '' && organisation_address_town == '' && organisation_address_postcode !=''
+            self.errors.messages.delete(:organisation_address_postcode)
+            errors.add(:error_postcode, :blank)
+        end
+    end
+
   end
 end
