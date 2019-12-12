9a92cdb86e7592296dcb9688aadf0836c4b5f60e
Chee-Ng <chee@Envy-x360.localdomain>
2019-12-12 16:01:52 +0000

    Change error message for buyer detail postcode
    different if postcode is entered but address is not completed

--- app/models/facilities_management/buyer_detail.rb
+++ app/models/facilities_management/buyer_detail.rb
@@ -1,8 +1,6 @@
 module FacilitiesManagement
   class BuyerDetail < ApplicationRecord
 
-    attr_accessor :error_postcode
-
     belongs_to :user, foreign_key: :user_id, inverse_of: :buyer_detail
 
     validates :full_name, presence: true, on: :update
@@ -11,7 +9,7 @@ module FacilitiesManagement
     validates :telephone_number, numericality: { greater_than: 0, message: :blank }, on: :update
     validates :organisation_name, presence: true, on: :update
     validates :organisation_address_postcode, presence: true, on: :update
-    validate  :validate_postcode_exist, on: :update
+    validate  :address_entered_when_postcode_provided, on: :update
     validates :central_government, inclusion: { in: [true, false], message: :blank }, on: :update
 
     validates :organisation_address_postcode, presence: true, on: :update_address
@@ -22,10 +20,10 @@ module FacilitiesManagement
       "#{organisation_address_line_1}#{', ' + organisation_address_line_2 if organisation_address_line_2.present?}#{', ' + organisation_address_town}#{', ' + organisation_address_county if organisation_address_county.present?}#{', ' + organisation_address_postcode}"
     end
 
-    def validate_postcode_exist
-            self.errors.messages.delete(:organisation_address_postcode)  if organisation_address_line_1 == '' && organisation_address_town == '' && organisation_address_postcode !=''
-            errors.add(:error_postcode, :blank)  if organisation_address_line_1 == '' && organisation_address_town == '' && organisation_address_postcode !=''
-    end
+    private
 
+    def address_entered_when_postcode_provided
+      errors.add(:organisation_address_postcode, :address_not_complete)  if organisation_address_line_1 == '' && organisation_address_town == '' && organisation_address_postcode !=''
+    end
   end
 end
