05c8d4d91c9fdaae655e302dd8da7f2f903a8d97
James Kavanagh <james.kavanagh@crowncommercial.gov.uk>
2019-12-12 16:05:25 +0000

    Added a method to return any buildings that require service information
    Undid the changes after clarifation with requirements
    
    Fixed issue with duplicated code
    
    Added function for evaluation
    
    Added the hint below service info
    
    fixed stylistic look
    
    Fixed for rubocop
    
    Fixed border problems
    
    Added the function to the scope of the ProcurementBuilding
    
    Fixed for rubocop
    
    Merge conflict fix

--- app/models/facilities_management/procurement_building.rb
+++ app/models/facilities_management/procurement_building.rb
@@ -3,6 +3,7 @@ module FacilitiesManagement
   class ProcurementBuilding < ApplicationRecord
     default_scope { order(name: :asc) }
     scope :active, -> { where(active: true) }
+    scope :requires_service_information, -> { select { |pb| pb.service_codes.any? { |code| FacilitiesManagement::ServicesAndQuestions.new.codes.include?(code) } } }
     belongs_to :procurement, foreign_key: :facilities_management_procurement_id, inverse_of: :procurement_buildings
     has_many :procurement_building_services, foreign_key: :facilities_management_procurement_building_id, inverse_of: :procurement_building, dependent: :destroy
     accepts_nested_attributes_for :procurement_building_services, allow_destroy: true
