78edeb49841b15d18c1cdd87d6a331f5afd72336
James Kavanagh <james.kavanagh@crowncommercial.gov.uk>
2019-12-19 12:22:57 +0000

    Created inital view
    Removed secondary button

--- config/routes.rb
+++ config/routes.rb
@@ -229,6 +229,8 @@ Rails.application.routes.draw do
     get '/prototypes/results', to: 'prototype#results'
     get '/prototypes/pricing', to: 'prototype#pricing'
     get '/prototypes/what-next', to: 'prototype#what_next'
+    get '/prototypes/new-invoicing-contact-details', to: 'prototype#new_invoicing_contact_details'
+    get '/prototypes/new-invoicing-contact-details-edit-address-path', to: 'prototype#new_invoicing_contact_details_edit_address_path'
     get '/dynamic-accordian', to: 'home#dynamic_accordian'
     get '/supplier-results-v1', to: 'home#supplier_results_v1'
     get '/supplier-results-v2', to: 'home#supplier_results_v2'
