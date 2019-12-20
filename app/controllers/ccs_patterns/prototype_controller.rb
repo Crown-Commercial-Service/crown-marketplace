7da91b7f89dabdae2b04186061ffea53aff8de98
Tim South <timothy.south@crowncommercial.gov.uk>
2019-12-20 15:43:48 +0000

    Created view for new notices contact details

--- app/controllers/ccs_patterns/prototype_controller.rb
+++ app/controllers/ccs_patterns/prototype_controller.rb
@@ -106,6 +106,7 @@ module CcsPatterns
     end
 
     # rubocop:disable Metrics/MethodLength
+    # rubocop:disable Metrics/AbcSize
     def page_definitions
       @page_definitions ||= {
         default: {
@@ -283,5 +284,6 @@ module CcsPatterns
     end
     # rubocop:enable Metrics/AbcSize
     # rubocop:enable Metrics/MethodLength
+    # rubocop:enable Metrics/AbcSize
   end
 end
