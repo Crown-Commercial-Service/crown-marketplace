a5d1b25e5ff23d39569ba38395fd6bce93837147
James Kavanagh <james.kavanagh@crowncommercial.gov.uk>
2019-12-20 12:48:07 +0000

    Initial notices view
    Fixed for rubocop

--- app/models/facilities_management/new_procurement_data.rb
+++ app/models/facilities_management/new_procurement_data.rb
@@ -6,6 +6,7 @@ module FacilitiesManagement
     attribute :route_to_market
     attribute :confirm_direct_award
     attribute :select_an_invoicing_contact
+    attribute :select_a_notice
     attribute :name
     attribute :job_title
     attribute :email_address
