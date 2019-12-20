5d8c1197a9cc6511e698a8ce7fee33dd97d50c8b
Tim South <timothy.south@crowncommercial.gov.uk>
2019-12-20 15:56:19 +0000

    changes to comply eith rubocop

--- app/controllers/ccs_patterns/prototype_controller.rb
+++ app/controllers/ccs_patterns/prototype_controller.rb
@@ -84,7 +84,6 @@ module CcsPatterns
     end
 
     # rubocop:disable Metrics/MethodLength
-    # rubocop:disable Metrics/AbcSize
     def page_definitions
       @page_definitions ||= {
         default: {
@@ -222,8 +221,6 @@ module CcsPatterns
         },
       }.freeze
     end
-    # rubocop:enable Metrics/AbcSize
     # rubocop:enable Metrics/MethodLength
-    # rubocop:enable Metrics/AbcSize
   end
 end
