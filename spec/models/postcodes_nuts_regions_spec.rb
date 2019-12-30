a7371aba3d55abf526d23a8670017ecc3fa92478
Mo <mo-zag@users.noreply.github.com>
2019-12-30 14:20:03 +0000

    pass Codacy/PR Quality Review
    Codacy/PR Quality Review

--- spec/models/postcodes_nuts_regions_spec.rb
+++ spec/models/postcodes_nuts_regions_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PostcodesNutsRegions, type: :model do
   it 'Create a record' do
-    postcode_nuts_save_record = PostcodesNutsRegions.new(postcode: 'AB101AB', code: 'UKM50').save
+    postcode_nuts_save_record = described_class.new(postcode: 'AB101AB', code: 'UKM50').save
     expect(postcode_nuts_save_record).to eq(true)
   end
   it 'Get a record' do
-    postcode_nuts_save_record = PostcodesNutsRegions.new(postcode: 'AB101AB', code: 'UKM50').save
+    postcode_nuts_save_record = described_class.new(postcode: 'AB101AB', code: 'UKM50').save
     expect(postcode_nuts_save_record).to eq(true)
-    get_postcode_record = PostcodesNutsRegions.find_by(postcode: 'AB101AB')
+    get_postcode_record = described_class.find_by(postcode: 'AB101AB')
     expect(get_postcode_record).to have_attributes(postcode: 'AB101AB', code: 'UKM50')
   end
 end
