5640007c2e9750955bc2fab167f010304fc33f83
Mo <mo-zag@users.noreply.github.com>
2020-01-02 17:00:54 +0000

    Update docker-entrypoint.sh
    temporarily removed to test deployment

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -5,7 +5,7 @@ bundle exec rails db:migrate
 bundle exec rails db:static
 # temporarily disabling this as it's giving AccessDenied from S3
 #bundle exec rails db:postcode
-bundle exec rails db:importpostcoderegion
+#bundle exec rails db:importpostcoderegion
 
 bundle exec sidekiq -C ./config/sidekiq.yml -d -L ./log/sidekiq.log -e production if ENV['APP_RUN_SIDEKIQ'].present?
 
