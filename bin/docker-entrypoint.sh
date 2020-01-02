141ffc30fff31b28743203c5fbeedd961b4cded3
Iulia R Ionita <iulia@ionita.me>
2020-01-02 16:07:05 +0000

    Temporarily disabling postcodes import

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -3,7 +3,8 @@
 bundle exec rails db:migrate
 
 bundle exec rails db:static
-bundle exec rails db:postcode
+# temporarily disabling this as it's giving AccessDenied from S3
+#bundle exec rails db:postcode
 bundle exec rails db:importpostcoderegion
 
 bundle exec sidekiq -C ./config/sidekiq.yml -d -L ./log/sidekiq.log -e production if ENV['APP_RUN_SIDEKIQ'].present?
