12b265bbe1fa0a900a8078e5569768182357b71b
Clarence Brown <clarence.brown@crowncommercial.gov.uk>
2020-01-02 12:28:21 +0000

    added path to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -234,6 +234,7 @@ Rails.application.routes.draw do
     get '/prototypes/contract-details', to: 'prototype#contract_details'
     get '/prototypes/invoicing-contact-details', to: 'prototype#invoicing_contact_details'
     get '/prototypes/new-invoicing-contact-details', to: 'prototype#new_invoicing_contact_details'
+    get '/prototypes/authorised_representative', to: 'prototype#authorised_representative'
     get '/prototypes/new-invoicing-contact-details-edit-address', to: 'prototype#new_invoicing_contact_details_edit_address'
     get '/prototypes/invoicing-contact-details-edit-address', to: 'prototype#invoicing_contact_details_edit_address'
     get '/prototypes/new-authorised-representative-details', to: 'prototype#new_authorised_representative_details'
