fb3cd8f53f9983de97fadb8dc9e06b266289b535
Tim South <timothy.south@crowncommercial.gov.uk>
2020-01-07 09:24:29 +0000

    confirgured the controller to work with helper

--- app/models/facilities_management/supplier/offer.rb
+++ app/models/facilities_management/supplier/offer.rb
@@ -2,11 +2,5 @@ module FacilitiesManagement
   class Supplier::Offer
     include ActiveModel::Model
     include Virtus.model
-
-    attribute :respond_to_contract_offer
-    attribute :contract_not_accepted
-    attribute :contract_name
-    attribute :contract_number
-    attribute :buyer_name
   end
 end
