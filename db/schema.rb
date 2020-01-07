039d98a8f4f69bdc2f56c7d58fb1ac01c23511de
Mo <mo-zag@users.noreply.github.com>
2020-01-07 09:12:56 +0000

    Postcode to nuts
    Postcode to Nuts Region

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_131101) do
+
+ActiveRecord::Schema.define(version: 2019_12_30_124912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
