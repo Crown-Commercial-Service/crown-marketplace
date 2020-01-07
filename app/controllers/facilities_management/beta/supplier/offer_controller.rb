770b46ce786f4ddcd9e2b7a2b8bd70306175de05
Clarence Brown <clarence.brown@crowncommercial.gov.uk>
2020-01-07 13:29:50 +0000

    fixed whitespace issues

--- app/controllers/facilities_management/beta/supplier/offer_controller.rb
+++ app/controllers/facilities_management/beta/supplier/offer_controller.rb
@@ -9,8 +9,8 @@ module FacilitiesManagement
         def declined; end
           
         def accepted
-      @page_data[:contract_name] = 'School facilities London'
-      @page_data[:contract_number] = 'RM330-DA2234-2019'
+          @page_data[:contract_name] = 'School facilities London'
+          @page_data[:contract_number] = 'RM330-DA2234-2019'
         end
 
         def accepted; end
@@ -63,7 +63,6 @@ module FacilitiesManagement
               back_text: 'View prototypes'
             },
             accepted: {
-              # TODO: add the link when path is known
               back_url: '#',
               back_text: 'Back',
               back_label: 'Back',
