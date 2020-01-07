354c1d8c54b5b1b8da27f4049a3b42e36bbada33
Clarence Brown <clarence.brown@crowncommercial.gov.uk>
2020-01-07 13:39:45 +0000

    fixed whitespace again

--- app/controllers/facilities_management/beta/supplier/offer_controller.rb
+++ app/controllers/facilities_management/beta/supplier/offer_controller.rb
@@ -7,7 +7,7 @@ module FacilitiesManagement
         before_action :set_page_model
 
         def declined; end
-          
+
         def accepted
           @page_data[:contract_name] = 'School facilities London'
           @page_data[:contract_number] = 'RM330-DA2234-2019'
