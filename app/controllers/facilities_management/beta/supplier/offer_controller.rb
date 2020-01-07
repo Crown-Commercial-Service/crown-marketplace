1c4bb7f65c1ac76c4f1d0fc8e22eb35d0dba6685
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-01-07 17:32:38 +0000

    working text input box

--- app/controllers/facilities_management/beta/supplier/offer_controller.rb
+++ app/controllers/facilities_management/beta/supplier/offer_controller.rb
@@ -6,19 +6,11 @@ module FacilitiesManagement
         before_action :set_page_detail
         before_action :set_page_model
 
-        def declined
-          @page_data[:contract_name] = 'School facilities London'
-          @page_data[:contract_number] = 'RM330-DA2234-2019'
-        end
+        def declined; end
 
-        def accepted
-          @page_data[:contract_name] = 'School facilities London'
-          @page_data[:contract_number] = 'RM330-DA2234-2019'
-        end
+        def accepted; end
 
         def respond_to_contract_offer
-          @page_data[:contract_name] = 'School facilities London'
-          @page_data[:contract_number] = 'RM330-DA2334-2019'
           @page_data[:buyer_name] = 'Coal Authority'
           @page_data[:form_text] = 'Do you accept the contract offer from Coal Authority?'
           @page_data[:checked] = ''
@@ -90,17 +82,8 @@ module FacilitiesManagement
               caption1: 'School facilities London',
               continuation_text: 'Confirm and continue',
               secondary_text: 'Cancel',
-              # TODO: add the link when the page is created - contract summary page
+              # TODO: add the link when the page is created
               secondary_url: '#'
-            },
-            respond_to_contract_offer: {
-              back_url: ccs_patterns_path,
-              back_text: 'Back',
-              back_label: 'Back',
-              page_title: 'Respond to the contract offer',
-              caption1: 'School facilities London',
-              continuation_text: 'Confirm and continue',
-              secondary_text: 'Cancel'
             }
           }.freeze
         end
