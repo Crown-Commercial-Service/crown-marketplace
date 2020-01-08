5e76ccb0e211945b362c259c563df09895a58ea1
Chee-Ng <chee.ng@crowncommercial.gov.uk>
2020-01-08 13:54:55 +0000

    developed supplier login page
    removing whitespace

--- app/controllers/facilities_management/beta/supplier/home_controller.rb
+++ app/controllers/facilities_management/beta/supplier/home_controller.rb
@@ -1,13 +1,13 @@
 module FacilitiesManagement
   module Beta
     module Supplier
-      class HomeController < FacilitiesManagement::Beta::Supplier::FrameworkController
+      class HomeController < FrameworkController
         before_action :authenticate_user!, except: :index
         before_action :authorize_user, except: :index
 
         def index
           if user_signed_in?
-            redirect_to facilities_management_beta_supplier_dashboard_index_path
+            redirect_to facilities_management_beta_supplier_offer_declined_path
           else
             redirect_to facilities_management_beta_supplier_new_user_session_path
           end
