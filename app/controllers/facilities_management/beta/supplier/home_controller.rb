c6ac6ea604de1ed4551403e9a5c32ac11c20b2b1
Chee-Ng <chee.ng@crowncommercial.gov.uk>
2020-01-08 13:54:55 +0000

    developed supplier login page
    removing whitespace

--- app/controllers/facilities_management/beta/supplier/home_controller.rb
+++ app/controllers/facilities_management/beta/supplier/home_controller.rb
@@ -7,7 +7,11 @@ module FacilitiesManagement
 
         def index
           if user_signed_in?
+<<<<<<< HEAD
             redirect_to facilities_management_beta_supplier_supplier_account_dashboard_path
+=======
+            redirect_to facilities_management_beta_supplier_offer_declined_path
+>>>>>>> developed supplier login page
           else
             redirect_to facilities_management_beta_supplier_new_user_session_path
           end
