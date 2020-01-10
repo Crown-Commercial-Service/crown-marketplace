e39874f3e4bd4ef52922a8e94d3d368e00496fda
ramchurnk <58424189+ramchurnk@users.noreply.github.com>
2020-01-10 13:21:32 +0000

    Update contract_summary_controller.rb

--- app/controllers/facilities_management/beta/supplier/contract_summary_controller.rb
+++ app/controllers/facilities_management/beta/supplier/contract_summary_controller.rb
@@ -12,6 +12,15 @@ module FacilitiesManagement
 
         def accepted_contract_offer; end
 
+        def declined_offer
+          offer_timestamp = '21 November 2019, 8:45pm'
+          reason = 'conflict of interest'
+          received_timestamp = '20 November 2019, 2:00pm'
+          @page_data[:status_message] = { status: "You declined this contract offer on #{offer_timestamp}.",
+                                          message: "Your reason for declining was: #{reason}",
+                                          message2: "This contract offer was received on #{received_timestamp}." }
+        end
+
         private
 
         def set_page_model
@@ -81,6 +90,15 @@ module FacilitiesManagement
               caption1: 'Cabinet office FM services',
               continuation_text: false,
               secondary_text: 'Return to dashboard'
+            },
+            declined_offer: {
+              back_url: facilities_management_beta_supplier_supplier_account_dashboard_path,
+              back_label: 'Back',
+              back_text: 'Back',
+              page_title: 'Contract summary',
+              caption1: 'Schools facilities London',
+              secondary_text: 'Return to dashboard',
+              secondary_url: facilities_management_beta_supplier_supplier_account_dashboard_path
             }
           }.freeze
         end
