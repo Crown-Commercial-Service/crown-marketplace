02d68c6847370157e08f64f44e4d4246b3b41553
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-01-10 12:13:29 +0000

    accepted contract view

--- app/controllers/facilities_management/beta/supplier/contract_summary_controller.rb
+++ app/controllers/facilities_management/beta/supplier/contract_summary_controller.rb
@@ -5,42 +5,17 @@ module FacilitiesManagement
         skip_before_action :authenticate_user!
         before_action :set_page_detail
         before_action :set_page_model
-<<<<<<< HEAD
-        
-        def received_contract_offer
-          @page_data[:procurement_data] = { contract_type: 'recieved', contract_name: 'School facilities London', buyer: 'Cabinet office', date_offer_expires: DateTime.new(2019, 7, 7, 8, 2, 0).in_time_zone('London'), contract_number: 'RM330-DA2234-2019', contract_value: '£752,026', framework: 'RM3830', sub_lot: 'sub-lot 1a', initial_call_off_period_start: DateTime.new(2019, 7, 7, 8, 2, 0).in_time_zone('London'), initial_call_off_period_end: DateTime.new(2026, 7, 7, 8, 2, 0).in_time_zone('London'), 
-            mobilisation_period_start: DateTime.new(2019, 10, 3, 8, 2, 0).in_time_zone('London'), mobilisation_period_end: DateTime.new(2019, 10, 31, 8, 2, 0).in_time_zone('London'),
-            optional_call_off_period_start_1: DateTime.new(2026, 10, 3, 8, 2, 0).in_time_zone('London'), optional_call_off_end_1: DateTime.new(2027, 10, 31, 8, 2, 0).in_time_zone('London'),
-            optional_call_off_period_start_2: DateTime.new(2027, 10, 3, 8, 2, 0).in_time_zone('London'), optional_call_off_end_2: DateTime.new(2028, 10, 31, 8, 2, 0).in_time_zone('London'),
-            buildings_and_services: [{building: 'Barton court store', services: []}, {building: 'CCS London office 5th floor', services: ['High voltage (HV) and switchgear maintenance', 'Locksmith services', 'Helpdesk services']}, {building: 'Phoenix house', services: []}, {building: 'Vale court', services: []}, {building: 'W Cabinet office 3rd floor', services: []}]
-          }
-          @page_data[:buyer_details] = { title: 'Miss', full_name: 'Evelyn Smith', telephone: '0300 821 4554', email: 'evelyn@cleaningltd.co.uk', building_name: 'Cleaning London LTD', street_name: '', city: 'London', county: '', postcode: 'SW1 1ET' }
-        end
-        
-        def accepted_contract_offer
-          @page_data[:procurement_data] = { contract_type: 'recieved', contract_name: 'School facilities London', buyer: 'Cabinet office', date_offer_expires: DateTime.new(2019, 7, 7, 8, 2, 0).in_time_zone('London'), contract_number: 'RM330-DA2234-2019', contract_value: '£752,026', framework: 'RM3830', sub_lot: 'sub-lot 1a', initial_call_off_period_start: DateTime.new(2019, 7, 7, 8, 2, 0).in_time_zone('London'), initial_call_off_period_end: DateTime.new(2026, 7, 7, 8, 2, 0).in_time_zone('London'), 
-            mobilisation_period_start: DateTime.new(2019, 10, 3, 8, 2, 0).in_time_zone('London'), mobilisation_period_end: DateTime.new(2019, 10, 31, 8, 2, 0).in_time_zone('London'),
-            optional_call_off_period_start_1: DateTime.new(2026, 10, 3, 8, 2, 0).in_time_zone('London'), optional_call_off_end_1: DateTime.new(2027, 10, 31, 8, 2, 0).in_time_zone('London'),
-            optional_call_off_period_start_2: DateTime.new(2027, 10, 3, 8, 2, 0).in_time_zone('London'), optional_call_off_end_2: DateTime.new(2028, 10, 31, 8, 2, 0).in_time_zone('London'),
-            buildings_and_services: [{building: 'Barton court store', services: []}, {building: 'CCS London office 5th floor', services: ['High voltage (HV) and switchgear maintenance', 'Locksmith services', 'Helpdesk services']}, {building: 'Phoenix house', services: []}, {building: 'Vale court', services: []}, {building: 'W Cabinet office 3rd floor', services: []}]
-          }
-          @page_data[:buyer_details] = { title: 'Miss', full_name: 'Evelyn Smith', telephone: '0300 821 4554', email: 'evelyn@cleaningltd.co.uk', building_name: 'Cleaning London LTD', street_name: '', city: 'London', county: '', postcode: 'SW1 1ET' }
-        end
-=======
 
         def received_contract_offer; end
 
         def live_contract; end
->>>>>>> 677300b0eca67cc9202034d63bd3939f974a65bf
+
+        def accepted_contract_offer; end
 
         private
 
         def set_page_model
-<<<<<<< HEAD
-          @page_data[:model_object] = FacilitiesManagement::Supplier::SupplierAccount.new
-=======
           @page_data[:model_object] = nil
->>>>>>> 677300b0eca67cc9202034d63bd3939f974a65bf
         end
 
         # rubocop:disable Metrics/AbcSize
@@ -60,8 +35,6 @@ module FacilitiesManagement
                                                page_details(action_name)[:secondary_url],
                                                page_details(action_name)[:secondary_text])
           )
-<<<<<<< HEAD
-=======
           @page_data[:procurement_data] = { contract_name: 'School facilities London', buyer: 'Cabinet office', date_offer_expires: DateTime.new(2019, 7, 7, 8, 2, 0).in_time_zone('London'), contract_number: 'RM330-DA2234-2019', contract_value: '£752,026', framework: 'RM3830', sub_lot: 'sub-lot 1a',
                                             initial_call_off_period_start: Date.new(2019, 11, 1), initial_call_off_period_end: Date.new(2016, 10, 31),
                                             date_contract_received: DateTime.new(2019, 11, 20, 14, 0, 0).in_time_zone('London'), date_contract_accepted: DateTime.new(2019, 6, 23, 14, 20, 0).in_time_zone('London'),
@@ -71,7 +44,6 @@ module FacilitiesManagement
                                             buildings_and_services: [{ building: 'Barton court store', services: [] }, { building: 'CCS London office 5th floor', services: ['High voltage (HV) and switchgear maintenance', 'Locksmith services', 'Helpdesk services'] }, { building: 'Phoenix house', services: [] }, { building: 'Vale court', services: [] }, { building: 'W Cabinet office 3rd floor', services: [] }] }
           @page_data[:buyer_details] = { title: 'Miss', full_name: 'Evelyn Smith', telephone: '0300 821 4554', email: 'evelyn@cleaningltd.co.uk', building_name: 'Cleaning London LTD', street_name: '', city: 'London', county: '', postcode: 'SW1 1ET' }
           @page_data[:call_off_documents_creation_date] = DateTime.new(2019, 5, 14, 10, 47, 0).in_time_zone('London')
->>>>>>> 677300b0eca67cc9202034d63bd3939f974a65bf
         end
 
         def page_details(action)
@@ -85,8 +57,6 @@ module FacilitiesManagement
               back_label: 'Return to prototype index',
               back_text: 'View prototypes'
             },
-<<<<<<< HEAD
-=======
             live_contract: {
               back_url: facilities_management_beta_supplier_supplier_account_dashboard_path,
               back_label: 'Back',
@@ -96,13 +66,21 @@ module FacilitiesManagement
               secondary_text: 'Return to dashboard',
               secondary_url: facilities_management_beta_supplier_supplier_account_dashboard_path
             },
->>>>>>> 677300b0eca67cc9202034d63bd3939f974a65bf
             received_contract_offer: {
               back_url: facilities_management_beta_supplier_supplier_account_dashboard_path,
               back_label: 'Back',
               back_text: 'Back',
               page_title: 'Contract summary',
               caption1: 'Schools facilities London'
+            },
+            accepted_contract_offer: {
+              back_url: facilities_management_beta_supplier_supplier_account_dashboard_path,
+              back_label: 'Back',
+              back_text: 'Back',
+              page_title: 'Contract summary',
+              caption1: 'Cabinet office FM services',
+              continuation_text: false,
+              secondary_text: 'Return to dashboard'
             }
           }.freeze
         end
