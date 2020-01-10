1435c06d4209b2b93dfed84867d2d58806db983f
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-01-10 12:13:29 +0000

    accepted contract view

--- config/routes.rb
+++ config/routes.rb
@@ -160,6 +160,7 @@ Rails.application.routes.draw do
         get 'offer-accepted', to: 'offer#accepted'
         get 'supplier-account-dashboard', to: 'supplier_account#index'
         get 'contract-summary/received-contract-offer', to: 'contract_summary#received_contract_offer'
+        get 'contract-summary/live-contract', to: 'contract_summary#live_contract'
         get 'contract-summary/accepted-contract-offer', to: 'contract_summary#accepted_contract_offer'
       end
     end
