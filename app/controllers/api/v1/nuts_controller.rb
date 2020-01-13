fd9b30db89a626ed961603fb23afb022f6664c76
Mo <mo-zag@users.noreply.github.com>
2020-01-13 14:48:25 +0000

    Choose region
    If no region is found for a post code we check the region via dropdown.

--- app/controllers/api/v1/nuts_controller.rb
+++ app/controllers/api/v1/nuts_controller.rb
@@ -24,36 +24,11 @@ module Api
       def find_region_query
         postcode_to_str = params['postcode'].to_s
         postcode_to_region = postcode_to_str[0, 3]
-        postcode_to_region_rm_white_space = postcode_to_region.delete(' ')
 
-        result = Postcode::PostcodeChecker.find_region postcode_to_region_rm_white_space
+        result = Postcode::PostcodeChecker.find_region postcode_to_region
         render json: { status: 200, result: result }
       rescue StandardError => e
         render json: { status: 404, error: e.to_s }
-      end
-
-      def find_region_query_by_postcode
-        result = get_region_postcode params['postcode']
-        if result.length.positive?
-        else
-          result = get_region_by_prefix params['postcode']
-        end
-        render json: { status: 200, result: result }
-      rescue StandardError => e
-        render json: { status: 404, error: e.to_s }
-      end
-
-      def get_region_by_prefix(postcode)
-        original_postcode_to_str = postcode.to_s
-        original_postcode_to_region = original_postcode_to_str[0, 3]
-        original_postcode_rm_white_space = original_postcode_to_region.delete(' ')
-        Postcode::PostcodeChecker.find_region original_postcode_rm_white_space
-      end
-
-      def get_region_postcode(postcode)
-        postcode_to_str = postcode.to_s
-        postcode_to_region_rm_white_space = postcode_to_str.delete(' ')
-        Postcode::PostcodeChecker.find_region postcode_to_region_rm_white_space
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -140,13 +140,13 @@ Rails.application.routes.draw do
       get '/start', to: 'journey#start', as: 'journey_start'
       get 'spreadsheet-test', to: 'spreadsheet_test#index', as: 'spreadsheet_test'
       get 'spreadsheet-test/dm-spreadsheet-download', to: 'spreadsheet_test#dm_spreadsheet_download', as: 'dm_spreadsheet_download'
-      get '/direct-award/sending-the-contract', to: 'direct_award_contract#sending_the_contract'
-      get '/direct-award/review-and-generate-documents', to: 'direct_award_contract#review_and_generate_documents'
-      get '/direct-award/awaiting-response', to: 'direct_award_contract#show'
       resources :procurements do
+        post 'continue'
+        get 'summary'
         get 'results'
         get 'direct_award_pricing'
         get 'further_competition'
+        put 'set_route_to_market'
       end
       resources :procurement_buildings, only: %i[show edit update]
       resources :procurement_buildings_services, only: %i[show update]
@@ -159,13 +159,13 @@ Rails.application.routes.draw do
         get 'respond-to-contract-offer', to: 'offer#respond_to_contract_offer'
         get 'offer-accepted', to: 'offer#accepted'
         get 'supplier-account-dashboard', to: 'supplier_account#index'
-        get 'contract-summary/received-contract-offer', to: 'contract_summary#received_contract_offer'
-        get 'contract-summary/contract-withdrawn', to: 'contract_summary#contract_withdrawn'
-        get 'contract-summary/live-contract', to: 'contract_summary#live_contract'
-        get 'contract-summary/accepted-contract-offer', to: 'contract_summary#accepted_contract_offer'
-        get 'contract-summary/not-signed-offer', to: 'contract_summary#not_signed_offer'
-        get 'contract-summary/declined-offer', to: 'contract_summary#declined_offer'
-        get 'contract-summary/not-responded-to-contract-offer', to: 'contract_summary#not_responded_to_contract_offer'
+        get 'contract-summary/received-contract-offer', to: 'supplier_account#show'
+        get 'contract-summary/accepted-contract-offer', to: 'supplier_account#show'
+        get 'contract-summary/declined-offer', to: 'supplier_account#show'
+        get 'contract-summary/live-contract', to: 'supplier_account#show'
+        get 'contract-summary/not-responded', to: 'supplier_account#show'
+        get 'contract-summary/not-signed', to: 'supplier_account#show'
+        get 'contract-summary/contract-withdrawn', to: 'supplier_account#show'
       end
     end
 
@@ -341,7 +341,6 @@ Rails.application.routes.draw do
       get '/search-postcode/:postcode', to: 'nuts#show_post_code'
       get '/serach-nuts-code/:code', to: 'nuts#show_nuts_code'
       get '/find-region/:postcode', to: 'nuts#find_region_query'
-      get '/find-region-postcode/:postcode', to: 'nuts#find_region_query_by_postcode'
     end
   end
 
