a543c050dca5bea853ca4c6cc6f5c88d1f2e9b92
Mo <mo-zag@users.noreply.github.com>
2020-01-13 14:48:25 +0000

    Choose region
    If no region is found for a post code we check the region via dropdown.

--- app/controllers/api/v1/nuts_controller.rb
+++ app/controllers/api/v1/nuts_controller.rb
@@ -5,6 +5,8 @@ module Api
     # return json
     class NutsController < ApplicationController
       protect_from_forgery with: :exception
+      before_action :authenticate_user!
+
       def show_post_code
         result = PostcodesNutsRegions.select(:id, :code, :postcode).find_by(postcode: params[:postcode].delete(' '))
         render json: { status: 200, result: result }
@@ -14,6 +16,16 @@ module Api
 
       def show_nuts_code
         result = PostcodesNutsRegions.select(:id, :code, :postcode).find_by(code: params[:code])
+        render json: { status: 200, result: result }
+      rescue StandardError => e
+        render json: { status: 404, error: e.to_s }
+      end
+
+      def find_region_query
+        postcode_to_str = params['postcode'].to_s
+        postcode_to_region = postcode_to_str[0, 3]
+
+        result = Postcode::PostcodeChecker.find_region postcode_to_region
         render json: { status: 200, result: result }
       rescue StandardError => e
         render json: { status: 404, error: e.to_s }
