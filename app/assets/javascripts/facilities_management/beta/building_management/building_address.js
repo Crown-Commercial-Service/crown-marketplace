e2849428255bdbe8c95c8b463cbbe39796d4f45f
Mo <mo-zag@users.noreply.github.com>
2020-01-16 13:40:25 +0000

    Select region in from postcode
    User can select a region in postcodes now from cant find your address screen.

--- app/assets/javascripts/facilities_management/beta/building_management/building_address.js
+++ app/assets/javascripts/facilities_management/beta/building_management/building_address.js
@@ -17,11 +17,20 @@ $(function () {
         let town = $('#fm-bm-address-town').val();
         let county = $('#fm-bm-address-county').val();
         let postcode = $('#fm-bm-address-postcode').val();
+        let region = (typeof $('#fm-bm-region-dropdown').val() != 'undefined')? $('#fm-bm-region-dropdown').val():'';
         let errorMsg = '';
         let isValid = true;
         let elemID = '';
 
         clearErrors();
+
+
+
+        if (region.length == 0) {
+            errorMsg = 'Select your Region';
+            isValid = false;
+            elemID = 'fm-bm-region-dropdown';
+        }
 
         if (!postcode || pageUtils.isPostCodeValid(postcode) === false) {
             errorMsg = 'Enter a real postcode, like AA1 1AA';
@@ -49,6 +58,46 @@ $(function () {
 
     };
 
+    const searchForRegion = function (postCode) {
+
+        $.get(encodeURI("/api/v1/find-region-postcode/" + postCode))
+            .done(function (data) {
+                $('#fm-bm-region-dropdown').remove();
+                if (data && data.result) {
+                    let regions = data.result;
+                    var select_dropdown = '<select id="fm-bm-region-dropdown" class="govuk-select govuk-!-width-two-thirds govuk-!-margin-top-0"></select>';
+                    $('#fm-bm-address-region-display').html(select_dropdown);
+                    var region_text = (data.result.length > 1) ? " Region's found" : " Region found";
+                    select_dropdown_default_text = '<option value="">' + data.result.length + region_text + '</option>';
+                    $('#fm-bm-region-dropdown').append(select_dropdown_default_text);
+
+                    for (let x = 0; x < regions.length; x++) {
+                        let region_obj = regions[x];
+                        select_dropdown_options = '<option value="' + region_obj.region + '">' + region_obj.region + '</option>';
+                        $('#fm-bm-region-dropdown').append(select_dropdown_options);
+                    }
+
+                    $('.fm-bm-region-txt').show();
+
+                }
+
+            })
+            .fail(function (data) {
+                console.log(data);
+            });
+    };
+
+    $('#fm-bm-address-wrapper').on('keyup', '#fm-bm-address-postcode', function (e) {
+        e.preventDefault();
+        var postcode = $(this).val();
+
+        if (postcode.length > 1 ) {
+            searchForRegion($(this).val());
+        }
+        
+    });
+
+
     $('#fm-bm-address-save-and-continue').on('click', function (e) {
         let isValid = validateAddress();
 
@@ -74,12 +123,15 @@ $(function () {
         let town = $('#fm-bm-address-town').val();
         let county = $('#fm-bm-address-county').val();
         let postcode = $('#fm-bm-address-postcode').val();
+        let region = $('#fm-bm-region-dropdown').val();
 
         address['fm-address-line-1'] = address_line_one;
         address['fm-address-line-2'] = address_line_two;
         address['fm-address-town'] = town;
         address['fm-address-county'] = county;
         address['fm-address-postcode'] = postcode;
+        address['fm-address-region'] = region;
+        
 
         $.ajax({
             url: 'save-address',
