0c0ab7fc7d0c912a44381e86924758b6e43956c2
Iulia R Ionita <iulia@ionita.me>
2020-01-17 16:51:12 +0000

    Added the postcodes to nuts import to a background process

--- app/workers/facilities_management/postcodes_to_nuts_worker.rb
+++ app/workers/facilities_management/postcodes_to_nuts_worker.rb
@@ -1,14 +1,13 @@
+require 'rake'
 module FacilitiesManagement
   class PostcodesToNutsWorker
     include Sidekiq::Worker
-    sidekiq_options queue: 'fm', retry: false, backtrace: 20
+    sidekiq_options queue: 'fm'
 
-    def perform(data_file)
-      logger.info 'Starting PostodesNutsRegions import'
-      CSV.foreach(data_file) do |row|
-        logger.info "Importing #{row}"
-        ActiveRecord::Base.connection.execute("INSERT into postcodes_nuts_regions (postcode, code, created_at, updated_at) values('" + row[0].delete(' ') + "', '" + row[1] + "', NOW(), NOW())")
-      end
+    def perform
+      Rake::Task.clear
+      Rails.application.load_tasks
+      Rake::Task['db:importpostcoderegion'].invoke
     end
   end
 end
