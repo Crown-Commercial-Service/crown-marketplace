2edd11a0dad2f4f8289e1d1f6edc77ccf8247f0f
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-01-20 15:47:07 +0000

    Created table for live contracts on the dashboard

--- app/helpers/facilities_management/beta/procurements_helper.rb
+++ app/helpers/facilities_management/beta/procurements_helper.rb
@@ -99,7 +99,7 @@ module FacilitiesManagement::Beta::ProcurementsHelper
                         no_supplier_response: 'No supplier response',
                         awaiting_contract_signature: 'Awaiting contract signature',
                         accepted_not_signed: 'Accepted not signed',
-                        closed: 'closed' }.freeze
+                        accepted_and_signed: 'Accepted and signed' }.freeze
 
   def procurement_state(procurement_state)
     return procurement_state.humanize unless PROCUREMENT_STATE.key?(procurement_state.to_sym)
