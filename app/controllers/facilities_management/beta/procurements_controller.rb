5f1bc35f3481f4a8cc946358c6c206551e2a0b2e
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-01-20 15:47:07 +0000

    Created table for live contracts on the dashboard

--- app/controllers/facilities_management/beta/procurements_controller.rb
+++ app/controllers/facilities_management/beta/procurements_controller.rb
@@ -16,6 +16,7 @@ module FacilitiesManagement
         @searches = current_user.procurements.where(aasm_state: FacilitiesManagement::Procurement::SEARCH)
         @sent_offers = current_user.procurements.where(aasm_state: FacilitiesManagement::Procurement::SENT_OFFER)
         @in_draft = current_user.procurements.where(aasm_state: :DA_draft)
+        @contracts = current_user.procurements.where(aasm_state: :accepted_and_signed)
       end
 
       def show
