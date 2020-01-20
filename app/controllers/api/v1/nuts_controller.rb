888f5571d6fba7d49b5cdc44292e75582c476522
Mo <mo-zag@users.noreply.github.com>
2020-01-20 11:10:08 +0000

    Update nuts_controller.rb
    Resulved rubocop

--- app/controllers/api/v1/nuts_controller.rb
+++ app/controllers/api/v1/nuts_controller.rb
@@ -33,14 +33,11 @@ module Api
       end
 
       def find_region_query_by_postcode
-
         result = get_region_postcode params['postcode']
-
-        if result.length() > 0
+        if result.length.positive?
         else
           result = get_region_by_prefix params['postcode']
         end
-
         render json: { status: 200, result: result }
       rescue StandardError => e
         render json: { status: 404, error: e.to_s }
@@ -50,15 +47,13 @@ module Api
         original_postcode_to_str = postcode.to_s
         original_postcode_to_region = original_postcode_to_str[0, 3]
         original_postcode_rm_white_space = original_postcode_to_region.delete(' ')
-        result = Postcode::PostcodeChecker.find_region original_postcode_rm_white_space
-        return result
+        Postcode::PostcodeChecker.find_region original_postcode_rm_white_space
       end
 
       def get_region_postcode(postcode)
         postcode_to_str = postcode.to_s
         postcode_to_region_rm_white_space = postcode_to_str.delete(' ')
-        result = Postcode::PostcodeChecker.find_region postcode_to_region_rm_white_space
-        return result     
+        Postcode::PostcodeChecker.find_region postcode_to_region_rm_white_space
       end
     end
   end
