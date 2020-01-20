c61185f54add4260b3ddbaab27dc0ac70d655901
Iulia R Ionita <iulia@ionita.me>
2020-01-20 15:22:53 +0000

    Changed migration to recreate the fm_rates table
    Added default timpestamp, otherwise rake db:static fails
    
    added base view
    
    added closed contract state
    
    adding closed date

--- db/migrate/20200113193447_change_fm_rates_attributes.rb
+++ db/migrate/20200113193447_change_fm_rates_attributes.rb
@@ -1,19 +1,15 @@
 class ChangeFmRatesAttributes < ActiveRecord::Migration[5.2]
-  def up
-    add_column :fm_rates, :standard, :string, limit: 1
-    add_column :fm_rates, :direct_award, :boolean
-    change_column :fm_rates, :code, :string, unique: false, limit: 5
-    remove_foreign_key(:fm_rates, name: 'fm_rates_code_key') if foreign_key_exists?(:fm_rates, name: 'fm_rates_code_key')
-    remove_index :fm_rates, ['code'] if index_exists?(:fm_rates, ['code'])
-    remove_index :fm_rates, name: 'fm_rates_code_key' if index_exists?(:fm_rates, name: 'fm_rates_code_key')
-    add_index :fm_rates, ['code'], unique: false
-  end
+  def change
+    # recreating fm_rates table with force:true so it removes the previous table first
+    create_table :fm_rates, id: :uuid, force: true do |t|
+      t.string :code, limit: 5
+      t.decimal :framework
+      t.decimal :benchmark
+      t.string :standard, limit: 1
+      t.boolean :direct_award
 
-  def down
-    remove_column :fm_rates, :standard, :string
-    remove_column :fm_rates, :direct_award, :boolean
-    change_column :fm_rates, :code, :text
-    remove_index :fm_rates, ['code']
-    add_index :fm_rates, ['code']
+      t.datetime 'created_at', default: -> { 'CURRENT_TIMESTAMP' }
+      t.datetime 'updated_at', default: -> { 'CURRENT_TIMESTAMP' }
+    end
   end
 end
