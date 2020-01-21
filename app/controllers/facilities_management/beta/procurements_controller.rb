0011e7261b15e0944bfbeee3f3536516cb03f7dc
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-01-21 13:56:42 +0000

    default changes in migration file and controller

--- app/controllers/facilities_management/beta/procurements_controller.rb
+++ app/controllers/facilities_management/beta/procurements_controller.rb
@@ -14,7 +14,7 @@ module FacilitiesManagement
       def index
         @procurements = current_user.procurements
         @searches = current_user.procurements.where(aasm_state: FacilitiesManagement::Procurement::SEARCH)
-        @sent_offers = current_user.procurements.where(aasm_state: FacilitiesManagement::Procurement::SENT_OFFER)
+        @sent_offers = current_user.procurements.where(aasm_state: FacilitiesManagement::Procurement::SENT_OFFER, is_contract_closed: false)
         @in_draft = current_user.procurements.DA_draft
         @contracts = current_user.procurements.where(aasm_state: :accepted_and_signed)
         @closed_contracts = current_user.procurements.where(is_contract_closed: true)
