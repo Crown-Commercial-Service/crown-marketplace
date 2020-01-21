e59c54d6545e5cc2fbf3300b2d4b076d52af2869
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-01-21 13:56:42 +0000

    default changes in migration file and controller

--- db/migrate/20200121102051_add_contract_closed_information_to_facilities_management_procurements.rb
+++ db/migrate/20200121102051_add_contract_closed_information_to_facilities_management_procurements.rb
@@ -1,6 +1,6 @@
 class AddContractClosedInformationToFacilitiesManagementProcurements < ActiveRecord::Migration[5.2]
   def change
     add_column :facilities_management_procurements, :closed_contract_date, :date
-    add_column :facilities_management_procurements, :is_contract_closed, :boolean
+    add_column :facilities_management_procurements, :is_contract_closed, :boolean, default: false
   end
 end
