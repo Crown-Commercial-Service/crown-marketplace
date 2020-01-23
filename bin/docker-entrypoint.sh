4fa58a584f1ed6a767f8df07292917d3e809117e
Iulia R Ionita <iulia@ionita.me>
2020-01-23 10:12:46 +0000

    Temporarily stop sidekiq and the bg job from running

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -5,8 +5,8 @@ bundle exec rails db:migrate
 bundle exec rails db:static
 # temporarily disabling this as it's giving AccessDenied from S3
 #bundle exec rails db:postcode
-bundle exec rails db:run_postcodes_to_nuts_worker
+#bundle exec rails db:run_postcodes_to_nuts_worker
 
-bundle exec sidekiq -C ./config/sidekiq.yml -d -L ./log/sidekiq.log -e production if ENV['APP_RUN_SIDEKIQ'].present?
+#bundle exec sidekiq -C ./config/sidekiq.yml -d -L ./log/sidekiq.log -e production if ENV['APP_RUN_SIDEKIQ'].present?
 
 bundle exec rails server
