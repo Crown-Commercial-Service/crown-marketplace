067713afe8f33e01b3c793014e8e5d1756fbe1b2
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-01-23 10:38:35 +0000

    allowed payment method to be nullable because of migration issue

--- db/migrate/20200123092237_add_payment_methods_to_faciilities_management_procurements.rb
+++ db/migrate/20200123092237_add_payment_methods_to_faciilities_management_procurements.rb
@@ -1,5 +1,5 @@
 class AddPaymentMethodsToFaciilitiesManagementProcurements < ActiveRecord::Migration[5.2]
   def change
-    add_column :facilities_management_procurements, :payment_method, :string, null: false
+    add_column :facilities_management_procurements, :payment_method, :string
   end
 end
