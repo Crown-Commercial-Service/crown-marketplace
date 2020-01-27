1e808be8f6c61a251eb6c4a28aa37d2f0b89aeae
Chee-Ng <chee.ng@crowncommercial.gov.uk>
2020-01-27 15:59:35 +0000

    added shoulda-matchers gem created new procurement_contact_detail table created associated models created factories for models created rspec for testing of each models, including parent class

---
+++ spec/models/facilities_management/procurement_notices_contact_detail_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe FacilitiesManagement::ProcurementNoticesContactDetail, type: :model do
+  let(:procurement_notices_contact_detail) { create(:facilities_management_procurement_notices_contact_detail) }
+
+  describe 'associations' do
+    it { should belong_to(:procurement).class_name('FacilitiesManagement::Procurement') }
+  end
+end
