1d7add678473889f88a0dede8f7c160d1c094138
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-01-30 13:38:41 +0000

    Created specs for the validation

--- spec/factories/facilities_management_procurement_pension_funds.rb
+++ spec/factories/facilities_management_procurement_pension_funds.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :facilities_management_procurement_pension_fund, class: FacilitiesManagement::ProcurementPensionFund do
     name { Faker::Name.unique.name[0..150] }
-    percentage { rand 1..100 }
+    percentage { rand(1..100).to_s }
   end
 end
