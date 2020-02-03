d23e0b22c9aacbd6b9d65a8c7d4662430c8752ab
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-02-03 23:15:55 +0000

    Local government pension scheme error messages
    Validation spec for pension funds
    
    Put form error in the right place
    
    Back end validation for pension funds with messages
    
    Javascript for front end validation

--- app/models/facilities_management/procurement_pension_fund.rb
+++ app/models/facilities_management/procurement_pension_fund.rb
@@ -1,5 +1,8 @@
 module FacilitiesManagement
   class ProcurementPensionFund < ApplicationRecord
     belongs_to :procurement, class_name: 'FacilitiesManagement::Procurement', foreign_key: :facilities_management_procurement_id, inverse_of: :procurement_pension_funds, optional: true
+    validates :name, :percentage, presence: true
+    validates :name, length: { maximum: 150 }
+    validates :percentage, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
   end
 end
