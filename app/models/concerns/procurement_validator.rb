00cc42bc17af2e3dc0557ae79d20a8d29288353f
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-02-03 10:10:22 +0000

    added validation for notices page

--- app/models/concerns/procurement_validator.rb
+++ app/models/concerns/procurement_validator.rb
@@ -32,6 +32,8 @@ module ProcurementValidator
 
     validates :using_buyer_detail_for_authorised_detail, inclusion: { in: [true, false] }, on: %i[authorised_representative]
 
+    validates :using_buyer_detail_for_notices_detail, inclusion: { in: [true, false] }, on: %i[notices_contact_details]
+
     #############################################
     # Validation rules for contract-dates
     # these rules need to cover
