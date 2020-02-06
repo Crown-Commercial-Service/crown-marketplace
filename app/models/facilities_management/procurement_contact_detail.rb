7933b732f7d5e420eb6bdcca536b7a0839086a37
Julian Young <52413509+ManFromMons@users.noreply.github.com>
2020-02-06 16:22:39 +0000

    Feature/fm da journey integration (#1334)
    * Correcting issues for 500 error
    
    * Added code missing from merge
    
    Journey and dashboard behaving properly
    
    * Rubocop space missing and method name too long
    
    * removed redundant code and added individual back link
    
    * links
    
    * link
    
    * missing params and spell check
    
    * amended controller so validation works, changed formatting of invoicing page for language file, added text to lang file
    
    * added new method for rubocop
    
    * merge conflicts and lost pension stuff in the language file
    
    * i18n-tasks normalize
    
    * Fixed up sec policy doc, requirements view, pensions are now in play along with payment method
    
    * make travis safe
    
    * display correct page when user click on differnt button on the invoice contact detail page
    write to db when user submit
    
    the invoicing contact detail will be functional but dropbox for postcode still need to be finish
    
    Full working prototype- checkpoint
    
    text inserted in locale file
    
    * rubocop...
    
    * Changed status text
    
    * Remove orig file!
    
    * Corrected info display of pension status in contract details
    
    * red box around user input appear when needed
    error message inserted in locale file
    
    * display error bar if user didnt select any option for invoice contact detail
    
    * Safe nav through params in the controller
    
    * Remove empty file
    
    * Remove bug from Update action
    
    * Corrected logic typo
    
    * Merge conflict
    
    Co-authored-by: ramchurnk <58424189+ramchurnk@users.noreply.github.com>
    Co-authored-by: Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>

--- app/models/facilities_management/procurement_contact_detail.rb
+++ app/models/facilities_management/procurement_contact_detail.rb
@@ -1,15 +1,20 @@
 module FacilitiesManagement
   class ProcurementContactDetail < ApplicationRecord
-    validates :name, presence: true, format: { with: /\A([a-zA-Z\- ]*[\.]?[a-zA-Z\- ]*)\z/ }, length: { maximum: 50 }, on: :update
-    validates :job_title, presence: true, format: { with: /\A([a-zA-Z\- ]*)\z/ }, length: { maximum: 150 }, on: :update
+    validates :name, presence: true, format: { with: /\A([a-zA-Z\- ]*[\.]?[a-zA-Z\- ]*)\z/ }, length: { maximum: 50 }, on: :new_invoicing_contact_details
+    validates :job_title, presence: true, format: { with: /\A([a-zA-Z\- ]*)\z/ }, length: { maximum: 150 }, on: :new_invoicing_contact_details
+    validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, on: :new_invoicing_contact_details
 
-    validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, on: :update
-    validates :organisation_address_line_1, presence: true, format: { with: /\A([a-zA-Z\- (0-9)]*)\z/ }, on: :update
-    validates :organisation_address_town, presence: true, format: { with: /\A([a-zA-Z\- ]*)\z/ }, on: :update
-    validates :organisation_address_postcode, presence: true, format: { with: /\A([a-zA-Z (0-9)]*)\z/ }, on: :update
+    validates :organisation_address_line_1, presence: true, format: { with: /\A([a-zA-Z\- (0-9)]*)\z/ }, on: :new_invoicing_address
+    validates :organisation_address_town, presence: true, format: { with: /\A([a-zA-Z\- ]*)\z/ }, on: :new_invoicing_address
+    validates :organisation_address_postcode, presence: true, format: { with: /\A([a-zA-Z (0-9)]*)\z/ }, on: :new_invoicing_address
+    validates :organisation_address_postcode, presence: true, format: { with: /\A([a-zA-Z (0-9)]*)\z/ }, on: :new_invoicing_contact_details
 
-    def full_organisation_address
-      "#{organisation_address_line_1}#{', ' + organisation_address_line_2 if organisation_address_line_2.present?}#{', ' + organisation_address_town}#{', ' + organisation_address_county if organisation_address_county.present?}#{', ' + organisation_address_postcode}"
+    def contact_address
+      "#{organisation_address_line_1}#{', ' + organisation_address_line_2 if organisation_address_line_2&.present?}#{', ' + organisation_address_town}#{', ' + organisation_address_county if organisation_address_county&.present?}#{', ' + organisation_address_postcode}"
+    end
+
+    def valid_contact_address?
+      organisation_address_line_1&.present? && organisation_address_town&.present? && organisation_address_postcode&.present?
     end
   end
 end
