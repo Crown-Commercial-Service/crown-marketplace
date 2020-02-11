e7d7d0ac572a02e7a731ebeefb70e216bc1e5ee6
Clarence-Brown <clarence.brown@crowncommercial.gov.uk>
2020-02-11 14:43:18 +0000

    removed restrictions on buyer detail and contact detail input fields

--- app/models/facilities_management/procurement_contact_detail.rb
+++ app/models/facilities_management/procurement_contact_detail.rb
@@ -1,17 +1,17 @@
 module FacilitiesManagement
   class ProcurementContactDetail < ApplicationRecord
-    validates :name, presence: true, format: { with: /\A([a-zA-Z\- ]*[\.]?[a-zA-Z\- ]*)\z/ }, length: { maximum: 50 }, on: %i[new_invoicing_contact_details new_authorised_representative_details new_notices_contact_details]
-    validates :job_title, presence: true, format: { with: /\A([a-zA-Z\- ]*)\z/ }, length: { maximum: 150 }, on: %i[new_invoicing_contact_details new_authorised_representative_details new_notices_contact_details]
+    validates :name, presence: true, length: { maximum: 50 }, on: %i[new_invoicing_contact_details new_authorised_representative_details new_notices_contact_details]
+    validates :job_title, presence: true, length: { maximum: 150 }, on: %i[new_invoicing_contact_details new_authorised_representative_details new_notices_contact_details]
     validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, on: %i[new_invoicing_contact_details new_authorised_representative_details new_notices_contact_details]
     validates :organisation_address_postcode, presence: true, format: { with: /\A([a-zA-Z (0-9)]*)\z/ }, on: %i[new_invoicing_contact_details new_authorised_representative_details new_notices_contact_details]
     validate  :address_entered_when_postcode_provided, on: %i[new_invoicing_contact_details new_authorised_representative_details new_notices_contact_details]
 
-    validates :organisation_address_line_1, presence: true, format: { with: /\A([a-zA-Z\- (0-9)]*)\z/ }, on: %i[new_invoicing_address new_authorised_representative_address new_notices_address]
-    validates :organisation_address_town, presence: true, format: { with: /\A([a-zA-Z\- ]*)\z/ }, on: %i[new_invoicing_address new_authorised_representative_address new_notices_address]
+    validates :organisation_address_line_1, presence: true, on: %i[new_invoicing_address new_authorised_representative_address new_notices_address]
+    validates :organisation_address_town, presence: true, on: %i[new_invoicing_address new_authorised_representative_address new_notices_address]
     validates :organisation_address_postcode, presence: true, format: { with: /\A([a-zA-Z (0-9)]*)\z/ }, on: %i[new_invoicing_address new_authorised_representative_address new_notices_address]
 
     def contact_address
-      "#{organisation_address_line_1}#{', ' + organisation_address_line_2 if organisation_address_line_2&.present?}#{', ' + organisation_address_town}#{', ' + organisation_address_county if organisation_address_county&.present?}#{', ' + organisation_address_postcode}"
+      [organisation_address_line_1, organisation_address_line_2, organisation_address_town, organisation_address_county, organisation_address_postcode].reject(&:nil?).reject(&:empty?).join(', ')
     end
 
     def valid_contact_address?
