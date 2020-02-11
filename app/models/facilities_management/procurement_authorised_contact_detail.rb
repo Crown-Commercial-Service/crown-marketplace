364d7fd5de77ac8c9e92580cee51d41ee1b3b4fd
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-02-11 09:11:53 +0000

    Fixed validation for telephone number

--- app/models/facilities_management/procurement_authorised_contact_detail.rb
+++ app/models/facilities_management/procurement_authorised_contact_detail.rb
@@ -1,6 +1,6 @@
 module FacilitiesManagement
   class ProcurementAuthorisedContactDetail < ProcurementContactDetail
     belongs_to :procurement, class_name: 'FacilitiesManagement::Procurement', foreign_key: :facilities_management_procurement_id, inverse_of: :authorised_contact_detail
-    validates :telephone_number, presence: true, format: { with: /\A[\s()\d-]{10,14}\d\z/ }, length: { maximum: 15 }
+    validates :telephone_number, presence: true, format: { with: /\A[\s()\d-]{10,14}\d\z/ }, length: { maximum: 15 }, on: :new_authorised_representative_details
   end
 end
