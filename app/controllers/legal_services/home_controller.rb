4ef5fbc540f9fe3547d807e14d065f7cefdc5145
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-02-12 11:47:21 +0000

    changed back links in prototype controller, caption3 optional in layout helper
    created basic view for page
    
    We should specify full name of framework controller when inheriting from it
    
    Added capiton3 param wherever needed
    
    added procurement in return url
    
    hard coded wording into the view
    
    table
    
    formatting
    
    some changes

--- app/controllers/legal_services/home_controller.rb
+++ app/controllers/legal_services/home_controller.rb
@@ -1,5 +1,5 @@
 module LegalServices
-  class HomeController < FrameworkController
+  class HomeController < LegalServices::FrameworkController
     before_action :authenticate_user!, except: :index
     before_action :authorize_user, except: :index
 
