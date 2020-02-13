c8d256d3aa5d076264aea2e85005d9cc7b2f7695
Ubuntu <ubuntu@ip-172-31-7-62.us-east-2.compute.internal>
2020-02-13 14:15:47 +0000

    Create Accepted and signed page

--- app/controllers/facilities_management/beta/procurement_direct_award_contract_controller.rb
+++ app/controllers/facilities_management/beta/procurement_direct_award_contract_controller.rb
@@ -21,6 +21,7 @@ module FacilitiesManagement
         @page_data[:procurement_data] = { contract_name: 'School facilities London', supplier: 'Cleaning London LTD', date_offer_expires: DateTime.new(2019, 7, 7, 8, 2, 0).in_time_zone('London'), contract_number: 'RM330-DA2234-2019', contract_value: 752026, framework: 'RM3830', sub_lot: 'sub-lot 1a',
                                           initial_call_off_period: 7, initial_call_off_start_date: Date.new(2019, 11, 1), initial_call_off_end_date: Date.new(2016, 10, 31),
                                           contract_sent_date: DateTime.new(2019, 6, 22, 14, 20, 0).in_time_zone('London'), contract_accepted_date: DateTime.new(2019, 6, 23, 12, 20, 0).in_time_zone('London'), expiration_date: DateTime.new(2019, 7, 23, 14, 20, 0).in_time_zone('London'), date_contract_received: DateTime.new(2019, 11, 20, 14, 0, 0).in_time_zone('London'), date_responded_to_contract: DateTime.new(2019, 6, 23, 14, 20, 0).in_time_zone('London'), date_contract_signed: DateTime.new(2019, 6, 23, 14, 20, 0).in_time_zone('London'), date_contract_closed: DateTime.new(2019, 11, 22, 14, 37, 0).in_time_zone('London'),
+                                          contract_start_date: Date.new(2019, 7, 17), contract_end_date: Date.new(2026, 7, 16),
                                           mobilisation_period: 4, optional_call_off_extensions_1: 1, optional_call_off_extensions_2: 1, optional_call_off_extensions_3: nil, optional_call_off_extensions_4: nil,
                                           buildings_and_services: [{ building: 'Barton court store', service_codes: [] }, { building: 'CCS London office 5th floor', service_codes: ['C.13', 'C.20', 'N.1'] }, { building: 'Phoenix house', service_codes: [] }, { building: 'Vale court', service_codes: [] }, { building: 'W Cabinet office 3rd floor', service_codes: [] }], call_off_documents_creation_date: DateTime.new(2019, 5, 14, 10, 47, 0).in_time_zone('London') }
         # TODO: When db intigrated this section can be refactored or removed
@@ -72,6 +73,24 @@ module FacilitiesManagement
         @page_details ||= page_definitions[:default].merge(page_definitions[action.to_sym])
       end
 
+      def set_continuation_text
+        case find_status(request.path_info)
+        when 'awaiting signature'
+          return 'Confirm if contract signed or not'
+        else
+          return nil
+        end        
+      end
+
+      def set_secondary_text
+        case find_status(request.path_info)
+        when 'signed'
+          return 'Make a copy of your requirements'
+        else
+          'Close this procurement'
+        end
+      end
+
       def page_definitions
         @page_definitions ||= {
           default: {
@@ -106,11 +125,11 @@ module FacilitiesManagement
             back_text: 'Back',
             page_title: 'Contract summary',
             caption1: 'School facilities London',
-            continuation_text: 'Confirm if contract signed or not',
+            continuation_text: set_continuation_text,
             # TODO: add when the page has been added
             return_url: '#',
             return_text: 'Return to procurement dashboard',
-            secondary_text: 'Close this procurement'
+            secondary_text: set_secondary_text
           }
         }.freeze
       end
