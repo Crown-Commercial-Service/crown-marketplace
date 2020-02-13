d09d072c985659092bce8f65915e8e732788d391
Ubuntu <ubuntu@ip-172-31-7-62.us-east-2.compute.internal>
2020-02-13 14:15:47 +0000

    Create Accepted and signed page

--- app/helpers/facilities_management/beta/procurement_direct_award_contract_helper.rb
+++ app/helpers/facilities_management/beta/procurement_direct_award_contract_helper.rb
@@ -8,7 +8,7 @@ module FacilitiesManagement::Beta::ProcurementDirectAwardContractHelper
   def warning_message
     warning_messages = { 'awaiting response': "This offer was sent to the supplier on #{format_date_time(@page_data[:procurement_data][:contract_sent_date])}.",
                          'awaiting signature': 'Awaiting your confirmation of signed contract.',
-                         'signed': '',
+                         'signed': "You confirmed that the contract period is between #{format_date(@page_data[:procurement_data][:contract_start_date])} and #{format_date(@page_data[:procurement_data][:contract_end_date])}.",
                          'not-signed': '',
                          'declined': '',
                          'no response': '',
@@ -16,5 +16,5 @@ module FacilitiesManagement::Beta::ProcurementDirectAwardContractHelper
     warning_messages.each { |status, text| return text if @page_data[:procurement_data][:status] == status.to_s }
   end
 
-  WARNINGS = { 'awaiting response': 'Sent', 'awaiting signature': 'Accepted, awaiting contract signature', 'signed': '', 'not-signed': '', 'declined': '', 'no response': '', 'closed': '' }.freeze
+  WARNINGS = { 'awaiting response': 'Sent', 'awaiting signature': 'Accepted, awaiting contract signature', 'signed': 'Accepted and signed', 'not-signed': '', 'declined': '', 'no response': '', 'closed': '' }.freeze
 end
