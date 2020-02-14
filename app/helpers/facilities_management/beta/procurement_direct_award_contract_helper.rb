1a86e3506410ddbbe4627fe06cfa0b78f219fffc
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-02-14 09:23:39 +0000

    Fix code quality issues

--- app/helpers/facilities_management/beta/procurement_direct_award_contract_helper.rb
+++ app/helpers/facilities_management/beta/procurement_direct_award_contract_helper.rb
@@ -15,9 +15,9 @@ module FacilitiesManagement::Beta::ProcurementDirectAwardContractHelper
                          accepted_and_signed: "You confirmed that the contract period is between #{format_date(@page_data[:procurement_data][:contract_start_date])} and #{format_date(@page_data[:procurement_data][:contract_end_date])}.",
                          accepted_not_signed: "You confirmed on the #{format_date_time@page_data[:procurement_data][:contract_not_signed_date]} that the contract has not been signed.",
                          supplier_declined: "The supplier declined this offer #{format_date_time(@page_data[:procurement_data][:date_contract_declined])}",
-                         no_supplier_response: 'The supplier did not respond to your contract offer within the required 2 working days (48 hours).'}
+                         no_supplier_response: 'The supplier did not respond to your contract offer within the required 2 working days (48 hours).' }
     if @page_data[:procurement_data][:closed]
-      "This contract offer was closed on #{format_date_time(@page_data[:procurement_data][:date_contract_closed])}"
+      "This contract offer was closed on #{format_date_time(@page_data[:procurement_data][:date_contract_closed])}."
     else
       warning_messages.each { |status, text| return text if @page_data[:procurement_data][:aasm_state] == status }
     end
