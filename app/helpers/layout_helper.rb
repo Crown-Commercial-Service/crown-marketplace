0c48a0afd671857c14bed3189582401e6da68892
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-02-21 13:53:27 +0000

    Update review and generate documents page
    Refactor layout helper
    
    Fix chnage link issue
    
    Add the generation of the contract number
    
    Cleared up the layout
    
    further layout fixes and translation file update
    
    Implemented the routing
    
    Added new column to table
    
    Updated helper and date method
    
    Generate the contract number at the correct stage
    
    Removed unneccesery db migration
    
    Remove method from helper

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -121,7 +121,7 @@ module LayoutHelper
     buttons << form_builder.submit(page_description.navigation_details.primary_text, class: 'govuk-button govuk-!-margin-right-4', data: { disable_with: false }, name: [page_description.navigation_details.primary_name, 'commit'].find(&:present?)) if primary_button
     buttons << form_builder.submit(page_description.navigation_details.secondary_text, class: "govuk-button #{red_secondary_button ? 'govuk-button--warning' : 'govuk-button--secondary'}", data: { disable_with: false }, name: [page_description.navigation_details.secondary_name, 'commit'].find(&:present?)) if secondary_button
     buttons << capture { tag.br } if secondary_button || primary_button
-    buttons << link_to(page_description.navigation_details.return_text, page_description.navigation_details.return_url, role: 'button', class: 'govuk-link') if return_link
+    buttons << link_to(page_description.navigation_details.return_text, page_description.navigation_details.return_url, role: 'button', class: 'govuk-link govuk-caption-m') if return_link
     content_tag :div, class: 'govuk-!-margin-top-5' do
       buttons
     end
