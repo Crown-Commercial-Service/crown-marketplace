d74a96d83ae195372ece370ef1a97d0add14ddb6
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-02-25 13:15:30 +0000

    Remove back link
    Update to_lower_case method

--- spec/helpers/facilities_management/beta/procurements_helper_spec.rb
+++ spec/helpers/facilities_management/beta/procurements_helper_spec.rb
@@ -14,4 +14,22 @@ RSpec.describe FacilitiesManagement::Beta::ProcurementsHelper, type: :helper do
       end
     end
   end
+
+  describe '#to_lower_case' do
+    context 'when the string starts with an acronym' do
+      it 'will not change the first letter to lower case' do
+        expect(to_lower_case('CCTV / alarm monitoring')).to eq 'CCTV / alarm monitoring'
+        expect(to_lower_case('CAFM system')).to eq 'CAFM system'
+        expect(to_lower_case('IT equipment cleaning')).to eq 'IT equipment cleaning'
+      end
+    end
+
+    context 'when the string does not start with an acronym' do
+      it 'will change the first letter to lower case' do
+        expect(to_lower_case('Compliance plans, specialist surveys and audits')).to eq 'compliance plans, specialist surveys and audits'
+        expect(to_lower_case('standby power system maintenance')).to eq 'standby power system maintenance'
+        expect(to_lower_case('gEneral waste')).to eq 'gEneral waste'
+      end
+    end
+  end
 end
