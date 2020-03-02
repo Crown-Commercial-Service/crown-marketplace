e36f24f1320f7f8744af8a1cfe33edd16ef14ca0
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-03-02 11:50:02 +0000

    languages file and rubocop

--- app/helpers/facilities_management/beta/procurements/contracts_helper.rb
+++ app/helpers/facilities_management/beta/procurements/contracts_helper.rb
@@ -10,18 +10,23 @@ module FacilitiesManagement::Beta::Procurements::ContractsHelper
   end
 
   def warning_message
-    warning_messages = { sent: "This offer was sent to the supplier on #{format_date_time(@contract.offer_sent_date)}.",
-                         accepted: 'Awaiting your confirmation of signed contract.',
-                         signed: "You confirmed that the contract period is between #{format_date(@contract.contract_start_date)} and #{format_date(@contract.contract_end_date)}.",
-                         not_signed: "You confirmed on the #{format_date_time @contract.contract_signed_date} that the contract has not been signed.",
-                         declined: "The supplier declined this offer #{format_date_time(@contract.supplier_response_date)}.",
-                         expired: 'The supplier did not respond to your contract offer within the required 2 working days (48 hours).' }
+    warning_messages = { sent: "#{t('facilities_management.beta.procurements.contracts_helper.warning_message.sent')} #{format_date_time(@contract.offer_sent_date)}.",
+                         accepted: t('facilities_management.beta.procurements.contracts_helper.warning_message.accepted'),
+                         signed: "#{t('facilities_management.beta.procurements.contracts_helper.warning_message.signed')} #{format_date(@contract.contract_start_date)} and #{format_date(@contract.contract_end_date)}.",
+                         not_signed: "#{t('facilities_management.beta.procurements.contracts_helper.warning_message.not_signed_1')} #{format_date_time @contract.contract_signed_date} #{t('facilities_management.beta.procurements.contracts_helper.warning_message.not_signed_2')}.",
+                         declined: "#{t('facilities_management.beta.procurements.contracts_helper.warning_message.declined')} #{format_date_time(@contract.supplier_response_date)}.",
+                         expired: t('facilities_management.beta.procurements.contracts_helper.warning_message.expired') }
     if @contract.closed?
-      "This contract offer was closed on #{format_date_time(@contract.contract_closed_date)}."
+      "#{t('facilities_management.beta.procurements.contracts_helper.warning_message.closed')} #{format_date_time(@contract.contract_closed_date)}."
     else
       warning_messages[@contract.aasm_state.to_sym]
     end
   end
 
-  WARNINGS = { sent: 'Sent', accepted: 'Accepted, awaiting contract signature', signed: 'Accepted and signed', not_signed: 'Accepted, not signed', declined: 'Declined', expired: 'Not responded' }.freeze
+  WARNINGS = { sent: 'Sent',
+               accepted: 'Accepted, awaiting contract signature',
+               signed: 'Accepted and signed',
+               not_signed: 'Accepted, not signed',
+               declined: 'Declined',
+               expired: 'Not responded' }.freeze
 end
