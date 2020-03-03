a39fddba0a93104d4cd1774c61e7a87ff9f24ba7
Chris Gannon <cjgannon@gmail.com>
2020-03-03 16:08:15 +0000

    Change filename display for active_storage

--- db/migrate/20200127104344_change_postcodes_to_nuts_regions.rb
+++ db/migrate/20200127104344_change_postcodes_to_nuts_regions.rb
@@ -1,7 +1,7 @@
 class ChangePostcodesToNutsRegions < ActiveRecord::Migration[5.2]
   def change
     ActiveRecord::Base.connection.execute('TRUNCATE postcodes_nuts_regions')
-    # add_index :postcodes_nuts_regions, :postcode, unique: true
+    add_index :postcodes_nuts_regions, :postcode, unique: true
     change_column :postcodes_nuts_regions, :postcode, :string, limit: 20
     change_column :postcodes_nuts_regions, :code, :string, limit: 20
   end
