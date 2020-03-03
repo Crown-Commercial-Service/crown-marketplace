58087a127e39051c70d529345ab6cd920842a714
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-03-03 14:22:29 +0000

    refactored partial code to reflect sizing and contract supplier details

--- app/models/facilities_management/supplier_detail.rb
+++ app/models/facilities_management/supplier_detail.rb
@@ -3,7 +3,7 @@ module FacilitiesManagement
     belongs_to :user, foreign_key: :user_id, inverse_of: :supplier_detail, optional: true
 
     def full_organisation_address
-      [organisation_address_line_1, organisation_address_line_2, organisation_address_town, organisation_address_county, organisation_address_postcode].reject(&:nil?).reject(&:empty?).join(', ')
+      [address_line_1, address_line_2, address_town, address_county, address_postcode].reject(&:nil?).reject(&:empty?).join(', ')
     end
   end
 end
