0faf9e4754409ed20a2d77526fd773ac19e155d4
Iulia R Ionita <iulia@ionita.me>
2020-03-04 10:54:02 +0000

    Only sidekiq should be running
    Sidekiq does not need to run in the background
    
    changed sidekiq to log to stdout

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,31 +1,29 @@
 #!/usr/bin/env bash
 
-echo TCPAddr $CLAMAV_SERVER_IP > /etc/clamav/clamd.conf && echo TCPSocket 3310 >> /etc/clamav/clamd.conf
+#echo TCPAddr $CLAMAV_SERVER_IP > /etc/clamav/clamd.conf && echo TCPSocket 3310 >> /etc/clamav/clamd.conf
 
-bundle exec rails db:migrate
+#bundle exec rails db:migrate
 
-if [ "$APP_RUN_SIDEKIQ" = 'TRUE' ]; then
-  bundle exec sidekiq -C ./config/sidekiq.yml -d -L ./log/sidekiq.log -e production
-fi
+bundle exec sidekiq -C ./config/sidekiq.yml -e production
 
-if [ "$APP_RUN_STATIC_TASK" = 'TRUE' ]; then
-  bundle exec rails db:static
-fi
-
-if [ "$APP_RUN_PC_TABLE_MIGRATION" = 'TRUE' ]; then
-  bundle exec rails db:pctable
-fi
-
-if [ "$APP_RUN_POSTCODES_IMPORT" = 'TRUE' ]; then
-  bundle exec rails db:postcode
-fi
-
-if [ "$APP_RUN_NUTS_IMPORT" = 'TRUE' ]; then
-  bundle exec rails db:run_postcodes_to_nuts_worker
-fi
-
-if [ "$APP_RUN_PROCUREMENTS_CLEANUP" = 'TRUE' ]; then
-  bundle exec rails procurements:cleanup
-fi
-
-bundle exec rails server
+#if [ "$APP_RUN_STATIC_TASK" = 'TRUE' ]; then
+#  bundle exec rails db:static
+#fi
+#
+#if [ "$APP_RUN_PC_TABLE_MIGRATION" = 'TRUE' ]; then
+#  bundle exec rails db:pctable
+#fi
+#
+#if [ "$APP_RUN_POSTCODES_IMPORT" = 'TRUE' ]; then
+#  bundle exec rails db:postcode
+#fi
+#
+#if [ "$APP_RUN_NUTS_IMPORT" = 'TRUE' ]; then
+#  bundle exec rails db:run_postcodes_to_nuts_worker
+#fi
+#
+#if [ "$APP_RUN_PROCUREMENTS_CLEANUP" = 'TRUE' ]; then
+#  bundle exec rails procurements:cleanup
+#fi
+#
+#bundle exec rails server
