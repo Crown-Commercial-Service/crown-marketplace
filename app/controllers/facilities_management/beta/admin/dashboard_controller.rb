f8e9b29edc8e5d50e6e131935b715ee928a54c7d
Iulia R Ionita <iulia@ionita.me>
2020-03-10 16:46:13 +0000

    Only fm ccs_employees can now access the facilities management admin dashboard
    Added some tests
    
    Rubocop fix

--- app/controllers/facilities_management/beta/admin/dashboard_controller.rb
+++ app/controllers/facilities_management/beta/admin/dashboard_controller.rb
@@ -1,9 +1,9 @@
 module FacilitiesManagement
   module Beta
     module Admin
-      class DashboardController < FacilitiesManagement::Beta::FrameworkController
-        # before_action :authenticate_user!, except: :index
-        # before_action :authorize_user, except: :index
+      class DashboardController < FacilitiesManagement::Beta::Admin::FrameworkController
+        before_action :authenticate_user!, except: :index
+        before_action :authorize_user, except: :index
 
         def index; end
       end
