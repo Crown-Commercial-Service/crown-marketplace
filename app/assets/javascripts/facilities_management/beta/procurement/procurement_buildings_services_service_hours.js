b99418235ae139d0c6357c5d39fff0be4b2edf63
ManFromMons <julianyoung@outlook.com>
2020-03-10 16:23:33 +0000

    Logic to copy details and change UI....

--- app/assets/javascripts/facilities_management/beta/procurement/procurement_buildings_services_service_hours.js
+++ app/assets/javascripts/facilities_management/beta/procurement/procurement_buildings_services_service_hours.js
@@ -4,13 +4,27 @@ function SvcHoursDataUI(jqContainer) {
     this.dataContainer = jqContainer.find(".servicehoursdata");
     let formObject = jqContainer.closest("form");
     if (formObject.length > 0) {
+        this.sections = this.findMondaysToFridays();
         this.formHelper = new FormValidationComponent(formObject[0], this.validateForm, true);
         this.formHelper.prevErrorMessage = this.formHelper.errorMessage;
     }
 }
 
+SvcHoursDataUI.prototype.findMondaysToFridays = function () {
+    let sections = {};
+    let elements = this.dataContainer.find(".govuk-form-group");
+    for (let idx = 0; idx < elements.length; idx++) {
+        let tmpSection = elements[idx];
+        if (tmpSection.id.indexOf("_form-group") > 0) {
+            let sectionName = tmpSection.id.slice(0, tmpSection.id.indexOf("_"));
+            sections[sectionName] = tmpSection;
+        }
+    }
+    return sections;
+};
+
 SvcHoursDataUI.prototype.validateForm = function (_formElements) {
-    let isValid = true;
+    let isValid;
 
     this.clearBannerErrorList();
     this.clearAllFieldErrors();
@@ -61,7 +75,7 @@ SvcHoursDataUI.prototype.validateForm = function (_formElements) {
         return isValid;
     };
 
-    this.checkRadioButtons = function ( choices ) {
+    this.checkRadioButtons = function (choices) {
         let isValid = true;
 
         isValid = this.fnCheckRadioButton("monday", choices) && isValid;
@@ -75,12 +89,12 @@ SvcHoursDataUI.prototype.validateForm = function (_formElements) {
         return isValid;
     };
 
-    this.validateTimes = function ( day, choices ) {
+    this.validateTimes = function (day, choices) {
         let timeIsValid = this.fnCheckTime(day, "start", choices);
         return this.fnCheckTime(day, "end", choices) && timeIsValid;
     };
-    
-    this.validateTwelveHourTime = function( time, digits ){
+
+    this.validateTwelveHourTime = function (time, digits) {
         if (digits === "12" || digits === "24") {
             return time = (parseInt(time) - 1200).toString();
         } else {
@@ -88,38 +102,39 @@ SvcHoursDataUI.prototype.validateForm = function (_formElements) {
         }
     };
 
-    this.validateChronologicalSequence = function ( day, choices ) {
-        let isValid = true ;
+    this.validateChronologicalSequence = function (day, choices) {
+        let isValid = true;
 
         let afternoonStart = choices[day]["start"]["ampmElem"].val() === 'PM';
         let startTime = (parseInt(choices[day]["start"]["hour"].value) + (afternoonStart ? 12 : 0)) + choices[day]["start"]["minute"].value.padStart(2, '0');
         let afternoonEnd = choices[day]["end"]["ampmElem"].val() === 'PM';
         let endTime = (parseInt(choices[day]["end"]["hour"].value) + (afternoonEnd ? 12 : 0)) + choices[day]["end"]["minute"].value.padStart(2, '0');
-        let startTimeFirstDigits = startTime.substring(0,2);
-        let endTimeFirstDigits = endTime.substring(0,2);
-        startTime = this.validateTwelveHourTime( startTime, startTimeFirstDigits);
-        endTime = this.validateTwelveHourTime( endTime, endTimeFirstDigits);
+        let startTimeFirstDigits = startTime.substring(0, 2);
+        let endTimeFirstDigits = endTime.substring(0, 2);
+        startTime = this.validateTwelveHourTime(startTime, startTimeFirstDigits);
+        endTime = this.validateTwelveHourTime(endTime, endTimeFirstDigits);
 
-        if ( parseInt(endTime) <= parseInt(startTime)) {
+        if (parseInt(endTime) <= parseInt(startTime)) {
             isValid = false;
             choices[String(day)]["end"].status = false;
             choices[String(day)]["end"].errorType = "min";
         }
-        if (endTimeFirstDigits.substring(0,1) === "0" || (endTimeFirstDigits === "12" && afternoonEnd)){
+        if (endTimeFirstDigits.substring(0, 1) === "0" || (endTimeFirstDigits === "12" && afternoonEnd)) {
             isValid = false;
             choices[String(day)]["end"].status = false;
             choices[String(day)]["end"].errorType = "invalid";
             this.displayTimeErrors(day, "start", choices);
         }
-        if (startTimeFirstDigits.substring(0,1) === "0" || (startTimeFirstDigits === "12" && afternoonStart)) {
+        if (startTimeFirstDigits.substring(0, 1) === "0" || (startTimeFirstDigits === "12" && afternoonStart)) {
             isValid = false;
             choices[String(day)]["start"].status = false;
             choices[String(day)]["start"].errorType = "invalid";
             this.displayTimeErrors(day, "start", choices);
         }
         return isValid;
-    } ;
-    this.displayTimeErrors = function (day, part, choices ) {
+    };
+
+    this.displayTimeErrors = function (day, part, choices) {
         if (choices[day][part].status === false) {
             this.toggleError($("." + day + "_" + part + "_time"), true, choices[day][part].errorType ? choices[day][part].errorType : "invalid");
             this.addErrorClass(choices[day][part]["hour"].elem);
@@ -129,14 +144,14 @@ SvcHoursDataUI.prototype.validateForm = function (_formElements) {
     };
 
     let dailyChoices = {};
-    isValid = this.checkRadioButtons(dailyChoices) ;
+    isValid = this.checkRadioButtons(dailyChoices);
 
     for (let key in dailyChoices) {
         if (dailyChoices.hasOwnProperty(key)) {
             if (dailyChoices[key].status === "none") {
                 this.toggleError($(dailyChoices[key].field[0]), true, "required");
             } else if (dailyChoices[key].field.value === "hourly") {
-                let isTimeValid = this.validateTimes (key, dailyChoices[key]);
+                let isTimeValid = this.validateTimes(key, dailyChoices[key]);
                 isValid = isTimeValid && isValid;
 
                 if (!isTimeValid) {
@@ -169,6 +184,66 @@ SvcHoursDataUI.prototype.initialise = function () {
     for (i = 0; i < inputs.length; i++) {
         this.restrictInput(inputs[i]);
     }
+
+    $("#copy_details").on("click", this.applyDetails.bind(this));
+};
+
+SvcHoursDataUI.prototype.applyDetails = function () {
+    let sourceDetails = null;
+
+    for (let key in this.sections) {
+        if (key === "monday") {
+            sourceDetails = this.copyElementDetails(key, this.sections[key]);
+        } else if ("sunday saturday".indexOf(key) < 0) {
+            this.pasteElementDetails(key, sourceDetails, this.sections[key]);
+        }
+        if (null !== sourceDetails && sourceDetails.choice === "") break;
+    }
+};
+
+SvcHoursDataUI.prototype.pasteElementDetails = function (day, sourceDetails, htmlSection) {
+    let jqSection = $(htmlSection);
+
+    if (sourceDetails.choice !== "") {
+        jqSection.find("#" + day + "_" + sourceDetails.choice).prop("checked", true);
+        jqSection.find("div.govuk-radios.govuk-radios--conditional").click();
+        if (sourceDetails.choice === "hourly") {
+            jqSection.find("#" + day + "_start_hour").val(sourceDetails.startHour);
+            jqSection.find("#" + day + "_start_minute").val(sourceDetails.startMinute);
+            jqSection.find("#" + day + "_start_ampm").val(sourceDetails.startAmPm);
+            jqSection.find("#" + day + "_end_hour").val(sourceDetails.endHour);
+            jqSection.find("#" + day + "_end_minute").val(sourceDetails.endMinute);
+            jqSection.find("#" + day + "_end_ampm").val(sourceDetails.endAmPm);
+        }
+    }
+};
+
+SvcHoursDataUI.prototype.copyElementDetails = function (day, htmlSection) {
+    let jqSection = $(htmlSection);
+    let sourceData = {
+        choice: "",
+        startHour: "",
+        startMinute: "",
+        startAmPm: "",
+        endHour: "",
+        endMinute: "",
+        endAmPm: ""
+    };
+    let selectedChoice = jqSection.find("input[type=radio]:checked");
+
+    if (selectedChoice.length > 0) {
+        sourceData.choice = selectedChoice[0].value;
+
+        if (sourceData.choice === "hourly") {
+            sourceData.startHour = jqSection.find("#" + day + "_start_hour").val();
+            sourceData.startMinute = jqSection.find("#" + day + "_start_minute").val();
+            sourceData.startAmPm = jqSection.find("#" + day + "_start_ampm").val();
+            sourceData.endHour = jqSection.find("#" + day + "_end_hour").val();
+            sourceData.endMinute = jqSection.find("#" + day + "_end_minute").val();
+            sourceData.endAmPm = jqSection.find("#" + day + "_end_ampm").val();
+        }
+    }
+    return sourceData;
 };
 
 SvcHoursDataUI.prototype.restrictInput = function (jqElem) {
