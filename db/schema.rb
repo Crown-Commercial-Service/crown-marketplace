1436413075c45020bd0ad9a7bd31de1f2643d7fd
ManFromMons <julianyoung@outlook.com>
2020-03-11 11:56:33 +0000

    Add migration to ensure fm_rates index doesn't get dropped.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_111231) do
+ActiveRecord::Schema.define(version: 2020_03_11_113711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
