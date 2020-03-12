6dd86e826523f39772452ea90c8f1da82469a18e
Iulia R Ionita <iulia@ionita.me>
2020-03-12 12:07:37 +0000

    DashboardController should redirect to sign in

--- app/controllers/facilities_management/beta/admin/dashboard_controller.rb
+++ app/controllers/facilities_management/beta/admin/dashboard_controller.rb
@@ -2,8 +2,6 @@ module FacilitiesManagement
   module Beta
     module Admin
       class DashboardController < FacilitiesManagement::Beta::Admin::FrameworkController
-        before_action :authenticate_user!, except: :index
-        before_action :authorize_user, except: :index
 
         def index; end
       end
