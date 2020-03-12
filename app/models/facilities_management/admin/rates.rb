fa1633ecff351313c58213d12ceb3073f614ddf2
mo-zag <mo-zag@users.noreply.github.com>
2020-03-12 12:09:06 +0000

    Fm-809-admin-tool-average-framework-rates-view-only (#1544)
    * New further competition page
    
    Further competition updated page
    
    * Removed unwanted Li
    
    Removed list from list element.
    
    * Update _further_competition_partial.html.erb
    
    Updated some minor changes
    
    * Update _further_competition_partial.html.erb
    
    Updated chnages from design
    
    * Update _further_competition.html.erb
    
    Removed return to result button
    
    * Update _further_competition_partial.html.erb
    
    Added requirements
    
    * Update _further_competition.html.erb
    
    Removed unwanted link
    
    * Update _further_competition_partial.html.erb
    
    Added name of contract to link for download
    
    * New branch
    
    Created admin routes and startup data
    
    * Update _further_competition_partial.html.erb
    
    Updated apostrophe
    
    * Created new benchmark rates
    
    Edit screen to update benchmark rates
    
    * Create supplier_framework_rates.erb
    
    New framework rates edit page.
    
    * Added translation file text
    
    Now text is pulled from translation file.
    
    * Added text from translation file
    
    Added text from translation file into benchmark rates
    
    * Updated framework rates with translation
    
    Updated framework rates text to use translation file
    
    * New test
    
    Added new test for frameworks

---
+++ app/models/facilities_management/admin/rates.rb
@@ -0,0 +1,7 @@
+module FacilitiesManagement
+  module Admin
+    class Rates < ApplicationRecord
+      self.table_name = 'fm_rates'
+    end
+  end
+end
---
+++ app/models/facilities_management/admin/static_data_admin.rb
@@ -0,0 +1,15 @@
+module FacilitiesManagement
+  module Admin
+    class StaticDataAdmin < ApplicationRecord
+      self.table_name = 'fm_static_data'
+
+      def self.work_packages
+        find_by(key: 'work_packages').value
+      end
+
+      def self.services
+        find_by(key: 'services').value
+      end
+    end
+  end
+end
