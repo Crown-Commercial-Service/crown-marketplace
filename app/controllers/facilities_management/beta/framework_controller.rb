b133ef0ddecde58672d5e5e81f0ae3591de6ef0d
ManFromMons <julianyoung@outlook.com>
2020-03-16 16:11:12 +0000

    Schema changes from running db:migrate - please commit these Logic in the framework controller in beta - added a before_action to validate buyer_detail...

--- app/controllers/facilities_management/beta/framework_controller.rb
+++ app/controllers/facilities_management/beta/framework_controller.rb
@@ -3,11 +3,19 @@ module FacilitiesManagement
     class FrameworkController < ::ApplicationController
       before_action :authenticate_user!
       before_action :authorize_user
-
+      before_action :capture_buyer_detail
+      
       protected
-
+      
       def authorize_user
         authorize! :read, FacilitiesManagement
+      end
+
+      def capture_buyer_detail
+        if current_user&.fm_buyer_details_incomplete? &&
+          request.fullpath != edit_facilities_management_beta_buyer_detail_path(FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user))
+          redirect_to(edit_facilities_management_beta_buyer_detail_path(FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user)))
+        end
       end
     end
   end
