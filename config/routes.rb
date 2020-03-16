2fa03ccbea3940c78a96c4b7862ac0be83e11343
anthonyoleary <anthony.oleary@finddx.org>
2020-03-16 13:54:15 +0000

    fixed route conflict

--- config/routes.rb
+++ config/routes.rb
@@ -171,17 +171,15 @@ Rails.application.routes.draw do
         get '/', to: 'admin_account#admin_account'
         get '/start', to: 'dashboard#index'
         get '/gateway', to: 'gateway#index'
-      end
-      namespace :admin do
-        get 'supplier-benchmark-rates', to: 'supplier_rates#supplier_benchmark_rates'
-        get 'supplier-framework-rates', to: 'supplier_rates#supplier_framework_rates'
-      end
-      namespace :admin do
-        get 'supplier-benchmark-rates', to: 'supplier_rates#supplier_benchmark_rates'
-        get 'supplier-framework-rates', to: 'supplier_rates#supplier_framework_rates'
-      end
-      namespace :admin do
         get 'sublot-data', to: 'sublot_data_services_prices#index'
+      end
+      namespace :admin do
+        get 'supplier-benchmark-rates', to: 'supplier_rates#supplier_benchmark_rates'
+        get 'supplier-framework-rates', to: 'supplier_rates#supplier_framework_rates'
+      end
+      namespace :admin do
+        get 'supplier-benchmark-rates', to: 'supplier_rates#supplier_benchmark_rates'
+        get 'supplier-framework-rates', to: 'supplier_rates#supplier_framework_rates'
       end
     end
 
