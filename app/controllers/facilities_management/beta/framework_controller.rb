2172785f520ec03d25b6546fd172fde03b0e0e10
ManFromMons <julianyoung@outlook.com>
2020-03-17 12:41:07 +0000

    Added logic to ensure this decision is made only for specific paths. Starting a search however doesn't go to beta - this will change in the near future and the loophole will be closed.

--- app/controllers/facilities_management/beta/framework_controller.rb
+++ app/controllers/facilities_management/beta/framework_controller.rb
@@ -4,18 +4,26 @@ module FacilitiesManagement
       before_action :authenticate_user!
       before_action :authorize_user
       before_action :capture_buyer_detail
-      
+
       protected
-      
+
       def authorize_user
         authorize! :read, FacilitiesManagement
       end
 
       def capture_buyer_detail
-        if current_user&.fm_buyer_details_incomplete? &&
-          request.fullpath != edit_facilities_management_beta_buyer_detail_path(FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user))
+        if guarded_path? && current_user&.fm_buyer_details_incomplete? &&
+           request.fullpath != edit_facilities_management_beta_buyer_detail_path(FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user))
           redirect_to(edit_facilities_management_beta_buyer_detail_path(FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user)))
         end
+      end
+
+      private
+
+      def guarded_path?
+        full_path = request.fullpath
+
+        %w[building procurement choose-].any? { |sp| full_path.include?(sp) }
       end
     end
   end
