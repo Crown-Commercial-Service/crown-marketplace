4ae96d3965fff3598c2c14e7b9e689a28a590830
ManFromMons <julianyoung@outlook.com>
2020-03-17 17:49:34 +0000

    Code refactor

--- app/controllers/facilities_management/beta/framework_controller.rb
+++ app/controllers/facilities_management/beta/framework_controller.rb
@@ -11,11 +11,12 @@ module FacilitiesManagement
         authorize! :read, FacilitiesManagement
       end
 
+      def buyer_path
+        @buyer_path ||= edit_facilities_management_beta_buyer_detail_path(FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user))
+      end
+
       def redirect_to_buyer_detail
-        if guarded_path? && current_user&.fm_buyer_details_incomplete? &&
-           request.fullpath != edit_facilities_management_beta_buyer_detail_path(FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user))
-          redirect_to(edit_facilities_management_beta_buyer_detail_path(FacilitiesManagement::BuyerDetail.find_or_create_by(user: current_user), params: { return_to: { url: request.path, params: request.query_parameters } }))
-        end
+        redirect_to(buyer_path, params: { return_to: { url: request.path, params: request.query_parameters } }) if guarded_path? && current_user&.fm_buyer_details_incomplete? && request.path != buyer_path
       end
 
       private
