b4d89c45d33a2fdb92adcf646805c0bfd6582589
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-03-17 14:08:55 +0000

    Small changes to make pages meet design
    Refactored the procurement_contract page
    
    Added try catch to stop crashing on local due to not running sidkiq

--- app/controllers/facilities_management/beta/procurements/contracts_controller.rb
+++ app/controllers/facilities_management/beta/procurements/contracts_controller.rb
@@ -2,6 +2,9 @@ module FacilitiesManagement
   module Beta
     module Procurements
       class ContractsController < FacilitiesManagement::Beta::FrameworkController
+        include FacilitiesManagement::ControllerLayoutHelper
+        include FacilitiesManagement::Beta::Procurements::ContractsHelper
+
         before_action :set_procurement
         before_action :set_contract
         before_action :set_page_detail, only: %i[show edit]
@@ -71,72 +74,6 @@ module FacilitiesManagement
                   :contract_end_date_mm,
                   :contract_end_date_yyyy
                 )
-        end
-
-        # rubocop:disable Metrics/AbcSize
-        def set_page_detail
-          @page_description = LayoutHelper::PageDescription.new(
-            LayoutHelper::HeadingDetail.new(page_details(action_name)[:page_title],
-                                            page_details(action_name)[:caption1],
-                                            page_details(action_name)[:caption2],
-                                            page_details(action_name)[:sub_title],
-                                            page_details(action_name)[:caption3]),
-            LayoutHelper::BackButtonDetail.new(page_details(action_name)[:back_url],
-                                               page_details(action_name)[:back_label],
-                                               page_details(action_name)[:back_text]),
-            LayoutHelper::NavigationDetail.new(page_details(action_name)[:continuation_text],
-                                               page_details(action_name)[:return_url],
-                                               page_details(action_name)[:return_text],
-                                               page_details(action_name)[:secondary_url],
-                                               page_details(action_name)[:secondary_text])
-          )
-        end
-        # rubocop:enable Metrics/AbcSize
-
-        def page_details(action)
-          action = 'edit' if action == 'update'
-          @page_details ||= page_definitions[:default].merge(page_definitions[action.to_sym])
-        end
-
-        def set_continuation_text
-          case @contract.aasm_state
-          when 'accepted'
-            'Confirm if contract signed or not'
-          when 'not_signed', 'declined', 'expired'
-            "View next supplier's price"
-          end
-        end
-
-        def set_secondary_text
-          if @contract.closed? || @contract.aasm_state == 'signed'
-            'Make a copy of your requirements'
-          else
-            'Close this procurement'
-          end
-        end
-
-        def page_definitions
-          @page_definitions ||= {
-            default: {
-              back_label: 'Back',
-              back_text: 'Back',
-              back_url: facilities_management_beta_procurements_path,
-              return_text: 'Return to procurements dashboard',
-              return_url: facilities_management_beta_procurements_path,
-            },
-            show: {
-              page_title: 'Contract summary',
-              caption1: @procurement.contract_name,
-              continuation_text: set_continuation_text,
-              return_text: 'Return to procurements dashboard',
-              secondary_text: set_secondary_text,
-            },
-            edit: {
-              back_url: facilities_management_beta_procurement_contract_path(@procurement),
-              continuation_text: 'Close this procurement',
-              secondary_text: 'Cancel',
-            },
-          }.freeze
         end
       end
     end
