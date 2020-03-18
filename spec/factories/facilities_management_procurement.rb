0306af4bba763e115d7d6738402231d514dd51ad
Clarence Brown <clarence.brown@crowncommercial.gov.uk>
2020-03-18 14:33:38 +0000

    updated contract names to be quick search names and removed unncessary columns
    replaced quick search name with contract name
    
    altered spec files to match contract name

--- spec/factories/facilities_management_procurement.rb
+++ spec/factories/facilities_management_procurement.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :facilities_management_procurement, class: FacilitiesManagement::Procurement do
-    name { Faker::Name.unique.name }
-    contract_name { 'Contract name' }
+    contract_name { Faker::Name.unique.name }
     estimated_cost_known { 12345 }
     tupe { false }
     initial_call_off_period { 1 }
