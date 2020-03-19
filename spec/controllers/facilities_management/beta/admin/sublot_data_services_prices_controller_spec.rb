0701db656c4e28d31e92710413cf35fca3b87336
anthonyoleary <anthony.oleary@finddx.org>
2020-03-19 11:21:02 +0000

    added request spec

--- spec/controllers/facilities_management/beta/admin/sublot_data_services_prices_controller_spec.rb
+++ spec/controllers/facilities_management/beta/admin/sublot_data_services_prices_controller_spec.rb
@@ -6,20 +6,30 @@ RSpec.describe FacilitiesManagement::Beta::Admin::SublotDataServicesPricesContro
   describe 'GET index' do
     context 'when index page is rendered' do
       it 'returns http success' do
-        get :index, params: { id: 123 }
+        get :index, params: { id: 'ca57bf4c-e8a5-468a-95f4-39fcf730c770' }
         expect(response).to have_http_status(:ok)
       end
 
+      it 'returns http redirect for invalid id' do
+        get :index, params: { id: '124' }
+        expect(response).to have_http_status(:found)
+      end
+
       it 'renders the index page' do
-        get :index, params: { id: 123 }
+        get :index, params: { id: 'ca57bf4c-e8a5-468a-95f4-39fcf730c770' }
         expect(response).to render_template(:index)
+      end
+
+      it 'retrieves correct list and name data' do
+        get :index, params: { id: 'ca57bf4c-e8a5-468a-95f4-39fcf730c770' }
+        expect(assigns(:list_service_types).size).to eq 13
+        expect(assigns(:supplier_name)).to eq 'Abernathy and Sons'
       end
 
       it 'retrieves correct data' do
         get :index, params: { id: 'ca57bf4c-e8a5-468a-95f4-39fcf730c770' }
-        expect(assigns(:list_service_types).size).to eq 13
-        expect(assigns(:supplier_name)).to eq 'Abernathy and Sons'
         expect(assigns(:supplier_rate_data_checkboxes).size).to eq 116
+        expect(assigns(:supplier_data_ratecard_prices).size).to eq 1
         expect(assigns(:supplier_data_ratecard_discounts).size).to eq 1
       end
     end
