12c901b9c00cf171fdbf37830a4ea1379e2d90b1
Chris Gannon <cjgannon@gmail.com>
2020-03-20 14:32:47 +0000

    Fix formatting errors on word doc

--- app/controllers/facilities_management/beta/procurements/contracts/documents_controller.rb
+++ app/controllers/facilities_management/beta/procurements/contracts/documents_controller.rb
@@ -8,6 +8,7 @@ module FacilitiesManagement
             @supplier = @contract.supplier
             @procurement = @contract.procurement
             @buyer_detail = @procurement.user.buyer_detail
+            @supplier_detail = FacilitiesManagement::SupplierDetail.find_by(contact_email: @supplier.data['contact_email'])
             @invoice_contact_detail = @procurement.using_buyer_detail_for_invoice_details? ? @buyer_detail : @procurement.invoice_contact_detail
             @authorised_contact_detail = @procurement.using_buyer_detail_for_invoice_details? ? @buyer_detail : @procurement.authorised_contact_detail
             @notice_contact_detail = @procurement.using_buyer_detail_for_notices_detail? ? @buyer_detail : @procurement.notices_contact_detail
