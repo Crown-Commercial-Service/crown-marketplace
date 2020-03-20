579a9948b110c33794e59f0938b6ac360562a024
Chris Gannon <cjgannon@gmail.com>
2020-03-20 07:52:19 +0000

    Add order form document for contract

---
+++ app/controllers/facilities_management/beta/procurements/contracts/documents_controller.rb
@@ -0,0 +1,23 @@
+module FacilitiesManagement
+  module Beta
+    module Procurements
+      module Contracts
+        class DocumentsController < FacilitiesManagement::Beta::FrameworkController
+          def call_off_schedule
+            @contract = FacilitiesManagement::ProcurementSupplier.find(params[:contract_id])
+            @supplier = @contract.supplier
+            @procurement = @contract.procurement
+            @buyer_detail = @procurement.user.buyer_detail
+            @invoice_contact_detail = @procurement.using_buyer_detail_for_invoice_details? ? @buyer_detail : @procurement.invoice_contact_detail
+            @authorised_contact_detail = @procurement.using_buyer_detail_for_invoice_details? ? @buyer_detail : @procurement.authorised_contact_detail
+            @notice_contact_detail = @procurement.using_buyer_detail_for_notices_detail? ? @buyer_detail : @procurement.notices_contact_detail
+
+            respond_to do |format|
+              format.docx { headers['Content-Disposition'] = 'attachment; filename="Attachment 4 - Order Form and Call-off Schedule (DA).docx"' }
+            end
+          end
+        end
+      end
+    end
+  end
+end
