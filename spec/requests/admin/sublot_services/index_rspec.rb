d3dfadfa87514c6976ea182701ad4a7585a58c08
anthonyoleary <anthony.oleary@finddx.org>
2020-03-24 14:17:10 +0000

    added new lot 1c view

--- spec/requests/admin/sublot_services/index_rspec.rb
+++ spec/requests/admin/sublot_services/index_rspec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'admin sublot data services prices', type: :request do
   it 'verify page displayed' do
-    get '/facilities-management/beta/admin/sublot-services/ca57bf4c-e8a5-468a-95f4-39fcf730c770'
+    get '/facilities-management/beta/admin/sublot-services/ca57bf4c-e8a5-468a-95f4-39fcf730c770/1b'
     expect(response).to redirect_to facilities_management_beta_admin_gateway_path
   end
 end
