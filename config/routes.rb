fd1f26a9e2deed67da4ed6475c73eab5e284941a
mo-zag <mo-zag@users.noreply.github.com>
2020-03-25 14:31:41 +0000

    Feature/fm 825 admin tool sub lot 1b regions view only and fm826 admin tool  sub lot 1c regions view only (#1634)
    * Regions sub lot 1a
    
    Select regions sub-lot-a
    
    * New lots regions 1a, 1c, 1b
    
    New lots regions 1a, 1c, 1b
    
    * updated
    
    Upadted path to dashboard and removed unused yml text

--- config/routes.rb
+++ config/routes.rb
@@ -176,6 +176,7 @@ Rails.application.routes.draw do
         get 'call-off-benchmark-rates', to: 'supplier_rates#supplier_benchmark_rates'
         get 'average-framework-rates', to: 'supplier_rates#supplier_framework_rates'
         get 'supplier-framework-data', to: 'suppliers_framework_data#index'
+        get 'sublot-regions/:id/:lot_type', to: 'sublot_regions#sublot_region', as: 'get_sublot_regions'
         get 'sublot-data/:id', to: 'sublot_data_services_prices#index', as: 'get_sublot_data'
         get 'sublot-services/:id/:lot', to: 'sublot_services#index', as: 'get_sublot_services'
       end
