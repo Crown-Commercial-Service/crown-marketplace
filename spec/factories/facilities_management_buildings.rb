4f7bb006b324b6b9466a2145d1adbaf9c2ddaf95
ManFromMons <julianyoung@outlook.com>
2020-03-26 11:04:37 +0000

    New building factories

--- spec/factories/facilities_management_buildings.rb
+++ spec/factories/facilities_management_buildings.rb
@@ -1,4 +1,30 @@
 FactoryBot.define do
+  factory :facilities_management_building_ar_defaults, class: FacilitiesManagement::Building do
+    id { SecureRandom.uuid }
+    user_id { create(:user).id }
+    updated_by { Faker::Internet.unique.email }
+  end
+  
+  factory :facilities_management_building_ar, class: FacilitiesManagement::Building do
+    id { SecureRandom.uuid }
+    user_id { create(:user).id }
+    updated_by { Faker::Internet.unique.email }
+    gia { 1002 }
+    building_name { 'non-json-building' }
+    description { 'non-json description' }
+    region { 'Essex' }
+    building_ref { 'SS310DR' }
+    building_type { 'General office - Customer Facing' }
+    security_type { 'Baseline personnel security standard (BPSS)' }
+    address_town { 'Southend' }
+    address_county { 'Essex' }
+    address_line_1 { '1 Mariners Court' }
+    address_line_2 { 'Floor 2' }
+    address_region { 'Essex2' }
+    address_region_code { 'UKH1' }
+    address_postcode { 'SS31 0DR' }
+  end
+
   factory :facilities_management_building, class: FacilitiesManagement::Buildings do
     id { SecureRandom.uuid }
     user_id { create(:user).id }
