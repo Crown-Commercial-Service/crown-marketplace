12367ef45d711c0972bcabf96884678724e9a218
ManFromMons <julianyoung@outlook.com>
2020-03-28 17:37:02 +0000

    Prevent Back navigation and back-to-start link.

--- app/assets/javascripts/facilities_management/beta/buyer/buyer_details.js
+++ app/assets/javascripts/facilities_management/beta/buyer/buyer_details.js
@@ -1,5 +1,15 @@
 $(function () {
-    if ($('#buyer-details').length > 0) {
+    if ($('#buyer_details').length > 0) {
+
+        history.pushState(null, document.title, location.href);
+        window.addEventListener('popstate', function (event)
+        {
+            window.status = "Stopped back";
+            history.pushState(null, document.title, location.href);
+        });
+
+        $('.back_to_start_link').prop('href', '#');
+
         let selectedAddress;
         const focusElem = function (elem) {
             elem.focus();
