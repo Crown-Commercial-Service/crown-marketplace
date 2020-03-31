b30acccfe3b8f42c18dfd99ef6d0d240c19feccb
mo-zag <mo-zag@users.noreply.github.com>
2020-03-31 11:04:12 +0100

    Feature/FM 876 - FM-877 - FM-878 admin tool sub lot 1a regions update action (#1658)
    * Regions sub lot 1a
    
    Select regions sub-lot-a
    
    * New lots regions 1a, 1c, 1b
    
    New lots regions 1a, 1c, 1b
    
    * updated
    
    Upadted path to dashboard and removed unused yml text
    
    * updated
    
    updating regions
    
    * updated chnages
    
    changes supplier update msg
    Added flash msg
    
    * Save supplier regions
    
    User can save supplier regions
    
    * Update index.erb
    
    Added varinces link
    
    * Update index.erb
    
    Services links updated

--- app/controllers/facilities_management/beta/admin/sublot_regions_controller.rb
+++ app/controllers/facilities_management/beta/admin/sublot_regions_controller.rb
@@ -8,17 +8,17 @@ module FacilitiesManagement
 
         def render_unprocessable_entity_response(exception)
           logger.error exception.message
-          redirect_to facilities_management_beta_admin_path, flash: { error: 'Invalid supplier ID lot 1b processing' }
+          redirect_to facilities_management_beta_admin_path, flash: { error: 'Invalid supplier ID processing' }
         end
 
         def render_not_found_response(exception)
           logger.error exception.message
-          redirect_to facilities_management_beta_admin_path, flash: { error: 'Invalid supplier ID lot 1b not found' }
+          redirect_to facilities_management_beta_admin_path, flash: { error: 'Invalid supplier ID not found' }
         end
 
         def render_no_method_error_response(exception)
           logger.error exception.message
-          redirect_to facilities_management_beta_admin_path, flash: { error: 'Invalid supplier ID lotb method not found' }
+          redirect_to facilities_management_beta_admin_path, flash: { error: 'Invalid supplier ID method not found' }
         end
 
         # uses one controller to show
@@ -32,10 +32,18 @@ module FacilitiesManagement
           h = {}
           FacilitiesManagement::Region.all.each { |x| h[x.code] = x.name }
           @supplier = FacilitiesManagement::Admin::SuppliersAdmin.find(params['id'])
-          @supplier_lot_a = @supplier.data['lots'].select { |lot| lot['lot_number'] == params['lot_type'] }
+          @supplier_lot = @supplier.data['lots'].select { |lot| lot['lot_number'] == params['lot_type'] }
           @sublot_region_name = 'Sub-lot ' + params['lot_type'] + ' regions'
-          @selected_supplier_regions = FacilitiesManagement::Beta::Supplier::SupplierRegionsHelper.supllier_selected_regions(@supplier_lot_a)
+          @selected_supplier_regions = FacilitiesManagement::Beta::Supplier::SupplierRegionsHelper.supllier_selected_regions(@supplier_lot)
           @subregions = h
+        end
+
+        def update_sublot_regions
+          @supplier = FacilitiesManagement::Admin::SuppliersAdmin.find(params['id'])
+          @supplier.data['lots'].each { |lot| lot['lot_number'] == params['lot_type'] ? lot['regions'] = params[:regions] : '' }
+          @supplier_lot_updated = @supplier.data['lots'].select { |lot| lot['lot_number'] == params['lot_type'] }
+          @supplier.save
+          redirect_to facilities_management_beta_admin_supplier_framework_data_path, flash: { success: 'The updated supplier will provide services to the selected regions.' }
         end
       end
     end
