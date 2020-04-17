19ab037d3946507b4f1e1d78a4ddeb9f868a7d8e
Andy <andy@wireworldmedia.co.uk>
2020-04-17 09:09:39 +0100

    Adjust spec to suit

--- spec/services/procurement_csv_export_spec.rb
+++ spec/services/procurement_csv_export_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe ProcurementCsvExport do
   let!(:procurement_in_search) { create(:facilities_management_procurement) }
   let!(:procurement_in_results) { create(:facilities_management_procurement_direct_award, aasm_state: 'results') }
 
+  let(:start_date) { Time.zone.today - 1 }
+  let(:end_date) { Time.zone.today + 1 }
+
   # rubocop:disable Rails/SkipsModelValidations
   let!(:procurement_in_da) do
     proc = create(:facilities_management_procurement_with_contact_details)
@@ -13,7 +16,7 @@ RSpec.describe ProcurementCsvExport do
   # rubocop:enable Rails/SkipsModelValidations
 
   describe '.find_procurements' do
-    subject(:found_procurements) { described_class.find_procurements }
+    subject(:found_procurements) { described_class.find_procurements(start_date, end_date) }
 
     describe 'Procurement' do
       context 'with no contracts' do
@@ -34,10 +37,19 @@ RSpec.describe ProcurementCsvExport do
         end
       end
     end
+
+    context 'with dates before last update' do
+      let(:start_date) { Time.zone.today - 5 }
+      let(:end_date) { Time.zone.today - 3 }
+
+      it 'finds no procurements' do
+        expect(found_procurements).to be_none
+      end
+    end
   end
 
   describe '.find_contracts' do
-    subject(:found_contracts) { described_class.find_contracts }
+    subject(:found_contracts) { described_class.find_contracts(start_date, end_date) }
 
     describe 'Procurement' do
       context 'with no contracts' do
@@ -56,6 +68,15 @@ RSpec.describe ProcurementCsvExport do
         it 'includes procurement' do
           expect(found_contracts.map(&:procurement)).to include(procurement_in_da)
         end
+      end
+    end
+
+    context 'with dates before last update' do
+      let(:start_date) { Time.zone.today - 5 }
+      let(:end_date) { Time.zone.today - 3 }
+
+      it 'finds no contracts' do
+        expect(found_contracts).to be_none
       end
     end
   end
