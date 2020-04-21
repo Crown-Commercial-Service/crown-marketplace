93303cedad652fb2efb97bf4cd024991e41d34eb
ManFromMons <julianyoung@outlook.com>
2020-04-21 12:50:27 +0100

    Rubocop

--- db/migrate/20200421100958_updatebuildingdata.rb
+++ db/migrate/20200421100958_updatebuildingdata.rb
@@ -3,7 +3,6 @@ class Updatebuildingdata < ActiveRecord::Migration[5.2]
     FacilitiesManagement::Building.all.each do |building|
       building.updated_at = Time.current
       building.save!(validate: false)
-      puts JSON.pretty_generate(building.to_json)
     end
   end
 
