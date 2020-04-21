9e5b8d6b0ab9332a12951b8f9329abde1643c2ab
ManFromMons <julianyoung@outlook.com>
2020-04-21 15:14:38 +0100

    Added more meaningful view of missing migrations if migration files are accessible on the server

--- app/controllers/ccs_patterns/dbdata_controller.rb
+++ app/controllers/ccs_patterns/dbdata_controller.rb
@@ -3,10 +3,17 @@ module CcsPatterns
     skip_before_action :authenticate_user!
 
     def index
-      @migration_list = ActiveRecord::Base.connection.execute('select * from schema_migrations order by version desc')
-      @view_list = ActiveRecord::Base.connection.execute('select * from INFORMATION_SCHEMA.views WHERE table_schema = ANY (current_schemas(false))')
+      @migration_list = ActiveRecord::Base.connection.execute('select * from schema_migrations order by version desc').to_a.map { |m| m['version'] }
+      begin
+        @file_message = ''
+        @migration_file_list = Dir.entries('./db/migrates /').reject { |f| f.length <= 2 }.sort.reverse
+      rescue StandardError => e
+        @file_message = "#{e.message}.  Current working directory: #{Dir.getwd}"
+        @migration_file_list = []
+      end
+      @view_list      = ActiveRecord::Base.connection.execute('select * from INFORMATION_SCHEMA.views WHERE table_schema = ANY (current_schemas(false))')
       @advisory_locks = ActiveRecord::Base.connection.execute('SELECT pid, locktype, mode FROM pg_locks')
-      @kill_message = params[:kill_message]
+      @kill_message   = params[:kill_message]
     end
 
     def killpid
