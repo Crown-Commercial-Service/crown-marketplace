77b3f16b2918233de8455febce322d6f10fa46a7
ManFromMons <julianyoung@outlook.com>
2020-04-21 12:48:54 +0100

    Rubocop

--- app/controllers/ccs_patterns/dbdata_controller.rb
+++ app/controllers/ccs_patterns/dbdata_controller.rb
@@ -3,8 +3,8 @@ module CcsPatterns
     skip_before_action :authenticate_user!
 
     def index
-      @migration_list = ActiveRecord::Base.connection.execute("select * from schema_migrations order by version desc")
-      @view_list = ActiveRecord::Base.connection.execute("select * from INFORMATION_SCHEMA.views WHERE table_schema = ANY (current_schemas(false))")
+      @migration_list = ActiveRecord::Base.connection.execute('select * from schema_migrations order by version desc')
+      @view_list = ActiveRecord::Base.connection.execute('select * from INFORMATION_SCHEMA.views WHERE table_schema = ANY (current_schemas(false))')
     end
   end
 end
