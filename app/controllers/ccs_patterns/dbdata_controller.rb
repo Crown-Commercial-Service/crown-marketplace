d66e53c6456f571c248c2ac1e4db2232e08babc0
Julian Young <52413509+ManFromMons@users.noreply.github.com>
2020-04-22 12:51:05 +0100

    Feature/fm 856 building robustness (#1799)
    * Added robustness to the buildings display for unknown building data
    And a csv dump of buildings data in the metadata page in ccs-patterns
    
    * So, for some reason, a result list array of hashes has turned from string keys to symbol keys !
    This was stopping the region drop down from display regions...
    
    * Improve building data extract

--- app/controllers/ccs_patterns/dbdata_controller.rb
+++ app/controllers/ccs_patterns/dbdata_controller.rb
@@ -21,12 +21,35 @@ module CcsPatterns
       @advisory_locks = ActiveRecord::Base.connection.execute('SELECT pid, locktype, mode FROM pg_locks')
       @kill_message   = params[:kill_message]
 
-      @building_csv_text = CSV.generate do |csv|
+      @building_csv_text = CSV.generate(row_sep: '\n') do |csv|
         csv << FacilitiesManagement::Building.attribute_names
         FacilitiesManagement::Building.find_each do |building|
           csv << building.attributes.values
         end
       end
+
+      index_sql = <<~SQL
+        select
+        t.relname as table_name,
+            i.relname as index_name,
+            a.attname as column_name
+        from
+            pg_class t,
+            pg_class i,
+            pg_index ix,
+            pg_attribute a
+        where
+            t.oid = ix.indrelid
+            and i.oid = ix.indexrelid
+            and a.attrelid = t.oid
+            and a.attnum = ANY(ix.indkey)
+            and t.relkind = 'r'
+        order by
+            t.relname,
+            i.relname;
+      SQL
+
+      @index_list = ActiveRecord::Base.connection.execute(index_sql)
     end
     # rubocop:enable Metrics/AbcSize
 
