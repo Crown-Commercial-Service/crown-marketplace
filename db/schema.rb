6522fe5602d880f0fb9577a11640b278f828c29d
ManFromMons <julianyoung@outlook.com>
2020-04-24 11:17:58 +0100

    Remove reverse-engineered tables from schema

--- db/schema.rb
+++ db/schema.rb
@@ -37,15 +37,7 @@ ActiveRecord::Schema.define(version: 2020_04_22_110415) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-
-  create_table "eurostat_nuts_regions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "postcode", limit: 20
-    t.string "code", limit: 20
-    t.datetime "created_at", default: -> { "now()" }, null: false
-    t.datetime "updated_at", default: -> { "now()" }, null: false
-    t.index ["postcode"], name: "index_eurostat_nuts_regions_on_postcode", unique: true
-  end
-
+  
   create_table "facilities_management_buildings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }
     t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }
@@ -512,13 +504,7 @@ ActiveRecord::Schema.define(version: 2020_04_22_110415) do
     t.string "nuts2_code", limit: 255
     t.index ["code"], name: "nuts_regions_code_key", unique: true
   end
-
-  create_table "ons_nuts3_regions", id: false, force: :cascade do |t|
-    t.string "NUTS318CD", limit: 255
-    t.string "NUTS318NM", limit: 255
-    t.integer "FID"
-  end
-
+  
   create_table "os_address", id: false, force: :cascade do |t|
     t.bigint "uprn", null: false
     t.bigint "udprn"
