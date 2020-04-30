2d0be67fb81f6c84d961beb83a4b0d589e584ba5
Julian Young <52413509+ManFromMons@users.noreply.github.com>
2020-04-30 13:47:32 +0100

    Feature/fm 902 small buildings changes (#1879)
    * Changes for test iteration 2. FM-1018, FM-902, FM-792
    
    * Rubocop Metrics/PerceivedComplexity
    
    * Force through change to rebuild
    
    * Adjusted content for Scotting Enhanced security
    
    * Grammar change in security descriptor
    
    * Further grammatical fix
    
    * Content change and corrections for FM-1022 and 1020
    
    * Content changes and bug fix for:
    FM-1037 - Postcode Lookup failing on round-trip with malformed postcode - this unblocks FM-896
    FM-1022 - Security Type Content
    FM-1018 - Addition of new gia Validation message
    FM-1020 - Final changes to Building Type selections
    
    * Content adjustment for security types

--- app/controllers/facilities_management/beta/buildings_controller.rb
+++ app/controllers/facilities_management/beta/buildings_controller.rb
@@ -28,9 +28,10 @@ module FacilitiesManagement
       # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Style/AndOr
       def create
         @page_data[:model_object] = current_user.buildings.build(building_params)
+        @page_data[:model_object].postcode_entry = ensure_postcode_is_valid(@page_data[:model_object].postcode_entry)
 
         if params[:add_address].present?
-          @page_data[:model_object].address_postcode = @page_data[:model_object].postcode_entry
+          @page_data[:model_object].address_postcode = ensure_postcode_is_valid(@page_data[:model_object].postcode_entry)
           rebuild_page_data(@page_data[:model_object])
           rebuild_page_description('add_address')
           render action: :add_address and return
@@ -56,9 +57,10 @@ module FacilitiesManagement
 
       def update
         @page_data[:model_object].assign_attributes(building_params)
+        @page_data[:model_object].postcode_entry = ensure_postcode_is_valid(@page_data[:model_object].postcode_entry)
 
         if params[:add_address].present?
-          @page_data[:model_object].address_postcode = @page_data[:model_object].postcode_entry
+          @page_data[:model_object].address_postcode = ensure_postcode_is_valid(@page_data[:model_object].postcode_entry)
           rebuild_page_description 'add_address'
           render action: :add_address and return
         end
