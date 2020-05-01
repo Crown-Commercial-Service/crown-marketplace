2240df7c54cb5dbb1508db9575273a218f8e1582
ManFromMons <julianyoung@outlook.com>
2020-05-01 01:24:54 +0100

    Update to postcode views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_142805) do
+ActiveRecord::Schema.define(version: 2020_05_01_001023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -37,7 +37,15 @@ ActiveRecord::Schema.define(version: 2020_04_24_142805) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-  
+
+  create_table "eurostat_nuts_regions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "postcode", limit: 20
+    t.string "code", limit: 20
+    t.datetime "created_at", default: -> { "now()" }, null: false
+    t.datetime "updated_at", default: -> { "now()" }, null: false
+    t.index ["postcode"], name: "index_eurostat_nuts_regions_on_postcode", unique: true
+  end
+
   create_table "facilities_management_buildings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }
     t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }
@@ -505,7 +513,13 @@ ActiveRecord::Schema.define(version: 2020_04_24_142805) do
     t.string "nuts2_code", limit: 255
     t.index ["code"], name: "nuts_regions_code_key", unique: true
   end
-  
+
+  create_table "ons_nuts3_regions", id: false, force: :cascade do |t|
+    t.string "NUTS318CD", limit: 255
+    t.string "NUTS318NM", limit: 255
+    t.integer "FID"
+  end
+
   create_table "os_address", id: false, force: :cascade do |t|
     t.bigint "uprn", null: false
     t.bigint "udprn"
@@ -603,6 +617,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_142805) do
     t.string "code", limit: 20
     t.datetime "created_at", default: -> { "now()" }, null: false
     t.datetime "updated_at", default: -> { "now()" }, null: false
+    t.index ["code"], name: "nuts_code_postcode_nuts_regions"
     t.index ["postcode"], name: "index_postcodes_nuts_regions_on_postcode", unique: true
   end
 
