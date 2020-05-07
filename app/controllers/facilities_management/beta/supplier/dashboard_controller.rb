e4f46aa3fe6a813c73a87b2f42d5b5d8d6f2ca4c
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2020-05-07 15:20:05 +0100

    Update dashboard_controller.rb

--- app/controllers/facilities_management/beta/supplier/dashboard_controller.rb
+++ app/controllers/facilities_management/beta/supplier/dashboard_controller.rb
@@ -5,7 +5,6 @@ module FacilitiesManagement
         include FacilitiesManagement::ControllerLayoutHelper
         include FacilitiesManagement::Beta::Supplier::DashboardHelper
 
-        before_action :authorize_user
         before_action :set_supplier
         before_action :set_page_detail
 
