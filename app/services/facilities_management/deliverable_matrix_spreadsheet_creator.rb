d21c9fdb4932a5cea6ea2bd9db67fcaf2f8b7b90
anthonyoleary <anthony.oleary@finddx.org>
2020-05-07 13:47:40 +0100

    FM-1081 fix, did not add a unit test as the tests are skipped

--- app/services/facilities_management/deliverable_matrix_spreadsheet_creator.rb
+++ app/services/facilities_management/deliverable_matrix_spreadsheet_creator.rb
@@ -43,6 +43,7 @@ class FacilitiesManagement::DeliverableMatrixSpreadsheetCreator
     @package.to_stream.read
   end
 
+  # rubocop:disable Metrics/AbcSize
   def build
     @package = Axlsx::Package.new do |p|
       p.workbook.styles do |s|
@@ -61,7 +62,7 @@ class FacilitiesManagement::DeliverableMatrixSpreadsheetCreator
           style_service_matrix_sheet(sheet, standard_column_style)
         end
 
-        unless @units_of_measure_values.nil?
+        unless units_of_measure_values.nil?
           p.workbook.add_worksheet(name: 'Volume') do |sheet|
             add_header_row(sheet, ['Service Reference',	'Service Name',	'Metric per annum'])
             number_volume_services = add_volumes_information(sheet)
@@ -77,10 +78,11 @@ class FacilitiesManagement::DeliverableMatrixSpreadsheetCreator
       end
     end
   end
+  # rubocop:enable Metrics/AbcSize
 
   def list_of_allowed_volume_services
     # FM-736 requirement
-    @list_of_allowed_volume_services ||= FacilitiesManagement::ServicesAndQuestions.new.code.uniq
+    @list_of_allowed_volume_services ||= FacilitiesManagement::ServicesAndQuestions.new.codes.uniq
   end
 
   private
@@ -270,8 +272,7 @@ class FacilitiesManagement::DeliverableMatrixSpreadsheetCreator
     allowed_volume_services.each do |s|
       new_row = [s['code'], s['name'], s['metric']]
       @active_procurement_buildings.each do |b|
-        uvs = @units_of_measure_values.select { |u| b.building_id == u[:building_id] }
-
+        uvs = @units_of_measure_values.flatten.select { |u| b.building_id == u[:building_id] }
         suv = uvs.find { |u| s['code'] == u[:service_code] }
 
         new_row << calculate_uom_value(suv) if suv
