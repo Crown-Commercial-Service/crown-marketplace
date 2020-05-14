13a384155dfd0e19bc3f70fa275585e0c0c4550e
ManFromMons <julianyoung@outlook.com>
2020-05-14 15:45:26 +0100

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2020_05_13_080506) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "eurostat_nuts_regions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "postcode", limit: 20
+    t.string "code", limit: 20
+    t.datetime "created_at", default: -> { "now()" }, null: false
+    t.datetime "updated_at", default: -> { "now()" }, null: false
+    t.index ["postcode"], name: "index_eurostat_nuts_regions_on_postcode", unique: true
+  end
+
   create_table "facilities_management_buildings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }
     t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }
@@ -506,6 +514,12 @@ ActiveRecord::Schema.define(version: 2020_05_13_080506) do
     t.index ["code"], name: "nuts_regions_code_key", unique: true
   end
 
+  create_table "ons_nuts3_regions", id: false, force: :cascade do |t|
+    t.string "NUTS318CD", limit: 255
+    t.string "NUTS318NM", limit: 255
+    t.integer "FID"
+  end
+
   create_table "os_address", id: false, force: :cascade do |t|
     t.bigint "uprn", null: false
     t.bigint "udprn"
@@ -601,8 +615,9 @@ ActiveRecord::Schema.define(version: 2020_05_13_080506) do
   create_table "postcodes_nuts_regions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "postcode", limit: 20
     t.string "code", limit: 20
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", default: -> { "now()" }, null: false
+    t.datetime "updated_at", default: -> { "now()" }, null: false
+    t.index ["code"], name: "nuts_code_postcode_nuts_regions"
     t.index ["postcode"], name: "index_postcodes_nuts_regions_on_postcode", unique: true
   end
 
