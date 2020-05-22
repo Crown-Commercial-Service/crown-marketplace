aa82e3f12ce758a44dd5e45bbe5e957bf4c1b6c7
Chee-Ng <chee.ng@crowncommercial.gov.uk>
2020-05-22 12:06:02 +0100

    redo validation for buyer detail postcode validation

--- app/models/facilities_management/buyer_detail.rb
+++ app/models/facilities_management/buyer_detail.rb
@@ -10,6 +10,7 @@ module FacilitiesManagement
     validates :organisation_address_postcode, presence: true, format: { with: /\A([a-zA-Z (0-9)]*)\z/ }, on: :update
     validate  :address_entered_when_postcode_provided, on: :update
     validates :central_government, inclusion: { in: [true, false], message: :blank }, on: :update
+    validate :valid_postcode?, on: %i[update update_address], if: -> { organisation_address_postcode.present? }
 
     validates :organisation_address_postcode, presence: true, format: { with: /\A([a-zA-Z (0-9)]*)\z/ }, on: :update_address
     validates :organisation_address_town, presence: true, on: :update_address
@@ -19,6 +20,14 @@ module FacilitiesManagement
 
     def full_organisation_address
       [organisation_address_line_1, organisation_address_line_2, organisation_address_town, organisation_address_county].reject(&:nil?).reject(&:empty?).join(', ') + " #{organisation_address_postcode}"
+    end
+
+    def valid_postcode?
+      pc = UKPostcode.parse(organisation_address_postcode)
+      return true if pc.full_valid?
+
+      errors.add(:organisation_address_postcode, :invalid)
+      false
     end
 
     private
