61b5bf82868ef5d922e92992776cc218f095f682
ManFromMons <julianyoung@outlook.com>
2020-05-29 12:19:26 +0100

    Reduce region lookup to outcode again. Ensure PS address class is not returned in lookup results

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_215825) do
+ActiveRecord::Schema.define(version: 2020_05_29_110446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
