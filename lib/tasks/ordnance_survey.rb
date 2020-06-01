434d8e346ceb47b144b35ec1845954d1bce9cacb
Julian Young <52413509+ManFromMons@users.noreply.github.com>
2020-06-01 15:41:24 +0100

    Ensure initial pctable task doesn't fail (#2151)

--- lib/tasks/ordnance_survey.rb
+++ lib/tasks/ordnance_survey.rb
@@ -53,8 +53,8 @@ module OrdnanceSurvey
     query = <<~SQL
         DO $$
         BEGIN
-        drop view postcode_lookup cascade;
-        drop view os_address_view_2 cascade;
+        drop view if exists postcode_lookup cascade;
+        drop view if exists os_address_view_2 cascade;
         CREATE VIEW os_address_view_2
         (building, street_address, postal_town, postcode_locator, sub_building_name, building_name, pao_name,
          sao_name, house_number, street_description, village, post_town, formatted_postcode, building_ref, uprn,
