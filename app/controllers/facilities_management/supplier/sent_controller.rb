9321aeda7f0698ef89c913b61879da7a4587c739
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-06-01 12:29:13 +0100

    Update supplier dashboard
    recieved offer
    
    Respond to offfer
    
    Sent validation
    
    Fix tabbing issue

--- app/controllers/facilities_management/supplier/sent_controller.rb
+++ app/controllers/facilities_management/supplier/sent_controller.rb
@@ -1,6 +1,6 @@
 class FacilitiesManagement::Supplier::SentController < FacilitiesManagement::Supplier::FrameworkController
   def index
-    authorize! :manage, @contract
     @contract = FacilitiesManagement::ProcurementSupplier.find(params[:contract_id])
+    authorize! :manage, @contract
   end
 end
