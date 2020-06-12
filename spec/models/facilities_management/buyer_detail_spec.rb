932832abbb4a2c84e12721f2dd05cdf6f87e4bc3
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-06-12 14:16:19 +0100

    buyer account controller spec updates

--- spec/models/facilities_management/buyer_detail_spec.rb
+++ spec/models/facilities_management/buyer_detail_spec.rb
@@ -52,8 +52,7 @@ RSpec.describe FacilitiesManagement::BuyerDetail, type: :model do
       it 'will throw an error' do
         buyer_detail.organisation_address_postcode = 'SAN TA1'
         buyer_detail.save
-        # binding.pry
-        # expect(buyer_detail.attributes[:organisation_address_postcode].errors[:invalid].first).to eq 'Enter a valid postcode, for example SW1A 1AA'
+
         expect(buyer_detail.errors.messages).to eq(organisation_address_postcode: ['Enter a valid postcode, for example SW1A 1AA'])
       end
     end
