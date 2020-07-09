c7cc6f944ee0b61eb0893eb5d2298143160dc075
Chris Gannon <cjgannon@gmail.com>
2020-07-09 11:26:09 +0100

    Do service hours updates in batches

--- db/migrate/20200701092909_add_personnel_to_existing_service_hours.rb
+++ db/migrate/20200701092909_add_personnel_to_existing_service_hours.rb
@@ -1,11 +1,12 @@
 class AddPersonnelToExistingServiceHours < ActiveRecord::Migration[5.2]
   def self.up
-    procurement_building_services = FacilitiesManagement::ProcurementBuildingService.all
-
-    procurement_building_services.each do |procurement_building_service|
-      if procurement_building_service.service_hours.friday.service_choice.present? && procurement_building_service.service_hours.personnel.nil?
-        procurement_building_service.service_hours.personnel = 1
-        procurement_building_service.save
+    FacilitiesManagement::ProcurementBuildingService.find_in_batches do |group|
+      sleep(50)
+      group.each do |procurement_building_service|
+        if procurement_building_service.service_hours.friday.service_choice.present? && procurement_building_service.service_hours.personnel.nil?
+          procurement_building_service.service_hours.personnel = 1
+          procurement_building_service.save
+        end
       end
     end
   end
