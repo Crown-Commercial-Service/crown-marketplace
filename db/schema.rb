d7a76ab9613de239a9dadb5d1de06872bec445eb
Chris Gannon <cjgannon@gmail.com>
2020-07-14 15:41:28 +0100

    Add zero external area to buildings with a nil area

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_113607) do
+ActiveRecord::Schema.define(version: 2020_07_14_140233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
