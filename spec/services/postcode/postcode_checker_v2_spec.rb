dfb3b7b2c7f957789851b90ed57a94b6421bd2c2
Iulia R Ionita <iulia@ionita.me>
2020-07-14 12:37:57 +0100

    Postcodes now are casted to uppercase even on create
    Fixed regions
    
    Rubocop fix

--- spec/services/postcode/postcode_checker_v2_spec.rb
+++ spec/services/postcode/postcode_checker_v2_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Postcode::PostcodeCheckerV2 do
   describe '#find_region' do
     context 'when region cannot be found for postcode' do
       it 'returns all Nuts2Region regions' do
-        expect(described_class.find_region('AB1 2CD').count).to eq Nuts2Region.all.count
+        expect(described_class.find_region('AB1 2CD').count).to eq 73
       end
     end
   end
