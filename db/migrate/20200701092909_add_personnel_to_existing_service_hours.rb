7df6318bebbab234bd2e07561eb61413bf74af24
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-07-22 09:50:40 +0100

    Update migration to have a better querey

--- db/migrate/20200701092909_add_personnel_to_existing_service_hours.rb
+++ db/migrate/20200701092909_add_personnel_to_existing_service_hours.rb
@@ -1,7 +1,7 @@
 class AddPersonnelToExistingServiceHours < ActiveRecord::Migration[5.2]
   def self.up
-    FacilitiesManagement::ProcurementBuildingService.find_in_batches do |group|
-      sleep(50)
+    FacilitiesManagement::ProcurementBuildingService.where(code: %w[H.4 H.5 I.1 I.2 I.3 I.4 J.1 J.2 J.3 J.4 J.5 J.6]).find_in_batches do |group|
+      sleep(5)
       group.each do |procurement_building_service|
         if procurement_building_service.service_hours.friday.service_choice.present? && procurement_building_service.service_hours.personnel.nil?
           procurement_building_service.service_hours.personnel = 1
