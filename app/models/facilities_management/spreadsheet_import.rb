2325fcfb72b17ba5fdbf186c0df6484c89552a41
Iulia R Ionita <iulia@ionita.me>
2020-07-30 14:04:45 +0100

    Building and services and standards import done - happy path only

--- app/models/facilities_management/spreadsheet_import.rb
+++ app/models/facilities_management/spreadsheet_import.rb
@@ -4,13 +4,16 @@ module FacilitiesManagement
 
     belongs_to :procurement, class_name: 'FacilitiesManagement::Procurement', foreign_key: :facilities_management_procurement_id, inverse_of: :spreadsheet_imports
 
-    has_one_attached :spreadsheet_file
+    mount_uploader :spreadsheet_file, SpreadsheetFileUploader
 
     aasm do
       state :in_progress, initial: true
       state :succeeded, :failed
-      event :finish_import do
+      event :succeed do
         transitions from: :in_progress, to: :succeeded
+      end
+      event :fail do
+        transitions from: :in_progress, to: :failed
       end
     end
   end
