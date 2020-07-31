dda598e4780207075a0edb897e93d981588646ca
ramchurnk <kunal.ramchurn@crowncommercial.gov.uk>
2020-07-31 11:35:05 +0100

    LS accessibility statement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, except: %i[status index cookies landing_page accessibility_statement_fm not_permitted]
+  before_action :authenticate_user!, except: %i[status index cookies landing_page accessibility_statement_fm accessibility_statement_mc accessibility_statement_ls not_permitted]
 
   def index
     redirect_to ccs_homepage_url
@@ -19,6 +19,10 @@ class HomeController < ApplicationController
 
   def accessibility_statement_mc
     params[:service] = 'management_consultancy' if params[:service].nil?
+  end
+
+  def accessibility_statement_ls
+    params[:service] = 'legal_services' if params[:service].nil?
   end
 
   def not_permitted
