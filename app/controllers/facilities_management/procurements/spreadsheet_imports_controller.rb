272fadb29763883bcc4a76d7549355f69c5a1fd7
Andy <andy@wireworldmedia.co.uk>
2020-08-05 17:02:43 +0100

    Start localisation of spreadsheet validation messages

--- app/controllers/facilities_management/procurements/spreadsheet_imports_controller.rb
+++ app/controllers/facilities_management/procurements/spreadsheet_imports_controller.rb
@@ -11,6 +11,7 @@ module FacilitiesManagement
           FacilitiesManagement::SpreadsheetImporter.new(@spreadsheet_import).data_import # TODO: This will become a background job
           redirect_to facilities_management_procurement_spreadsheet_import_path(procurement_id: @spreadsheet_import.procurement.id, id: @spreadsheet_import.id)
         else
+          puts ">>>>>>> Errors:\n  #{@spreadsheet_import.errors.full_messages.join("\n  ")}" # TODO: remove
           render :new
         end
       end
--- app/models/facilities_management/spreadsheet_import.rb
+++ app/models/facilities_management/spreadsheet_import.rb
@@ -4,12 +4,15 @@ module FacilitiesManagement
 
     belongs_to :procurement, class_name: 'FacilitiesManagement::Procurement', foreign_key: :facilities_management_procurement_id, inverse_of: :spreadsheet_imports
 
+    TRANSLATION_SCOPE = %i[errors messages bulk_upload].freeze
     has_one_attached :spreadsheet_file
-    validates :spreadsheet_file, attached: true
-    validates :spreadsheet_file, content_type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    validates :spreadsheet_file, size: { less_than: 10.megabytes }
-    validates :spreadsheet_file, antivirus: true
-    validate :spreadsheet_file_validation
+    validates :spreadsheet_file, attached: { message: I18n.t(:not_attached, scope: TRANSLATION_SCOPE)}
+    validates :spreadsheet_file, antivirus: { message: I18n.t(:malicious, scope: TRANSLATION_SCOPE) }
+    validates :spreadsheet_file, size: { less_than: 10.megabytes, message: I18n.t(:too_large, scope: TRANSLATION_SCOPE) }
+    validates :spreadsheet_file, content_type: { with: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                                                 message: I18n.t(:wrong_content_type, scope: TRANSLATION_SCOPE) }
+    validate :spreadsheet_file_ext_validation
+    validate :spreadsheet_basic_data_validation
 
     aasm do
       state :importing, initial: true
@@ -19,14 +22,24 @@ module FacilitiesManagement
       end
       event :fail do
         transitions from: :importing, to: :failed
+        after do
+          spreadsheet_file.purge
+        end
       end
     end
 
-    def spreadsheet_file_validation
+    def spreadsheet_file_ext_validation
       return unless spreadsheet_file.attached?
 
-      errors[:base] << 'File extension must be .xlsx' unless spreadsheet_file.blob.filename.to_s.end_with?('.xlsx')
-      spreadsheet_file.purge if errors.any?
+      errors[:base] << I18n.t(:wrong_extension, scope: TRANSLATION_SCOPE) unless spreadsheet_file.blob.filename.to_s.end_with?('.xlsx')
+    end
+
+    def spreadsheet_basic_data_validation
+      return if errors.any?
+
+      FacilitiesManagement::SpreadsheetImporter.new(self).basic_data_validation.each do |error_message|
+        errors[:base] << error_message
+      end
     end
   end
 end
