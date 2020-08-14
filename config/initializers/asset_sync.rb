3550e2646ff264e457ec33a768fb2fd095da601b
Chris Gannon <cjgannon@gmail.com>
2020-08-14 14:45:51 +0100

    only run asset sync in production

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,5 +1,6 @@
-if defined?(AssetSync) && Rails.env.production?
+if defined?(AssetSync)
   AssetSync.configure do |config|
+    config.enabled = Rails.env.production?
     config.fog_provider = 'AWS'
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
     config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
