baeafb906ecac60b5ba2226b4418688a8cd29fff
Chris Gannon <cjgannon@gmail.com>
2020-08-20 14:06:02 +0100

    Add asset sync

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -2,9 +2,9 @@ if defined?(AssetSync)
   AssetSync.configure do |config|
     config.enabled = Rails.env.production? && ENV['APP_RUN_PRECOMPILE_ASSETS'] == 'TRUE'
     config.fog_provider = 'AWS'
-    config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    config.aws_iam_roles = true
     config.aws_session_token = ENV['AWS_SESSION_TOKEN'] if ENV.key?('AWS_SESSION_TOKEN')
     config.fog_directory = ENV['ASSETS_BUCKET']
+    config.fog_region = 'eu-west-2'
   end
 end
