5b576c451e41ba20d3ac896375d9c59c07f3aa09
Kunal Ramchurn <kunal.ramchurn@crowncommercial.gov.uk>
2020-08-24 15:57:50 +0100

    DA eligible tag added to services page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -353,5 +353,9 @@ module ApplicationHelper
 
     content_tag :strong, text, class: ['govuk-tag'] << extra_classes[status]
   end
+
+  def da_eligible?(code)
+    %w[C.5 E.4 G.1 G.3 H.4 H.5 I.1 I.2 I.3 I.4 J.1 J.2 J.3 J.4 J.5 J.6 K.1 K.2 K.3 K.4 K.5 K.6 K.7].include? code
+  end
 end
 # rubocop:enable Metrics/ModuleLength
