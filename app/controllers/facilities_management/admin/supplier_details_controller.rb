c25fddad34ecbc9a0f45d5aab0754ca2d51bbaf8
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-01-08 10:15:41 +0000

    Create routes for the supplier details
    Create index page
    
    Add validations for the supplier details
    
    Add validations for the supplier contact information
    
    Add validations for the duns and registration number
    
    Add validation for the address
    
    Make the show page and add links
    
    Create the edit page for the supplier name
    
    Add the page for the supplier contact information
    
    Complete the pages for updating supplier details
    
    Add tests for the controller
    
    Add update tests for the controller
    
    Remove the unneeded supplier replacement task

---
+++ app/controllers/facilities_management/admin/supplier_details_controller.rb
@@ -0,0 +1,53 @@
+module FacilitiesManagement
+  module Admin
+    class SupplierDetailsController < FacilitiesManagement::Admin::FrameworkController
+      before_action :set_supplier, except: :index
+      before_action :set_page, only: %i[edit update]
+
+      def index
+        @suppliers = FacilitiesManagement::Admin::SuppliersAdmin.select(:supplier_id, :supplier_name, :contact_email).order(:supplier_name)
+      end
+
+      def show; end
+
+      def edit; end
+
+      def update
+        @supplier.assign_attributes(supplier_params)
+
+        if @supplier.save(context: @page)
+          redirect_to facilities_management_admin_supplier_detail_path
+        else
+          render :edit
+        end
+      end
+
+      private
+
+      def set_supplier
+        @supplier = FacilitiesManagement::Admin::SuppliersAdmin.find(params[:id])
+      end
+
+      def set_page
+        @page = params[:page].to_sym
+      end
+
+      def supplier_params
+        params.require(:facilities_management_admin_suppliers_admin).permit(permitted_params)
+      end
+
+      def permitted_params
+        case @page
+        when :supplier_name
+          %i[supplier_name]
+        when :supplier_contact_information
+          %i[contact_name contact_email contact_phone]
+        when :additional_supplier_information
+          %i[duns registration_number]
+        when :supplier_address
+          %i[address_line_1 address_line_2 address_town address_county address_postcode]
+        end
+      end
+    end
+  end
+end
