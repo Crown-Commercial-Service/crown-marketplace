62713025f4f5e6471d1082b0cffba385ce45c448
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-01-13 09:37:41 +0000

    Add migration to assign suppliers to users

---
+++ db/migrate/20210113092431_link_user_accounts_to_supplier_details.rb
@@ -0,0 +1,24 @@
+class LinkUserAccountsToSupplierDetails < ActiveRecord::Migration[5.2]
+  class SupplierDetail < ApplicationRecord
+    self.table_name = 'facilities_management_supplier_details'
+  end
+
+  class SupplierUser < ApplicationRecord
+    self.table_name = 'users'
+  end
+
+  def up
+    SupplierDetail.all.each do |supplier_detail|
+      next if supplier_detail.user_id.present?
+
+      contact_email = supplier_detail.contact_email
+      supplier_user = SupplierUser.find_by(email: contact_email)
+      next unless supplier_user
+
+      supplier_detail.user_id = supplier_user.id
+      supplier_detail.save
+    end
+  end
+
+  def down; end
+end
