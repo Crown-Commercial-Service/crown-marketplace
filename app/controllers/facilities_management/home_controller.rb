5bd5564269fb3ff5f9e8f2c9ee53513654dacde7
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-02-19 14:45:04 +0000

    Remove landing page
    Update how not permitted works
    
    Update accessibility statements to be in section
    
    Update the cookies page to be in service
    
    Update the header banner for admin
    
    Add specs for updated routes

--- app/controllers/facilities_management/home_controller.rb
+++ app/controllers/facilities_management/home_controller.rb
@@ -1,8 +1,14 @@
 module FacilitiesManagement
   class HomeController < FacilitiesManagement::FrameworkController
-    before_action :authenticate_user!, except: :index
-    before_action :authorize_user, except: :index
+    before_action :authenticate_user!, except: %i[index not_permitted accessibility_statement cookies]
+    before_action :authorize_user, except: %i[index not_permitted accessibility_statement cookies]
 
     def index; end
+
+    def not_permitted; end
+
+    def accessibility_statement; end
+
+    def cookies; end
   end
 end
