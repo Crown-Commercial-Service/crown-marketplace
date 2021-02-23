310265797cd372d5d415eb332f2a28bab7922e07
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-02-23 10:12:52 +0000

    Add the error pages
    Make small changes to error pages
    
    Add support message to bottom of page
    
    Remove old public pages
    
    Update format of the pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,3 +1,25 @@
 class ErrorsController < ApplicationController
-  layout 'errors'
+  def not_found
+    respond_to do |format|
+      format.html { render status: :not_found, layout: 'error' }
+    end
+  end
+
+  def unacceptable
+    respond_to do |format|
+      format.html { render status: :unprocessable_entity, layout: 'error' }
+    end
+  end
+
+  def internal_error
+    respond_to do |format|
+      format.html { render status: :internal_server_error, layout: 'error' }
+    end
+  end
+
+  def service_unavailable
+    respond_to do |format|
+      format.html { render status: :service_unavailable, layout: 'error' }
+    end
+  end
 end
