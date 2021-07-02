a8fe9e28f12bac6176feb36aa13020cd0b4a6c24
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-07-02 16:07:39 +0100

    Fix after the rebase

--- app/controllers/facilities_management/rm3830/procurements/edit_buildings_controller.rb
+++ app/controllers/facilities_management/rm3830/procurements/edit_buildings_controller.rb
@@ -3,7 +3,7 @@ module FacilitiesManagement
     module Procurements
       class EditBuildingsController < FacilitiesManagement::FrameworkController
         include Procurements::EditBuildingsControllerNavigation
-        include FacilitiesManagement::PageDetail::EditBuildings
+        include FacilitiesManagement::PageDetail::Buildings
         include FacilitiesManagement::FindAddressConcern
         include BuildingsConcern
 
