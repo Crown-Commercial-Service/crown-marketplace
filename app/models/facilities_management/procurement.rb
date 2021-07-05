42ee33987df17d5799f12d34531a456ccfa0028d
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-07-05 16:29:36 +0100

    Make quick view work

--- app/models/facilities_management/procurement.rb
+++ app/models/facilities_management/procurement.rb
@@ -2,8 +2,7 @@ module FacilitiesManagement
   class Procurement < ApplicationRecord
     include ProcurementValidator
 
-    # Belongs to a buyer
-    belongs_to :user, inverse_of: :procurements
+    self.abstract_class = true
 
     has_many :optional_call_off_extensions, foreign_key: :facilities_management_procurement_id, inverse_of: :procurement, dependent: :destroy
     accepts_nested_attributes_for :optional_call_off_extensions, allow_destroy: true
