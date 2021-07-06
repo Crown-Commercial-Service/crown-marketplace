5697cfa40801c9d0b2ebaa936b011b764d7bdc13
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-07-06 08:35:01 +0100

    Update the method to generate contract numbers

--- spec/services/facilities_management/contract_number_generator_spec.rb
+++ spec/services/facilities_management/contract_number_generator_spec.rb
@@ -1,34 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe FacilitiesManagement::ContractNumberGenerator do
-  let(:contract_number_generator) { described_class.new(procurement_state: procurement_state, used_numbers: []) }
+  let(:contract_number_generator) { described_class.new(procurement_state: procurement_state, framework: framework, used_numbers: []) }
 
   describe '.new_number' do
-    context 'with a procurement in direct_award' do
-      let(:procurement_state) { :direct_award }
+    context 'with a RM3830 framework' do
+      let(:framework) { 'RM3830' }
 
-      it 'has the correct format' do
-        expect(contract_number_generator.new_number).to match(/\ARM3830-DA\d{4}-\d{4}\z/)
+      context 'with a procurement in direct_award' do
+        let(:procurement_state) { :direct_award }
+
+        it 'has the correct format' do
+          expect(contract_number_generator.new_number).to match(/\ARM3830-DA\d{4}-\d{4}\z/)
+        end
+
+        it 'has the current year as the final 4 digits' do
+          current_year = Date.current.year.to_s
+
+          expect(contract_number_generator.new_number.split('-')[2]).to eq(current_year)
+        end
       end
 
-      it 'has the current year as the final 4 digits' do
-        current_year = Date.current.year.to_s
+      context 'with a procurement going to further_competition' do
+        let(:procurement_state) { :further_competition }
 
-        expect(contract_number_generator.new_number.split('-')[2]).to eq(current_year)
+        it 'has the correct format' do
+          expect(contract_number_generator.new_number).to match(/\ARM3830-FC\d{4}-\d{4}\z/)
+        end
+
+        it 'has the current year as the final 4 digits' do
+          current_year = Date.current.year.to_s
+
+          expect(contract_number_generator.new_number.split('-')[2]).to eq(current_year)
+        end
       end
     end
 
-    context 'with a procurement going to further_competition' do
-      let(:procurement_state) { :further_competition }
+    context 'with an RM6232 framework' do
+      let(:framework) { 'RM6232' }
 
-      it 'has the correct format' do
-        expect(contract_number_generator.new_number).to match(/\ARM3830-FC\d{4}-\d{4}\z/)
+      context 'with a procurement in direct_award' do
+        let(:procurement_state) { :direct_award }
+
+        it 'has the correct format' do
+          expect(contract_number_generator.new_number).to match(/\ARM6232-DA\d{4}-\d{4}\z/)
+        end
+
+        it 'has the current year as the final 4 digits' do
+          current_year = Date.current.year.to_s
+
+          expect(contract_number_generator.new_number.split('-')[2]).to eq(current_year)
+        end
       end
 
-      it 'has the current year as the final 4 digits' do
-        current_year = Date.current.year.to_s
+      context 'with a procurement going to further_competition' do
+        let(:procurement_state) { :further_competition }
 
-        expect(contract_number_generator.new_number.split('-')[2]).to eq(current_year)
+        it 'has the correct format' do
+          expect(contract_number_generator.new_number).to match(/\ARM6232-FC\d{4}-\d{4}\z/)
+        end
+
+        it 'has the current year as the final 4 digits' do
+          current_year = Date.current.year.to_s
+
+          expect(contract_number_generator.new_number.split('-')[2]).to eq(current_year)
+        end
       end
     end
   end
