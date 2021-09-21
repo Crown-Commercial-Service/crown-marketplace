9cb03e519355b7beb968b59ec1e94a64e0c36413
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-09-21 10:51:18 +0100

    Update the migrations to do some parts seperately

---
+++ db/migrate/20210715113723_rename_facilities_management_indexes.rb
@@ -0,0 +1,19 @@
+class RenameFacilitiesManagementIndexes < ActiveRecord::Migration[6.0]
+  INDEXES = [
+    %i[facilities_management_management_reports index_facilities_management_management_reports_on_user_id index_fm_rm3830_management_reports_on_user_id user_id],
+    %i[facilities_management_supplier_details index_facilities_management_supplier_details_on_contact_email index_fm_rm3830_supplier_details_on_contact_email contact_email],
+    %i[facilities_management_supplier_details index_facilities_management_supplier_details_on_supplier_name index_fm_rm3830_supplier_details_on_supplier_name supplier_name]
+  ].freeze
+
+  def up
+    INDEXES.each do |table_name, old_index, new_index, column|
+      rename_index table_name, old_index, new_index if ActiveRecord::Base.connection.index_exists?(table_name, column, name: old_index)
+    end
+  end
+
+  def down
+    INDEXES.each do |table_name, old_index, new_index, column|
+      rename_index table_name, new_index, old_index if ActiveRecord::Base.connection.index_exists?(table_name, column, name: new_index)
+    end
+  end
+end
