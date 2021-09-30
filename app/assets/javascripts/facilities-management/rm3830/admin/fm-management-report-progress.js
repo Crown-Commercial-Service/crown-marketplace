c27f149e286ad37aa7d18b077943c087f6122121
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-09-30 11:19:58 +0100

    Fix the issue with the admin upload
    Make page update automatically
    
    Add specs for management report controllers
    
    Add specs for management repor csv generator

---
+++ app/assets/javascripts/facilities-management/rm3830/admin/fm-management-report-progress.js
@@ -0,0 +1,50 @@
+const managementReportProgress = {
+  continue: true,
+  currentState: 'generating_csv',
+  interval: 15000,
+
+  init() {
+    this.statusURL = `${window.location.pathname}/status`;
+    this.updateStatusURL = `${window.location.pathname}/update_status`;
+
+    setTimeout(this.checkImportProgress, this.interval);
+  },
+
+  checkImportProgress() {
+    $.ajax({
+      type: 'GET',
+      url: managementReportProgress.statusURL,
+      data: $(this).serialize(),
+      dataType: 'json',
+      success(data) {
+        managementReportProgress.currentState = data.status;
+      },
+      error() {
+        managementReportProgress.continue = false;
+      },
+      complete() {
+        managementReportProgress.processImportStatus();
+      },
+    });
+  },
+
+  processImportStatus() {
+    if (this.continue) {
+      if (this.currentState === 'completed') {
+        this.processComplete();
+      } else {
+        setTimeout(this.checkImportProgress, this.interval);
+      }
+    }
+  },
+
+  processComplete() {
+    window.location.reload();
+  },
+};
+
+$(() => {
+  if ($('#management-report-status').length && $('.management-report-state-generating_csv').length) {
+    managementReportProgress.init();
+  }
+});
