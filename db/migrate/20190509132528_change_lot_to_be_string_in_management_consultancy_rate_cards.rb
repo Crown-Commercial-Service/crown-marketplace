7733a8c14c6d17831153f35f9ef254f1b75832e6
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-03-31 13:35:32 +0100

    Disable Rails/ReversibleMigration for old migrations

--- db/migrate/20190509132528_change_lot_to_be_string_in_management_consultancy_rate_cards.rb
+++ db/migrate/20190509132528_change_lot_to_be_string_in_management_consultancy_rate_cards.rb
@@ -1,5 +1,7 @@
+# rubocop:disable Rails/ReversibleMigration
 class ChangeLotToBeStringInManagementConsultancyRateCards < ActiveRecord::Migration[5.2]
   def change
     change_column :management_consultancy_rate_cards, :lot, :string
   end
 end
+# rubocop:enable Rails/ReversibleMigration
