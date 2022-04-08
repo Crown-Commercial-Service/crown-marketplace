982065c6ead0c9e2c67d414cae09ab2b5dac3b26
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-04-08 10:41:30 +0100

    As the user testing will mainly deal with the frontend, I thought it would be worth developing the service to select the services based on the potential user input.
    As part of this I:
    - added methods to the service model to determine the lot number
    - added a method to the supplier model to search for the suppliers who meet the requirements
    - added the SuppliersSelector which deals with the logic of getting the lot number and suppliers

--- app/models/facilities_management/rm6232/supplier.rb
+++ app/models/facilities_management/rm6232/supplier.rb
@@ -2,6 +2,14 @@ module FacilitiesManagement
   module RM6232
     class Supplier < ApplicationRecord
       has_many :lot_data, inverse_of: :supplier, class_name: 'FacilitiesManagement::RM6232::Supplier::LotData', dependent: :destroy, foreign_key: :facilities_management_rm6232_supplier_id
+
+      def self.select_suppliers(lot_code, service_codes, region_codes)
+        joins(:lot_data)
+          .where('facilities_management_rm6232_supplier_lot_data.lot_code': lot_code)
+          .where('facilities_management_rm6232_supplier_lot_data.service_codes @> ?', "{#{service_codes.join(',')}}")
+          .where('facilities_management_rm6232_supplier_lot_data.region_codes @> ?', "{#{region_codes.join(',')}}")
+          .order(:supplier_name)
+      end
     end
   end
 end
