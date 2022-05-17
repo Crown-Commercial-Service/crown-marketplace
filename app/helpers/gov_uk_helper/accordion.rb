08872f4d4e830ffef396b2d7c9fffa001a6699d4
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-05-17 10:43:17 +0100

    I wanted to refactor the accordion sections because I have been finding it difficult to get it to work consistently as expected. This will help with the development of the new framework which includes accordion sections.
    The main change was to put nearly all the logic to create the accordion into a separate helper. We can then just pass in some attributes and the data in it’s sections which will then generate the accordion as we want it.

--- app/helpers/gov_uk_helper/accordion.rb
+++ app/helpers/gov_uk_helper/accordion.rb
@@ -1,20 +1,38 @@
+# rubocop:disable Metrics/ModuleLength
 module GovUKHelper::Accordion
-  def govuk_accordion(title, &block)
-    @title = title
-    tag.div(class: 'govuk-accordion', id: "accordion-with-summary-sections-for-#{title}", data: { module: 'govuk-accordion' }, &block)
-  end
-
-  def govuk_accordion_add_section(index, heading_text, **options, &block)
-    class_list = ['govuk-accordion__section']
-    class_list << options.delete(:class)
-
-    tag.div(class: class_list.compact.join(' '), **options) do
+  def govuk_accordion(name, sections, &block)
+    tag.div(class: 'govuk-accordion', id: "accordion-with-summary-sections-for-#{name}", data: { module: 'govuk-accordion' }) do
       capture do
-        concat(govuk_accordion_heading(index, heading_text))
-        concat(govuk_accordion_content(index, &block))
+        sections.each.with_index(1) do |section, index|
+          concat(tag.div(class: 'govuk-accordion__section') do
+            capture do
+              concat(govuk_accordion_heading(index, section[:name]))
+              concat(govuk_accordion_content(index, name, section, &block))
+            end
+          end)
+        end
       end
     end
   end
+
+  def govuk_accordion_with_checkboxes(name, sections, model_name, attribute, &block)
+    tag.div(class: 'govuk-accordion', id: "accordion-with-summary-sections-for-#{name}", data: { module: 'govuk-accordion' }) do
+      capture do
+        sections.each.with_index(1) do |(section_id, section), index|
+          concat(tag.div(class: 'govuk-accordion__section chooser-section', data: { section: section_id, sectionname: section[:name] }) do
+            capture do
+              concat(govuk_accordion_heading(index, section[:name]))
+              concat(govuk_accordion_content(index, section[:name], section) do
+                check_boxes_for_section(section_id, model_name, attribute, section[:name], section[:items], &block)
+              end)
+            end
+          end)
+        end
+      end
+    end
+  end
+
+  private
 
   def govuk_accordion_heading(index, heading_text)
     tag.div(class: 'govuk-accordion__section-header') do
@@ -24,15 +42,78 @@ module GovUKHelper::Accordion
     end
   end
 
-  def govuk_accordion_content(index, &block)
-    tag.div(class: 'govuk-accordion__section-content', id: govuk_accordion_content_id(index), aria: { labelledby: govuk_accordion_heading_id(index) }, &block)
+  def govuk_accordion_content(index, name, section, &block)
+    tag.div(class: 'govuk-accordion__section-content', id: govuk_accordion_content_id(name, index), aria: { labelledby: govuk_accordion_heading_id(index) }) do
+      yield(section) if block
+    end
+  end
+
+  def check_boxes_for_section(section_id, model_name, attribute, section_name, items, &block)
+    tag.div(class: 'govuk-form-group chooser-input', sectionname: section_name, section: section_id) do
+      tag.div(class: 'govuk-checkboxes') do
+        capture do
+          items.each do |item|
+            concat(tag.div(class: 'govuk-checkboxes__item') do
+              capture do
+                concat(check_box_item(section_id, model_name, attribute, item))
+                concat(check_box_label(section_id, model_name, item, &block))
+              end
+            end)
+          end
+          if items.length > 1
+            concat(tag.p('Or', class: 'govuk-body govuk-!-margin-top-4'))
+            concat(select_all_checkbox(section_id, model_name))
+          end
+        end
+      end
+    end
+  end
+
+  def check_box_item(section_id, model_name, attribute, item)
+    check_box_tag(
+      "#{attribute}[]",
+      item[:value],
+      item[:selected],
+      title: item[:name],
+      class: 'govuk-checkboxes__input',
+      sectionid: section_id,
+      id: "#{model_name}_#{item[:code]}"
+    )
+  end
+
+  def check_box_label(section_id, model_name, item, &block)
+    tag.label(style: 'padding-top: 0px', class: 'govuk-label govuk-checkboxes__label', for: "#{model_name}_#{item[:code]}") do
+      capture do
+        concat(item[:name])
+        concat(tag.div(item[:description], class: 'govuk-hint')) if item[:description]
+        concat(tag.div do
+          yield(section_id, item) if block
+        end)
+      end
+    end
+  end
+
+  def select_all_checkbox(section_id, model_name)
+    tag.div(class: 'govuk-checkboxes__item ccs-select-all') do
+      capture do
+        concat(tag.input(
+                 class: 'govuk-checkboxes__input',
+                 id: "#{model_name}_#{section_id}_all",
+                 name: 'section-checkbox_select_all',
+                 type: 'checkbox',
+                 value: ''
+               ))
+        concat(check_box_label(section_id, model_name, { name: 'Select all', code: "#{section_id}_all" }))
+      end
+    end
   end
 
   def govuk_accordion_heading_id(index)
     "accordion-with-summary-sections-heading-#{index + 1}"
   end
 
-  def govuk_accordion_content_id(index)
-    "accordion-with-summary-sections-for-#{@title}-content-#{index + 1}"
+  def govuk_accordion_content_id(name, index)
+    "accordion-with-summary-sections-for-#{name}-content-#{index + 1}"
   end
 end
+# rubocop:enable Metrics/ModuleLength
