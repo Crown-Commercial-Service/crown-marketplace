1a55997f25764a45b4f515f2494f377e6b3486b8
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-06-08 17:06:22 +0100

    Added the summary page for buildings so that we can see the selected buildings by the user.
    I was also able to update the feature tests for both the show and edit pages for ‘buildings’ to show how they worked.

--- app/models/facilities_management/rm6232/procurement_building.rb
+++ app/models/facilities_management/rm6232/procurement_building.rb
@@ -6,6 +6,9 @@ module FacilitiesManagement
 
       belongs_to :procurement, foreign_key: :facilities_management_rm6232_procurement_id, inverse_of: :procurement_buildings
       belongs_to :building, class_name: 'FacilitiesManagement::Building'
+
+      delegate :building_name, to: :building
+      delegate :address_no_region, to: :building
     end
   end
 end
--- spec/helpers/facilities_management/rm6232/details_helper_spec.rb
+++ spec/helpers/facilities_management/rm6232/details_helper_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe FacilitiesManagement::RM6232::DetailsHelper, type: :helper do
     context 'when the section is buildings' do
       let(:section) { :buildings }
 
-      pending 'returns Buildings summary' do
+      it 'returns Buildings summary' do
         expect(result).to eq('Buildings summary')
       end
     end
