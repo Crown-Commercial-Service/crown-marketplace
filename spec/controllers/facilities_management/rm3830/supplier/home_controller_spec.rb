4d214a14a2707ec25096587eafb41c5ab269d00b
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-07-08 10:10:38 +0100

    We have been asked to add Google Tag Manager to the application.
    This has been done with some other minor updates relating to cookies.

--- spec/controllers/facilities_management/rm3830/supplier/home_controller_spec.rb
+++ spec/controllers/facilities_management/rm3830/supplier/home_controller_spec.rb
@@ -27,6 +27,60 @@ RSpec.describe FacilitiesManagement::RM3830::Supplier::HomeController, type: :co
     end
   end
 
+  describe 'PUT update_cookie_settings' do
+    context 'when enableing the cookies' do
+      before do
+        %i[_ga_cookie _gi_cookie _cls_cookie].each do |cookie_name|
+          cookies[cookie_name] = { value: 'test_cookie', domain: '.crowncommercial.gov.uk', path: '/' }
+        end
+
+        put :update_cookie_settings, params: { ga_cookie_usage: 'true' }
+      end
+
+      it 'sets the cookie' do
+        expect(cookies[:crown_marketplace_google_analytics_enabled]).to eq('true')
+
+        %i[_ga_cookie _gi_cookie _cls_cookie].each do |cookie_name|
+          expect(cookies[cookie_name]).to eq 'test_cookie'
+        end
+      end
+
+      it 'updates the cookies_updated param' do
+        expect(controller.params[:cookies_updated]).to be true
+      end
+
+      it 'renders the cookie_settings template' do
+        expect(response).to render_template('home/cookie_settings')
+      end
+    end
+
+    context 'when disabling the cookies' do
+      before do
+        cookies[:crown_marketplace_google_analytics_enabled] = { value: 'true' }
+
+        %i[_ga_cookie _gi_cookie _cls_cookie].each do |cookie_name|
+          cookies[cookie_name] = { value: 'test_cookie', domain: '.crowncommercial.gov.uk', path: '/' }
+        end
+
+        put :update_cookie_settings, params: { ga_cookie_usage: 'false' }
+      end
+
+      it 'deletes the cookie' do
+        %w[_ga_cookie _gi_cookie _cls_cookie crown_marketplace_google_analytics_enabled].each do |cookie_name|
+          expect(response.cookies[cookie_name]).to be_nil
+        end
+      end
+
+      it 'updates the cookies_updated param' do
+        expect(controller.params[:cookies_updated]).to be true
+      end
+
+      it 'renders the cookie_settings template' do
+        expect(response).to render_template('home/cookie_settings')
+      end
+    end
+  end
+
   describe 'GET not_permitted' do
     it 'renders the not_permitted page' do
       get :not_permitted
--- spec/views/shared/google/_analytics.html.erb_spec.rb
+++ spec/views/shared/google/_analytics.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'shared/_google_analytics.html.erb' do
+RSpec.describe 'shared/google/_analytics.html.erb' do
   before do
     allow(Marketplace).to receive(:google_analytics_tracking_id)
       .and_return(google_analytics_tracking_id)
@@ -10,7 +10,7 @@ RSpec.describe 'shared/_google_analytics.html.erb' do
     let(:google_analytics_tracking_id) { nil }
 
     it 'does not render anything' do
-      render partial: 'shared/google_analytics'
+      render partial: 'shared/google/analytics'
 
       expect(rendered).to be_blank
     end
@@ -20,7 +20,7 @@ RSpec.describe 'shared/_google_analytics.html.erb' do
     let(:google_analytics_tracking_id) { 'UA-999-9' }
 
     it 'renders the Google Analytics tracking code' do
-      render partial: 'shared/google_analytics'
+      render partial: 'shared/google/analytics'
 
       expect(rendered).to match(/script/)
       expect(rendered).to match(/UA-999-9/)
