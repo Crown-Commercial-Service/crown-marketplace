63f823aaeb89ba69c0185656b4bae0ec7c19c258
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-08-16 12:24:08 +0100

    Make sure unrecognised framework is at the top of the call backs

--- app/controllers/facilities_management/framework_controller.rb
+++ app/controllers/facilities_management/framework_controller.rb
@@ -1,8 +1,8 @@
 module FacilitiesManagement
   class FrameworkController < ::ApplicationController
+    before_action :raise_if_unrecognised_live_framework
     before_action :authenticate_user!
     before_action :authorize_user
-    before_action :raise_if_unrecognised_live_framework
     before_action :redirect_to_buyer_detail
 
     protected
