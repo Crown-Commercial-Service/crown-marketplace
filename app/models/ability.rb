d7d481ee5de9257b794939cd9d06ccdc8e09068a
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-09-26 09:51:33 +0100

    Update the ability config to better reflect the abilities different roles should have

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,20 +4,21 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     cannot :manage, :all
-    if user.has_role? :ccs_admin
-      can :manage, :all
-    else
-      admin_tool_specific_auth(user)
-      service_specific_auth(user)
-      fm_supplier_specific_auth(user)
-      allow_list_specific_auth(user)
+
+    if user.has_role?(:fm_access)
+      service_buyer_specific_auth(user)
+      service_supplier_specific_auth(user)
+      service_admin_specific_auth(user)
     end
+
+    allow_list_specific_auth(user)
+    super_admin_specific_auth(user)
   end
 
   private
 
-  def service_specific_auth(user)
-    return unless user.has_role?(:buyer) && user.has_role?(:fm_access)
+  def service_buyer_specific_auth(user)
+    return unless user.has_role?(:buyer)
 
     can :read, FacilitiesManagement
     can :manage, FacilitiesManagement::RM3830::Procurement, user_id: user.id
@@ -26,22 +27,24 @@ class Ability
     can :manage, FacilitiesManagement::Building, user_id: user.id
   end
 
-  def admin_tool_specific_auth(user)
-    return unless user.has_role?(:fm_access) && user.has_role?(:ccs_employee)
-
-    can :read, :all
-    can :manage, FacilitiesManagement::Admin
-
-    return unless user.has_role?(:ccs_developer)
-
-    can :manage, FacilitiesManagement::Framework
-  end
-
-  def fm_supplier_specific_auth(user)
-    return unless user.has_role?(:fm_access) && user.has_role?(:supplier)
+  def service_supplier_specific_auth(user)
+    return unless user.has_role?(:supplier)
 
     can :read, FacilitiesManagement::RM3830::SupplierDetail
     can :manage, FacilitiesManagement::RM3830::ProcurementSupplier, supplier: user.supplier_detail
+  end
+
+  def service_admin_specific_auth(user)
+    return unless user.has_role?(:ccs_employee)
+
+    can :read, :all
+    can :manage, FacilitiesManagement::Admin
+  end
+
+  def super_admin_specific_auth(user)
+    return unless user.has_role?(:ccs_developer)
+
+    can :manage, FacilitiesManagement::Framework
   end
 
   def allow_list_specific_auth(user)
