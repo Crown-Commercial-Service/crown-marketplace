71bcbebace1e3adebc3a4e0b6ace9dd273494a86
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-11-28 15:49:36 +0000

    We are going to give CSC the ability to manage users in the Digital Foundations project. In this commit I have added the ability to search for users.
    In addition, I have (as always) added unit, feature and accessibility tests to account for the changes.

--- features/step_definitions/crown_marketplace/cognito_steps.rb
+++ features/step_definitions/crown_marketplace/cognito_steps.rb
@@ -12,6 +12,22 @@ Then('I am able to create a user account with:') do |user_account_details_table|
   stub_admin_cognito(:create_user, **user_account_details)
 end
 
+Given('I am going to do a search to find users') do
+  stub_admin_cognito(:find_users)
+end
+
+Given('I search for {string} and there are no users') do |search|
+  stub_admin_cognito(:find_users, search: search, users: [])
+end
+
+Given('I search for {string} there are the following users:') do |search, user_details|
+  stub_admin_cognito(:find_users, search: search, users: user_details.raw.map { |user_detail| { email: user_detail[0], account_status: user_detail[1] == 'enabled' } })
+end
+
 Then('I cannot create a user account because of the {string} error') do |error_key|
   stub_cognito_admin_with_error(:create_user, error_key)
+end
+
+Then('I cannot find any user accounts because of the {string} error') do |error_key|
+  stub_cognito_admin_with_error(:find_users, error_key)
 end
