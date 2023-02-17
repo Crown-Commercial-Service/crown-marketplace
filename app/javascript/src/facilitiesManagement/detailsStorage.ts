32c097c0375b27bde5ca0f2db6d7d8b99fd6820f
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2023-02-17 15:55:01 +0000

    I’m planning on updating the project so that we use Rails 7 and Ruby v3. One of the major changes from rails 6 to 7 is how JavaScript works (changing from webpacker to other tools).
    However, we are still using sprockets for our JavaScript so I have decided to change to using webpack to make that final transition a bit easier.

---
+++ app/javascript/src/facilitiesManagement/detailsStorage.ts
@@ -0,0 +1,101 @@
+enum ContactDetailType {
+  TextInput,
+  RadioInput
+}
+
+type ContactDetailOption = {
+  name: string
+  $element: JQuery<HTMLInputElement>
+  type: ContactDetailType
+}
+
+abstract class ContactDetail {
+  protected detailName: string
+  protected $element: JQuery<HTMLInputElement>
+
+  constructor(detailName: string, $element: JQuery<HTMLInputElement>) {
+    this.detailName = detailName
+    this.$element = $element
+  }
+
+  removeDetail = (): void => {
+    window.sessionStorage.removeItem(this.detailName)
+  }
+}
+
+class TextInputContactDetail extends ContactDetail {
+  storeDetail = (): void => {
+    window.sessionStorage[this.detailName] = this.$element.val()
+  }
+
+  fillInDetail = (): void => {
+    this.$element.val(window.sessionStorage[this.detailName])
+  }
+}
+
+class RadioInputContactDetail extends ContactDetail {
+  storeDetail = (): void => {
+    window.sessionStorage[this.detailName] = this.$element.is(':checked')
+  }
+
+  fillInDetail = (): void => {
+    this.$element.prop('checked', (window.sessionStorage[this.detailName] === 'true'))
+  }
+}
+
+class ContactDetails {
+  private id: string
+  private contactDetails: Array<TextInputContactDetail | RadioInputContactDetail>
+
+  constructor (id: string, contactDetails: ContactDetailOption[]) {
+    this.id = id
+    this.contactDetails = contactDetails.map((contactDetailOption) => {
+      switch(contactDetailOption.type) {
+      case ContactDetailType.TextInput:
+        return new TextInputContactDetail(contactDetailOption.name, contactDetailOption.$element)
+      case ContactDetailType.RadioInput:
+        return new RadioInputContactDetail(contactDetailOption.name, contactDetailOption.$element)
+      }
+    })
+  }
+
+  storeDetails = (): void => {
+    this.contactDetails.forEach(contactDetail => contactDetail.storeDetail())
+
+    window.sessionStorage[this.id] = true
+  }
+
+  fillInDetails = (): void => {
+    this.contactDetails.forEach(contactDetail => contactDetail.fillInDetail())
+  }
+
+  removeDetails = (): void => {
+    this.contactDetails.forEach(contactDetail => contactDetail.removeDetail())
+
+    window.sessionStorage.removeItem(this.id)
+  }
+}
+
+const initDetailsStorage = (
+  id: string,
+  contactDetailsOptions: ContactDetailOption[],
+  $contactDetailsForm: JQuery<HTMLFormElement>,
+  $contactDetailsAddressForm: JQuery<HTMLFormElement>
+): void => {
+  if (typeof (Storage) !== 'undefined') {
+    const contactDetails = new ContactDetails(id, contactDetailsOptions)
+
+    if ($contactDetailsForm.length && window.sessionStorage[id]) contactDetails.fillInDetails()
+
+    if ($contactDetailsForm.length) {
+      $('#cant-find-address-link').on('click', () => contactDetails.storeDetails())
+
+      $('input[type="submit"]').on('click', () => contactDetails.removeDetails())
+    } else if (!$contactDetailsAddressForm.length) {
+      contactDetails.removeDetails()
+    }
+  }
+}
+
+export { ContactDetailType, ContactDetailOption }
+export default initDetailsStorage
--- app/typescript/facilitiesManagement/integerInput.ts
+++
@@ -1,9 +0,0 @@
-const limitInputToInteger = (): void => {
-  $('.ccs-integer-field').on('keypress', (event: JQuery.KeyPressEvent) => {
-    if ((event.key < '0' || event.key > '9')) event.preventDefault()
-  })
-}
-
-$(() => {
-  if ($('.ccs-integer-field').length) limitInputToInteger()
-})
