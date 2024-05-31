568d640bf40019e0112485e61ed7fa5ca8452ff3
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2024-05-31 09:39:15 +0100

    Debug issue with table name

--- db/migrate/20231031103327_rename_tables_with_long_names.rb
+++ db/migrate/20231031103327_rename_tables_with_long_names.rb
@@ -1,5 +1,10 @@
 class RenameTablesWithLongNames < ActiveRecord::Migration[7.1]
   def change
+    ActiveRecord::Base.connection.tables.each do |table_name|
+      puts "----- #{table_name} -----"
+      puts ActiveRecord::Base.connection.columns(table_name).map(&:name)
+    end
+
     rename_table :facilities_management_rm3830_procurement_building_service_lifts, :facilities_management_procurement_building_service_lifts
     rename_table :facilities_management_rm3830_procurement_call_off_extensions, :facilities_management_rm3830_procurement_extensions
     rename_table :facilities_management_rm6232_procurement_call_off_extensions, :facilities_management_rm6232_procurement_extensions
