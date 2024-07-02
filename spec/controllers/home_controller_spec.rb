ef986a4ed06d8b9059978fe93c3d04c8b4c5c188
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2024-07-02 15:02:24 +0100

    Update the healthcheck route to check for the apps status

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,14 +4,22 @@ RSpec.describe HomeController do
   describe 'GET index' do
     it 'redirects to the CCS home page' do
       get :index
+
       expect(response).to redirect_to('https://www.crowncommercial.gov.uk/')
     end
   end
 
-  describe 'GET status' do
+  describe 'GET healthcheck' do
     it 'displays status information about the app' do
-      get :status
-      expect(response).to render_template(:status, layout: false)
+      get :healthcheck
+
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      expect(response.parsed_body).to eq(
+        {
+          'status' => 'ok',
+          'git_commit' => 'expected-git-commit'
+        }
+      )
     end
   end
 end
