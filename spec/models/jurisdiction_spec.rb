c5eedf1858e17ba35c1e4578228164d87bda6bf6
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2025-09-22 16:42:26 +0100

    Added the `category` attribute to the jurisdictions

--- spec/models/jurisdiction_spec.rb
+++ spec/models/jurisdiction_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe Jurisdiction do
   end
 
   it 'has all the jurisdictions loaded' do
-    expect(described_class.count).to eq(248)
+    expect(described_class.count).to eq(322)
+  end
+
+  describe 'scopes' do
+    it 'has 8 jurisdictions for the core scope' do
+      expect(described_class.core.count).to eq(8)
+    end
+
+    it 'has 240 jurisdictions for the non_core scope' do
+      expect(described_class.non_core.count).to eq(240)
+    end
   end
 end
