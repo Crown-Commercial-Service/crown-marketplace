8a0350a2b536da752d2a6c8183c16758996449ce
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2025-12-10 15:05:14 +0000

    update fm rm 6378 add information about your requiremnts- pfi question

--- app/models/facilities_management/rm6378/journey/information_about_your_requirements.rb
+++ app/models/facilities_management/rm6378/journey/information_about_your_requirements.rb
@@ -7,6 +7,13 @@ module FacilitiesManagement
 
       validates :estimated_contract_duration, numericality: { only_integer: true, greater_than: 0, less_than: 100 }
 
+
+      PFI_OPTIONS = %w[yes no].freeze
+
+      attribute :private_finance_initiative
+      validates :private_finance_initiative, inclusion: PFI_OPTIONS
+
+
       def next_step_class
         Journey::Procurement
       end
