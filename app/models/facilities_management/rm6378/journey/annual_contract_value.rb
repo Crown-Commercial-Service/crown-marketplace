746923b63adbc0c353cb5779e87ec776388196f2
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2025-12-10 11:53:45 +0000

    update fm rm 6378 add information about your requirements page

--- app/models/facilities_management/rm6378/journey/annual_contract_value.rb
+++ app/models/facilities_management/rm6378/journey/annual_contract_value.rb
@@ -8,7 +8,7 @@ module FacilitiesManagement
       validates :annual_contract_value, numericality: { only_integer: true, greater_than: 0, less_than: 1_000_000_000_000 }
 
       def next_step_class
-        Journey::Procurement
+        Journey::InformationAboutYourRequirements
       end
     end
   end
---
+++ app/models/facilities_management/rm6378/journey/information_about_your_requirements.rb
@@ -0,0 +1,15 @@
+module FacilitiesManagement
+  module RM6378
+    class Journey::InformationAboutYourRequirements
+      include Steppable
+
+      attribute :estimated_contract_duration, Numeric
+
+      validates :estimated_contract_duration, numericality: { only_integer: true, greater_than: 0, less_than: 100 }
+
+      def next_step_class
+        Journey::Procurement
+      end
+    end
+  end
+end
