975c71ab7ce6cfc7eacb9828d3494b146e465470
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2025-12-11 17:39:00 +0000

    update fm rm 6378 add information about your requiremnts- testing

--- app/models/facilities_management/rm6378/journey/information_about_your_requirements.rb
+++ app/models/facilities_management/rm6378/journey/information_about_your_requirements.rb
@@ -1,18 +1,25 @@
 module FacilitiesManagement
   module RM6378
     class Journey::InformationAboutYourRequirements
+      DATE_ATTIBUTES = %i[contract_start_date].freeze
+
       include Steppable
-
-      attribute :estimated_contract_duration, Numeric
-
-      validates :estimated_contract_duration, numericality: { only_integer: true, greater_than: 0, less_than: 100 }
-
+      include DateValidations
 
       PFI_OPTIONS = %w[yes no].freeze
 
+      attribute :contract_start_date_dd
+      attribute :contract_start_date_mm
+      attribute :contract_start_date_yyyy
+      attribute :estimated_contract_duration, Numeric
       attribute :private_finance_initiative
-      validates :private_finance_initiative, inclusion: PFI_OPTIONS
 
+      validates :contract_start_date, presence: true
+      validate  -> { ensure_date_valid(:contract_start_date, false) }
+      validate  -> { ensure_date_is_after(contract_start_date, DateTime.now.in_time_zone('London'), :contract_start_date, :date_in_future) }
+
+      validates :estimated_contract_duration, numericality: { only_integer: true, greater_than: 0, less_than: 100 }
+      validates :private_finance_initiative, inclusion: PFI_OPTIONS
 
       def next_step_class
         Journey::Procurement
