b152b254ba985d6b197fec0b6448b370154a38ed
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-02-26 10:31:42 +0000

    rm6378 service spec links

--- db/migrate/20260225161439_create_facilities_management_rm6378_work_packages.rb
+++
@@ -1,9 +0,0 @@
-class CreateFacilitiesManagementRM6378WorkPackages < ActiveRecord::Migration[8.1]
-  def change
-    create_table :facilities_management_rm6378_work_packages do |t|
-      t.string :code
-      t.string :name
-      t.boolean :selectable
-    end
-  end
-end
