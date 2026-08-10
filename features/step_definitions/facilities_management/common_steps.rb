dc898edaabb20f9de49ba8be20dd311da421e479
chloeprecaj <chloe.precaj1@crowncommercial.gov.uk>
2026-08-10 12:20:08 +0100

    unit

--- features/step_definitions/facilities_management/common_steps.rb
+++ features/step_definitions/facilities_management/common_steps.rb
@@ -80,8 +80,19 @@ Then('I click on the {string} link') do |link_text|
   page.click_link(link_text)
 end
 
-Then('I enter {string} for the Average estimated contract value') do |value|
-  fill_in 'annual_contract_value', with: value
+Then('I enter {string} for {string}') do |value, field_name|
+  clean_field_name = field_name.sub(/^the /i, '')
+
+  case clean_field_name
+  when 'Average estimated contract value', 'annual contract cost'
+    fill_in 'annual_contract_value', with: value
+  else
+    fill_in clean_field_name, with: value
+  end
+end
+
+Then('I enter {string} for the {string}') do |value, field_name|
+  step "I enter '#{value}' for '#{field_name}'"
 end
 
 Then('the caption is {string}') do |caption|
@@ -111,12 +122,3 @@ PAGE_HEADING = {
   'supplier' => 'Facilities management supplier account',
   'admin' => 'Managing framework and supplier data'
 }.freeze
-
-Then('I enter {string} for the {string}') do |value, field_name|
-  case field_name
-  when 'Average estimated contract value', 'annual contract cost'
-    fill_in 'annual_contract_value', with: value
-  else
-    fill_in field_name, with: value
-  end
-end
